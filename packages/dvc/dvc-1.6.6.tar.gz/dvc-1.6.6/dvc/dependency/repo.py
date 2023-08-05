109ecebe497418359ea7f7802b4bbdc715d55404
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:09 +0000

    Add package dvc/1.6.6/dvc-1.6.6.tar.gz

---
+++ packages/dvc/dvc-1.6.6.tar.gz/dvc-1.6.6/dvc/dependency/repo.py
@@ -0,0 +1,111 @@
+import os
+
+from voluptuous import Required
+
+from dvc.exceptions import OutputNotFoundError
+from dvc.path_info import PathInfo
+
+from .local import LocalDependency
+
+
+class RepoDependency(LocalDependency):
+    PARAM_REPO = "repo"
+    PARAM_URL = "url"
+    PARAM_REV = "rev"
+    PARAM_REV_LOCK = "rev_lock"
+
+    REPO_SCHEMA = {
+        PARAM_REPO: {
+            Required(PARAM_URL): str,
+            PARAM_REV: str,
+            PARAM_REV_LOCK: str,
+        }
+    }
+
+    def __init__(self, def_repo, stage, *args, **kwargs):
+        self.def_repo = def_repo
+        super().__init__(stage, *args, **kwargs)
+
+    def _parse_path(self, tree, path):
+        return None
+
+    @property
+    def is_in_repo(self):
+        return False
+
+    @property
+    def repo_pair(self):
+        d = self.def_repo
+        rev = d.get(self.PARAM_REV_LOCK) or d.get(self.PARAM_REV)
+        return d[self.PARAM_URL], rev
+
+    def __str__(self):
+        return "{} ({})".format(self.def_path, self.def_repo[self.PARAM_URL])
+
+    def _make_repo(self, *, locked=True):
+        from dvc.external_repo import external_repo
+
+        d = self.def_repo
+        rev = (d.get("rev_lock") if locked else None) or d.get("rev")
+        return external_repo(d["url"], rev=rev)
+
+    def _get_checksum(self, locked=True):
+        from dvc.tree.repo import RepoTree
+
+        with self._make_repo(locked=locked) as repo:
+            try:
+                return repo.find_out_by_relpath(self.def_path).info["md5"]
+            except OutputNotFoundError:
+                path = PathInfo(os.path.join(repo.root_dir, self.def_path))
+
+                # we want stream but not fetch, so DVC out directories are
+                # walked, but dir contents is not fetched
+                tree = RepoTree(repo, stream=True)
+
+                # We are polluting our repo cache with some dir listing here
+                if tree.isdir(path):
+                    return self.repo.cache.local.tree.get_hash(
+                        path, tree=tree
+                    ).value
+                return tree.get_file_hash(path)
+
+    def workspace_status(self):
+        current_checksum = self._get_checksum(locked=True)
+        updated_checksum = self._get_checksum(locked=False)
+
+        if current_checksum != updated_checksum:
+            return {str(self): "update available"}
+
+        return {}
+
+    def status(self):
+        return self.workspace_status()
+
+    def save(self):
+        pass
+
+    def dumpd(self):
+        return {self.PARAM_PATH: self.def_path, self.PARAM_REPO: self.def_repo}
+
+    def download(self, to):
+        with self._make_repo() as repo:
+            if self.def_repo.get(self.PARAM_REV_LOCK) is None:
+                self.def_repo[self.PARAM_REV_LOCK] = repo.get_rev()
+
+            cache = self.repo.cache.local
+            with repo.use_cache(cache):
+                _, _, cache_infos = repo.fetch_external([self.def_path])
+            cache.checkout(to.path_info, cache_infos[0])
+
+    def update(self, rev=None):
+        if rev:
+            self.def_repo[self.PARAM_REV] = rev
+
+        with self._make_repo(locked=False) as repo:
+            self.def_repo[self.PARAM_REV_LOCK] = repo.get_rev()
+
+    def changed_checksum(self):
+        # From current repo point of view what describes RepoDependency is its
+        # origin project url and rev_lock, and it makes RepoDependency
+        # immutable, hence its impossible for checksum to change.
+        return False
---
+++ packages/dvc/dvc-1.6.6.tar.gz/dvc-1.6.6/tests/unit/tree/test_repo.py
@@ -0,0 +1,400 @@
+import os
+import shutil
+from unittest import mock
+
+import pytest
+
+from dvc.hash_info import HashInfo
+from dvc.path_info import PathInfo
+from dvc.tree.repo import RepoTree
+
+
+def test_exists(tmp_dir, dvc):
+    tmp_dir.gen("foo", "foo")
+    dvc.add("foo")
+    (tmp_dir / "foo").unlink()
+
+    tree = RepoTree(dvc)
+    assert tree.exists("foo")
+
+
+def test_open(tmp_dir, dvc):
+    tmp_dir.gen("foo", "foo")
+    dvc.add("foo")
+    (tmp_dir / "foo").unlink()
+
+    tree = RepoTree(dvc)
+    with dvc.state:
+        with tree.open("foo", "r") as fobj:
+            assert fobj.read() == "foo"
+
+
+def test_open_dirty_hash(tmp_dir, dvc):
+    tmp_dir.dvc_gen("file", "file")
+    (tmp_dir / "file").write_text("something")
+
+    tree = RepoTree(dvc)
+    with tree.open("file", "r") as fobj:
+        assert fobj.read() == "something"
+
+
+def test_open_dirty_no_hash(tmp_dir, dvc):
+    tmp_dir.gen("file", "file")
+    (tmp_dir / "file.dvc").write_text("outs:\n- path: file\n")
+
+    tree = RepoTree(dvc)
+    with tree.open("file", "r") as fobj:
+        assert fobj.read() == "file"
+
+
+def test_open_in_history(tmp_dir, scm, dvc):
+    tmp_dir.gen("foo", "foo")
+    dvc.add("foo")
+    dvc.scm.add(["foo.dvc", ".gitignore"])
+    dvc.scm.commit("foo")
+
+    tmp_dir.gen("foo", "foofoo")
+    dvc.add("foo")
+    dvc.scm.add(["foo.dvc", ".gitignore"])
+    dvc.scm.commit("foofoo")
+
+    for rev in dvc.brancher(revs=["HEAD~1"]):
+        if rev == "workspace":
+            continue
+
+        tree = RepoTree(dvc)
+        with tree.open("foo", "r") as fobj:
+            assert fobj.read() == "foo"
+
+
+def test_isdir_isfile(tmp_dir, dvc):
+    tmp_dir.gen({"datafile": "data", "datadir": {"foo": "foo", "bar": "bar"}})
+
+    tree = RepoTree(dvc)
+    assert tree.isdir("datadir")
+    assert not tree.isfile("datadir")
+    assert not tree.isdvc("datadir")
+    assert not tree.isdir("datafile")
+    assert tree.isfile("datafile")
+    assert not tree.isdvc("datafile")
+
+    dvc.add(["datadir", "datafile"])
+    shutil.rmtree(tmp_dir / "datadir")
+    (tmp_dir / "datafile").unlink()
+
+    assert tree.isdir("datadir")
+    assert not tree.isfile("datadir")
+    assert tree.isdvc("datadir")
+    assert not tree.isdir("datafile")
+    assert tree.isfile("datafile")
+    assert tree.isdvc("datafile")
+
+
+def test_isdir_mixed(tmp_dir, dvc):
+    tmp_dir.gen({"dir": {"foo": "foo", "bar": "bar"}})
+
+    dvc.add(str(tmp_dir / "dir" / "foo"))
+
+    tree = RepoTree(dvc)
+    assert tree.isdir("dir")
+    assert not tree.isfile("dir")
+
+
+@pytest.mark.parametrize(
+    "dvcfiles,extra_expected",
+    [
+        (False, []),
+        (
+            True,
+            [
+                PathInfo("dir") / "subdir1" / "foo1.dvc",
+                PathInfo("dir") / "subdir1" / "bar1.dvc",
+                PathInfo("dir") / "subdir2" / "foo2.dvc",
+            ],
+        ),
+    ],
+)
+def test_walk(tmp_dir, dvc, dvcfiles, extra_expected):
+    tmp_dir.gen(
+        {
+            "dir": {
+                "subdir1": {"foo1": "foo1", "bar1": "bar1"},
+                "subdir2": {"foo2": "foo2"},
+            }
+        }
+    )
+    dvc.add(str(tmp_dir / "dir"), recursive=True)
+    tmp_dir.gen({"dir": {"foo": "foo", "bar": "bar"}})
+    tree = RepoTree(dvc)
+
+    expected = [
+        PathInfo("dir") / "subdir1",
+        PathInfo("dir") / "subdir2",
+        PathInfo("dir") / "subdir1" / "foo1",
+        PathInfo("dir") / "subdir1" / "bar1",
+        PathInfo("dir") / "subdir2" / "foo2",
+        PathInfo("dir") / "foo",
+        PathInfo("dir") / "bar",
+    ]
+
+    actual = []
+    for root, dirs, files in tree.walk("dir", dvcfiles=dvcfiles):
+        for entry in dirs + files:
+            actual.append(os.path.join(root, entry))
+
+    expected = [str(path) for path in expected + extra_expected]
+    assert set(actual) == set(expected)
+    assert len(actual) == len(expected)
+
+
+def test_walk_onerror(tmp_dir, dvc):
+    def onerror(exc):
+        raise exc
+
+    tmp_dir.dvc_gen("foo", "foo")
+    tree = RepoTree(dvc)
+
+    # path does not exist
+    for _ in tree.walk("dir"):
+        pass
+    with pytest.raises(OSError):
+        for _ in tree.walk("dir", onerror=onerror):
+            pass
+
+    # path is not a directory
+    for _ in tree.walk("foo"):
+        pass
+    with pytest.raises(OSError):
+        for _ in tree.walk("foo", onerror=onerror):
+            pass
+
+
+def test_isdvc(tmp_dir, dvc):
+    tmp_dir.gen({"foo": "foo", "bar": "bar", "dir": {"baz": "baz"}})
+    dvc.add("foo")
+    dvc.add("dir")
+    tree = RepoTree(dvc)
+    assert tree.isdvc("foo")
+    assert not tree.isdvc("bar")
+    assert tree.isdvc("dir")
+    assert not tree.isdvc("dir/baz")
+    assert tree.isdvc("dir/baz", recursive=True)
+
+
+def make_subrepo(dir_, scm, config=None):
+    dir_.mkdir(parents=True)
+    with dir_.chdir():
+        dir_.scm = scm
+        dir_.init(dvc=True, subdir=True)
+        if config:
+            dir_.add_remote(config=config)
+
+
+def test_subrepos(tmp_dir, scm, dvc):
+    tmp_dir.scm_gen(
+        {"dir": {"repo.txt": "file to confuse RepoTree"}},
+        commit="dir/repo.txt",
+    )
+
+    subrepo1 = tmp_dir / "dir" / "repo"
+    subrepo2 = tmp_dir / "dir" / "repo2"
+
+    for repo in [subrepo1, subrepo2]:
+        make_subrepo(repo, scm)
+
+    subrepo1.dvc_gen({"foo": "foo", "dir1": {"bar": "bar"}}, commit="FOO")
+    subrepo2.dvc_gen(
+        {"lorem": "lorem", "dir2": {"ipsum": "ipsum"}}, commit="BAR"
+    )
+
+    dvc.tree._reset()
+    tree = RepoTree(dvc, subrepos=True, fetch=True)
+
+    def assert_tree_belongs_to_repo(ret_val):
+        method = tree._get_repo
+
+        def f(*args, **kwargs):
+            r = method(*args, **kwargs)
+            assert r.root_dir == ret_val.root_dir
+            return r
+
+        return f
+
+    with mock.patch.object(
+        tree,
+        "_get_repo",
+        side_effect=assert_tree_belongs_to_repo(subrepo1.dvc),
+    ):
+        assert tree.exists(subrepo1 / "foo") is True
+        assert tree.exists(subrepo1 / "bar") is False
+
+        assert tree.isfile(subrepo1 / "foo") is True
+        assert tree.isfile(subrepo1 / "dir1" / "bar") is True
+        assert tree.isfile(subrepo1 / "dir1") is False
+
+        assert tree.isdir(subrepo1 / "dir1") is True
+        assert tree.isdir(subrepo1 / "dir1" / "bar") is False
+        assert tree.isdvc(subrepo1 / "foo") is True
+
+    with mock.patch.object(
+        tree,
+        "_get_repo",
+        side_effect=assert_tree_belongs_to_repo(subrepo2.dvc),
+    ):
+        assert tree.exists(subrepo2 / "lorem") is True
+        assert tree.exists(subrepo2 / "ipsum") is False
+
+        assert tree.isfile(subrepo2 / "lorem") is True
+        assert tree.isfile(subrepo2 / "dir2" / "ipsum") is True
+        assert tree.isfile(subrepo2 / "dir2") is False
+
+        assert tree.isdir(subrepo2 / "dir2") is True
+        assert tree.isdir(subrepo2 / "dir2" / "ipsum") is False
+        assert tree.isdvc(subrepo2 / "lorem") is True
+
+
+@pytest.mark.parametrize(
+    "dvcfiles,extra_expected",
+    [
+        (False, []),
+        (
+            True,
+            [
+                PathInfo("dir") / "repo" / "foo.dvc",
+                PathInfo("dir") / "repo" / "dir1.dvc",
+                PathInfo("dir") / "repo2" / "lorem.dvc",
+                PathInfo("dir") / "repo2" / "dir2.dvc",
+            ],
+        ),
+    ],
+)
+def test_subrepo_walk(tmp_dir, scm, dvc, dvcfiles, extra_expected):
+    tmp_dir.scm_gen(
+        {"dir": {"repo.txt": "file to confuse RepoTree"}},
+        commit="dir/repo.txt",
+    )
+
+    subrepo1 = tmp_dir / "dir" / "repo"
+    subrepo2 = tmp_dir / "dir" / "repo2"
+
+    subdirs = [subrepo1, subrepo2]
+    for dir_ in subdirs:
+        make_subrepo(dir_, scm)
+
+    subrepo1.dvc_gen({"foo": "foo", "dir1": {"bar": "bar"}}, commit="FOO")
+    subrepo2.dvc_gen(
+        {"lorem": "lorem", "dir2": {"ipsum": "ipsum"}}, commit="BAR"
+    )
+
+    # using tree that does not have dvcignore
+    dvc.tree._reset()
+    tree = RepoTree(dvc, subrepos=True, fetch=True)
+    expected = [
+        PathInfo("dir") / "repo",
+        PathInfo("dir") / "repo.txt",
+        PathInfo("dir") / "repo2",
+        PathInfo("dir") / "repo" / ".dvcignore",
+        PathInfo("dir") / "repo" / ".gitignore",
+        PathInfo("dir") / "repo" / "foo",
+        PathInfo("dir") / "repo" / "dir1",
+        PathInfo("dir") / "repo" / "dir1" / "bar",
+        PathInfo("dir") / "repo2" / ".dvcignore",
+        PathInfo("dir") / "repo2" / ".gitignore",
+        PathInfo("dir") / "repo2" / "lorem",
+        PathInfo("dir") / "repo2" / "dir2",
+        PathInfo("dir") / "repo2" / "dir2" / "ipsum",
+    ]
+
+    actual = []
+    for root, dirs, files in tree.walk("dir", dvcfiles=dvcfiles):
+        for entry in dirs + files:
+            actual.append(os.path.join(root, entry))
+
+    expected = [str(path) for path in expected + extra_expected]
+    assert set(actual) == set(expected)
+    assert len(actual) == len(expected)
+
+
+def test_repo_tree_no_subrepos(tmp_dir, dvc, scm):
+    tmp_dir.scm_gen(
+        {"dir": {"repo.txt": "file to confuse RepoTree"}},
+        commit="dir/repo.txt",
+    )
+    tmp_dir.dvc_gen({"lorem": "lorem"}, commit="add foo")
+
+    subrepo = tmp_dir / "dir" / "repo"
+    make_subrepo(subrepo, scm)
+    subrepo.dvc_gen({"foo": "foo", "dir1": {"bar": "bar"}}, commit="FOO")
+    subrepo.scm_gen({"ipsum": "ipsum"}, commit="BAR")
+
+    # using tree that does not have dvcignore
+    dvc.tree._reset()
+    tree = RepoTree(dvc, subrepos=False, fetch=True)
+    expected = [
+        tmp_dir / ".dvcignore",
+        tmp_dir / ".gitignore",
+        tmp_dir / "lorem",
+        tmp_dir / "lorem.dvc",
+        tmp_dir / "dir",
+        tmp_dir / "dir" / "repo.txt",
+    ]
+
+    actual = []
+    for root, dirs, files in tree.walk(tmp_dir, dvcfiles=True):
+        for entry in dirs + files:
+            actual.append(os.path.normpath(os.path.join(root, entry)))
+
+    expected = [str(path) for path in expected]
+    assert set(actual) == set(expected)
+    assert len(actual) == len(expected)
+
+    assert tree.isfile(tmp_dir / "lorem") is True
+    assert tree.isfile(tmp_dir / "dir" / "repo" / "foo") is False
+    assert tree.isdir(tmp_dir / "dir" / "repo") is False
+    assert tree.isdir(tmp_dir / "dir") is True
+
+    assert tree.isdvc(tmp_dir / "lorem") is True
+    assert tree.isdvc(tmp_dir / "dir" / "repo" / "dir1") is False
+
+    assert tree.exists(tmp_dir / "dir" / "repo.txt") is True
+    assert tree.exists(tmp_dir / "repo" / "ipsum") is False
+
+
+def test_get_hash_cached_file(tmp_dir, dvc, mocker):
+    tmp_dir.dvc_gen({"foo": "foo"})
+    tree = RepoTree(dvc)
+    dvc_tree_spy = mocker.spy(tree._dvctrees[dvc.root_dir], "get_file_hash")
+    assert tree.get_hash(PathInfo(tmp_dir) / "foo") == HashInfo(
+        "md5", "acbd18db4cc2f85cedef654fccc4a4d8",
+    )
+    assert dvc_tree_spy.called
+
+
+def test_get_hash_cached_dir(tmp_dir, dvc, mocker):
+    tmp_dir.dvc_gen(
+        {"dir": {"foo": "foo", "bar": "bar", "subdir": {"data": "data"}}}
+    )
+    tree = RepoTree(dvc)
+    get_file_hash_spy = mocker.spy(tree, "get_file_hash")
+    dvc_tree_spy = mocker.spy(tree._dvctrees[dvc.root_dir], "get_dir_hash")
+    assert tree.get_hash(PathInfo(tmp_dir) / "dir") == HashInfo(
+        "md5", "8761c4e9acad696bee718615e23e22db.dir",
+    )
+    assert not get_file_hash_spy.called
+    assert dvc_tree_spy.called
+
+
+def test_get_hash_cached_granular(tmp_dir, dvc, mocker):
+    tmp_dir.dvc_gen(
+        {"dir": {"foo": "foo", "bar": "bar", "subdir": {"data": "data"}}}
+    )
+    tree = RepoTree(dvc, fetch=True)
+    dvc_tree_spy = mocker.spy(tree._dvctrees[dvc.root_dir], "get_file_hash")
+    subdir = PathInfo(tmp_dir) / "dir" / "subdir"
+    assert tree.get_hash(subdir) == HashInfo(
+        "md5", "af314506f1622d107e0ed3f14ec1a3b5.dir",
+    )
+    assert tree.get_hash(subdir / "data") == HashInfo(
+        "md5", "8d777f385d3dfec8815d20f7496026dc",
+    )
+    assert dvc_tree_spy.called
