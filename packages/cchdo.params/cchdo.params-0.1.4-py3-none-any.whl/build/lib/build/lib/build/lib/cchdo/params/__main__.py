956a9836729e3ebb34363b608f9ce709e05d85be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:44 +0000

    Add package cchdo.params/0.1.4/cchdo.params-0.1.4-py3-none-any.whl

---
+++ packages/cchdo.params/cchdo.params-0.1.4-py3-none-any.whl/build/lib/build/lib/build/lib/cchdo/params/__main__.py
@@ -0,0 +1,29 @@
+import click
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+def dump_db():
+    from importlib.resources import path
+    import sqlite3
+
+    with path("cchdo.params", "params.sqlite3") as p:
+        with sqlite3.connect(p) as conn:
+            with open(f"{p}.sql", "w") as f:
+                for line in conn.iterdump():
+                    f.write(f"{line}\n")
+
+
+@cli.command()
+def params_to_json():
+    import json
+    from . import WHPNames
+
+    print(json.dumps(WHPNames.legacy_json, indent=2))
+
+
+cli()
