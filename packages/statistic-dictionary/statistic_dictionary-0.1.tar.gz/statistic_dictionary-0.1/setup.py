7bd7639b6c6b147a189a1183fce3505f418140bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:46 +0000

    Add package statistic-dictionary/0.1/statistic_dictionary-0.1.tar.gz

---
+++ packages/statistic-dictionary/statistic_dictionary-0.1.tar.gz/statistic_dictionary-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='statistic_dictionary',
+      version='0.1',
+      description='Statistic functions',
+      packages=['statistic_dictionary'],
+      author = 'Pyae Phyo Kyaw',
+      author_email= 'pyaekyaw1571999@gmail.com',
+    zip_safe=False)
---
+++ packages/statistic-dictionary/statistic_dictionary-0.1.tar.gz/statistic_dictionary-0.1/statistic_dictionary/statistic_dictionary.py
@@ -0,0 +1,5 @@
+def average(list):
+    total=0
+    for num in list:
+        total+=num
+    return total/len(list)
