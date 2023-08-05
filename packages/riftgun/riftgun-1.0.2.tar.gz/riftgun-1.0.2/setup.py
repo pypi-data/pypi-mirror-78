2171166ecbecc1fdea2610e24aefbaf6343ad7f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:44 +0000

    Add package riftgun/1.0.2/riftgun-1.0.2.tar.gz

---
+++ packages/riftgun/riftgun-1.0.2.tar.gz/riftgun-1.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='riftgun',
+    version='1.0.2',
+    packages=['riftgun'],
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/dragdev-studios/RiftGun',
+    license='Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International Public License',
+    author='EEKIM10',
+    author_email='eek@clicksminuteper.net',
+    description='A new module for providing easy remote support with discord bots'
+)
