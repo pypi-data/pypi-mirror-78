83ebcb319907478e71dab563027b537af1695c3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:04 +0000

    Add package ls-distributions/1.0/ls_distributions-1.0.tar.gz

---
+++ packages/ls-distributions/ls_distributions-1.0.tar.gz/ls_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ls_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['ls_distributions'],
+      author='Li Shuang',
+      author_email='li.shuang.0425@hotmail.com',
+      zip_safe=False)
