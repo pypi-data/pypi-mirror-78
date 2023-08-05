b7b670f78275d985363291999b971fe93e5f730f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:22 +0000

    Add package interpretability-engine/0.11.2/interpretability-engine-0.11.2.tar.gz

---
+++ packages/interpretability-engine/interpretability-engine-0.11.2.tar.gz/interpretability-engine-0.11.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='interpretability-engine',
+    version='0.11.2',
+    author='Parmentier Laurent',
+    author_email='laurent.parmentier@corp.ovh.com',
+    packages=find_packages(),
+    scripts=['bin/interpretability-engine'],
+    url='https://github.com/ovh/interpretability-engine',
+    description='Interpret Machine Learning black-box models deployed on Serving Engine',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+    ],
+    install_requires=required,
+    include_package_data=True
+)
