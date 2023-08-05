67b2d2d5085a8009106f19f78a05c1e62fa08472
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:12 +0000

    Add package miceforest/1.0.5/miceforest-1.0.5.tar.gz

---
+++ packages/miceforest/miceforest-1.0.5.tar.gz/miceforest-1.0.5/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="miceforest",
+    version="1.0.5",
+    author="Samuel Wilson",
+    license="MIT",
+    author_email="samwilson303@gmail.com",
+    description="Perform MICE",
+    keywords=['MICE','Imputation','Missing Values','Missing','Random Forest'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=['sklearn',
+                      'numpy',
+                      'pandas',
+                      'seaborn',
+                      'matplotlib'
+                      ],
+    url="https://github.com/AnotherSamWilson/miceforest",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
