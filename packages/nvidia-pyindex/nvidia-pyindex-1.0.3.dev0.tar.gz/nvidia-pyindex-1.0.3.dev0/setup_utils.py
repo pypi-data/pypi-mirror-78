11d9bfb359c1f55449046f335761954a2e3dee2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:56 +0000

    Add package nvidia-pyindex/1.0.3.dev0/nvidia-pyindex-1.0.3.dev0.tar.gz

---
+++ packages/nvidia-pyindex/nvidia-pyindex-1.0.3.dev0.tar.gz/nvidia-pyindex-1.0.3.dev0/setup_utils.py
@@ -0,0 +1,24 @@
+import subprocess
+import shlex
+import json
+
+
+def get_configuration_files():
+
+    command = 'python -c "from pip._internal.configuration import ' \
+              'get_configuration_files; print(get_configuration_files())"'
+
+    command = shlex.split(command)
+
+    try:
+        output = subprocess.check_output(command, stderr=subprocess.STDOUT)
+        output = output.decode()
+    except subprocess.CalledProcessError as e:
+        output = e.output.decode()
+        raise RuntimeError(output) from e
+
+    output = output.replace("'", '"')
+    output = json.loads(output)
+
+    return output
+
