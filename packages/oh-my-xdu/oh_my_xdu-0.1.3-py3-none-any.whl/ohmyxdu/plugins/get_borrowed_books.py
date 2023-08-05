94546130b41cdf55482fee8842541dc2f02fbef0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:08 +0000

    Add package oh-my-xdu/0.1.3/oh_my_xdu-0.1.3-py3-none-any.whl

---
+++ packages/oh-my-xdu/oh_my_xdu-0.1.3-py3-none-any.whl/ohmyxdu/plugins/get_borrowed_books.py
@@ -0,0 +1,35 @@
+from typing import Optional, List
+from itertools import count
+
+from loguru import logger
+
+from ohmyxdu.auth.wx import WXAuth
+
+SERVICE_URL = 'http://202.117.121.7:8080/oaCampus/library/getReturn.do'
+
+
+def get_borrowed_books(*, limit: Optional[int] = None) -> List[dict]:
+    """
+    获取当前所有（或指定数量）的借书记录，按时间降序
+
+    :param limit: 指定输出数量
+    :return: 获取到的课本信息
+    """
+
+    token = WXAuth()
+
+    if limit is None:
+        limit = float('inf')
+
+    books = []
+    for offset in count(1):
+        data = token.post(SERVICE_URL, data={'offset': offset}).json()
+        logger.debug(data)
+        if data['msgState'] != 1 or len(books) > limit:
+            break
+        books.extend(data['list'])
+    books = books[:min(limit, len(books))]
+    for book in books:
+        logger.success(f'应还日期:{book["returnDate"]} 《{book["title"]}》')
+
+    return books
---
+++ packages/oh-my-xdu/oh_my_xdu-0.1.3-py3-none-any.whl/ohmyxdu/plugins/get_network_usage.py
@@ -0,0 +1,46 @@
+from typing import List, NamedTuple
+
+from loguru import logger
+from parsel import Selector
+
+from ohmyxdu.auth.zfw import ZFWAuth
+
+
+class Package(NamedTuple):
+    package_name: str
+    used_flow: str
+    left_flow: str
+    paid_left_flow: str
+    settling_day: str
+    expires_day: str
+
+
+def get_network_usage() -> List[Package]:
+    """
+    获取校园网流量使用情况
+
+    :return: 流量包信息
+    """
+
+    token = ZFWAuth()
+
+    resp = token.get('https://zfw.xidian.edu.cn/home')
+
+    if resp.status_code != 200:
+        logger.error('登录失败，尝试检查网络连接与账号密码')
+
+    html = Selector(resp.text)
+    package_tags = html.css('#w3-container>table>tbody>tr')
+
+    packages = []
+    for package_tag in package_tags:
+        packages.append(Package(*[tag.get() for tag in package_tag.css('td::text')]))
+
+    for package in packages:
+        logger.opt(colors=True).success('套餐名称:<blue>{}</blue> '
+                                        '已用流量:<blue>{}</blue> '
+                                        '剩余流量:<blue>{}</blue> '
+                                        '充值剩余流量:<blue>{}</blue> '
+                                        '结算日期:<blue>{}</blue> '
+                                        '套餐到期时间:<blue>{}</blue> ', *package)
+    return packages
