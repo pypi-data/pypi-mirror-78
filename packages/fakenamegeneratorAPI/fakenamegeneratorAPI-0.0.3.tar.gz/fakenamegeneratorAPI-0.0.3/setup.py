671dc07b0106fa87ec2718eb051ca0bb89ac5f49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:54 +0000

    Add package fakenamegeneratorAPI/0.0.3/fakenamegeneratorAPI-0.0.3.tar.gz

---
+++ packages/fakenamegeneratorAPI/fakenamegeneratorAPI-0.0.3.tar.gz/fakenamegeneratorAPI-0.0.3/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fakenamegeneratorAPI",
+    python_requires=">=3.7",
+    version="0.0.3",
+    packages=setuptools.find_packages(),
+    url="https://github.com/catarium/fakenamegenerator_API",
+    license="Apache-2.0 License",
+    author="Catarium",
+    author_email="tartarxan@gmail.com",
+    description="my version of fakenamegenerator API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "requests~=2.24.0",
+        "beautifulsoup4~=4.9.1",
+        "setuptools~=49.6.0", ],
+    include_package_data=False,
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Internet",
+    ],
+)
