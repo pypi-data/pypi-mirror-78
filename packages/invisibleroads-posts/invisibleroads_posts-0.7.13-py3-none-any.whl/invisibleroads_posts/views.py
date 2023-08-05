7ce0afa5231ea82460a3da9f90d5169ecec9383d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:59 +0000

    Add package invisibleroads-posts/0.7.13/invisibleroads_posts-0.7.13-py3-none-any.whl

---
+++ packages/invisibleroads-posts/invisibleroads_posts-0.7.13-py3-none-any.whl/invisibleroads_posts/views.py
@@ -0,0 +1,45 @@
+from pyramid.httpexceptions import HTTPBadRequest
+
+
+def expect_integer(
+        request, key, parse=None, minimum=None, maximum=None, default=None):
+    value = expect_value(
+        request, key, int, 'expected integer', default=default)
+    if parse:
+        try:
+            value = parse(value)
+        except ValueError:
+            raise HTTPBadRequest({key: 'unexpected value'})
+    if minimum and value < minimum:
+        raise HTTPBadRequest({key: 'expected value >= %s' % minimum})
+    if maximum and value > maximum:
+        raise HTTPBadRequest({key: 'expected value <= %s' % maximum})
+    return value
+
+
+def expect_value(request, key, parse=None, message=None, default=None):
+    value = get_value(request, key, default)
+    if parse:
+        try:
+            value = parse(value)
+        except (KeyError, ValueError):
+            raise HTTPBadRequest({key: message or 'bad'})
+    return value
+
+
+def get_value(request, key, default=None):
+    try:
+        return request.matchdict[key]
+    except KeyError:
+        pass
+    try:
+        return request.params[key]
+    except KeyError:
+        pass
+    try:
+        return request.json_body[key]
+    except KeyError:
+        pass
+    if default is not None:
+        return default
+    raise HTTPBadRequest({key: 'required'})
