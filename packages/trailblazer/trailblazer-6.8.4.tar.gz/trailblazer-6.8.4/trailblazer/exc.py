c68264f9f20d57395dce1560816dbb36360d7c7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:15:22 +0000

    Add package trailblazer/6.8.4/trailblazer-6.8.4.tar.gz

---
+++ packages/trailblazer/trailblazer-6.8.4.tar.gz/trailblazer-6.8.4/trailblazer/exc.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+
+class TrailblazerError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+class MissingFileError(TrailblazerError):
+    pass
+
+
+class MipStartError(TrailblazerError):
+    pass
+
+
+class ConfigError(TrailblazerError):
+    def __init__(self, message, errors=None):
+        self.message = message
+        self.errors = errors
