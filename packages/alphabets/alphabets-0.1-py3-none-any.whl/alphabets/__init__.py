ba5e87c650d525acae1ddf8d8c68d1f2f5362740
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:54 +0000

    Add package alphabets/0.1/alphabets-0.1-py3-none-any.whl

---
+++ packages/alphabets/alphabets-0.1-py3-none-any.whl/alphabets/__init__.py
@@ -0,0 +1,69 @@
+"""
+
+Alphabet Module By Aditya Shrivastava [version 0.1 ] 
+Copyright (c) 2020 Aditya Shrivastava. All Rights Reserved
+
+For Site Documentation, visit myprogramarchives.blogspot.com
+
+INSTRUCTION:
+
+Please put this file in (folder where python is installed)\Lib\site-packages
+
+"""
+
+
+
+
+
+#small alphabets seperated and can be iterated(looped) 
+
+def small():
+    ''' Returns a list of alphabets in small'''
+
+    return ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+#small alphabets together in one line and can be iterated(looped) 
+
+def smallOneLine():
+    ''' Returns small alphabets in single line'''
+
+    return 'abcdefghijklmnopqrstuvwxyz'
+	
+#capital alphabets seperated and can be iterated(looped)	
+
+def capital():
+    ''' Returns a list of alphabets in CAPITAL '''
+    return ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+#capital alphabets together in oneline and can be iterated(looped)	
+
+def capitalOneLine():
+    ''' Returns capital alphabets in single line'''
+    return 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+	
+#small vowels seperated and can be iterated(looped)
+
+def vowels():
+    ''' Returns small vowels in list'''
+    return ['a','e','i','o','u']    
+	
+#small vowels together in one line and can be iterated(looped)	
+
+def vowelsOneLine():
+    ''' Return small vowels in oneline'''
+    return 'aeiou'
+	
+#Capital vowels seperated and can be iterated(looped)	
+
+def vowelsCapital():
+    ''' Returns capital vowels in list'''
+    return ['A','E','I','O','U']    
+	
+#Capital vowels in one line and can be iterated(looped)	
+
+def vowelsCapitalOneLine():
+    ''' Returns capital vowels in single line'''
+    return 'AEIOU'
+
+    
+
