aca3fe8a16307308b65f82950fc4f1f1009eac08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:32 +0000

    Add package JATA/0.3.1/JATA-0.3.1-py3-none-any.whl

---
+++ packages/JATA/JATA-0.3.1-py3-none-any.whl/pixplot_notebook/__init__.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import
+from pixplot_notebook.pixplot import process_images, parse
+import subprocess
+# specify version number
+import pkg_resources
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+try:
+
+    import pixplot
+
+except BaseException as e:
+    install('pixplot')
+    import pixplot
+
+    print("Success installing pixplot_notebook packages:)")
+
+__version__ = pkg_resources.get_distribution('pixplot').version
