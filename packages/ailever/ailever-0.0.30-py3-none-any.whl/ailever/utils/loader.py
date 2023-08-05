24c6423c316242e17c6dd4cde03c95c018fb2069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:14 +0000

    Add package ailever/0.0.30/ailever-0.0.30-py3-none-any.whl

---
+++ packages/ailever/ailever-0.0.30-py3-none-any.whl/ailever/utils/loader.py
@@ -0,0 +1,32 @@
+import os
+from urllib.request import urlretrieve
+
+def storage(name):
+    urlretrieve('https://raw.githubusercontent.com/ailever/ailever/master/storage/'+name, f'./{name}')
+    print(f'[AILEVER] The file "{name}" is downloaded!')
+
+
+def repository(repo):
+    if repo == 'ailever':
+        os.system('git clone https://github.com/ailever/ailever.git')
+        print(f'[AILEVER] The repository "{repo}" is successfully cloned!')
+    elif repo == 'programming-language':
+        os.system('git clone https://github.com/ailever/programming-language.git')
+        print(f'[AILEVER] The repository "{repo}" is successfully cloned!')
+    elif repo == 'numerical-method':
+        os.system('git clone https://github.com/ailever/numerical-method.git')
+        print(f'[AILEVER] The repository "{repo}" is successfully cloned!')
+    elif repo == 'statistics':
+        os.system('git clone https://github.com/ailever/statistics.git')
+        print(f'[AILEVER] The repository "{repo}" is successfully cloned!')
+    elif repo == 'applications':
+        os.system('git clone https://github.com/ailever/applications.git')
+        print(f'[AILEVER] The repository "{repo}" is successfully cloned!')
+    elif repo == 'deep-learning':
+        os.system('git clone https://github.com/ailever/deep-learning.git')
+        print(f'[AILEVER] The repository "{repo}" is successfully cloned!')
+
+
+def cloud(name=None):
+    urlretrieve('https://docs.google.com/uc?export=download&id=1gkLmsatRci0BeU3xp2VD1ArOpceOorVq', f'./template.pptx')
+    print(f'[AILEVER] The file "template.pptx" is downloaded!')
