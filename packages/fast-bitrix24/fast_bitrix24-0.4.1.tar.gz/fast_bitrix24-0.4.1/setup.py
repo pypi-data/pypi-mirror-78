987ae43887a8f8d643359f10a25b30c25431ec38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:50 +0000

    Add package fast-bitrix24/0.4.1/fast_bitrix24-0.4.1.tar.gz

---
+++ packages/fast-bitrix24/fast_bitrix24-0.4.1.tar.gz/fast_bitrix24-0.4.1/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fast_bitrix24",
+    version="0.4.1",
+    author="Alexey Leshchenko",
+    author_email="leshchenko@gmail.com",
+    description="Высокоуровневый API для Python 3.7+ для быстрого получения данных от Битрикс24 через REST API. Параллельные запросы к серверу, упаковка запросов в батчи, контроль скорости запросов.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/leshchenko1979/fast_bitrix24",
+    packages=['fast_bitrix24'],
+    exclude_package_data={
+        'fast_bitrix24': ["test**"]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        'aiohttp',
+        'asyncio',
+        'tqdm'
+    ],
+    license="MIT"
+)
