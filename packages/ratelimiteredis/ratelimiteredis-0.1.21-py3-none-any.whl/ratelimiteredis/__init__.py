c817b82b81be9a4f2dc2db43da22cb8f9c4d3b07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:17 +0000

    Add package ratelimiteredis/0.1.21/ratelimiteredis-0.1.21-py3-none-any.whl

---
+++ packages/ratelimiteredis/ratelimiteredis-0.1.21-py3-none-any.whl/ratelimiteredis/__init__.py
@@ -0,0 +1,7 @@
+from .main import RateLimit as RateLimit
+from .redis import RedisInterface as RedisInterface, redisinterface as redisinterface
+
+__all__ = ("RateLimit", "RedisInterface", "redisinterface")
+
+__author__ = "PredaaA"
+__version__ = "0.1.21"
