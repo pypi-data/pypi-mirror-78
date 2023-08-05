7d33d503650840bdb7108723de1b6165c0e7e8bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:07 +0000

    Add package oort-cloud/0.9.12/oort_cloud-0.9.12-py2.py3-none-any.whl

---
+++ packages/oort-cloud/oort_cloud-0.9.12-py2.py3-none-any.whl/oort/server/app/static/js/main-app.js
@@ -0,0 +1,47 @@
+Vue.component('v-select', VueSelect.VueSelect)
+
+var app = new Vue({
+  el: '#vuejs',
+  data: {
+    source: null,
+    state: { folders: [] },
+    selected_folder: null,
+    pending_uploads: [],
+    current_uploads: [],
+    finished_uploads: [],
+    error_uploads: [],
+    progresses: [],
+    show_tables: {
+      pending: true,
+      current: true,
+      finished: true,
+      error: true
+    }
+  },
+  computed: {
+    selectButtonTitle () {
+      return this.selected_folder ? this.selected_folder.path : '-'
+    }
+  },
+  mounted: function () {
+    const self = this
+    this.source = new EventSource('/uploads')
+    this.source.onmessage = function (event) {
+      const json_data = JSON.parse(event.data)
+
+      self.state = json_data.state
+      if (self.state.folders.length === 1) {
+        self.selected_folder = self.state.folders[0]
+      }
+
+      self.pending_uploads = json_data.pending
+      self.current_uploads = json_data.current
+      self.finished_uploads = json_data.finished
+      self.error_uploads = json_data.errors
+
+      self.progresses = self.current_uploads.map(upload => upload.progress)
+    }
+  }
+})
+
+
