64fc7e9aa4e21c29f74451b749b2d3279d46070d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:29 +0000

    Add package ptf-optimize/0.0.5/ptf_optimize-0.0.5.tar.gz

---
+++ packages/ptf-optimize/ptf_optimize-0.0.5.tar.gz/ptf_optimize-0.0.5/setup.py
@@ -0,0 +1,28 @@
+# TODO: Fill out this file with information about your package
+
+# HINT: Go back to the object-oriented programming lesson "Putting Code on PyPi" and "Exercise: Upload to PyPi"
+
+# HINT: Here is an example of a setup.py file
+# https://packaging.python.org/tutorials/packaging-projects/
+
+import setuptools
+
+
+setuptools.setup(
+    name="ptf_optimize", # Replace with your own username
+    version="0.0.5",
+    author="Juan Piao",
+    author_email="juanpiao13@gmail.com",
+    description="get optimal portfolio",
+    
+    long_description_content_type="text/markdown",
+    url="https://github.com/JPL13",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    zip_safe=False
+)
