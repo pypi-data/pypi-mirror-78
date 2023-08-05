c723d3123a50ad0935ed924a049c33c90602559e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:26 +0000

    Add package paramsurvey/0.4.4/paramsurvey-0.4.4.tar.gz

---
+++ packages/paramsurvey/paramsurvey-0.4.4.tar.gz/paramsurvey-0.4.4/test/integration/test-multiprocessing.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [ ! -z "$COVERAGE" ]; then
+    COVERAGE="--cov-report= --cov-append --cov-branch --cov paramsurvey -v -v"
+fi
+
+PARAMSURVEY_BACKEND=multiprocessing pytest $COVERAGE $PYTEST_STDERR_VISIBLE $1
+PARAMSURVEY_BACKEND=multiprocessing pytest $COVERAGE $PYTEST_STDERR_VISIBLE $1/test-multiprocessing-failure.py
+
