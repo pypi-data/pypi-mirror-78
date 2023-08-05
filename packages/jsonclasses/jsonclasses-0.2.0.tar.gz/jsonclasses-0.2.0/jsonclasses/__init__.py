49334f2ed9989838001cffbd1212ad241ef5243a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:47 +0000

    Add package jsonclasses/0.2.0/jsonclasses-0.2.0.tar.gz

---
+++ packages/jsonclasses/jsonclasses-0.2.0.tar.gz/jsonclasses-0.2.0/jsonclasses/__init__.py
@@ -0,0 +1,5 @@
+from .jsonclass import jsonclass
+from .types import types
+from .json_object import JSONObject
+from .persistable_json_object import PersistableJSONObject
+from .json_encoder import JSONEncoder
