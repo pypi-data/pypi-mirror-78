f67823794f302b6239642e4c70a02488c77a286d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:21 +0000

    Add package django-data-browser/2.2.10/django_data_browser-2.2.10-py3-none-any.whl

---
+++ packages/django-data-browser/django_data_browser-2.2.10-py3-none-any.whl/data_browser/common.py
@@ -0,0 +1,61 @@
+import logging
+
+from django import http
+
+from . import version
+
+MAKE_PUBLIC_CODENAME = "make_view_public"
+
+
+def can_make_public(user):
+    return user.has_perm(f"data_browser.{MAKE_PUBLIC_CODENAME}")
+
+
+def JsonResponse(data):
+    res = http.JsonResponse(data, safe=False)
+    res["X-Version"] = version
+    res["Access-Control-Expose-Headers"] = "X-Version"
+    return res
+
+
+def HttpResponse(*args, **kwargs):
+    res = http.HttpResponse(*args, **kwargs)
+    res["X-Version"] = version
+    res["Access-Control-Expose-Headers"] = "X-Version"
+    return res
+
+
+def debug_log(msg):  # pragma: no cover
+    if settings.DEBUG:
+        logging.getLogger(__name__).warning(f"DDB: {msg}")
+
+
+def all_subclasses(cls):
+    res = set()
+    queue = {cls}
+    while queue:
+        cls = queue.pop()
+        subs = set(cls.__subclasses__())
+        queue.update(subs - res)
+        res.update(subs)
+    return res
+
+
+class Settings:
+    _defaults = {
+        "DATA_BROWSER_ALLOW_PUBLIC": False,
+        "DATA_BROWSER_AUTH_USER_COMPAT": True,
+        "DATA_BROWSER_DEFAULT_ROW_LIMIT": 1000,
+        "DATA_BROWSER_DEV": False,
+        "DATA_BROWSER_FE_DSN": None,
+    }
+
+    def __getattr__(self, name):
+        from django.conf import settings
+
+        if hasattr(settings, name):
+            return getattr(settings, name)
+        return self._defaults[name]
+
+
+settings = Settings()
---
+++ packages/django-data-browser/django_data_browser-2.2.10-py3-none-any.whl/data_browser/fe_build/precache-manifest.c4889899b384c6d9330f004c45c85f2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29d0a2fdf0bfd01e0825e344df729a7c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "38e5b867a78b82a243fc",
+    "url": "./static/css/main.bac4aaf9.chunk.css"
+  },
+  {
+    "revision": "baef56cf7e3e03c5fdcd",
+    "url": "./static/js/2.3fdc1d6e.chunk.js"
+  },
+  {
+    "revision": "66486fd8359f40be208121c37c8c9200",
+    "url": "./static/js/2.3fdc1d6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38e5b867a78b82a243fc",
+    "url": "./static/js/main.4f9b62f3.chunk.js"
+  },
+  {
+    "revision": "aba697584663a7cb794c",
+    "url": "./static/js/runtime-main.c3c9eabf.js"
+  }
+]);
