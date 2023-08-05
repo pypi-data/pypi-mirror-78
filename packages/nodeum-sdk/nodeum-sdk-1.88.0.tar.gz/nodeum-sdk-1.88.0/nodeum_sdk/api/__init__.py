46e3d358147799a68e1af84882016d51fb0a7787
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:09 +0000

    Add package nodeum-sdk/1.88.0/nodeum-sdk-1.88.0.tar.gz

---
+++ packages/nodeum-sdk/nodeum-sdk-1.88.0.tar.gz/nodeum-sdk-1.88.0/nodeum_sdk/api/__init__.py
@@ -0,0 +1,28 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from nodeum_sdk.api.cloud_buckets_api import CloudBucketsApi
+from nodeum_sdk.api.cloud_connectors_api import CloudConnectorsApi
+from nodeum_sdk.api.containers_api import ContainersApi
+from nodeum_sdk.api.files_api import FilesApi
+from nodeum_sdk.api.metadata_api import MetadataApi
+from nodeum_sdk.api.mounts_api import MountsApi
+from nodeum_sdk.api.nas_api import NasApi
+from nodeum_sdk.api.nas_shares_api import NasSharesApi
+from nodeum_sdk.api.pools_api import PoolsApi
+from nodeum_sdk.api.statistics_api import StatisticsApi
+from nodeum_sdk.api.systems_api import SystemsApi
+from nodeum_sdk.api.tape_drives_api import TapeDrivesApi
+from nodeum_sdk.api.tape_libraries_api import TapeLibrariesApi
+from nodeum_sdk.api.tapes_api import TapesApi
+from nodeum_sdk.api.task_callbacks_api import TaskCallbacksApi
+from nodeum_sdk.api.task_destinations_api import TaskDestinationsApi
+from nodeum_sdk.api.task_executions_api import TaskExecutionsApi
+from nodeum_sdk.api.task_metadata_api import TaskMetadataApi
+from nodeum_sdk.api.task_options_api import TaskOptionsApi
+from nodeum_sdk.api.task_schedules_api import TaskSchedulesApi
+from nodeum_sdk.api.task_sources_api import TaskSourcesApi
+from nodeum_sdk.api.tasks_api import TasksApi
+from nodeum_sdk.api.users_api import UsersApi
