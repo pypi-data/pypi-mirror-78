00c6f2683c3691abb614228a9c4330e5a3c562b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:22 +0000

    Add package Ninja-Preview/2.1/Ninja-Preview-2.1.tar.gz

---
+++ packages/Ninja-Preview/Ninja-Preview-2.1.tar.gz/Ninja-Preview-2.1/Ninja_Preview/Ninja_preview.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+import sys
+import os
+from base64 import b64decode
+from PyQt5.QtCore import QCoreApplication, QSettings, QResource
+from PyQt5.QtGui import QGuiApplication, QIcon
+from PyQt5.QtQml import QQmlApplicationEngine
+from Ninja_Preview.preview import Preview
+from Ninja_Preview._ninjapreview_resource import rcc
+
+
+def dummy_run():
+    pass
+
+def cleanUp():
+    if os.path.exists('_ninjapreview_resource.rcc'):
+        os.remove('_ninjapreview_resource.rcc')
+
+dec = b64decode(rcc)
+
+with open('_ninjapreview_resource.rcc', 'wb') as rcc_f:
+    rcc_f.write(dec)
+
+QResource.registerResource("_ninjapreview_resource.rcc")
+
+qApp = QGuiApplication(sys.argv)
+
+QCoreApplication.setOrganizationName("Deuteronomy Works")
+QCoreApplication.setApplicationName("Ninja-Preview")
+settings = QSettings()
+
+qApp.setWindowIcon(QIcon(":icons/logo.png"))
+qApp.aboutToQuit.connect(cleanUp)
+
+engine = QQmlApplicationEngine()
+
+preview = Preview()
+
+engine.rootContext().setContextProperty('preview', preview)
+
+engine.load("qrc:///UI/main.qml")
+
+engine.quit.connect(qApp.quit)
+
+sys.exit(qApp.exec_())
---
+++ packages/Ninja-Preview/Ninja-Preview-2.1.tar.gz/Ninja-Preview-2.1/README.md
@@ -0,0 +1,11 @@
+# Ninja-Preview-wheel [![Downloads](https://pepy.tech/badge/ninja-preview)](https://pepy.tech/project/ninja-preview)
+The wheel distribution repo for Ninja-Preview
+
+# Change Log
+## V2.1-beta
+* Remove PyQt5 dependency, depend on Qmlview
+
+## V2.1
+* Revert entry points name to Ninja-Preview from Ninja_Preview
+* Bugfix
+** Clean up resource files when app closes
