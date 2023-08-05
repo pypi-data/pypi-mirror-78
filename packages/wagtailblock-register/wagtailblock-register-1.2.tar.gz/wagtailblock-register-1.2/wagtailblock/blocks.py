c147c1e94a0ce575064e9fb5f11a166f688359a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:14:50 +0000

    Add package wagtailblock-register/1.2/wagtailblock-register-1.2.tar.gz

---
+++ packages/wagtailblock-register/wagtailblock-register-1.2.tar.gz/wagtailblock-register-1.2/wagtailblock/blocks.py
@@ -0,0 +1,22 @@
+from wagtail.core.blocks import (
+    BaseStreamBlock,
+    StructBlock,
+    StreamBlock,
+    FloatBlock,
+    CharBlock,
+    ChoiceBlock,
+    ChooserBlock,
+    RawHTMLBlock,
+    TextBlock,
+    TimeBlock,
+    RichTextBlock,
+    DateTimeBlock,
+    DateBlock,
+    EmailBlock,
+    BooleanBlock,
+    BoundBlock,
+    IntegerBlock,
+    ListBlock,
+)
+from wagtail.images.blocks import ImageChooserBlock
+from wagtail.core.blocks.base import Block, BoundBlock, DeclarativeSubBlocksMetaclass
---
+++ packages/wagtailblock-register/wagtailblock-register-1.2.tar.gz/wagtailblock-register-1.2/wagtailblock/register.py
@@ -0,0 +1,34 @@
+from wagtail.utils.apps import get_app_submodules
+from django.conf import settings
+
+_block_list = []
+
+_searched_for_hooks = False
+
+
+def search_for_hooks():
+    global _searched_for_hooks
+    if not _searched_for_hooks:
+        list(get_app_submodules(getattr(settings, "WAGTAILBLOCK_COLLECTOR", "blocks")))
+        _searched_for_hooks = True
+
+
+def register_block(block):
+    if block().label is None:
+        label = block.__name__
+    else:
+        label = block().label
+            
+        block_label = block.__name__
+
+    if block().meta.icon == "placeholder":
+        icon = "fa-leaf"
+    else:
+        icon = block().meta.icon
+
+    _block_list.append((block_label, block(icon=icon, label="{}".format(label))))
+
+
+def call_blocks():
+    search_for_hooks()
+    return _block_list
