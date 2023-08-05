5c9f1411852f94248ae9fc74d707759e4018acff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:16 +0000

    Add package algoliaqb/0.1/algoliaqb-0.1.tar.gz

---
+++ packages/algoliaqb/algoliaqb-0.1.tar.gz/algoliaqb-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(name='algoliaqb',
+      version='0.1',
+      description='Algolia Query Builder',
+      url='http://github.com/ryanb58/algoliaqb',
+      author='Taylor Brazelton',
+      author_email='taylor.r.brazelton@gmail.com',
+      license='MIT',
+      packages=['algoliaqb'],
+      zip_safe=False
+)
