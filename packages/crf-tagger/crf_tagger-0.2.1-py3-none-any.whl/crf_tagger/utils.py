a8942b9fc2b63830c3069a4398a0516a10a4c2d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:29 +0000

    Add package crf-tagger/0.2.1/crf_tagger-0.2.1-py3-none-any.whl

---
+++ packages/crf-tagger/crf_tagger-0.2.1-py3-none-any.whl/crf_tagger/utils.py
@@ -0,0 +1,11 @@
+sfrom itertools import chain
+
+
+def flatten(y):
+    """
+    Flatten a list of lists.
+
+    >>> flatten([[1,2], [3,4]])
+    [1, 2, 3, 4]
+    """
+    return list(chain.from_iterable(y))
