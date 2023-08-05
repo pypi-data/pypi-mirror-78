edb9fef48adb800b93900a7fabe9f65a8edbf28b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:54 +0000

    Add package django-categories/1.8/django-categories-1.8.tar.gz

---
+++ packages/django-categories/django-categories-1.8.tar.gz/django-categories-1.8/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import categories
+import os
+
+try:
+    long_description = open('README.rst').read()
+except IOError:
+    long_description = ''
+
+try:
+    reqs = open(os.path.join(os.path.dirname(__file__), 'requirements.txt')).read()
+except (IOError, OSError):
+    reqs = ''
+
+setup(
+    name='django-categories',
+    version=categories.get_version(),
+    description='A way to handle one or more hierarchical category trees in django.',
+    long_description=long_description,
+    author='Corey Oordt',
+    author_email='coreyoordt@gmail.com',
+    include_package_data=True,
+    url='http://github.com/callowayproject/django-categories',
+    packages=find_packages(exclude=['example*', ]),
+    classifiers=[
+        'Framework :: Django',
+    ],
+    install_requires=reqs,
+    dependency_links=[]
+)
