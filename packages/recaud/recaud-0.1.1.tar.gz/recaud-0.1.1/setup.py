87d4b81c2d3708af99e064de179e65329eac12e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:29 +0000

    Add package recaud/0.1.1/recaud-0.1.1.tar.gz

---
+++ packages/recaud/recaud-0.1.1.tar.gz/recaud-0.1.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['recaud']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pyyaml>=5.3.1,<6.0.0']
+
+entry_points = \
+{'console_scripts': ['recaud = recaud.recaud:main']}
+
+setup_kwargs = {
+    'name': 'recaud',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'dkllrjr',
+    'author_email': 'dg.kllr.jr@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
