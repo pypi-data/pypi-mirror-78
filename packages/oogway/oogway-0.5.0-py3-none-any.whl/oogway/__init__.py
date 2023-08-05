cfbfefbc80b81fdf258d3908d567836a7d0e4329
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:51 +0000

    Add package oogway/0.5.0/oogway-0.5.0-py3-none-any.whl

---
+++ packages/oogway/oogway-0.5.0-py3-none-any.whl/oogway/__init__.py
@@ -0,0 +1,11 @@
+from .key import Key
+from .validate import validate
+from .convert import convert
+from .operation import operation
+from .fees import get_fees
+from .request import request_payment, parse_request
+from .net import Net
+from .rpc import AuthServiceProxy as Node
+
+name = "oogway"
+__version__ = "0.5.0"
