94cf0ff0253a957acd370f01b4e3a063cbc4c501
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:00 +0000

    Add package cms-distributions/0.2/cms_distributions-0.2.tar.gz

---
+++ packages/cms-distributions/cms_distributions-0.2.tar.gz/cms_distributions-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='cms_distributions',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['cms_distributions'],
+      author="Cornelius M",
+      author_email="jdialogc@gmail.com",
+      zip_safe=False)
