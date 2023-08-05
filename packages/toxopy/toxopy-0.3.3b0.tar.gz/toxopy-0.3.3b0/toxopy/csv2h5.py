b893762341084d9e35fb68d3189b77aa9c4bf063
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:00 +0000

    Add package toxopy/0.3.3b0/toxopy-0.3.3b0.tar.gz

---
+++ packages/toxopy/toxopy-0.3.3b0.tar.gz/toxopy-0.3.3b0/toxopy/csv2h5.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from tqdm import tqdm
+import glob
+
+
+def csv2h5(directory=None, files=None):
+
+    if directory is None:
+
+        for file in tqdm(files):
+
+            df = pd.read_csv(file)
+
+            df.to_hdf(file.strip('.csv') + '.h5',
+                      'data',
+                      mode='w',
+                      format='table')
+            del df
+
+    else:
+
+        csv_files_dir = directory + '/*.csv'
+
+        files = glob.glob(csv_files_dir)
+
+        for file in tqdm(files):
+
+            df = pd.read_csv(file)
+
+            df.to_hdf(file.strip('.csv') + '.h5',
+                      'data',
+                      mode='w',
+                      format='table')
+            del df
