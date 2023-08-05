da5e45971db29aae5c7414c824720c16e0aecea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:19 +0000

    Add package apywrapper/0.1.1/apywrapper-0.1.1-py3-none-any.whl

---
+++ packages/apywrapper/apywrapper-0.1.1-py3-none-any.whl/apywrapper/_api.py
@@ -0,0 +1,28 @@
+from httpx._types import HeaderTypes
+
+from ._abc import Api
+from ._request import HttpClient, make_request
+
+
+class Apy(Api):
+    """
+    Apy class
+    """
+
+    def __init__(self, host: str, headers: HeaderTypes) -> None:
+        self.http_client = HttpClient(base_url=host, headers=headers)
+
+    def get(self, path: str):
+        return make_request(path, self.http_client.get_request)
+
+    def post(self, path: str):
+        return make_request(path, self.http_client.post_request)
+
+    def put(self, path: str):
+        return make_request(path, self.http_client.put_request)
+
+    def delete(self, path: str):
+        return make_request(path, self.http_client.delete_request)
+
+    def patch(self, path: str):
+        return make_request(path, self.http_client.patch_request)
