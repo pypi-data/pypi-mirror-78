afbdd133227b468e8c527e4f178bd59e1fba1e20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:14 +0000

    Add package aw-distributions/0.1/aw_distributions-0.1.tar.gz

---
+++ packages/aw-distributions/aw_distributions-0.1.tar.gz/aw_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='aw_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author='Wah Chi Shing', 
+      author_email='wahchishing@gmail.com', 
+      zip_safe=False)
