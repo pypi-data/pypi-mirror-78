3d6e1f7ea097af221c3a0babf52323e8304f03a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:44 +0000

    Add package django-cancan/0.3.1/django-cancan-0.3.1.tar.gz

---
+++ packages/django-cancan/django-cancan-0.3.1.tar.gz/django-cancan-0.3.1/cancan/middleware.py
@@ -0,0 +1,35 @@
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+from django.utils.deprecation import MiddlewareMixin
+from django.utils.module_loading import import_string
+
+from .ability import Ability, AbilityValidator
+
+
+class CanCanMiddleware(MiddlewareMixin):
+    def process_request(self, request):
+        assert hasattr(request, 'user'), (
+            "Cancan authentication middleware requires authenticationMiddleware middleware "
+            "to be installed. Edit your MIDDLEWARE setting to insert "
+            "'django.contrib.auth.middleware.AuthenticationMiddleware' before."
+            "'cancan.middleware.CanCanMiddleware'"
+        )
+
+        assert hasattr(settings, 'CANCAN'), (
+            "CANCAN section not found in settings"
+        )
+        assert 'ABILITIES' in settings.CANCAN, (
+            "CANCAN['ABILITIES'] is missing. It must point to a function"
+        )
+        fn_name = settings.CANCAN['ABILITIES']
+
+        declare_abilities = import_string(settings.CANCAN['ABILITIES'])
+
+        assert callable(declare_abilities), (
+            f"{fn_name}  must be callabe function fn(user: User, ability: Ability)"
+        )
+
+        ability = Ability(request.user)
+        declare_abilities(request.user, ability)
+
+        request.ability = AbilityValidator(ability)
