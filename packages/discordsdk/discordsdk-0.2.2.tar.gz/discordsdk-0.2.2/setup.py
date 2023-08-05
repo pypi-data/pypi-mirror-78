ea4b51d510b2c11de6ebfa0472361c9d3da59cae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:53 +0000

    Add package discordsdk/0.2.2/discordsdk-0.2.2.tar.gz

---
+++ packages/discordsdk/discordsdk-0.2.2.tar.gz/discordsdk-0.2.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="discordsdk",
+    version="0.2.2",
+    author="DoAltPlusF4 & NathaanTFM",
+    author_email="doaltplusf4@gmail.com",
+    description="Python wrapper around Discord's Game SDK library.",
+    license="LICENSE",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DoAltPlusF4/discord-sdk",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
