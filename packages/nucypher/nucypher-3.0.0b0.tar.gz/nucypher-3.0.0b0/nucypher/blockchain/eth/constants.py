ce740f4f42e325c1ca4c2d8833910a12edd0461c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:07 +0000

    Add package nucypher/3.0.0b0/nucypher-3.0.0b0.tar.gz

---
+++ packages/nucypher/nucypher-3.0.0b0.tar.gz/nucypher-3.0.0b0/nucypher/blockchain/eth/constants.py
@@ -0,0 +1,55 @@
+"""
+ This file is part of nucypher.
+
+ nucypher is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ nucypher is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with nucypher.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+#
+# Contract Names
+#
+
+DISPATCHER_CONTRACT_NAME = 'Dispatcher'
+STAKING_INTERFACE_ROUTER_CONTRACT_NAME = "StakingInterfaceRouter"
+NUCYPHER_TOKEN_CONTRACT_NAME = 'NuCypherToken'
+STAKING_ESCROW_CONTRACT_NAME = 'StakingEscrow'
+POLICY_MANAGER_CONTRACT_NAME = 'PolicyManager'
+STAKING_INTERFACE_CONTRACT_NAME = 'StakingInterface'
+PREALLOCATION_ESCROW_CONTRACT_NAME = 'PreallocationEscrow'
+ADJUDICATOR_CONTRACT_NAME = 'Adjudicator'
+WORKLOCK_CONTRACT_NAME = 'WorkLock'
+MULTISIG_CONTRACT_NAME = 'MultiSig'
+
+NUCYPHER_CONTRACT_NAMES = (
+    NUCYPHER_TOKEN_CONTRACT_NAME,
+    STAKING_ESCROW_CONTRACT_NAME,
+    POLICY_MANAGER_CONTRACT_NAME,
+    ADJUDICATOR_CONTRACT_NAME,
+    DISPATCHER_CONTRACT_NAME,
+    STAKING_INTERFACE_CONTRACT_NAME,
+    STAKING_INTERFACE_ROUTER_CONTRACT_NAME,
+    PREALLOCATION_ESCROW_CONTRACT_NAME,
+    WORKLOCK_CONTRACT_NAME,
+    MULTISIG_CONTRACT_NAME,
+)
+
+
+# Ethereum
+
+AVERAGE_BLOCK_TIME_IN_SECONDS = 14
+ETH_ADDRESS_BYTE_LENGTH = 20
+ETH_ADDRESS_STR_LENGTH = 40
+ETH_HASH_BYTE_LENGTH = 32
+LENGTH_ECDSA_SIGNATURE_WITH_RECOVERY = 65
+MAX_UINT16 = 65535
+NULL_ADDRESS = '0x' + '0' * 40
