15320509d3db0aa376309eac04a5c7064880ef90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:32 +0000

    Add package quantrading/0.1.22/quantrading-0.1.22-py3-none-any.whl

---
+++ packages/quantrading/quantrading-0.1.22-py3-none-any.whl/quantrading/backtest/__init__.py
@@ -0,0 +1,18 @@
+from .backtest import (
+    Strategy
+)
+from .simulation_result_utils import save_simulation_result_to_excel_file
+from .light_backtest import (
+    LightStrategy
+)
+from .utils import (
+    divide_code_list_by_quantiles,
+    apply_equal_weights,
+    get_no_rebalancing_port_daily_value_df,
+    get_static_weight_rebalancing_port_daily_value_df,
+    get_dynamic_weight_rebalancing_port_daily_value_df,
+    concat_simulation_result
+)
+from .backtest_open_close import (
+    OpenCloseStrategy
+)
---
+++ packages/quantrading/quantrading-0.1.22-py3-none-any.whl/quantrading/backtest/backtest.py
@@ -0,0 +1,169 @@
+import pandas as pd
+from datetime import timedelta
+from .portfolio import Portfolio
+from .simulation_result_utils import calc_performance_from_value_history
+from .trading_day import TradingDay
+from .backtest_base import BackTestBase
+
+
+class Strategy(BackTestBase):
+    def __init__(self, **kwargs):
+        self.name = kwargs.get("name")
+        self.start_date = kwargs.get("start_date")
+        self.end_date = kwargs.get("end_date")
+        self.market_df = kwargs.get("market_df")
+        self.rebalancing_periodic = kwargs.get("rebalancing_periodic", 'monthly')
+        self.rebalancing_moment = kwargs.get("rebalancing_moment", 'first')
+        self.name_for_result_column = kwargs.get('name_for_result_column', '전략')
+
+        self.on_data_before_n_days_of_rebalacing_days = kwargs.get('on_data_before_n_days_of_rebalacing_days', 1)
+
+        # set trading days & rebalancing days
+        trading_day = TradingDay(self.market_df.index.to_series().reset_index(drop=True))
+        self.trading_days = trading_day.get_trading_day_list(self.start_date, self.end_date).to_list()
+        self.rebalancing_days = trading_day.get_rebalancing_days(self.start_date, self.end_date,
+                                                                 self.rebalancing_periodic, self.rebalancing_moment)
+        self.date = self.start_date
+        self.market_df_pct_change = self.market_df.pct_change()
+
+        self.portfolio = Portfolio()
+        self.portfolio_log = pd.DataFrame()
+        self.order_weight_df = pd.DataFrame()
+        self.simulation_status = {}
+        self.simulation_result = {}
+        self.event_log = pd.DataFrame(columns=["datetime", "log"])
+        self.exist_reservation_order = False
+        self.reservation_order = pd.Series()
+        self.selected_asset_counts = pd.Series()
+
+    def initialize(self):
+        pass
+
+    def on_data(self):
+        pass
+
+    def run(self):
+        self.initialize()
+
+        end_date = self.end_date
+        while self.date <= end_date:
+            if self.is_trading_day():
+                self.on_start_of_day()
+                if self.exist_reservation_order:
+                    self.execute_reservation_order()
+                if self.is_rebalancing_day(self.on_data_before_n_days_of_rebalacing_days):
+                    self.on_data()
+                self.on_end_of_day()
+            self.date += timedelta(days=1)
+        self.on_end_of_algorithm()
+
+    def execute_reservation_order(self):
+        self.portfolio.set_allocations(self.reservation_order)
+        reservation_order_series = self.reservation_order
+        reservation_order_series.name = self.date
+        self.order_weight_df = pd.concat([self.order_weight_df, reservation_order_series.to_frame().T], axis=0)
+        self.selected_asset_counts.loc[self.date] = len(self.reservation_order)
+        self.reservation_order = pd.Series()
+        self.exist_reservation_order = False
+
+    def reserve_order(self, allocation: pd.Series):
+        self.reservation_order = allocation
+        self.exist_reservation_order = True
+
+    def log_event(self, msg: str):
+        self.event_log.loc[len(self.event_log)] = [self.date, msg]
+
+    def is_trading_day(self):
+        today = self.date
+        if today in self.trading_days:
+            return True
+        else:
+            return False
+
+    def is_rebalancing_day(self, next_n_trading_days):
+        today = self.date
+
+        if today == self.start_date:
+            if today in self.rebalancing_days:
+                return True
+            else:
+                return False
+
+        target_date_index = self.trading_days.index(today) + next_n_trading_days
+        if target_date_index >= len(self.trading_days):
+            return False
+
+        target_date = self.trading_days[target_date_index]
+        if target_date in self.rebalancing_days:
+            return True
+        else:
+            return False
+
+    def on_start_of_day(self):
+        pass
+
+    def on_end_of_day(self):
+        self.update_portfolio_value()
+        self.log_portfolio_value()
+
+    def on_end_of_algorithm(self):
+        pass
+
+    def log_portfolio_value(self):
+        port_value = self.portfolio.get_total_portfolio_value()
+        cash = self.portfolio.cash
+
+        self.portfolio_log.loc[self.date, "port_value"] = port_value
+        self.portfolio_log.loc[self.date, "cash"] = cash
+
+        for ticker, amount in self.portfolio.security_holding.items():
+            self.portfolio_log.loc[self.date, ticker + "_amount"] = amount
+
+    def update_portfolio_value(self):
+        self.portfolio.update_holdings_value(self.date, self.market_df_pct_change)
+
+    def get_daily_return(self):
+        return self.portfolio_log['port_value'].pct_change()
+
+    def get_result(self):
+        portfolio_log = self.portfolio_log
+        event_log = self.event_log
+        result = self.get_result_from_portfolio_log(portfolio_log)
+        result['event_log'] = event_log
+        return result
+
+    def get_result_from_portfolio_log(self, portfolio_log):
+        port_col_name = self.name_for_result_column
+        portfolio_log = portfolio_log.rename(columns={'port_value': port_col_name})
+
+        performance = calc_performance_from_value_history(portfolio_log[port_col_name])
+        performance_summary = performance.get('performance_summary')
+        performance_summary.name = port_col_name
+
+        annual_summary = performance.get('annual_summary')
+
+        multi_index = pd.MultiIndex.from_product([[port_col_name], annual_summary.columns])
+        annual_summary = pd.DataFrame(annual_summary.values, columns=multi_index, index=annual_summary.index.tolist())
+        performance['annual_summary'] = annual_summary
+
+        rebalancing_weight = self.order_weight_df
+
+        port_drawdown = performance.get("drawdown")
+        portfolio_log = pd.concat([portfolio_log, port_drawdown], axis=1)
+        performance["portfolio_log"] = portfolio_log
+
+        result = dict()
+        result['performance'] = performance
+        result['rebalancing_weight'] = rebalancing_weight
+        return result
+
+    def selected_asset_counts_to_csv(self, file_name=None, folder_path=None):
+        if file_name is None:
+            file_name = self.name
+
+        if folder_path is None:
+            path = f"./{file_name}.csv"
+        else:
+            path = f"./{folder_path}/{file_name}.csv"
+
+        self.selected_asset_counts.to_csv(path, encoding='cp949')
