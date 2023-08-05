305db098be56ec743b2dcbc2582eaa003e990260
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:29 +0000

    Add package rpdsnd-distributions/1.0/rpdsnd_distributions-1.0.tar.gz

---
+++ packages/rpdsnd-distributions/rpdsnd_distributions-1.0.tar.gz/rpdsnd_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='rpdsnd_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['rpdsnd_distributions'],
+      author = 'Rabin Paudel',
+      author_email = 'rabin.paudel@gmail.com',
+      zip_safe=False)
