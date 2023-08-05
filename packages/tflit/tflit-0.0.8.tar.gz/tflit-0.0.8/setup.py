32d8662191d1fb93061d97fe23deeb9fb57e3f42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:07 +0000

    Add package tflit/0.0.8/tflit-0.0.8.tar.gz

---
+++ packages/tflit/tflit-0.0.8.tar.gz/tflit-0.0.8/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+USERNAME = 'beasteers'
+NAME = 'tflit'
+VERSION = '0.0.8'
+
+setuptools.setup(
+    name=NAME,
+    version=VERSION,
+    description='tflite_runtime, but easier.',
+    long_description=open('README.md').read().strip(),
+    long_description_content_type='text/markdown',
+    author='Bea Steers',
+    author_email='bea.steers@gmail.com',
+    url='https://github.com/{}/{}'.format(USERNAME, NAME),
+    packages=setuptools.find_packages(),
+    # entry_points={'console_scripts': ['{name}={name}:main'.format(name=NAME)]},
+    install_requires=[
+        'numpy',
+        # 'tflite_runtime@{}'.format(get_tflite_url())
+    ],
+    extras_require={
+        'tests': ['pytest-cov'],
+    },
+    license='MIT License',
+    keywords='tflite runtime tensorflow keras deep machine learning model edge embedded compute cnn')
