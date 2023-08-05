c69efe8900a9d751cd4646061ea706d8047e9408
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:03 +0000

    Add package coupled-biased-random-walks/1.1.1/coupled_biased_random_walks-1.1.1.tar.gz

---
+++ packages/coupled-biased-random-walks/coupled_biased_random_walks-1.1.1.tar.gz/coupled_biased_random_walks-1.1.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+
+with open('README.md') as f:
+    long_description = f.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+
+setup(
+    name='coupled_biased_random_walks',
+    version='1.1.1',
+    author='Daniel Kaslovsky',
+    author_email='dkaslovsky@gmail.com',
+    license='MIT',
+    description='Outlier detection for categorical data',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['coupled_biased_random_walks'],
+    install_requires=requirements,
+    url='https://github.com/dkaslovsky/Coupled-Biased-Random-Walks',
+    keywords=['anomaly detection, outlier detection', 'categorical data', 'random walk'],
+    classifiers=[
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ]
+)
