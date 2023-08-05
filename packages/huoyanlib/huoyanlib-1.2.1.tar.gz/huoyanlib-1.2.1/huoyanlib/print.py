cb17940c2130e658d30e529de7fe49e9fe98afb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:27 +0000

    Add package huoyanlib/1.2.1/huoyanlib-1.2.1.tar.gz

---
+++ packages/huoyanlib/huoyanlib-1.2.1.tar.gz/huoyanlib-1.2.1/huoyanlib/print.py
@@ -0,0 +1,11 @@
+from time import *
+import sys, time
+
+
+def h_print(text):
+    sys.stdout.write("\r " + " " * 60 + "\r")
+    sys.stdout.flush()
+    for c in text:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.1)
