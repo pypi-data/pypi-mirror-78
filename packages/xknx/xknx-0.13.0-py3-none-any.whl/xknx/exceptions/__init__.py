c8ab061d6d9f1fb7be9a38ea11504c8c71886249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:53 +0000

    Add package xknx/0.13.0/xknx-0.13.0-py3-none-any.whl

---
+++ packages/xknx/xknx-0.13.0-py3-none-any.whl/xknx/exceptions/__init__.py
@@ -0,0 +1,6 @@
+"""Module for XKNX Exception handling."""
+# flake8: noqa
+from .exception import (
+    ConversionError, CouldNotParseAddress, CouldNotParseKNXIP,
+    CouldNotParseTelegram, DeviceIllegalValue, UnsupportedCEMIMessage,
+    XKNXException)
