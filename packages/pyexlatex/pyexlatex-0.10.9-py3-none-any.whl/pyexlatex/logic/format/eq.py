37900e115270e99a9dcf7e4ce025dfc6c3206c8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:15:11 +0000

    Add package pyexlatex/0.10.9/pyexlatex-0.10.9-py3-none-any.whl

---
+++ packages/pyexlatex/pyexlatex-0.10.9-py3-none-any.whl/pyexlatex/logic/format/eq.py
@@ -0,0 +1,21 @@
+from sympy import Eq, Expr, Symbol
+from sympy import latex
+
+
+def inline(eq: Eq) -> str:
+    return f'${latex(eq)}$'
+
+
+def offset(eq: Eq) -> str:
+    return f'\\begin{{equation}}\n\t{latex(eq)}\n\\end{{equation}}'
+
+
+def latex_partial(expr: Expr, eq_symbol: Symbol, wrt_symbol: Symbol, offset: bool = False) -> str:
+
+    begin_str = '$'
+    end_str = '$'
+    if offset:
+        begin_str = '\\begin{equation}\n\t'
+        end_str = '\n\\end{equation}'
+    return rf'{begin_str}\frac{{\partial {latex(eq_symbol)}}}{{\partial {latex(wrt_symbol)}}} = ' \
+        rf'{latex(expr)}{end_str}'
