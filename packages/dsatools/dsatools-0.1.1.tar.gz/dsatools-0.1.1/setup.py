98d5ea2c8364ead9ecf96dc58c96a6e5972d3190
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:49 +0000

    Add package dsatools/0.1.1/dsatools-0.1.1.tar.gz

---
+++ packages/dsatools/dsatools-0.1.1.tar.gz/dsatools-0.1.1/setup.py
@@ -0,0 +1,25 @@
+
+from setuptools import find_packages, setup
+
+setup(
+    name="dsatools",
+    packages=find_packages(),
+    version="0.1.1",
+    description=""" The library with a most popular Digital Signal Analysis (DSA) tools """,
+    author="Mikhail Ronkin",
+	author_email="mvRonkin@gmail.com",
+	url = 'https://github.com/MVRonkin/dsatools',
+	download_url = "https://github.com/MVRonkin/dsatools.git",
+	keywords = ['Digital Signal Analysis', 
+			'Signal Processing', 
+			'Digital Signal Processing',
+			'Digital Spectrum Processing',
+			'Digital Spectrum Analysis'],
+			
+    license="MIT",
+    install_requires=[
+          'numpy',
+          'scipy',
+		  'matplotlib'
+      ],
+)
