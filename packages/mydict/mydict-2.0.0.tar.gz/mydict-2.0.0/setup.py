017d0528863d197b9de919f5abdd4e7adb519a54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:05 +0000

    Add package mydict/2.0.0/mydict-2.0.0.tar.gz

---
+++ packages/mydict/mydict-2.0.0.tar.gz/mydict-2.0.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+import os
+
+description = 'A Python dict subclass which tries to act like JavaScript objects, so you can use the dot-notation (d.foo) to access members of the object.'
+
+try:
+    readme_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'README.md')
+    with open(readme_path) as ld_f:
+        long_description = ld_f.read()
+
+except ImportError:
+    long_description = description
+
+setup(
+    name='mydict',
+    version='2.0.0',
+    author=u'León Domingo',
+    author_email='leon.domingo@gmail.com',
+    description=description,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    keywords='dict javascript dot-notation',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Topic :: Utilities',
+    ],
+    url='https://github.com/leon-domingo/mydict',
+    packages=[
+        'mydict',
+    ],
+    install_requires=[
+        'stringcase',
+        'pytest',
+    ]
+)
