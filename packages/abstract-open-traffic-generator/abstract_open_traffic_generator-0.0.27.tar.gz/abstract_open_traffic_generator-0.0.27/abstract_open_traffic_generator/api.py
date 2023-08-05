fc7c69c33a2e5cd92af28f19e55aebb0544619e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:54 +0000

    Add package abstract-open-traffic-generator/0.0.27/abstract_open_traffic_generator-0.0.27.tar.gz

---
+++ packages/abstract-open-traffic-generator/abstract_open_traffic_generator-0.0.27.tar.gz/abstract_open_traffic_generator-0.0.27/abstract_open_traffic_generator/api.py
@@ -0,0 +1,15 @@
+
+
+class Api(object):
+    """TBD
+    """
+
+    def set_config(self, content):
+        """TBD
+        """
+        raise NotImplementedError
+
+    def get_results(self, content):
+        """TBD
+        """
+        raise NotImplementedError
