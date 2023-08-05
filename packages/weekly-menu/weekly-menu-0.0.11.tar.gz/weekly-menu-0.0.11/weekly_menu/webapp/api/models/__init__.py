7b46de5c6e0f54a6bcd6b50c29f71299dc93841f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:23 +0000

    Add package weekly-menu/0.0.11/weekly-menu-0.0.11.tar.gz

---
+++ packages/weekly-menu/weekly-menu-0.0.11.tar.gz/weekly-menu-0.0.11/weekly_menu/webapp/api/models/__init__.py
@@ -0,0 +1,5 @@
+from .ingredient import Ingredient
+from .recipe import Recipe, RecipeIngredient
+from .shopping_list import ShoppingList, ShoppingListItem
+from .user import User
+from .menu import Menu
