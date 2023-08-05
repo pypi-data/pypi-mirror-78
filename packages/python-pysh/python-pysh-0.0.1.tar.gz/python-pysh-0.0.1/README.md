d902ee02364d784c74f52dc14483138ed006d7e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:21 +0000

    Add package python-pysh/0.0.1/python-pysh-0.0.1.tar.gz

---
+++ packages/python-pysh/python-pysh-0.0.1.tar.gz/python-pysh-0.0.1/README.md
@@ -0,0 +1,20 @@
+# PySh
+
+A Python package that mimics the Oh-My-ZSH shell. This package creates a colorful ZSH prompt.
+
+## Installation
+To install PySh onto your system, run `pip install python-pysh` in your terminal.
+If the command errors out, try `pip install git+git://github.com/Python3-8/python-pysh`.
+If the second command errors out too, clone the GitHub repo for PySh and move the `pysh` folder to  
+your Python `site-packages` directory.
+
+## Usage
+
+The following command on your terminal will run the PySh shell.
+
+```sh
+$ pysh
+```
+
+## Contact
+You may contact the developer of PySh at: pranav.pooruli@gmail.com
---
+++ packages/python-pysh/python-pysh-0.0.1.tar.gz/python-pysh-0.0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name='python-pysh',
+    version='0.0.1',
+    description='A Python package that mimics the Oh-My-ZSH shell. This package creates a colorful ZSH prompt.',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Python3-8/python-pysh',
+    author='Pranav Balaji Pooruli',
+    author_email='pranav.pooruli@gmail.com',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+    ],
+    packages=['pysh'],
+    include_package_data=True,
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'pysh=pysh.pysh:main',
+        ]
+    },
+)
