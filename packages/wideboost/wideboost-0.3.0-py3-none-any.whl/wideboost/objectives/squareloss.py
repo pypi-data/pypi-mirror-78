ec7fe90616e163b68a2bebf29e238e5a23cdb8b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:09 +0000

    Add package wideboost/0.3.0/wideboost-0.3.0-py3-none-any.whl

---
+++ packages/wideboost/wideboost-0.3.0-py3-none-any.whl/wideboost/objectives/squareloss.py
@@ -0,0 +1,25 @@
+import numpy as np 
+
+def squareloss_gradient_hessian(X,B,Y):
+    # Loss = 1/2 (Y - X)^2
+
+    Y = Y.reshape([Y.shape[0],-1])
+
+    assert len(X.shape) == 2
+    assert len(B.shape) == 2
+    assert len(Y.shape) == 2
+
+    R = Y - X.dot(B)
+    dX = -R * B.transpose()
+    dX2 = np.ones(Y.shape) * np.square(B).transpose()
+
+    return dX, dX2
+
+## TODO: Multi-dimension Squareloss
+
+#Y = np.zeros([10,2])
+#B = 2*np.ones([])
+
+
+
+
