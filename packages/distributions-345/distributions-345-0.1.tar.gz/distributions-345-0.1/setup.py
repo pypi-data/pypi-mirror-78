8fe4e333cb6a593bb6abf3b62e210053d0b9a1c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:29 +0000

    Add package distributions-345/0.1/distributions-345-0.1.tar.gz

---
+++ packages/distributions-345/distributions-345-0.1.tar.gz/distributions-345-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions-345',
+      version='0.1',
+      description='Binomial and Gaussian distributions',
+      packages=['distributions-345'],
+      zip_safe=False,
+     author = 'munirat.sulaimon')
