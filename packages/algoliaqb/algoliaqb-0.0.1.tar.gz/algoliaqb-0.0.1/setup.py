dbebc151a251b8391011e179dc9e94c637e9850a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:39 +0000

    Add package algoliaqb/0.0.1/algoliaqb-0.0.1.tar.gz

---
+++ packages/algoliaqb/algoliaqb-0.0.1.tar.gz/algoliaqb-0.0.1/setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup
+
+readme_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'README.md')
+try:
+    from m2r import parse_from_file
+    readme = parse_from_file(readme_file)
+except ImportError:
+    # m2r may not be installed in user environment
+    with open(readme_file) as f:
+        readme = f.read()
+
+
+setup(name='algoliaqb',
+      version='0.0.1',
+      description='Algolia Query Builder',
+      long_description=readme,
+      keywords='funniest joke comedy flying circus',
+      url='http://github.com/ryanb58/algoliaqb',
+      author='Taylor Brazelton',
+      author_email='taylor.r.brazelton@gmail.com',
+      license='MIT',
+      packages=['algoliaqb'],
+      zip_safe=False
+)
