4f69ed2968cea296eab8a91817d988e10075363e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:23 +0000

    Add package vkbottle-types/0.1.1/vkbottle_types-0.1.1-py3-none-any.whl

---
+++ packages/vkbottle-types/vkbottle_types-0.1.1-py3-none-any.whl/vkbottle_types/responses/downloadedGames.py
@@ -0,0 +1,13 @@
+from typing import Optional
+
+from .base_response import BaseResponse
+
+
+class PaidStatusResponse(BaseResponse):
+    response: Optional["PaidStatusResponseModel"] = None
+
+
+class PaidStatusResponseModel(BaseResponse):
+    is_paid: Optional[bool] = None
+
+PaidStatusResponse.update_forward_refs()
