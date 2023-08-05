df2836cd2adbf935ee21d10682b71c29047eaa6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:52 +0000

    Add package SoccerNet/0.0.14/SoccerNet-0.0.14.tar.gz

---
+++ packages/SoccerNet/SoccerNet-0.0.14.tar.gz/SoccerNet-0.0.14/SoccerNet/__init__.py
@@ -0,0 +1,11 @@
+
+__version_info__ = ('0', '0', '14')
+__version__ = '.'.join(__version_info__)
+__authors__ = "Silvio Giancola"
+__authors_username__ = "giancos"
+__author_email__ = "silvio.giancola@kaust.edu.sa"
+__github__ = 'https://github.com/SilvioGiancola/SoccerNetv2'
+
+
+from SoccerNet.downloader import SoccerNetDownloader
+
