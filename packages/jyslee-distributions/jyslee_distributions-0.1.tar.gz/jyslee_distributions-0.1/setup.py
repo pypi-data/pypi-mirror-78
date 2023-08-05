c9e659debc1e44bb592f6dd1d8d5665b22db2ba2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:35 +0000

    Add package jyslee-distributions/0.1/jyslee_distributions-0.1.tar.gz

---
+++ packages/jyslee-distributions/jyslee_distributions-0.1.tar.gz/jyslee_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='jyslee_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['jyslee_distributions'],
+      zip_safe=False)
