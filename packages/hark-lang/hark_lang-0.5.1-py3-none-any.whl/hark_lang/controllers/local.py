e17501ba54c54aa847a15b6ba1b539425e7c0c49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:15 +0000

    Add package hark-lang/0.5.1/hark_lang-0.5.1-py3-none-any.whl

---
+++ packages/hark-lang/hark_lang-0.5.1-py3-none-any.whl/hark_lang/controllers/local.py
@@ -0,0 +1,136 @@
+"""Local Implementation"""
+import logging
+import sys
+import time
+import threading
+from functools import singledispatchmethod
+from typing import List
+
+from ..machine import future as fut
+from ..machine.arec import ARecPtr
+from ..machine.controller import Controller
+
+# https://docs.python.org/3/library/logging.html#logging.basicConfig
+LOG = logging.getLogger(__name__)
+
+
+class DataController(Controller):
+    def __init__(self):
+        self._machine_future = {}
+        self._machine_state = {}
+        self._machine_stopped = {}
+        self._machine_idx = 0  # always increasing machine counter
+        self._arec_idx = 0  # always increasing arec counter
+        self._probe_logs = []
+        self._probe_events = []
+        self._arecs = {}
+        self._lock = threading.RLock()
+        self.session_id = 0  # constant for local
+        self.executable = None
+        self.stdout = []  # shared standard output
+        self.broken = False
+        self.result = None
+
+    def set_executable(self, exe):
+        self.executable = exe
+
+    def set_entrypoint(self, fn_name: str):
+        pass  # N/A for local
+
+    ## Threads
+
+    def new_thread(self):
+        vmid = self._machine_idx
+        self._machine_idx += 1
+        return vmid
+
+    def get_thread_ids(self) -> List[int]:
+        return list(range(self._machine_idx))
+
+    def get_top_level_future(self):
+        return self.get_future(0)
+
+    def is_top_level(self, vmid):
+        return vmid == 0
+
+    def all_stopped(self):
+        return all(self._machine_stopped.values())
+
+    def set_stopped(self, vmid, stopped: bool):
+        self._machine_stopped[vmid] = stopped
+
+    def get_state(self, vmid):
+        return self._machine_state[vmid]
+
+    def set_state(self, vmid, state):
+        self._machine_state[vmid] = state
+
+    ## arecs
+
+    def new_arec(self) -> ARecPtr:
+        ptr = ARecPtr(self._arec_idx)
+        self._arec_idx += 1
+        return ptr
+
+    def set_arec(self, ptr, rec):
+        self._arecs[ptr] = rec
+
+    def get_arec(self, ptr):
+        return self._arecs[ptr]
+
+    def increment_ref(self, ptr):
+        self._arecs[ptr].ref_count += 1
+        return self._arecs[ptr].ref_count
+
+    def decrement_ref(self, ptr):
+        self._arecs[ptr].ref_count -= 1
+        return self._arecs[ptr]
+
+    def delete_arec(self, ptr):
+        self._arecs[ptr].deleted = True
+
+    def lock_arec(self, _):
+        return self._lock
+
+    ## probes
+
+    def set_probe_data(self, vmid, probe):
+        self._probe_logs.extend(probe.logs)
+        self._probe_events.extend(probe.events)
+
+    def get_probe_logs(self):
+        return list(self._probe_logs)
+
+    def get_probe_events(self):
+        return list(self._probe_events)
+
+    ## futures
+
+    def get_future(self, val):
+        if not isinstance(val, int):
+            raise TypeError(val)
+        return self._machine_future[val]
+
+    def set_future(self, vmid, future: fut.Future):
+        self._machine_future[vmid] = future
+
+    def add_continuation(self, fut_ptr, vmid):
+        self._machine_future[fut_ptr].continuations.append(vmid)
+
+    def set_future_chain(self, fut_ptr, chain):
+        self._machine_future[fut_ptr].chain = chain
+
+    def lock_future(self, _):
+        # Shared thread lock
+        return self._lock
+
+    ## stdout
+
+    def get_stdout(self):
+        return list(self.stdout)
+
+    def write_stdout(self, item):
+        # Print to real stdout at the same time. TODO maybe make this behaviour
+        # configurable.
+        sys.stdout.write(item.text)
+        self.stdout.append(item)
---
+++ packages/hark-lang/hark_lang-0.5.1-py3-none-any.whl/hark_lang/machine/controller.py
@@ -0,0 +1,245 @@
+"""Placeholder for the controller class"""
+
+import logging
+from typing import List
+
+from ..exceptions import UnexpectedError
+from . import types as mt
+from .arec import ActivationRecord
+from .future import Future
+from .probe import Probe
+from .state import State
+from .thread_failure import StackTraceItem, ThreadFailure
+
+LOG = logging.getLogger(__name__)
+
+
+class ControllerError(UnexpectedError):
+    """A general controller error"""
+
+
+class Controller:
+    def __init__(self):
+        raise NotImplementedError("Must be subclassed")
+
+    def supports_plugin(self, name: str):
+        return False
+
+    def toplevel_machine(self, fn_ptr: mt.TlFunctionPtr, args):
+        """Create a top-level machine"""
+        vmid = self.new_thread()
+        arec = ActivationRecord(
+            function=fn_ptr,
+            dynamic_chain=None,
+            vmid=vmid,
+            call_site=None,
+            bindings={},
+            ref_count=1,
+        )
+        self.set_entrypoint(fn_ptr.identifier)
+        self._init_thread(vmid, fn_ptr, args, arec)
+        return vmid
+
+    def thread_machine(self, caller_arec_ptr, caller_ip, fn_ptr, args):
+        """Create a new thread machine"""
+        vmid = self.new_thread()
+        arec = ActivationRecord(
+            function=fn_ptr,
+            dynamic_chain=caller_arec_ptr,
+            vmid=vmid,
+            call_site=caller_ip - 1,
+            bindings={},
+            ref_count=1,
+        )
+        self._init_thread(vmid, fn_ptr, args, arec)
+        return vmid
+
+    def _init_thread(self, vmid, fn_ptr, args, arec):
+        state = State(args)
+        entrypoint_ip = self.executable.locations[fn_ptr.identifier]
+        ptr = self.push_arec(vmid, arec)
+        state.current_arec_ptr = ptr
+        state.ip = entrypoint_ip
+        self.set_state(vmid, state)
+        future = Future()
+        self.set_future(vmid, future)
+        self.set_stopped(vmid, False)
+        return vmid
+
+    ##
+
+    def get_top_level_result(self):
+        """Get the return value of the top-level thread"""
+        # NOTE: if it's not resolved, value will be None
+        value = self.get_top_level_future().value
+        try:
+            return mt.to_py_type(value)
+        except TypeError as exc:  # it's None or TlFunctionPtr, for example
+            LOG.info(f"Can't return {value} ({type(value)}), returning None")
+            return None
+
+    ##
+
+    def push_arec(self, vmid, rec):
+        ptr = self.new_arec()
+        self.set_arec(ptr, rec)
+        if rec.dynamic_chain is not None:
+            self.increment_ref(rec.dynamic_chain)
+        return ptr
+
+    def pop_arec(self, ptr):
+        # If the given ptr has no more references, remove it from storage.
+        # Otherwise, just decrement the references.
+        collect_garbage = False
+        with self.lock_arec(ptr):
+            rec = self.decrement_ref(ptr)
+            if rec.ref_count == 0:
+                self.delete_arec(ptr)
+                collect_garbage = True
+
+        # Pop parent records until one is still being used
+        if collect_garbage:
+            while rec.dynamic_chain:
+                with self.lock_arec(rec.dynamic_chain):
+                    parent = self.decrement_ref(rec.dynamic_chain)
+                    if parent.ref_count > 0:
+                        break
+                self.delete_arec(rec.dynamic_chain)
+                rec = parent
+
+        return rec
+
+    ##
+
+    def resolve_future(self, vmid, value):
+        """Resolve a machine future, and any dependent futures"""
+        if isinstance(value, mt.TlFuturePtr):
+            raise TypeError(value)
+
+        with self.lock_future(vmid):
+            future = self.get_future(vmid)
+            future.resolved = True
+            future.value = value
+            self.set_future(vmid, future)
+
+            continuations = future.continuations
+            if future.chain:
+                continuations += self.resolve_future(future.chain, value)
+
+        if self.is_top_level(vmid):
+            self.result = mt.to_py_type(value)
+
+        LOG.info("Resolved %d to %s. Continuations: %s", vmid, value, continuations)
+        return continuations
+
+    def finish(self, vmid, value) -> list:
+        """Finish a machine, resolving its future
+
+        Return waiting machines to invoke, and the value to invoke them with
+
+        """
+        if not isinstance(value, mt.TlFuturePtr):
+            return value, self.resolve_future(vmid, value)
+
+        if type(vmid) is not int:
+            raise TypeError(vmid)
+
+        # Otherwise, VALUE is another future, and we can only resolve this machine's
+        # future if VALUE has also resolved. If VALUE hasn't resolved, we "chain"
+        # this machine's future to it.
+        next_future_id = value.vmid
+        with self.lock_future(next_future_id):
+            next_future = self.get_future(next_future_id)
+            if next_future.resolved:
+                return (next_future.value, self.resolve_future(vmid, next_future.value))
+            else:
+                LOG.info("Chaining %s to %s", vmid, value)
+                self.set_future_chain(next_future_id, vmid)
+                return None, []
+
+    def get_or_wait(self, vmid, future_ptr):
+        """Get the value of a future in the stack, or add a continuation
+
+        Return tuple:
+        - resolved (bool): whether the future has resolved
+        - value: The data value, or None if not resolved
+        """
+        if not isinstance(future_ptr, mt.TlFuturePtr):
+            raise TypeError(future_ptr)
+
+        if type(vmid) is not int:
+            raise TypeError(vmid)
+
+        with self.lock_future(future_ptr.vmid):
+            future = self.get_future(future_ptr.vmid)
+            if future.resolved:
+                value = future.value
+                LOG.info("%s has resolved: %s", future_ptr, value)
+            else:
+                value = None
+                self.add_continuation(future_ptr.vmid, vmid)
+                LOG.info("%d waiting on %s", vmid, future_ptr)
+                future = self.get_future(future_ptr.vmid)
+
+        return future.resolved, value
+
+    ##
+
+    def stop(self, vmid, finished_ok):
+        """Signal that a machine has stopped running"""
+        if not finished_ok:
+            self.broken = True
+        self.set_stopped(vmid, True)
+
+    ##
+
+    def get_failures(self) -> List[ThreadFailure]:
+        """Get information about failed threads"""
+        results = []
+        for vmid in self.get_thread_ids():
+            state = self.get_state(vmid)
+            if state.error_msg is not None:
+                stacktrace = self.get_stacktrace(vmid)
+                results.append(
+                    ThreadFailure(
+                        # --
+                        thread=vmid,
+                        error_msg=state.error_msg,
+                        stacktrace=stacktrace,
+                    )
+                )
+        return results
+
+    def get_stacktrace(self, vmid) -> List[StackTraceItem]:
+        """Get a stack trace for a thread"""
+        trace = []
+        state = self.get_state(vmid)
+        arec_ptr = state.current_arec_ptr
+        arec = self.get_arec(arec_ptr)
+
+        # Push the current frame
+        trace.append(
+            StackTraceItem(
+                caller_thread=vmid,
+                caller_ip=state.ip - 1,  # minus 1: IP is pre-advanced
+                caller_fn=arec.function.identifier,
+            )
+        )
+
+        # And then all parents
+        while True:
+            arec = self.get_arec(arec_ptr)
+            if arec.dynamic_chain is not None:
+                parent = self.get_arec(arec.dynamic_chain)
+                trace.append(
+                    StackTraceItem(
+                        caller_thread=parent.vmid,
+                        caller_ip=arec.call_site,
+                        caller_fn=parent.function.identifier,
+                    )
+                )
+            else:
+                break
+            arec_ptr = arec.dynamic_chain
+
+        return list(reversed(trace))
