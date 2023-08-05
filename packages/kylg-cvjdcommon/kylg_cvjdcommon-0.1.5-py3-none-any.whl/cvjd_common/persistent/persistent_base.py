21ee25ef35c288acd4d5ace7a472cfe4079582f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:24 +0000

    Add package kylg-cvjdcommon/0.1.5/kylg_cvjdcommon-0.1.5-py3-none-any.whl

---
+++ packages/kylg-cvjdcommon/kylg_cvjdcommon-0.1.5-py3-none-any.whl/cvjd_common/persistent/persistent_base.py
@@ -0,0 +1,17 @@
+from abc import ABCMeta, abstractmethod
+
+from cvjd_common.models.cv_match import CvMatch
+
+
+class PersistentBase(metaclass=ABCMeta):
+    """
+    Base Persistent, which behaves like an interface for persistent job status
+    """
+
+    @abstractmethod
+    def writeJobStatus(self, status: str, cvMatch: CvMatch):
+        pass
+
+    @abstractmethod
+    def readJobStatus(self, cvId: str) -> CvMatch:
+        pass
---
+++ packages/kylg-cvjdcommon/kylg_cvjdcommon-0.1.5-py3-none-any.whl/cvjd_common/persistent/redis_backend.py
@@ -0,0 +1,42 @@
+import redis
+from cvjd_common.models.cv_match import CvMatch
+import jsonpickle
+import logging
+from cvjd_common.models.job import Job, JobSchema
+from cvjd_common.persistent.persistent_base import PersistentBase
+
+
+cv_predict_status_key = "cv_predict_status"
+
+
+class RedisBackend(PersistentBase):
+
+    def __init__(self, addr, port, user, passwd):
+        logging.info("redis server %s:%s" % (addr, port))
+        self.redis_server = redis.Redis(host=addr, port=port,
+                                        password=user, username=passwd)
+
+    def writeJobStatus(self, status: str, cvMatch: CvMatch):
+        """
+        :param status: doing or finished
+        :param cvMatch: predict result
+        :return:
+        """
+        job = Job(status, cvMatch)
+        jsonDict = {cvMatch.cv.cv_id: jsonpickle.dumps(job)}
+
+        self.redis_server.hmset(cv_predict_status_key, jsonDict)
+
+    def readJobStatus(self, cvId: str):
+        """
+        :param cvId: cv id
+        :return:  (status, CvMatch)
+        """
+        logging.info("get status for cv id: " + cvId)
+        byteList = self.redis_server.hmget(cv_predict_status_key, cvId)
+        if byteList is not None and len(byteList) > 0:
+            logging.info(str(byteList[0]))
+            job = jsonpickle.decode(byteList[0])
+            return JobSchema().load(job).data
+        else:
+            return None
