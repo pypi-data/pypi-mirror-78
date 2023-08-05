5d6cbd63dce64705bef4f5bd5203b4bda98616d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:54 +0000

    Add package alphabets/0.1/alphabets-0.1.tar.gz

---
+++ packages/alphabets/alphabets-0.1.tar.gz/alphabets-0.1/README.txt
@@ -0,0 +1,9 @@
+Alphabet Module By Aditya Shrivastava [version 0.1 ] 
+Copyright (c) 2020 Aditya Shrivastava. All Rights Reserved
+
+INSTRUCTION:
+
+Please put this file in (folder where python is installed)\Lib\site-packages
+
+Documentation is available on:
+https://myprogramarchives.blogspot.com/2020/08/alphabets-module-in-python-by.html
---
+++ packages/alphabets/alphabets-0.1.tar.gz/alphabets-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(name="alphabets",
+version="0.1",
+description="Ready To Use Alphabets an vowels in list and single line",
+long_description="Alphabet Module by Aditya Shrivastava [version 0.1]. Copyright (c) 2020 Aditya Shrivastava. All rights reserved. Visit myprogramarchives.blogspot.com for more information",
+url='https://myprogramarchives.blogspot.com/2020/08/alphabets-module-in-python-by.html',
+author="Aditya Shrivastava",
+license='MIT',
+packages=['alphabets'],
+install_requires=[])
+
+
