eca49e4cda5bafff70352395befefb1da03a4da6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:56 +0000

    Add package target365-sdk/1.5.2/target365_sdk-1.5.2.tar.gz

---
+++ packages/target365-sdk/target365_sdk-1.5.2.tar.gz/target365_sdk-1.5.2/target365_sdk/models/oneclick_config.py
@@ -0,0 +1,23 @@
+from .model import Model
+
+class OneClickConfig(Model):
+
+    def _accepted_params(self):
+        return [
+            'configId',
+            'shortNumber',
+            'merchantId',
+            'serviceCode',
+            'businessModel',
+            'recurring',
+            'redirectUrl',
+            'onlineText',
+            'offlineText',
+            'age',
+            'isRestricted',
+            'invoiceText',
+            'price',
+            'timeout',
+            'created',
+            'lastModified',
+        ]
