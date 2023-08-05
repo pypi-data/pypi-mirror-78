62ed29d9a7098aafcc956577624ceb0634b6dbd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:09 +0000

    Add package qc-utils/20.8.1/qc_utils-20.8.1-py3-none-any.whl

---
+++ packages/qc-utils/qc_utils-20.8.1-py3-none-any.whl/qc_utils/__init__.py
@@ -0,0 +1,11 @@
+from qc_utils.qcmetric import QCMetric, QCMetricRecord
+
+__title__ = "qc-utils"
+__version__ = "20.08.1"
+__description__ = "Tool for representing ENCODE QC metrics"
+__url__ = "https://github.com/ENCODE-DCC/qc-utils"
+__uri__ = __url__
+__author__ = "Otto Jolanki"
+__email__ = "ojolanki@stanford.edu"
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2020 ENCODE DCC"
