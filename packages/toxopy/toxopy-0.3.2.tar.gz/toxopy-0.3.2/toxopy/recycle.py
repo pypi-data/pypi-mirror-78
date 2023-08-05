f23e644ff9e9152259532d371397c659a42e1e9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:55 +0000

    Add package toxopy/0.3.2/toxopy-0.3.2.tar.gz

---
+++ packages/toxopy/toxopy-0.3.2.tar.gz/toxopy-0.3.2/toxopy/recycle.py
@@ -0,0 +1,14 @@
+def trials():
+
+    return [
+        'no treatment', 'cat alone (1)', 'first saline', 'cat alone (2)',
+        'first urine', 'cat alone (3)', 'second saline', 'cat alone (4)',
+        'second urine', 'cat alone (5)'
+    ]
+
+
+def fwarnings():
+
+	import warnings
+
+	return warnings.filterwarnings("ignore")
