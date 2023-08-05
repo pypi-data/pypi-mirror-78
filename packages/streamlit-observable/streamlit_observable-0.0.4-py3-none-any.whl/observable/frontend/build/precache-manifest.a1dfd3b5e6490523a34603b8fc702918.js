439b4e5ff44a7582d69cce4dc2e24ecebd360af8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:14 +0000

    Add package streamlit-observable/0.0.4/streamlit_observable-0.0.4-py3-none-any.whl

---
+++ packages/streamlit-observable/streamlit_observable-0.0.4-py3-none-any.whl/observable/frontend/build/precache-manifest.a1dfd3b5e6490523a34603b8fc702918.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d3af2ed976a181c3d7d1d95334bd176",
+    "url": "./index.html"
+  },
+  {
+    "revision": "81008ce28e99f03fe92b",
+    "url": "./static/js/2.b7131ce8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.b7131ce8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d254f3552740bd58f73d",
+    "url": "./static/js/main.d74d14c5.chunk.js"
+  },
+  {
+    "revision": "6515c66d2a8747a146d578e1c038a822",
+    "url": "./static/js/main.d74d14c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
---
+++ packages/streamlit-observable/streamlit_observable-0.0.4-py3-none-any.whl/streamlit_observable/frontend/build/precache-manifest.5b46dc2e487fd2207c85c30b7c977719.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8784a3f288d66d43bfafbf512e5495fc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c6a228c730f4f6636146",
+    "url": "./static/js/2.dad34968.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.dad34968.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08d5b6f390e9dab425d2",
+    "url": "./static/js/main.5599c993.chunk.js"
+  },
+  {
+    "revision": "6515c66d2a8747a146d578e1c038a822",
+    "url": "./static/js/main.5599c993.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
