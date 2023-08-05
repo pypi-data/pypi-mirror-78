932d9c24245506bde42f7efdcb69f5642b43ad6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:15 +0000

    Add package mc-transformers/0.1.8/mc_transformers-0.1.8.tar.gz

---
+++ packages/mc-transformers/mc_transformers-0.1.8.tar.gz/mc_transformers-0.1.8/mc_transformers/data_classes.py
@@ -0,0 +1,37 @@
+from dataclasses import dataclass
+from typing import List, Optional, Union
+
+
+@dataclass(frozen=True)
+class InputExample:
+    """
+    A single training/test example for multiple choice
+
+    Args:
+        example_id: Unique id for the example.
+        question: string. The untokenized text of the second sequence (question).
+        contexts: list of str. The untokenized text of the first sequence (context of corresponding question).
+        endings: list of str. multiple choice's options. Its length must be equal to contexts' length.
+        label: (Optional) string. The label of the example. This should be
+        specified for train and dev examples, but not for test examples.
+    """
+
+    example_id: Union[str, int]
+    question: str
+    contexts: List[str]
+    endings: List[str]
+    label: Optional[str]
+
+
+@dataclass(frozen=True)
+class InputFeatures:
+    """
+    A single set of features of data.
+    Property names are the same names as the corresponding inputs to a model.
+    """
+
+    example_id: Union[str, int]
+    input_ids: List[List[int]]
+    attention_mask: Optional[List[List[int]]]
+    token_type_ids: Optional[List[List[int]]]
+    label: Optional[int]
