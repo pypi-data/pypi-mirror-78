9b3ac8378c441680bc2120a9f435fa139267d5d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:12 +0000

    Add package hkube-python-wrapper/2.0.16/hkube_python_wrapper-2.0.16-py2.py3-none-any.whl

---
+++ packages/hkube-python-wrapper/hkube_python_wrapper-2.0.16-py2.py3-none-any.whl/hkube_python_wrapper/cache/caching.py
@@ -0,0 +1,41 @@
+import datetime
+
+
+class Cache:
+    def __init__(self, config):
+        self._cache = dict()
+        self._maxCacheSize = config.get('maxCacheSize')
+        self.sumSize = 0
+
+
+    def update(self, key, value, size):
+        if (key in self._cache):
+            return key
+        while (self.sumSize + size) >= self._maxCacheSize * 1000 * 1000:
+            if not (self._cache.keys()):
+                print("Trying to insert a value of size " + str(size) + " bytes, larger than " + str(
+                    self._maxCacheSize) + "MB")
+                return None
+            self._remove_oldest()
+        self._cache[key] = {'timestamp': datetime.datetime.now(), 'size': size, 'value': value}
+        self.sumSize += size
+        return key
+
+    def __contains__(self, key):
+        return key in self._cache
+
+    def _remove_oldest(self):
+        oldest = None
+        for key in self._cache:
+            if oldest is None:
+                oldest = key
+            elif self._cache[key]['timestamp'] < self._cache[oldest]['timestamp']:
+                oldest = key
+        self.sumSize -= self._cache[oldest]['size']
+        self._cache.pop(oldest)
+
+    def get(self, key):
+        item = self._cache.get(key)
+        if(item is not None):
+            return item.get('value')
+        return None
---
+++ packages/hkube-python-wrapper/hkube_python_wrapper-2.0.16-py2.py3-none-any.whl/hkube_python_wrapper/communication/DataRequest.py
@@ -0,0 +1,35 @@
+from hkube_python_wrapper.communication.zmq.ZMQRequest import ZMQRequest
+from hkube_python_wrapper.util.encoding import Encoding
+
+
+class DataRequest:
+
+    def __init__(self, reqDetails):
+        encoding = reqDetails.get('encoding')
+        address = reqDetails.get('address')
+        timeout = reqDetails.get('timeout')
+        networkTimeout = reqDetails.get('networkTimeout')
+        options = {
+            u'tasks': reqDetails.get('tasks'),
+            u'taskId': reqDetails.get('taskId'),
+            u'dataPath': reqDetails.get('dataPath')
+        }
+        self.encoding = Encoding(encoding)
+        content = self.encoding.encode(options, plain_encode=True)
+        self.request = dict()
+        self.request.update(address)
+        self.request.update({"content": content, "timeout": timeout, "networkTimeout": networkTimeout})
+
+    def invoke(self):
+        try:
+            print('tcp://' + self.request['host'] + ':' + str(self.request['port']))
+            adapter = ZMQRequest(self.request)
+            response = adapter.invokeAdapter()
+            return self.encoding.decode(response)
+        except Exception as e:
+            return self._createError('unknown', str(e))
+        finally:
+            adapter.close()
+
+    def _createError(self, code, message):
+        return {'hkube_error': {'code': code, 'message': message}}
