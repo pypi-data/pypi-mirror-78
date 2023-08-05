60a2882d3dff11ad9d153e727661811aa7f39945
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:12 +0000

    Add package vkbottle-types/0.1.6/vkbottle-types-0.1.6.tar.gz

---
+++ packages/vkbottle-types/vkbottle-types-0.1.6.tar.gz/vkbottle-types-0.1.6/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['vkbottle_types', 'vkbottle_types.methods', 'vkbottle_types.responses']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pydantic>=1.6.1,<2.0.0']
+
+setup_kwargs = {
+    'name': 'vkbottle-types',
+    'version': '0.1.6',
+    'description': 'Types for vkbottle',
+    'long_description': None,
+    'author': 'timoniq',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
