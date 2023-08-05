c899763425ee9e27f1782334ba0da3889990212a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:11 +0000

    Add package tx-parallex/0.1.1/tx_parallex-0.1.1-py3-none-any.whl

---
+++ packages/tx-parallex/tx_parallex-0.1.1-py3-none-any.whl/tx/parallex/io.py
@@ -0,0 +1,27 @@
+import logging
+from .utils import mappend
+import jsonpickle
+from tx.functional.maybe import Nothing
+from tx.readable_log import format_message, getLogger
+from typing import List, Any, Dict, Tuple, Set, Callable, TypeVar
+
+logger = getLogger(__name__, logging.INFO)
+
+def write_to_disk(dqueue, path):
+    with open(path, "w") as db:
+        while True:
+            output = dqueue.get_next_output()
+            if output == Nothing:
+                break
+            else:
+                db.write(jsonpickle.encode(output.value) + "\n")
+
+
+def read_from_disk(path):
+    obj = {}
+    with open(path) as db:
+        for line in db:
+            obj = mappend(obj, jsonpickle.decode(line))
+    return obj
+
+        
