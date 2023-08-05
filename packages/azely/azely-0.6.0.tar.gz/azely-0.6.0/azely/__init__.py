d61572726172f01834eeca8bc62359ab319720ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:35 +0000

    Add package azely/0.6.0/azely-0.6.0.tar.gz

---
+++ packages/azely/azely-0.6.0.tar.gz/azely-0.6.0/azely/__init__.py
@@ -0,0 +1,23 @@
+# flake8: noqa
+__version__ = "0.6.0"
+__author__ = "Akio Taniguchi"
+
+
+# submodules
+from . import consts
+from . import utils
+from . import location
+from . import object
+from . import time
+from . import azel
+
+
+# aliases
+from .location import get_location
+from .object import get_object
+from .time import get_time
+from .azel import compute
+
+
+# for sphinx docs
+__all__ = dir()
