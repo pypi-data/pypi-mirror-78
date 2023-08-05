4f6500d5823b3527bcd4c437c1597c48c1dee0c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:15 +0000

    Add package ngboost/0.3.2/ngboost-0.3.2.tar.gz

---
+++ packages/ngboost/ngboost-0.3.2.tar.gz/ngboost-0.3.2/ngboost/distns/__init__.py
@@ -0,0 +1,7 @@
+from .distn import RegressionDistn, ClassificationDistn
+from .normal import Normal, NormalFixedVar
+from .multivariate_normal import MultivariateNormal
+from .lognormal import LogNormal
+from .exponential import Exponential
+from .categorical import k_categorical, Bernoulli
+from .poisson import Poisson
