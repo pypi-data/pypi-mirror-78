0bae618a4e5957555410a9541ede33709646e522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:51 +0000

    Add package uxs/0.3.0/uxs-0.3.0.tar.gz

---
+++ packages/uxs/uxs-0.3.0.tar.gz/uxs-0.3.0/uxs/base/wrappers/async_support/__init__.py
@@ -0,0 +1,9 @@
+"""
+Wrapper extensions for async methods.
+"""
+
+from .luno import luno
+
+__all__ = [
+    'luno',
+]
---
+++ packages/uxs/uxs-0.3.0.tar.gz/uxs-0.3.0/uxs/base/wrappers/sync_support/__init__.py
@@ -0,0 +1,9 @@
+"""
+Wrapper extensions for sync methods.
+"""
+
+from .luno import luno
+
+__all__ = [
+    'luno',
+]
