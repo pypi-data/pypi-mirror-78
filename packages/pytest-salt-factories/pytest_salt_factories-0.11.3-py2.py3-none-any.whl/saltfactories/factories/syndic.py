a07ca164f03a6e6cd444be2b4ebcbe5a2d3f9cb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:55 +0000

    Add package pytest-salt-factories/0.11.3/pytest_salt_factories-0.11.3-py2.py3-none-any.whl

---
+++ packages/pytest-salt-factories/pytest_salt_factories-0.11.3-py2.py3-none-any.whl/saltfactories/factories/syndic.py
@@ -0,0 +1,202 @@
+"""
+saltfactories.factories.minion
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Minion Factory
+"""
+
+try:
+    import salt.config
+    import salt.utils.files
+    import salt.utils.dictupdate
+except ImportError:  # pragma: no cover
+    # We need salt to test salt with saltfactories, and, when pytest is rewriting modules for proper assertion
+    # reporting, we still haven't had a chance to inject the salt path into sys.modules, so we'll hit this
+    # import error, but its safe to pass
+    pass
+
+from saltfactories.utils import ports
+
+
+class SyndicFactory:
+    @staticmethod
+    def default_config(
+        root_dir,
+        syndic_id,
+        config_defaults=None,
+        config_overrides=None,
+        master_port=None,
+        syndic_master_port=None,
+    ):
+        if config_defaults is None:
+            # config_defaults = {"syndic": salt.config.syndic_config(None, None)}
+            # We don't really want the whole default config dictionary
+            config_defaults = {"syndic": {}}
+        elif "syndic" in config_defaults and config_defaults["syndic"] is None:
+            config_defaults["syndic"] = {}
+        elif "syndic" not in config_defaults:
+            config_defaults["syndic"] = {}
+
+        if config_overrides is None:
+            config_overrides = {}
+
+        conf_dir = root_dir.join("conf").ensure(dir=True)
+        conf_d_dir = conf_dir.join("master.d").ensure(dir=True)
+        conf_file = conf_d_dir.join("syndic.conf").strpath
+
+        master_config = SyndicFactory.default_master_config(
+            root_dir,
+            conf_dir,
+            syndic_id,
+            config_defaults=config_defaults.get("master"),
+            config_overrides=config_overrides.get("master"),
+        )
+        minion_config = SyndicFactory.default_minion_config(
+            root_dir,
+            conf_dir,
+            syndic_id,
+            config_defaults=config_defaults.get("minion"),
+            config_overrides=config_overrides.get("minion"),
+            master_port=master_config["ret_port"],
+        )
+
+        _config_defaults = {
+            "id": syndic_id,
+            "master_id": syndic_id,
+            "conf_file": conf_file,
+            "root_dir": root_dir.strpath,
+            "syndic_master": "127.0.0.1",
+            "syndic_master_port": syndic_master_port or ports.get_unused_localhost_port(),
+            "syndic_pidfile": "run/syndic.pid",
+            "syndic_log_file": "logs/syndic.log",
+            "syndic_log_level_logfile": "debug",
+            "syndic_dir": "cache/syndics",
+            "pytest-syndic": {"log": {"prefix": "{{cli_name}}({})".format(syndic_id)},},
+        }
+        # Merge in the initial default options with the internal _config_defaults
+        salt.utils.dictupdate.update(
+            config_defaults.get("syndic"), _config_defaults, merge_lists=True
+        )
+
+        if config_overrides.get("syndic"):
+            # Merge in the default options with the syndic_config_overrides
+            salt.utils.dictupdate.update(
+                config_defaults.get("syndic"), config_overrides.get("syndic"), merge_lists=True
+            )
+
+        return {
+            "master": master_config,
+            "minion": minion_config,
+            "syndic": config_defaults["syndic"],
+        }
+
+    @staticmethod
+    def default_minion_config(
+        root_dir, conf_dir, minion_id, config_defaults=None, config_overrides=None, master_port=None
+    ):
+        if config_defaults is None:
+            config_defaults = salt.config.DEFAULT_MINION_OPTS.copy()
+            config_defaults.pop("user", None)
+            config_defaults = {}
+
+        conf_file = conf_dir.join("minion").strpath
+
+        _config_defaults = {
+            "id": minion_id,
+            "master_id": minion_id,
+            "conf_file": conf_file,
+            "root_dir": root_dir.strpath,
+            "interface": "127.0.0.1",
+            "master": "127.0.0.1",
+            "master_port": master_port or ports.get_unused_localhost_port(),
+            "tcp_pub_port": ports.get_unused_localhost_port(),
+            "tcp_pull_port": ports.get_unused_localhost_port(),
+            "pidfile": "run/minion.pid",
+            "pki_dir": "pki",
+            "cachedir": "cache",
+            "sock_dir": "run/minion",
+            "log_file": "logs/minion.log",
+            "log_level_logfile": "debug",
+            "loop_interval": 0.05,
+            #'multiprocessing': False,
+            "log_fmt_console": "%(asctime)s,%(msecs)03.0f [%(name)-17s:%(lineno)-4d][%(levelname)-8s][%(processName)18s(%(process)d)] %(message)s",
+            "log_fmt_logfile": "[%(asctime)s,%(msecs)03.0f][%(name)-17s:%(lineno)-4d][%(levelname)-8s][%(processName)18s(%(process)d)] %(message)s",
+            "hash_type": "sha256",
+            "pytest-minion": {"log": {"prefix": "{{cli_name}}({})".format(minion_id)},},
+        }
+        # Merge in the initial default options with the internal _config_defaults
+        salt.utils.dictupdate.update(config_defaults, _config_defaults, merge_lists=True)
+
+        if config_overrides:
+            # Merge in the default options with the minion_config_overrides
+            salt.utils.dictupdate.update(config_defaults, config_overrides, merge_lists=True)
+
+        return config_defaults
+
+    @staticmethod
+    def default_master_config(
+        root_dir, conf_dir, master_id, config_defaults=None, config_overrides=None,
+    ):
+        if config_defaults is None:
+            config_defaults = salt.config.DEFAULT_MASTER_OPTS.copy()
+            config_defaults.pop("user", None)
+            config_defaults = {}
+
+        conf_file = conf_dir.join("master").strpath
+        state_tree_root = root_dir.join("state-tree").ensure(dir=True)
+        state_tree_root_base = state_tree_root.join("base").ensure(dir=True).strpath
+        state_tree_root_prod = state_tree_root.join("prod").ensure(dir=True).strpath
+        pillar_tree_root = root_dir.join("pillar-tree").ensure(dir=True)
+        pillar_tree_root_base = pillar_tree_root.join("base").ensure(dir=True).strpath
+        pillar_tree_root_prod = pillar_tree_root.join("prod").ensure(dir=True).strpath
+
+        _config_defaults = {
+            "id": master_id,
+            "master_id": master_id,
+            "conf_file": conf_file,
+            "root_dir": root_dir.strpath,
+            "interface": "127.0.0.1",
+            "publish_port": ports.get_unused_localhost_port(),
+            "ret_port": ports.get_unused_localhost_port(),
+            "tcp_master_pub_port": ports.get_unused_localhost_port(),
+            "tcp_master_pull_port": ports.get_unused_localhost_port(),
+            "tcp_master_publish_pull": ports.get_unused_localhost_port(),
+            "tcp_master_workers": ports.get_unused_localhost_port(),
+            "worker_threads": 3,
+            "pidfile": "run/master.pid",
+            "pki_dir": "pki",
+            "cachedir": "cache",
+            "timeout": 3,
+            "sock_dir": "run/master",
+            "open_mode": True,
+            "fileserver_list_cache_time": 0,
+            "fileserver_backend": ["roots"],
+            "pillar_opts": False,
+            "peer": {".*": ["test.*"]},
+            "log_file": "logs/master.log",
+            "log_level_logfile": "debug",
+            "key_logfile": "logs/key.log",
+            "token_dir": "tokens",
+            "token_file": root_dir.join("ksfjhdgiuebfgnkefvsikhfjdgvkjahcsidk").strpath,
+            "file_buffer_size": 8192,
+            "log_fmt_console": "%(asctime)s,%(msecs)03.0f [%(name)-17s:%(lineno)-4d][%(levelname)-8s][%(processName)18s(%(process)d)] %(message)s",
+            "log_fmt_logfile": "[%(asctime)s,%(msecs)03.0f][%(name)-17s:%(lineno)-4d][%(levelname)-8s][%(processName)18s(%(process)d)] %(message)s",
+            "file_roots": {"base": state_tree_root_base, "prod": state_tree_root_prod},
+            "pillar_roots": {"base": pillar_tree_root_base, "prod": pillar_tree_root_prod},
+            "order_masters": False,
+            "max_open_files": 10240,
+            "pytest-master": {"log": {"prefix": "{{cli_name}}({})".format(master_id)},},
+        }
+        # Merge in the initial default options with the internal _config_defaults
+        salt.utils.dictupdate.update(config_defaults, _config_defaults, merge_lists=True)
+
+        if config_overrides:
+            # Merge in the default options with the master_config_overrides
+            salt.utils.dictupdate.update(config_defaults, config_overrides, merge_lists=True)
+
+        # Remove syndic related options
+        for key in list(config_defaults):
+            if key.startswith("syndic_"):
+                config_defaults.pop(key)
+
+        return config_defaults
---
+++ packages/pytest-salt-factories/pytest_salt_factories-0.11.3-py2.py3-none-any.whl/tests/functional/factories/test_master_factory.py
@@ -0,0 +1,249 @@
+"""
+    tests.functional.factories.test_master_factory
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Functional tests for the salt master factory
+"""
+import pytest
+
+
+def test_hook_basic_config_defaults(testdir):
+    testdir.makeconftest(
+        """
+        def pytest_saltfactories_master_configuration_defaults():
+            return {'zzzz': True}
+        """
+    )
+    p = testdir.makepyfile(
+        """
+        def test_basic_config_override(request, salt_factories):
+            master_config = salt_factories.configure_master(request, 'master-1')
+            assert 'zzzz' in master_config
+        """
+    )
+    res = testdir.runpytest("-v")
+    res.assert_outcomes(passed=1)
+
+
+def test_keyword_basic_config_defaults(request, salt_factories):
+    master_config = salt_factories.configure_master(
+        request, "master-1", config_defaults={"zzzz": True}
+    )
+    assert "zzzz" in master_config
+
+
+def test_interface_config_defaults(request, salt_factories):
+    interface = "172.17.0.1"
+    master_config = salt_factories.configure_master(
+        request, "master-1", config_defaults={"interface": interface}
+    )
+    assert master_config["interface"] != interface
+    assert master_config["interface"] == "127.0.0.1"
+
+
+def test_hook_basic_config_overrides(testdir):
+    testdir.makeconftest(
+        """
+        def pytest_saltfactories_master_configuration_overrides():
+            return {'zzzz': True}
+        """
+    )
+    p = testdir.makepyfile(
+        """
+        def test_basic_config_override(request, salt_factories):
+            master_config = salt_factories.configure_master(request, 'master-1')
+            assert 'zzzz' in master_config
+        """
+    )
+    res = testdir.runpytest("-v")
+    res.assert_outcomes(passed=1)
+
+
+def test_keyword_basic_config_overrides(request, salt_factories):
+    master_config = salt_factories.configure_master(
+        request, "master-1", config_overrides={"zzzz": True}
+    )
+    assert "zzzz" in master_config
+
+
+def test_interface_config_overrides(request, salt_factories):
+    interface = "172.17.0.1"
+    master_config = salt_factories.configure_master(
+        request, "master-1", config_overrides={"interface": interface}
+    )
+    assert master_config["interface"] != "127.0.0.1"
+    assert master_config["interface"] == interface
+
+
+def test_hook_simple_overrides_override_defaults(testdir):
+    testdir.makeconftest(
+        """
+        def pytest_saltfactories_master_configuration_defaults():
+            return {'zzzz': False}
+
+        def pytest_saltfactories_master_configuration_overrides():
+            return {'zzzz': True}
+        """
+    )
+    p = testdir.makepyfile(
+        """
+        def test_basic_config_override(request, salt_factories):
+            master_config = salt_factories.configure_master(request, 'master-1')
+            assert 'zzzz' in master_config
+            assert master_config['zzzz'] is True
+        """
+    )
+    res = testdir.runpytest("-v")
+    res.assert_outcomes(passed=1)
+
+
+def test_keyword_simple_overrides_override_defaults(request, salt_factories):
+    master_config = salt_factories.configure_master(
+        request, "master-1", config_defaults={"zzzz": False}, config_overrides={"zzzz": True}
+    )
+    assert "zzzz" in master_config
+    assert master_config["zzzz"] is True
+
+
+def test_hook_nested_overrides_override_defaults(testdir):
+    testdir.makeconftest(
+        """
+        def pytest_saltfactories_master_configuration_defaults():
+            return {
+                'zzzz': False,
+                'user': 'foobar',
+                'colors': {
+                    'black': True,
+                    'white': False
+                }
+            }
+
+        def pytest_saltfactories_master_configuration_overrides():
+            return {
+                'colors': {
+                    'white': True,
+                    'grey': False
+                }
+            }
+        """
+    )
+    p = testdir.makepyfile(
+        """
+        def test_basic_config_override(request, salt_factories):
+            master_config = salt_factories.configure_master(request, 'master-1')
+            assert 'zzzz' in master_config
+            assert master_config['zzzz'] is False
+            assert master_config['colors'] == {
+                'black': True,
+                'white': True,
+                'grey': False
+            }
+        """
+    )
+    res = testdir.runpytest("-v")
+    res.assert_outcomes(passed=1)
+
+
+def test_keyword_nested_overrides_override_defaults(request, salt_factories):
+    master_config = salt_factories.configure_master(
+        request,
+        "master-1",
+        config_defaults={
+            "zzzz": False,
+            "user": "foobar",
+            "colors": {"black": True, "white": False},
+        },
+        config_overrides={"colors": {"white": True, "grey": False}},
+    )
+    assert "zzzz" in master_config
+    assert master_config["zzzz"] is False
+    assert master_config["colors"] == {"black": True, "white": True, "grey": False}
+
+
+def test_nested_overrides_override_defaults(testdir):
+    testdir.makeconftest(
+        """
+        def pytest_saltfactories_master_configuration_defaults():
+            return {
+                'zzzz': True,
+                'user': 'foobar',
+                'colors': {
+                    'black': False,
+                    'white': True,
+                    'blue': False
+                }
+            }
+
+        def pytest_saltfactories_master_configuration_overrides():
+            return {
+                'colors': {
+                    'white': False,
+                    'grey': True,
+                    'blue': True
+                }
+            }
+        """
+    )
+    p = testdir.makepyfile(
+        """
+        def test_basic_config_override(request, salt_factories):
+            master_config = salt_factories.configure_master(
+                request,
+                'master-1',
+                config_defaults={
+                    'zzzz': False,
+                    'user': 'foobar',
+                    'colors': {
+                        'black': True,
+                        'white': False
+                    }
+                },
+                config_overrides={
+                    'colors': {
+                        'white': True,
+                        'grey': False
+                    }
+                }
+            )
+            assert 'zzzz' in master_config
+            assert master_config['zzzz'] is False
+            assert master_config['colors'] == {
+                'black': True,
+                'white': True,
+                'grey': False,
+                'blue': True
+            }
+        """
+    )
+    res = testdir.runpytest("-v")
+    res.assert_outcomes(passed=1)
+
+
+def test_provide_root_dir(testdir, request, salt_factories):
+    root_dir = testdir.mkdir("custom-root")
+    config_defaults = {"root_dir": root_dir}
+    master_config = salt_factories.configure_master(
+        request, "master-1", config_defaults=config_defaults
+    )
+    assert master_config["root_dir"] == root_dir
+
+
+def configure_kwargs_ids(value):
+    return "configure_kwargs={!r}".format(value)
+
+
+@pytest.mark.parametrize(
+    "configure_kwargs",
+    [{"config_defaults": {"user": "blah"}}, {"config_overrides": {"user": "blah"}}, {}],
+    ids=configure_kwargs_ids,
+)
+def test_provide_user(request, salt_factories, configure_kwargs):
+    master_config = salt_factories.configure_master(request, "master-1", **configure_kwargs)
+    if not configure_kwargs:
+        # salt-factories injects the current username
+        assert master_config["user"] is not None
+        assert master_config["user"] == salt_factories.get_running_username()
+    else:
+        # salt-factories does not override the passed user value
+        assert master_config["user"] != salt_factories.get_running_username()
+        assert master_config["user"] == "blah"
