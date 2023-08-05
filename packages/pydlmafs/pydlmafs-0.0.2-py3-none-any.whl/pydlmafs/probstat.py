0d1d1b485a18ffb65fbbb23befa64e63bdd6d2c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:41 +0000

    Add package pydlmafs/0.0.2/pydlmafs-0.0.2-py3-none-any.whl

---
+++ packages/pydlmafs/pydlmafs-0.0.2-py3-none-any.whl/pydlmafs/probstat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 10 16:24:16 2020
+
+@author: gabriel
+"""
+from random import choice
+from math import atan, pi, e, sqrt
+from calc import uni_integral
