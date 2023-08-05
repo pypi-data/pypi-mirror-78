9ca3e72286e0a1f049ae4d14488bf18b04a866da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:55 +0000

    Add package mys/0.28.0/mys-0.28.0.tar.gz

---
+++ packages/mys/mys-0.28.0.tar.gz/mys-0.28.0/mys/lib/mys.cpp
@@ -0,0 +1,37 @@
+#include "mys.hpp"
+
+shared_vector<shared_string> create_args(int argc, const char *argv[])
+{
+    int i;
+    shared_vector<shared_string> args = make_shared_vector<shared_string>({});
+
+    for (i = 0; i < argc; i++) {
+        args->push_back(make_shared_string(argv[i]));
+    }
+
+    return args;
+}
+
+std::ostream&
+operator<<(std::ostream& os, const Exception& e)
+{
+    os << e.m_name_p << ": " << e.what();
+
+    return os;
+}
+
+std::ostream&
+operator<<(std::ostream& os, const std::exception& e)
+{
+    os << e.what();
+
+    return os;
+}
+
+std::ostream&
+operator<<(std::ostream& os, const String& obj)
+{
+    os << *obj.m_string;
+
+    return os;
+}
