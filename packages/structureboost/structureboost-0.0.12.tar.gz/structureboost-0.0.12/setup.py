dfb93c2020b569408c4ba19c1bbaf833b74d494a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:30 +0000

    Add package structureboost/0.0.12/structureboost-0.0.12.tar.gz

---
+++ packages/structureboost/structureboost-0.0.12.tar.gz/structureboost-0.0.12/setup.py
@@ -0,0 +1,53 @@
+"""Setup for the structureboost package."""
+
+import setuptools
+import numpy
+
+with open('README.md') as f:
+    README = f.read()
+
+setuptools.setup(
+    author="Brian Lucena",
+    author_email="brianlucena@gmail.com",
+    name='structureboost',
+    license="MIT",
+    description="""StructureBoost is a Python package for gradient boosting
+                   using categorical structure.  See documentation at:
+                   https://structureboost.readthedocs.io/""",
+    version='0.0.12',
+    long_description=README,
+    url='https://github.com/numeristical/structureboost',
+    packages=['structureboost'],
+    package_dir={'structureboost':
+                 'structureboost'},
+    python_requires=">=3.5",
+    install_requires=[
+        "pandas>=1.0",
+        "numpy>=1.16",
+        "scipy>=1.3"],
+    ext_modules=[setuptools.Extension("graphs", ["structureboost/graphs.c"],
+                 include_dirs=[numpy.get_include()]),
+                 setuptools.Extension("structure_dt",
+                                      ["structureboost/structure_dt.c"],
+                                      include_dirs=[numpy.get_include()]),
+                 setuptools.Extension("structure_gb",
+                                      ["structureboost/structure_gb.c"],
+                                      include_dirs=[numpy.get_include()]),
+                 setuptools.Extension("structure_gb_mc",
+                                      ["structureboost/structure_gb_mc.c"],
+                                      include_dirs=[numpy.get_include()]),
+                 setuptools.Extension("structure_gb_multiclass",
+                                      ["structureboost/structure_gb_multiclass.c"],
+                                      include_dirs=[numpy.get_include()])],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ]
+)
---
+++ packages/structureboost/structureboost-0.0.12.tar.gz/structureboost-0.0.12/structureboost/__init__.py
@@ -0,0 +1,9 @@
+"""structureboost is a Python package for Gradient Boosting using categorical structure"""
+
+import graphs
+from structure_gb import StructureBoost
+# from structure_gb_multiclass import StructureBoostMulticlass
+from structure_gb_mc import StructureBoostMC
+from .utils import get_basic_config, apply_defaults, default_config_dict, ice_plot
+
+__version__ = '0.0.12'
