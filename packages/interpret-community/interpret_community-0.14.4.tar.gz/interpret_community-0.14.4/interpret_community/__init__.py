e9d635488c6a2bfa6368d5e38d17180655fd75c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:13 +0000

    Add package interpret-community/0.14.4/interpret_community-0.14.4.tar.gz

---
+++ packages/interpret-community/interpret_community-0.14.4.tar.gz/interpret_community-0.14.4/interpret_community/__init__.py
@@ -0,0 +1,39 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+"""Module for interpreting, including feature and class importance for blackbox, greybox and glassbox models.
+
+You can use model interpretability to explain why a model model makes the predictions it does and help build
+confidence in the model.
+"""
+
+from .tabular_explainer import TabularExplainer
+
+__all__ = ["TabularExplainer"]
+
+# Setup logging infrustructure
+import logging
+import os
+import atexit
+# Only log to disk if environment variable specified
+interpret_c_logs = os.environ.get('INTERPRET_C_LOGS')
+if interpret_c_logs is not None:
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    os.makedirs(os.path.dirname(interpret_c_logs), exist_ok=True)
+    handler = logging.FileHandler(interpret_c_logs, mode='w')
+    handler.setLevel(logging.INFO)
+    logger.addHandler(handler)
+    logger.info('Initializing logging file for interpret-community')
+
+    def close_handler():
+        handler.close()
+        logger.removeHandler(handler)
+    atexit.register(close_handler)
+
+__name__ = 'interpret_community'
+_major = '0'
+_minor = '14'
+_patch = '4'
+__version__ = '{}.{}.{}'.format(_major, _minor, _patch)
