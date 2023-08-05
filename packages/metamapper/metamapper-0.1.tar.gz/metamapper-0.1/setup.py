dc9ada3860f5236e0eda9a601d69c97302c9627b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:35 +0000

    Add package metamapper/0.1/metamapper-0.1.tar.gz

---
+++ packages/metamapper/metamapper-0.1.tar.gz/metamapper-0.1/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+setup(
+  name = 'metamapper',         # How you named your package folder (MyLib)
+  packages = ['metamapper'],   # Chose the same as "name"
+  version = '0.1',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'metamapper',   # Give a short description about your library
+  author = 'Scott Cruwys',                   # Type in your name
+  author_email = 'scott@metamapper.io',      # Type in your E-Mail
+  url = 'https://github.com/getmetamapper/metamapper',   # Provide either the link to your github or to your webs
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
