42061c68e9e98d9db10fd33bef38a53d7842b790
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:07 +0000

    Add package snowland-qyweixin/0.0.1/snowland_qyweixin-0.0.1-py3-none-any.whl

---
+++ packages/snowland-qyweixin/snowland_qyweixin-0.0.1-py3-none-any.whl/qywechat/common.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: 深圳星河软通科技有限公司 A.Star
+# @contact: astar@snowland.ltd
+# @site: www.astar.ltd
+# @file: common .py
+# @time: 2020/8/30 6:42
+# @Software: PyCharm
+
+
+GETTOKEN_URL = 'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={corpid}&corpsecret={corpsecret}'
