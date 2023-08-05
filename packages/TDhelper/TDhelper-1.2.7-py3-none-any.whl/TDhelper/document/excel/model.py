4e600c5b9bce5b6eacf962cf193239fbb79b42b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:01 +0000

    Add package TDhelper/1.2.7/TDhelper-1.2.7-py3-none-any.whl

---
+++ packages/TDhelper/TDhelper-1.2.7-py3-none-any.whl/TDhelper/document/excel/model.py
@@ -0,0 +1,53 @@
+from TDhelper.document.excel.meta.modelMeta import modelMeta
+class model(metaclass=modelMeta):
+    __excelHandle__= None # excel object.
+    __sheetHandle__= None # sheet object.
+    __count__= 0 # rows count.
+    __recordOffset__= 0 # now record offset.
+    def __init__(self, excelPath= None):
+        if excelPath:
+            self.Meta.file= excelPath
+        if self.Meta.file:
+            self.__initExcelHandle__()
+
+    def __initExcelHandle__(self):
+        return None
+
+    def __enter__(self):
+        return self
+
+    def readLine(self, lineOffset=1):
+        '''
+            读取一行数据
+            - params:
+            -   lineOffset: <int>, default 1, 读取的行偏移量.
+        '''
+        return True
+    
+    def rows(self):
+        '''
+            获取表格所有数据.
+            - return: <arrary>, defautl return [].
+        '''
+        return []
+
+    def getCount(self):
+        '''
+            获取总行数
+            - return: <int>
+        '''
+        return 0
+
+    def close(self):
+        return None
+    
+    class Meta:
+        '''
+            元数据
+            - file: <string>, 文件路径.
+            - sheet: <string>, sheet名称.
+            - extension: <string>, 文件类型(xlsx,csv)
+        '''
+        file= ''
+        sheet= 'sheet1'
+        extension= 'xlsx'
