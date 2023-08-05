fc199a976e30d11b213c93bfc29891cdd8a62cba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:25 +0000

    Add package sksearchspace/2020.8.0.0.23.1/sksearchspace-2020.8.0.0.23.1.tar.gz

---
+++ packages/sksearchspace/sksearchspace-2020.8.0.0.23.1.tar.gz/sksearchspace-2020.8.0.0.23.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(name="sksearchspace",
+      version="2020.8.0.0.23.1",
+      description="Defines search spaces for scikit-lean estimators",
+      author="Thomas J. Fan",
+      author_email="thomasjpfan@gmail.com",
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type="text/markdown",
+      url="https://github.com/thomasjpfan/sksearchspace",
+      packages=find_packages(include=["sksearchspace"]),
+      include_package_data=True,
+      python_requires='>=3.6',
+      install_requires=["scikit-learn==0.23.1", "ConfigSpace>=0.4.13"],
+      zip_safe=True,
+      license='MIT')
---
+++ packages/sksearchspace/sksearchspace-2020.8.0.0.23.1.tar.gz/sksearchspace-2020.8.0.0.23.1/sksearchspace/__init__.py
@@ -0,0 +1,5 @@
+from ._config import SearchSpace
+
+__all__ = [
+    'SearchSpace',
+]
