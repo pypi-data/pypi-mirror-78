064bb11e0350473bf432193e63da4b5e35997a59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:14:24 +0000

    Add package getmail/6.7/getmail-6.7-py3-none-any.whl

---
+++ packages/getmail/getmail-6.7-py3-none-any.whl/getmailcore/__init__.py
@@ -0,0 +1,35 @@
+'''A reliable mail-retriever toolkit.
+
+getmail is a reliable, modular, extensible mail retriever with support for
+simple and multidrop POP3 mailboxes, multidrop SDPS mailboxes, simple and
+multidrop IMAP mailboxes.  Also supports POP3- and IMAP-over-SSL, message
+filtering, and other features.
+
+getmail is Copyright (C) 1998-2019 Charles Cazabon.  All rights reserved.
+Distributed under the terms of the GNU General Public License version 2 (only).
+You should have received a copy of the license in the file COPYING.
+'''
+
+import sys
+
+__version__ = '6.7'
+
+__py_required__ = '2.7.18'
+__py_required_hex__ = 0x20712f0
+
+if sys.hexversion < __py_required_hex__:
+    raise ImportError('getmail version %s requires Python version %s '
+                      'or later'%(__version__,__py_required__))
+
+__all__ = [
+    'baseclasses',
+    'constants',
+    'destinations',
+    'exceptions',
+    'filters',
+    'imap_utf7',
+    'logging',
+    'message',
+    'retrievers',
+    'utilities',
+]
