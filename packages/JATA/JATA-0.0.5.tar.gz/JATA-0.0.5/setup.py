180fb24c149ee57870a424d0d220497196b2e81c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:41 +0000

    Add package JATA/0.0.5/JATA-0.0.5.tar.gz

---
+++ packages/JATA/JATA-0.0.5.tar.gz/JATA-0.0.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="JATA",  # Replace with your own username
+    version="0.0.5",
+    author="Blaise Albis Burdige",
+    author_email="albisbub@reed.edu",
+    description="A Digital Humanities Package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/albisbub/JATA-TOOLS",
+    packages=setuptools.find_namespace_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
