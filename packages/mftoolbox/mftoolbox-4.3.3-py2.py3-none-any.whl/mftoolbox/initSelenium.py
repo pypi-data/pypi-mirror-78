3dc8784f6f05b8990da3bd90ad393b978b7d9487
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:17 +0000

    Add package mftoolbox/4.3.3/mftoolbox-4.3.3-py2.py3-none-any.whl

---
+++ packages/mftoolbox/mftoolbox-4.3.3-py2.py3-none-any.whl/mftoolbox/initSelenium.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+def get_clear_browsing_button(driver):
+    """Find the "CLEAR BROWSING BUTTON" on the Chrome settings page."""
+    return driver.find_element_by_css_selector('* /deep/ #clearBrowsingDataConfirm')
+
+
+def clear_cache(driver, timeout=60):
+    """Clear the cookies and cache for the ChromeDriver instance."""
+    # navigate to the settings page
+    driver.get('chrome://settings/clearBrowserData')
+
+    # wait for the button to appear
+    wait = WebDriverWait(driver, timeout)
+    wait.until(get_clear_browsing_button)
+
+    # click the button to clear the cache
+    get_clear_browsing_button(driver).click()
+
+    # wait for the button to be gone before returning
+    wait.until_not(get_clear_browsing_button)
+
+
+
+caps = DesiredCapabilities().CHROME
+caps["pageLoadStrategy"] = "eager"
+# noinspection SpellCheckingInspection
+
+options = webdriver.ChromeOptions()
+
+chrome_preferences = {'profile.managed_default_content_settings.images': 2}
+options.add_argument("headless")
+# noinspection SpellCheckingInspection
+options.add_experimental_option("prefs", chrome_preferences)
+
+browser = webdriver.Chrome(options=options, service_args=['--silent'], desired_capabilities=caps)
+#clear_cache(browser)
