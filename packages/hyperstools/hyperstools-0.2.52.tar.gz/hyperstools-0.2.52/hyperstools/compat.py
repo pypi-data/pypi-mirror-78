8359996f54265783a4780e6eb3359e468c66cf0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:30 +0000

    Add package hyperstools/0.2.52/hyperstools-0.2.52.tar.gz

---
+++ packages/hyperstools/hyperstools-0.2.52.tar.gz/hyperstools-0.2.52/hyperstools/compat.py
@@ -0,0 +1,32 @@
+# encoding: utf-8
+
+import json
+import os
+
+
+def _close_old_connections(*args, **kwargs):
+    pass
+
+
+try:
+    import django
+
+    settings = django.conf.settings
+    defaultMq = settings.RABBITMQ
+    close_old_connections = django.db.close_old_connections
+except Exception:
+    close_old_connections = _close_old_connections
+
+if os.environ.get("RABBITMQ"):
+    defaultMq = json.loads(os.environ["RABBITMQ"])
+else:
+    defaultMq = {
+        "host": "127.0.0.1",
+        "port": "5672",
+        "user": "admin",
+        "password": "admin",
+        "vhost": "/",
+        "exchange": "test",
+        "queue": "test",
+        "routing_key": "test",
+    }
