f43f9f3bfbf5d9f07f0e627e71bc9f13b788d3d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:03 +0000

    Add package django-rest-authtoken/2.1.3/django-rest-authtoken-2.1.3.tar.gz

---
+++ packages/django-rest-authtoken/django-rest-authtoken-2.1.3.tar.gz/django-rest-authtoken-2.1.3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('README.md') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='django-rest-authtoken',
+    version='2.1.3',
+    author='Pascal Wichmann',
+    author_email='wichmannpas@gmail.com',
+    description='A simple token-based auth backend for Django Rest Framework storing cryptographically hashed tokens on server-side.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/wichmannpas/django-rest-authtoken',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+    ],
+)
