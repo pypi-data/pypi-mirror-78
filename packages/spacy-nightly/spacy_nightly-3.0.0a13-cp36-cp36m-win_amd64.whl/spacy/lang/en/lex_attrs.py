6c91cc6f3e1c26a6765d474c48388c9e6fe8f567
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:45 +0000

    Add package spacy-nightly/3.0.0a13/spacy_nightly-3.0.0a13-cp36-cp36m-win_amd64.whl

---
+++ packages/spacy-nightly/spacy_nightly-3.0.0a13-cp36-cp36m-win_amd64.whl/spacy/lang/en/lex_attrs.py
@@ -0,0 +1,29 @@
+from ...attrs import LIKE_NUM
+
+# fmt: off
+_num_words = [
+    "zero", "one", "two", "three", "four", "five", "six", "seven", "eight",
+    "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+    "sixteen", "seventeen", "eighteen", "nineteen", "twenty", "thirty", "forty",
+    "fifty", "sixty", "seventy", "eighty", "ninety", "hundred", "thousand",
+    "million", "billion", "trillion", "quadrillion", "gajillion", "bazillion"
+]
+# fmt: on
+
+
+def like_num(text: str) -> bool:
+    if text.startswith(("+", "-", "±", "~")):
+        text = text[1:]
+    text = text.replace(",", "").replace(".", "")
+    if text.isdigit():
+        return True
+    if text.count("/") == 1:
+        num, denom = text.split("/")
+        if num.isdigit() and denom.isdigit():
+            return True
+    if text.lower() in _num_words:
+        return True
+    return False
+
+
+LEX_ATTRS = {LIKE_NUM: like_num}
