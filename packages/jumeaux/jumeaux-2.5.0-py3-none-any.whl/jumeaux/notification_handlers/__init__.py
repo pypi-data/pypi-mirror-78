8fee65d906c43bab90703b52ed496ec3b697e68e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:52 +0000

    Add package jumeaux/2.5.0/jumeaux-2.5.0-py3-none-any.whl

---
+++ packages/jumeaux/jumeaux-2.5.0-py3-none-any.whl/jumeaux/notification_handlers/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import sys
+
+from jumeaux.logger import Logger
+from jumeaux.models import Notifier
+from jumeaux.domain.config.vo import NotifierType
+from jumeaux.notification_handlers.base import NotificationHandler
+from jumeaux.notification_handlers.slack import SlackNotificationHandler
+
+logger: Logger = Logger(__name__)
+
+
+def create_notification_handler(notifier: Notifier) -> NotificationHandler:
+    if notifier.type is NotifierType.SLACK:
+        return SlackNotificationHandler(
+            channel=notifier.channel,
+            username=notifier.username,
+            icon_emoji=notifier.icon_emoji,
+            icon_url=notifier.icon_url,
+        )
+    logger.error(f"Type {notifier.type} is not valid.")
+    sys.exit(1)
