114aa3af4d0b0b5a16a6638d71388eb924da5418
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:21 +0000

    Add package file-diff/1.0.3/file-diff-1.0.3.tar.gz

---
+++ packages/file-diff/file-diff-1.0.3.tar.gz/file-diff-1.0.3/setup.py
@@ -0,0 +1,39 @@
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='file-diff',
+    version="1.0.3",
+    description=(
+        'Specifies the tool for text content difference detection in the directory'
+    ),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='zifeiyushui8',
+    author_email='bngzifei@gmail.com',
+    maintainer='zifeiyushui8',
+    maintainer_email='bngzifei@gmail.com',
+    license='MIT License',
+    packages=find_packages(),
+    platforms=["all"],
+    include_package_data=True,
+    url='https://github.com/Bngzifei/files_diff.git',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries'
+    ],
+    install_requires=[]
+)
