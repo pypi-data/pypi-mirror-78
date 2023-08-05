96c922af029273193d39619a7cb620e4bb7d6c68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:46 +0000

    Add package easy-shortcut/0.0/easy_shortcut-0.0.tar.gz

---
+++ packages/easy-shortcut/easy_shortcut-0.0.tar.gz/easy_shortcut-0.0/easy_shortcut/__init__.py
@@ -0,0 +1,8 @@
+from win32com.client import Dispatch
+def shortcut(path,target,wDir,icon=None):
+    shell = Dispatch('WScript.Shell')
+    shortcut = shell.CreateShortCut(path)
+    shortcut.Targetpath = target
+    shortcut.WorkingDirectory = wDir
+    shortcut.IconLocation = icon
+    shortcut.save()
---
+++ packages/easy-shortcut/easy_shortcut-0.0.tar.gz/easy_shortcut-0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='easy_shortcut',
+      version='0.0',
+      description='Easy make shortcut',
+      packages=['easy_shortcut'],
+      author_email='foton-pc@inbox.ru',
+      zip_safe=False,
+      install_requires=['pywin32','winshell'])
