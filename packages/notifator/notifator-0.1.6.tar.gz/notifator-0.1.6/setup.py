55d518a29b9199948d94b0bc07c3f376de67756c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:17 +0000

    Add package notifator/0.1.6/notifator-0.1.6.tar.gz

---
+++ packages/notifator/notifator-0.1.6.tar.gz/notifator-0.1.6/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+from notifator.version import __version__
+#
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+exec(open('version.py').read())
+
+
+setup(
+    name="notifator",
+    description="Automatically created environment for python package",
+    author="jaromrax",
+    author_email="jaromrax@gmail.com",
+    license="GPL2",
+    version=__version__,
+    packages=find_packages(),
+    package_data={'notifator': ['data/*']},
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    scripts = ['bin/notifator'],
+    install_requires = ['numpy==1.16.2'],
+)
+#
+#   To RECOVER AND ACCESS THE Data later in module: :
+#  X DATA_PATH = pkg_resources.resource_filename('notifator', 'data/')
+#  X DB_FILE =   pkg_resources.resource_filename('notifator', 'data/file')
+#   DB_FILE = pkg_resources.resource_filename(
+#       pkg_resources.Requirement.parse('nuphy2'),
+#       'data/nubase2016.txt'
+#   )
+#   pip install -e .
+#   bumpversion patch minor major release
+#      release needed for pypi
