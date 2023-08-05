8ecef8ad0caa84d299c4a9f5318328deb805f1fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:20 +0000

    Add package xoeuf/2.2.0/xoeuf-2.2.0-py2.py3-none-any.whl

---
+++ packages/xoeuf/xoeuf-2.2.0-py2.py3-none-any.whl/xoeuf/models/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# Copyright (c) Merchise Autrement [~º/~] and Contributors
+# All rights reserved.
+#
+# This is free software; you can do what the LICENCE file allows you to.
+#
+
+"""A new object model.
+
+
+"""
+from .base import *  # noqa: reexport
+
+from xoeuf.odoo.models import *  # noqa: reexport
