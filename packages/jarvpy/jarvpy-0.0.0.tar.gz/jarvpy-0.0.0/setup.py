e256e4802619ac2e91c39b5325dc0b9224ca4d94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:29 +0000

    Add package jarvpy/0.0.0/jarvpy-0.0.0.tar.gz

---
+++ packages/jarvpy/jarvpy-0.0.0.tar.gz/jarvpy-0.0.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jarvpy", 
+    author="Vihaan Pundir",
+    author_email="vihaan.pundir26@gmail.com",
+    description="A ai package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8.5',
+)
