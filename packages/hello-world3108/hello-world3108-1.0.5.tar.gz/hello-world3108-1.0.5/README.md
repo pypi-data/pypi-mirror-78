3dd0cb57ff9cf35af15dd0876dd2f1c94d00cebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:27 +0000

    Add package hello-world3108/1.0.5/hello-world3108-1.0.5.tar.gz

---
+++ packages/hello-world3108/hello-world3108-1.0.5.tar.gz/hello-world3108-1.0.5/README.md
@@ -0,0 +1,12 @@
+# Hello to World by sandhya
+
+A Python package to say hello to everyone
+You can read **[Detailed Documentation](https://github.com/sandmadh/projectWork)** here.
+
+ please visit our website for more details
+This program is simple to use
+
+## Usage
+
+say_hello(name)
+
---
+++ packages/hello-world3108/hello-world3108-1.0.5.tar.gz/hello-world3108-1.0.5/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="hello-world3108",
+    version="1.0.5",
+    description="A Python package to get say hello to everyone",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/sandmadh/projectWork",
+    author="Sandhya Mulay",
+    author_email="sandhya.mulay20@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["hello_world_sandhya"],
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "hello-world-sandhya=hello_world_sandhya.app:say_hello",
+        ]
+    },
+)
