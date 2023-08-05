60daf010f819a3c22a3d365bd48c80d986fcfdab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:49 +0000

    Add package tookiyosi/0.0.2/tookiyosi-0.0.2.tar.gz

---
+++ packages/tookiyosi/tookiyosi-0.0.2.tar.gz/tookiyosi-0.0.2/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="tookiyosi",
+    version="0.0.2",
+    author="TookiYosi",
+    author_email="tookiyosi@mailinator.com",
+    description="A very epic package full of epicness",
+    long_description="A very epic package full of epicness and more!",
+    long_description_content_type="text/plain",
+    url="https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    packages=setuptools.find_packages(),
+    package_data={'': ['creeper.wav']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
