fe6e601ff02857dd49992c7e545ac5621103e4b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:33 +0000

    Add package vipy/1.8.24/vipy-1.8.24.tar.gz

---
+++ packages/vipy/vipy-1.8.24.tar.gz/vipy-1.8.24/vipy/__init__.py
@@ -0,0 +1,16 @@
+# Import all subpackages
+import vipy.show  # matplotlib first
+import vipy.activity
+import vipy.annotation
+import vipy.calibration
+import vipy.downloader
+import vipy.geometry
+import vipy.image
+import vipy.linalg
+import vipy.math
+import vipy.object
+import vipy.util
+import vipy.version
+import vipy.video
+import vipy.videosearch
+import vipy.visualize
