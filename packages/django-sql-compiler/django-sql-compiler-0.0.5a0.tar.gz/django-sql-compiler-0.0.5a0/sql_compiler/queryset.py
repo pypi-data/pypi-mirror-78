3c1432142d88cbf2c6e5dd7131284c0d08e22b14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:05 +0000

    Add package django-sql-compiler/0.0.5a0/django-sql-compiler-0.0.5a0.tar.gz

---
+++ packages/django-sql-compiler/django-sql-compiler-0.0.5a0.tar.gz/django-sql-compiler-0.0.5a0/sql_compiler/queryset.py
@@ -0,0 +1,30 @@
+from django.db import connections
+from django.db.models import QuerySet
+from django.utils.functional import cached_property
+from . import utils
+from . import compilers
+
+
+class SQLCompilerQuerySet(QuerySet):
+
+    @cached_property
+    def executable_query(self):
+
+        """
+        Takes the parameterized SQL query with the arg list and compiles the query so
+        that it can be used anywhere - as a subquery in a Raw SQL query, in a SQL GUI interface, etc.
+        """
+
+        connection = utils.validate_connection(connections[self.db])
+        backend_name = connection.settings_dict['ENGINE']
+
+        if not connection:
+            raise NotImplementedError('{} backend is not supported.'.format(backend_name))
+
+        query, params = self.query.sql_with_params()
+
+        compiler = compilers.compiler_map[backend_name]()
+        compiled_query = compiler.compile_sql(connection, query, params)
+        compiled_query = utils.bytes_to_string(compiled_query)
+
+        return compiled_query
