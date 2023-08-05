b288de45d35e371c770ec5c1f0bc056a629fcb01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:24 +0000

    Add package qspider/0.1.4/qspider-0.1.4.tar.gz

---
+++ packages/qspider/qspider-0.1.4.tar.gz/qspider-0.1.4/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="qspider",
+    version="0.1.4",
+    author="Tishacy",
+    author_email="",
+    description="An easy to use tools module for writing multi-thread and multi-process programs.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'termcolor>=1.1.0',
+        'colorama>=0.4.3',
+        'colored>=1.4.2'
+    ],
+    entry_points={
+        'console_scripts': [
+            'genqspider=qspider.core:genqspider'
+        ],
+    },
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
