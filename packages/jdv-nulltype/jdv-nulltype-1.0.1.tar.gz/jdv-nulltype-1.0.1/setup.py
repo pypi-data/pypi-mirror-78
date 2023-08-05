700cd0f1ce6c8ddb3f02d7cb63ac4b251312c286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:25 +0000

    Add package jdv-nulltype/1.0.1/jdv-nulltype-1.0.1.tar.gz

---
+++ packages/jdv-nulltype/jdv-nulltype-1.0.1.tar.gz/jdv-nulltype-1.0.1/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['nulltype']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['keats>=0.2.28,<0.3.0']
+
+setup_kwargs = {
+    'name': 'jdv-nulltype',
+    'version': '1.0.1',
+    'description': 'A singleton Null object in Python.',
+    'long_description': None,
+    'author': 'Justin Vrana',
+    'author_email': 'justin.vrana@gmail.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.5,<4.0',
+}
+
+
+setup(**setup_kwargs)
