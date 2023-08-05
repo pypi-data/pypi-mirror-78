69d7f358ce9e9383cc61ef72c61386fbf5165f84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:39 +0000

    Add package drf-jwt-util/0.1.8/drf_jwt_util-0.1.8.tar.gz

---
+++ packages/drf-jwt-util/drf_jwt_util-0.1.8.tar.gz/drf_jwt_util-0.1.8/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="drf_jwt_util",
+    version="0.1.8",
+    description="drf jwt util",
+    author='Shunsuke Aihara',
+    author_email='aihara@argmax.jp',
+    url='https://github.com/shunsukeaihara/drf_jwt_util',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=["django", "djangorestframework", "djangorestframework-jwt", "social-auth-app-django"],
+    tests_require=["tox", "factory_boy", "faker>=1.0.6"],
+    license="MIT License",
+)
