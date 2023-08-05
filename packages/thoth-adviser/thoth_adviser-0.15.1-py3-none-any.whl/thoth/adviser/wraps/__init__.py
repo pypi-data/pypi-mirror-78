e47fb992a1e0d0d6189e0147bf46e7d5dc1f80fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:52 +0000

    Add package thoth-adviser/0.15.1/thoth_adviser-0.15.1-py3-none-any.whl

---
+++ packages/thoth-adviser/thoth_adviser-0.15.1-py3-none-any.whl/thoth/adviser/wraps/__init__.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# thoth-adviser
+# Copyright(C) 2019, 2020 Fridolin Pokorny
+#
+# This program is free software: you can redistribute it and / or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""Wrap units implemented in adviser."""
+
+from .mkl_threads import MKLThreadsWrap
+from .no_onservation import NoObservationWrap
+from .no_semantic_interposition import NoSemanticInterpositionWrap
+from .intel_tensorflow import IntelTensorFlowWrap
+
+
+# Relative ordering of units is relevant, as the order specifies order
+# in which the asked to be registered - any dependencies between them
+# can be mentioned here.
+__all__ = [
+    "MKLThreadsWrap",
+    "NoObservationWrap",
+    "NoSemanticInterpositionWrap",
+    "IntelTensorFlowWrap",
+]
