5a626574c55c42d7271d3dcf3ab4520a1118470c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:09 +0000

    Add package camtasia/6.3.0/camtasia-6.3.0.tar.gz

---
+++ packages/camtasia/camtasia-6.3.0.tar.gz/camtasia-6.3.0/src/camtasia/__init__.py
@@ -0,0 +1,1 @@
+from .project import load_project, new_project, use_project  # noqa: F401
---
+++ packages/camtasia/camtasia-6.3.0.tar.gz/camtasia-6.3.0/src/camtasia/timeline/marker.py
@@ -0,0 +1,7 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Marker:
+    name: str
+    time: int
