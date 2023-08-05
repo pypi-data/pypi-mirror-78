0c66da33df9c8c2e7299c8d19821bdc2a2253bee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:01 +0000

    Add package jtalkbot/0.5.0/jtalkbot-0.5.0-py3-none-any.whl

---
+++ packages/jtalkbot/jtalkbot-0.5.0-py3-none-any.whl/jtalkbot/__main__.py
@@ -0,0 +1,241 @@
+"""main entry point """
+
+import argparse
+import asyncio
+import io
+import json
+import logging
+import os
+import sys
+from ctypes.util import find_library
+
+import discord
+
+from . import VERSION
+from . import openjtalk
+
+
+logging.basicConfig()
+LOG = logging.getLogger(__name__)
+LOG.setLevel(logging.INFO)
+LOG.info(f'jtalkbot {VERSION}')
+
+
+def load_config() -> dict:
+    """Search config file and return its config dict object.
+
+    The configuration file will be found in such order bellow:
+
+    1. ./jtalkbot-config.json
+    2. ~/jtalkbot-config.json
+    2. ~/.local/jtalkbot-config.json
+    3. {module directory}/jtalkbot-config.json
+    4. The file specified in JTALKBOT_CONFIG environment value.
+    """
+    global __config__
+    config_name = 'jtalkbot-config.json'
+    paths = [
+        config_name,
+        os.path.expanduser(f'~/{config_name}'),
+        os.path.expanduser(f'~/.local/{config_name}'),
+        os.path.join(os.path.dirname(__file__), config_name),
+        os.environ.get('JTALKBOT_CONFIG')
+    ]
+    for filename in paths:
+        if filename and os.path.exists(filename):
+            __config__ = os.path.abspath(filename)
+            LOG.info(f'config file: {__config__}')
+            with open(filename) as f:
+                return json.load(f)
+    LOG.error(f'{config_name} not found.')
+    sys.exit(1)
+
+
+CONFIG = load_config()
+
+
+class Bot(discord.Client):
+    """Bot class """
+
+    def __init__(self, *args, **kwds):
+        """Constructor. """
+
+        super().__init__(*args, **kwds)
+
+    async def on_ready(self):
+        """Called when the client is done preparing the data received
+        from Discord. """
+
+        LOG.info(f'Logged in as {self.user}.')
+
+
+    async def on_message(self, msg: discord.Message):
+        """Called when a `Message` is created and sent. """
+
+        if msg.author == self.user:
+            return
+
+        tch = msg.channel
+        vcl = discord.utils.get(
+            self.voice_clients, channel__guild=tch.guild, channel__name=tch.name)
+        if vcl:
+            LOG.info(f'Reading {msg.author}\'s post on t:{tch.guild}/{tch}.')
+            await talk(vcl, msg.content)
+
+
+    async def on_voice_state_update(self,
+                                    member: discord.Member,
+                                    before: discord.VoiceState,
+                                    after: discord.VoiceState):
+        """Called when a `Member` changes their `VoiceState`. """
+
+        if not before.channel and after.channel:
+            # someone connected the voice channel.
+            vch = after.channel
+            guild = vch.guild
+            if member == vch.guild.owner:
+                LOG.info(
+                    f'Guild owner {member} connected v:{guild}/{vch}.')
+                vcl = await vch.connect()
+            elif member == self.user:
+                LOG.info(f'{member} connected v:{guild}/{vch}.')
+                vcl = discord.utils.get(self.voice_clients, channel=vch)
+                if vcl:
+                    await talk(vcl, CONFIG['voice_hello'])
+                tch = discord.utils.get(guild.text_channels, name=vch.name)
+                if tch:
+                    await tch.send(CONFIG['text_start'])
+            else:
+                LOG.info(f'{member} connected v:{guild}/{vch}.')
+
+
+        elif before.channel and not after.channel:
+            # someone disconnected the voice channel.
+            vch = before.channel
+            guild = vch.guild
+            if member == vch.guild.owner:
+                LOG.info(
+                    f'Guild owner {member} disconnected v:{guild}/{vch}.')
+                vcl = discord.utils.get(self.voice_clients, channel=vch)
+                if vcl and vcl.is_connected():
+                    await vcl.disconnect()
+                tch = discord.utils.get(guild.text_channels, name=vch.name)
+                if tch:
+                    await tch.send(CONFIG['text_end'])
+            else:
+                LOG.info(f'{member} disconnected v:{guild}/{vch}.')
+
+
+async def talk(vcl: discord.VoiceClient,
+               text: str,
+               dic: str = None,
+               voice: str = None,
+               frameperiod: int = None,
+               allpass: float = None,
+               postfilter: float = None,
+               speedrate: float = None,
+               halftone: float = None,
+               threshold: float = None,
+               spectrum: float = None,
+               logf0: float = None,
+               volume: float = None,
+               buffersize: float = None):
+
+    if dic is None:
+        dic = CONFIG.get('open_jtalk_x')
+    if voice is None:
+        voice = CONFIG.get('open_jtalk_m')
+    if frameperiod is None:
+        frameperiod = CONFIG.get('open_jtalk_p')
+    if allpass is None:
+        allpass = CONFIG.get('open_jtalk_a')
+    if postfilter is None:
+        postfilter = CONFIG.get('open_jtalk_b')
+    if speedrate is None:
+        speedrate = CONFIG.get('open_jtalk_r')
+    if halftone is None:
+        halftone = CONFIG.get('open_jtalk_fm')
+    if threshold is None:
+        threshold = CONFIG.get('open_jtalk_u')
+    if spectrum is None:
+        spectrum = CONFIG.get('open_jtalk_jm')
+    if logf0 is None:
+        logf0 = CONFIG.get('open_jtalk_jf')
+    if volume is None:
+        volume = CONFIG.get('open_jtalk_g')
+    if buffersize is None:
+        buffersize = CONFIG.get('open_jtalk_z')
+
+    data = await openjtalk.async_talk(
+        text, dic=dic, voice=voice,
+        sampling=openjtalk.FREQ_48000HZ,
+        frameperiod=frameperiod, allpass=allpass,
+        postfilter=postfilter, speedrate=speedrate, halftone=halftone,
+        threshold=threshold, spectrum=spectrum, logf0=logf0, volume=volume,
+        buffersize=buffersize)
+    stream = io.BytesIO(data)
+    audio = discord.PCMAudio(stream)
+    sleeptime = 0.1
+    timeout = 6.0
+    for _ in range(int(timeout / sleeptime)):
+        if vcl.is_connected():
+            break
+        await asyncio.sleep(0.1)
+    else:
+        return
+    while vcl.is_playing():
+        await asyncio.sleep(0.1)
+    vcl.play(audio, after=lambda e: stream.close())
+
+
+def main():
+    """Main entry point. """
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-V', '--version', action='store_true',
+                        help='print the version number and exit')
+    parser.add_argument('-t', '--token',
+                        help='override the Discord bot token')
+    parser.add_argument('--open_jtalk_x',
+                        help='open_jtalk dictionary directory')
+    parser.add_argument('--open_jtalk_m',
+                        help='open_jtalk HTS voice file')
+    parser.add_argument('--open_jtalk_p', type=int,
+                        help='open_jtalk frame period (point)')
+    parser.add_argument('--open_jtalk_a', type=float,
+                        help='open_jtalk all-pass constant')
+    parser.add_argument('--open_jtalk_b', type=float,
+                        help='open_jtalk postfiltering coefficient')
+    parser.add_argument('--open_jtalk_r', type=float,
+                        help='open_jtalk speech speed rate')
+    parser.add_argument('--open_jtalk_fm', type=float,
+                        help='open_jtalk additional half-tone')
+    parser.add_argument('--open_jtalk_u', type=float,
+                        help='open_jtalk voiced/unvoiced threshold')
+    parser.add_argument('--open_jtalk_jm', type=float,
+                        help='open_jtalk weight of GV for spectrum')
+    parser.add_argument('--open_jtalk_jf', type=float,
+                        help='open_jtalk weight of GV for log F0')
+    parser.add_argument('--open_jtalk_g', type=float,
+                        help='open_jtalk volume (dB)')
+    parser.add_argument('--open_jtalk_z', type=int,
+                        help='open_jtalk audio buffer size (if i==0, turn off)')
+    args = parser.parse_args()
+
+    if args.version:
+        print(__package__, VERSION)
+        sys.exit()
+
+    # Overwrite CONFIG with args parameters which are specified
+    CONFIG.update((k, v) for (k, v) in vars(args).items() if v is not None)
+
+    discord.opus.load_opus(find_library('opus'))
+    if discord.opus.is_loaded():
+        LOG.info('Opus library is loaded.')
+
+    bot = Bot()
+    bot.run(CONFIG['token'])
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/jtalkbot/jtalkbot-0.5.0-py3-none-any.whl/jtalkbot/openjtalk.py
@@ -0,0 +1,532 @@
+"""Open JTalk wrapper.
+
+Open JTalk:
+    http://open-jtalk.sourceforge.net
+"""
+
+import asyncio
+import io
+import os
+import subprocess
+import sys
+import tempfile
+import wave
+from typing import List
+
+
+__all__ = [
+    'FREQ_44100HZ', 'FREQ_48000HZ',
+    'Agent', 'talk', 'async_talk'
+]
+
+
+ENCODING = sys.getfilesystemencoding()
+OPEN_JTALK = 'open_jtalk'
+DICT = '/opt/local/lib/open_jtalk/dic'
+VOICE = '/opt/local/lib/open_jtalk/voice/nitech/nitech_jp_atr503_m001.htsvoice'
+
+
+# pre-defined sampling frequency
+FREQ_44100HZ = 44100
+FREQ_48000HZ = 48000
+
+
+def find_command(command: str) -> str:
+    """Search PATH and return full path for the given command. """
+
+    if os.path.isabs(command):
+        return command
+
+    if os.path.dirname(command):
+        raise ValueError(
+            """command must be a absolute path or a simple name without
+            path separators """)
+
+    for dirname in os.get_exec_path():
+        full = os.path.join(dirname, command)
+        if os.path.exists(full):
+            return full
+
+    return None
+
+
+class Agent(object):
+    """Open JTalk command line option wrapper. """
+
+    def __init__(self, command: str, dic: str, voice: str, output: str, *,
+                 trace: str = None,
+                 sampling: int = None,
+                 frameperiod: int = None,
+                 allpass: float = None,
+                 postfilter: float = 0.0,
+                 speedrate: float = 1.0,
+                 halftone: float = 0.0,
+                 threshold: float = 0.5,
+                 spectrum: float = 1.0,
+                 logf0: float = 1.0,
+                 volume: float = 0.0,
+                 buffersize: float = 0,
+                 infile: str = None):
+        """Constructor. """
+
+        self._command = command
+        self._output = output
+        self._trace = trace
+        self._dic = dic
+        self._voice = voice
+        self._sampling = sampling
+        self._frameperiod = frameperiod
+        self._allpass = allpass
+        self._postfilter = postfilter
+        self._speedrate = speedrate
+        self._halftone = halftone
+        self._threshold = threshold
+        self._spectrum = spectrum
+        self._logf0 = logf0
+        self._volume = volume
+        self._buffersize = buffersize
+        self._infile = infile
+
+    @property
+    def command(self) -> str:
+        """Path to the open_jtalk command """
+        return self._command
+
+    @command.setter
+    def command(self, value: str):
+        self._command = value
+
+    @property
+    def dictionary(self) -> str:
+        """Path to the dictionary directory """
+        return self._dic
+
+    @dictionary.setter
+    def dictionary(self, value: str):
+        self._dic = value
+
+    @property
+    def voice(self) -> str:
+        """Path to the htc_voice file """
+        return self._voice
+
+    @voice.setter
+    def voice(self, value: str):
+        self._voice = value
+
+    @property
+    def output(self) -> str:
+        """Filename of output wav audio (generated speech) """
+        return self._output
+
+    @output.setter
+    def output(self, value: str):
+        self._output = value
+
+    @property
+    def trace(self) -> str:
+        """Filename of output trace information """
+        return self._trace
+
+    @trace.setter
+    def trace(self, value: str):
+        self._trace = value
+
+    @property
+    def sampling(self) -> int:
+        """Sampling frequency """
+        return self._sampling
+
+    @sampling.setter
+    def sampling(self, value: int):
+        self._sampling = value
+
+    @property
+    def frameperiod(self) -> int:
+        """Frame period (point) """
+        return self._frameperiod
+
+    @frameperiod.setter
+    def frameperiod(self, value: int):
+        self._frameperiod = value
+
+    @property
+    def allpass(self) -> float:
+        """all-pass constant """
+        return self._allpass
+
+    @allpass.setter
+    def allpass(self, value: float):
+        self._allpass = value
+
+    @property
+    def postfilter(self) -> float:
+        """Postfiltering coefficient """
+        return self._postfilter
+
+    @postfilter.setter
+    def postfilter(self, value: float):
+        self._postfilter = value
+
+    @property
+    def speedrate(self) -> float:
+        """Speech speed rate """
+        return self._speedrate
+
+    @speedrate.setter
+    def speedrate(self, value: float):
+        self._speedrate = value
+
+    @property
+    def halftone(self) -> float:
+        """Additional half-tone """
+        return self._halftone
+
+    @halftone.setter
+    def halftone(self, value: float):
+        self._halftone = value
+
+    @property
+    def threshold(self) -> float:
+        return self._threshold
+
+    @threshold.setter
+    def threshold(self, value: float):
+        self._threshold = value
+
+    @property
+    def spectrum(self) -> float:
+        """Weight of GV for spectrum """
+        return self._spectrum
+
+    @spectrum.setter
+    def spectrum(self, value: float):
+        self._spectrum = value
+
+    @property
+    def logf0(self) -> float:
+        """Weight of GV for log F0 """
+        return self._logf0
+
+    @logf0.setter
+    def logf0(self, value: float):
+        self._logf0 = value
+
+    @property
+    def volume(self) -> float:
+        return self._volume
+
+    @volume.setter
+    def volume(self, value: float):
+        self._volume = value
+
+    @property
+    def buffersize(self) -> int:
+        """Audio buffer size (if i == 0, turn off) """
+        return self._buffersize
+
+    @buffersize.setter
+    def buffersize(self, value: int):
+        self._buffersize = value
+
+    @property
+    def infile(self) -> str:
+        """Input text file """
+        return self._infile
+
+    @infile.setter
+    def infile(self, value: str):
+        self._infile = value
+
+    def build_args(self, *,
+                   command: str = None,
+                   dic: str = None,
+                   voice: str = None,
+                   output: str = None,
+                   trace: str = None,
+                   sampling: int = None,
+                   frameperiod: int = None,
+                   allpass: float = None,
+                   postfilter: float = None,
+                   speedrate: float = None,
+                   halftone: float = None,
+                   threshold: float = None,
+                   spectrum: float = None,
+                   logf0: float = None,
+                   volume: float = None,
+                   buffersize: float = None,
+                   infile: str = None) -> str:
+        """Build command line args """
+
+        cmd = self.command if command is None else command
+        x = self.dictionary if dic is None else dic
+        m = self.voice if voice is None else voice
+        ow = self.output if output is None else output
+        ot = self.trace if trace is None else trace
+        s = self.sampling if sampling is None else sampling
+        p = self.frameperiod if frameperiod is None else frameperiod
+        a = self.allpass if allpass is None else allpass
+        b = self.postfilter if postfilter is None else postfilter
+        r = self.speedrate if speedrate is None else speedrate
+        fm = self.halftone if halftone is None else halftone
+        u = self.threshold if threshold is None else threshold
+        jm = self.spectrum if spectrum is None else spectrum
+        jf = self.logf0 if logf0 is None else logf0
+        g = self.volume if volume is None else volume
+        z = self.buffersize if buffersize is None else buffersize
+        infile = self.infile if infile is None else infile
+        return build_args(
+            cmd, x, m, ow, ot=ot, s=s, p=p, a=a, b=b, r=r, fm=fm, u=u, jm=jm,
+            jf=jf, g=g, z=z, infile=infile)
+
+    def exec(self, *,
+             command: str = None,
+             dic: str = None,
+             voice: str = None,
+             output: str = None,
+             trace: str = None,
+             sampling: int = None,
+             frameperiod: int = None,
+             allpass: float = None,
+             postfilter: float = None,
+             speedrate: float = None,
+             halftone: float = None,
+             threshold: float = None,
+             spectrum: float = None,
+             logf0: float = None,
+             volume: float = None,
+             buffersize: float = None,
+             infile: str = None) -> int:
+        """Exec `open_jtalk` command with given options and return its
+        return code """
+
+        args = self.build_args(
+            command=command, dic=dic, voice=voice, output=output,
+            trace=trace, sampling=sampling, frameperiod=frameperiod,
+            allpass=allpass, postfilter=postfilter, speedrate=speedrate,
+            halftone=halftone, threshold=threshold, spectrum=spectrum,
+            logf0=logf0, volume=volume, buffersize=buffersize, infile=infile)
+        proc = subprocess.run(args)
+        return proc.returncode
+
+    def talk(self, text: str, *,
+             command: str = None,
+             dic: str = None,
+             voice: str = None,
+             trace: str = None,
+             sampling: int = None,
+             frameperiod: int = None,
+             allpass: float = None,
+             postfilter: float = None,
+             speedrate: float = None,
+             halftone: float = None,
+             threshold: float = None,
+             spectrum: float = None,
+             logf0: float = None,
+             volume: float = None,
+             buffersize: float = None) -> bytes:
+        """Generate wave data bytes for given text """
+
+        with tempfile.TemporaryDirectory() as tempdir:
+            output = os.path.join(tempdir, 'a.wav')
+            args = self.build_args(
+                command=command, dic=dic, voice=voice, output=output,
+                trace=trace, sampling=sampling, frameperiod=frameperiod,
+                allpass=allpass, postfilter=postfilter, speedrate=speedrate,
+                halftone=halftone, threshold=threshold, spectrum=spectrum,
+                logf0=logf0, volume=volume, buffersize=buffersize)
+            proc = subprocess.run(args, input=text.encode(ENCODING))
+            if proc.returncode == 0:
+                return mono_to_stereo(output)
+        return None
+
+    async def async_exec(self, *,
+                         command: str = None,
+                         dic: str = None,
+                         voice: str = None,
+                         output: str = None,
+                         trace: str = None,
+                         sampling: int = None,
+                         frameperiod: int = None,
+                         allpass: float = None,
+                         postfilter: float = None,
+                         speedrate: float = None,
+                         halftone: float = None,
+                         threshold: float = None,
+                         spectrum: float = None,
+                         logf0: float = None,
+                         volume: float = None,
+                         buffersize: float = None,
+                         infile: str = None) -> int:
+        """[Coroutine] Exec `open_jtalk` command with given options and
+        return its return code """
+
+        args = self.build_args(
+            command=command, dic=dic, voice=voice, output=output,
+            trace=trace, sampling=sampling, frameperiod=frameperiod,
+            allpass=allpass, postfilter=postfilter, speedrate=speedrate,
+            halftone=halftone, threshold=threshold, spectrum=spectrum,
+            logf0=logf0, volume=volume, buffersize=buffersize, infile=infile)
+        proc = await asyncio.create_subprocess_exec(*args)
+        await proc.communicate()
+        return proc.returncode
+
+    async def async_talk(self, text: str, *,
+                         command: str = None,
+                         dic: str = None,
+                         voice: str = None,
+                         trace: str = None,
+                         sampling: int = None,
+                         frameperiod: int = None,
+                         allpass: float = None,
+                         postfilter: float = None,
+                         speedrate: float = None,
+                         halftone: float = None,
+                         threshold: float = None,
+                         spectrum: float = None,
+                         logf0: float = None,
+                         volume: float = None,
+                         buffersize: float = None) -> bytes:
+        """[Coroutine] Generate wave data bytes for given text """
+
+        with tempfile.TemporaryDirectory() as tempdir:
+            output = os.path.join(tempdir, 'a.wav')
+            args = self.build_args(
+                command=command, dic=dic, voice=voice, output=output,
+                trace=trace, sampling=sampling, frameperiod=frameperiod,
+                allpass=allpass, postfilter=postfilter, speedrate=speedrate,
+                halftone=halftone, threshold=threshold, spectrum=spectrum,
+                logf0=logf0, volume=volume, buffersize=buffersize)
+            proc = await asyncio.create_subprocess_exec(
+                *args, stdin=asyncio.subprocess.PIPE)
+            await proc.communicate(text.encode(ENCODING))
+            if proc.returncode == 0:
+                return mono_to_stereo(output)
+        return None
+
+
+default_agent = Agent(OPEN_JTALK, DICT, VOICE, 'a.wav')
+
+
+def build_args(command: str, x: str, m: str, ow: str, *,
+               ot: str = None,
+               s: int = None,
+               p: int = None,
+               a: float = None,
+               b: float = None,
+               r: float = None,
+               fm: float = None,
+               u: float = None,
+               jm: float = None,
+               jf: float = None,
+               g: float = None,
+               z: int = None,
+               infile: str = None) -> List[str]:
+    """Build open_jtalk command line args. """
+
+    args = [command, '-x', x, '-m', m, '-ow', ow]
+    if ot is not None:
+        args += ['-ot', ot]
+    if s is not None:
+        args += ['-s', str(s)]
+    if p is not None:
+        args += ['-p', str(p)]
+    if a is not None:
+        args += ['-a', str(a)]
+    if b is not None:
+        args += ['-b', str(b)]
+    if r is not None:
+        args += ['-r', str(r)]
+    if fm is not None:
+        args += ['-fm', str(fm)]
+    if u is not None:
+        args += ['-u', str(u)]
+    if jm is not None:
+        args += ['-jm', str(jm)]
+    if jf is not None:
+        args += ['-jf', str(jf)]
+    if g is not None:
+        args += ['-g', str(g)]
+    if z is not None:
+        args += ['-z', str(z)]
+    if infile is not None:
+        args.append(infile)
+    return args
+
+
+def talk(text: str, *,
+         command: str = None,
+         dic: str = None,
+         voice: str = None,
+         trace: str = None,
+         sampling: int = None,
+         frameperiod: int = None,
+         allpass: float = None,
+         postfilter: float = None,
+         speedrate: float = None,
+         halftone: float = None,
+         threshold: float = None,
+         spectrum: float = None,
+         logf0: float = None,
+         volume: float = None,
+         buffersize: float = None) -> bytes:
+    """Generate wave data bytes for given text """
+
+    return default_agent.talk(
+        text, command=command, dic=dic, voice=voice,
+        trace=trace, sampling=sampling, frameperiod=frameperiod,
+        allpass=allpass, postfilter=postfilter, speedrate=speedrate,
+        halftone=halftone, threshold=threshold, spectrum=spectrum,
+        logf0=logf0, volume=volume, buffersize=buffersize)
+
+
+async def async_talk(text: str, *,
+                     command: str = None,
+                     dic: str = None,
+                     voice: str = None,
+                     trace: str = None,
+                     sampling: int = None,
+                     frameperiod: int = None,
+                     allpass: float = None,
+                     postfilter: float = None,
+                     speedrate: float = None,
+                     halftone: float = None,
+                     threshold: float = None,
+                     spectrum: float = None,
+                     logf0: float = None,
+                     volume: float = None,
+                     buffersize: float = None) -> bytes:
+    """[Coroutine] Generate wave data bytes for given text """
+
+    return await default_agent.async_talk(
+        text, command=command, dic=dic, voice=voice,
+        trace=trace, sampling=sampling, frameperiod=frameperiod,
+        allpass=allpass, postfilter=postfilter, speedrate=speedrate,
+        halftone=halftone, threshold=threshold, spectrum=spectrum,
+        logf0=logf0, volume=volume, buffersize=buffersize)
+
+
+def mono_to_stereo(file: str) -> bytes:
+    """Return stereo converted wave data from a monaural wave file. """
+
+    with io.BytesIO() as stream, \
+      wave.open(file, 'rb') as wi, \
+      wave.open(stream, 'wb') as wo:
+        wo.setnchannels(2)
+        wo.setsampwidth(wi.getsampwidth())
+        wo.setframerate(wi.getframerate())
+        nframes = wi.getnframes()
+        wo.setnframes(nframes)
+        gen_frames = (wi.readframes(1) for _ in range(nframes))
+        [wo.writeframesraw(f * 2) for f in gen_frames]
+        return stream.getvalue()
+
+
+def main():
+    data = talk('おやすみなさい')
+    with open('a.wav', 'wb') as f:
+        f.write(data)
+
+
+if __name__ == "__main__":
+    main()
