d219fe8c2079637e003067a6d0ec8e7ad2358a7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:17 +0000

    Add package aiworkflow/0.0.4/aiworkflow-0.0.4.tar.gz

---
+++ packages/aiworkflow/aiworkflow-0.0.4.tar.gz/aiworkflow-0.0.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="aiworkflow",
+    version="0.0.4",
+    author="Pyxeda AI",
+    author_email="support@pyxeda.ai",
+    description="Python Client to access Navigator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://navigator.pyxeda.ai",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
