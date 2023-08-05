79dc9b3b858cc81dbf31f2f6242eda7be904e95f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:24 +0000

    Add package LrnXPAnaToolbox/0.3.1/LrnXPAnaToolbox-0.3.1-py2.py3-none-any.whl

---
+++ packages/LrnXPAnaToolbox/LrnXPAnaToolbox-0.3.1-py2.py3-none-any.whl/LrnXPAnaToolbox/__init__.py
@@ -0,0 +1,12 @@
+"""Top-level package for UQDKR LrnXPAnaToolbox."""
+
+__author__ = """Kayané Elmayan Robach"""
+__email__ = 'kaya.robach@gmail.com'
+__version__ = '0.3.1'
+
+from .LrnXPAnaToolbox import LrnXPAnaToolbox
+from .LrnXPAnaToolbox import lib
+from .LrnXPAnaToolbox import cluster_students
+from .LrnXPAnaToolbox import create_marks
+from .LrnXPAnaToolbox import recommender
+from .LrnXPAnaToolbox import transform_data
