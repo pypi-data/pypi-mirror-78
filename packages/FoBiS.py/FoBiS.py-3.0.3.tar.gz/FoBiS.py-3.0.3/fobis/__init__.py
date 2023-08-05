2cc9fded5eaa375f2c808a980136a80083b135ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:14:33 +0000

    Add package FoBiS.py/3.0.3/FoBiS.py-3.0.3.tar.gz

---
+++ packages/FoBiS.py/FoBiS.py-3.0.3.tar.gz/FoBiS.py-3.0.3/fobis/__init__.py
@@ -0,0 +1,25 @@
+"""FoBiS.py main package"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+# Copyright (C) 2015  Stefano Zaghi
+#
+# This file is part of FoBiS.py.
+#
+# FoBiS.py is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# FoBiS.py is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with FoBiS.py. If not, see <http://www.gnu.org/licenses/>.
+
+from future import standard_library
+standard_library.install_aliases()
+from builtins import *
