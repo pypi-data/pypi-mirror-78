a91d26daade591a8e13b811c2d67c440208abd14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:48 +0000

    Add package StockerDataframe/0.3/StockerDataframe-0.3.tar.gz

---
+++ packages/StockerDataframe/StockerDataframe-0.3.tar.gz/StockerDataframe-0.3/setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup
+setup(name="StockerDataframe", 
+    version="0.3", 
+    description="Small description about the package",
+    long_description="Long description about the package",author="Pranjal", packages=['StockerDataframe'],install_requires=[])
