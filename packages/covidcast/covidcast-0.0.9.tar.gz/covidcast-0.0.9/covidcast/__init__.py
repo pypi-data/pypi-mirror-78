c2f4b211dd87c07e86e4e2f49645b65429baa7b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:14 +0000

    Add package covidcast/0.0.9/covidcast-0.0.9.tar.gz

---
+++ packages/covidcast/covidcast-0.0.9.tar.gz/covidcast-0.0.9/covidcast/__init__.py
@@ -0,0 +1,16 @@
+"""Fetch data from Delphi's COVIDcast API.
+
+The COVIDcast API provides daily updated data on the COVID-19 pandemic in the
+United States, including cases, deaths, medical records, nationwide symptom
+surveys, and other data collated by the Delphi research group at Carnegie Mellon
+University.
+
+Functions:
+
+* signal - Fetch a Pandas data frame for one signal.
+* metadata - Fetch metadata for all available signals.
+
+"""
+
+from .covidcast import signal, metadata
+from .plotting import plot_choropleth, get_geo_df
