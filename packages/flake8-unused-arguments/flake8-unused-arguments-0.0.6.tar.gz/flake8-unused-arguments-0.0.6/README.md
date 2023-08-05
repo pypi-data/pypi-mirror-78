18d214db612e40fc3af77d58d78f0ab9e00bc011
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:10 +0000

    Add package flake8-unused-arguments/0.0.6/flake8-unused-arguments-0.0.6.tar.gz

---
+++ packages/flake8-unused-arguments/flake8-unused-arguments-0.0.6.tar.gz/flake8-unused-arguments-0.0.6/README.md
@@ -0,0 +1,35 @@
+# flake8-unused-arguments
+
+A flake8 plugin that checks for unused function arguments.
+
+This package adds the following warnings:
+
+ - `U100` - An unused argument.
+ - `U101` - An unused argument starting with an underscore
+
+Configuration options also exist:
+ - `unused-arguments-ignore-abstract-functions` - don't show warnings for abstract functions.
+ - `unused-arguments-ignore-stub-functions` - don't show warnings for empty functions.
+ - `unused-arguments-ignore-variadic-names` - don't show warnings for unused *args and **kwargs.
+
+
+## Changelog
+
+0.0.6
+ - Stub functions that have docstrings are now correctly detected as stub functions
+ - Functions with only a docstring are considered stub functions
+
+0.0.5
+ - The positions reported are now for the arguments themselves, rather than the function
+
+0.0.4
+ - Wrong project name in the readme, whoopsies
+
+0.0.3
+ - treat functions that start with "raise NotImplementedError()" as stub functions
+
+0.0.2
+ - fixed error in packaging
+
+0.0.1
+ - initial release
---
+++ packages/flake8-unused-arguments/flake8-unused-arguments-0.0.6.tar.gz/flake8-unused-arguments-0.0.6/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+requires = [
+    "flake8 > 3.0.0",
+]
+
+setup(
+    name="flake8-unused-arguments",
+    license="MIT",
+    version="0.0.6",
+    description="flake8 extension to warn on unused function arguments",
+    author="Nathan Hoad",
+    author_email="nathan@hoad.io",
+    py_modules=["flake8_unused_arguments"],
+    url="https://github.com/nhoad/flake8-unused-arguments",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    install_requires=requires,
+    entry_points={
+        "flake8.extension": ["U10 = flake8_unused_arguments:Plugin"],
+    },
+    classifiers=[
+        "Framework :: Flake8",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Quality Assurance",
+    ],
+)
