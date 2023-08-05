7c528e3d632d6bcc299419536c517ad56c1e6bd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:49 +0000

    Add package datastore-dnnevo-client/0.1.1/datastore_dnnevo_client-0.1.1.tar.gz

---
+++ packages/datastore-dnnevo-client/datastore_dnnevo_client-0.1.1.tar.gz/datastore_dnnevo_client-0.1.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="datastore_dnnevo_client", # Replace with your own username
+    version="0.1.1",
+    author="mereszeta",
+    author_email="grzegorzszeremeta@gmail.com",
+    description="Client for datastore",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    install_requires = [
+        'requests'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
