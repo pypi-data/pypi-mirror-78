44fc5f28cc8ceb012cd612c42f6fe91d744ff28a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:50 +0000

    Add package rayou.cdk-url-shortener/0.1.0/rayou.cdk-url-shortener-0.1.0.tar.gz

---
+++ packages/rayou.cdk-url-shortener/rayou.cdk-url-shortener-0.1.0.tar.gz/rayou.cdk-url-shortener-0.1.0/src/rayou/cdk_url_shortener/_jsii/__init__.py
@@ -0,0 +1,33 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_apigateway._jsii
+import aws_cdk.aws_certificatemanager._jsii
+import aws_cdk.aws_dynamodb._jsii
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.aws_lambda_event_sources._jsii
+import aws_cdk.aws_route53._jsii
+import aws_cdk.aws_route53_targets._jsii
+import aws_cdk.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@rayou/cdk-url-shortener",
+    "0.1.0",
+    __name__[0:-6],
+    "cdk-url-shortener@0.1.0.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
