5d843da04373e290ac44d4f6921c2f85083a41be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:09 +0000

    Add package tuck/0.0.10/tuck-0.0.10.tar.gz

---
+++ packages/tuck/tuck-0.0.10.tar.gz/tuck-0.0.10/setup.py
@@ -0,0 +1,41 @@
+import os.path
+
+from setuptools import setup, find_packages  # type: ignore[import]
+
+my_dir = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(my_dir, 'README.md')) as f:
+    long_description = f.read()
+
+setup(
+    name='tuck',
+    version='0.0.10',
+    url='https://github.com/PeterJCLaw/tuck',
+    project_urls={
+        'Issue tracker': 'https://github.com/PeterJCLaw/tuck/issues',
+    },
+    description="Semi-automated Python formatting.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+    packages=find_packages(exclude=['tests']),
+
+    author="Peter Law",
+    author_email="PeterJCLaw@gmail.com",
+
+    classifiers=(
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development',
+    ),
+
+    install_requires=(
+        'asttokens',
+    ),
+)
