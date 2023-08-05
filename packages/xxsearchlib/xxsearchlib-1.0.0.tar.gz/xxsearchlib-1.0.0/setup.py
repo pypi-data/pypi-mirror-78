84ad7ebf49c5aede50ebb4d3f6af67d80ca8ed4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:44 +0000

    Add package xxsearchlib/1.0.0/xxsearchlib-1.0.0.tar.gz

---
+++ packages/xxsearchlib/xxsearchlib-1.0.0.tar.gz/xxsearchlib-1.0.0/setup.py
@@ -0,0 +1,16 @@
+import  setuptools
+
+setuptools.setup(
+    name="xxsearchlib",
+    version="1.0.0",
+    author="轩轩",
+    author_email="3603695237@qq.com",
+    description="轩轩搜索引擎库，由陈逸轩和郭燕铭制作，版权所有，禁止抄袭",
+    long_desription_content_type="text/markdown",
+    url="https://github.com/cyx200902/xxsearchlib",
+    classmethod=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
