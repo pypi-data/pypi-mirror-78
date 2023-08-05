b7061eb1a52b092f271afd7a0b4e721d27817013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:36 +0000

    Add package Rackio/0.7.1/Rackio-0.7.1-py3-none-any.whl

---
+++ packages/Rackio/Rackio-0.7.1-py3-none-any.whl/rackio/events.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""rackio/events.py
+
+This module defines the events history classes and function.
+"""
+
+from .models import Model, IntegerField, StringField
+
+
+class Event(Model):
+
+    user = StringField()
+    message = StringField()
+    description = StringField(default="")
+    classification = StringField()
+    priority = IntegerField(default=4)
+    date_time = StringField()
+    
