b71b85d501f266675f24a0305c8028b5dd31d147
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:18 +0000

    Add package botpunk/0.3/botpunk-0.3.tar.gz

---
+++ packages/botpunk/botpunk-0.3.tar.gz/botpunk-0.3/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+setup(
+  name = 'botpunk',         
+  packages = ['BOTPUNK'],   
+  version = '0.3',      
+  license='MIT',        
+  description = 'A bot that solves quizzes on jetpunk.com for you',   
+  author = 'Vincent WENDLING',                   
+  author_email = 'vincent.wendling@lilo.org',     
+  url = 'https://github.com/TisaneFruitRouge/botpunk',   
+  download_url = 'https://github.com/TisaneFruitRouge/botpunk/archive/v_03.tar.gz',    
+  keywords = ['BOT', 'QUIZZES', 'JETPUNK'],   
+  install_requires=[            
+          'selenium',
+          'beautifulsoup4',
+          'requests',
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3.8',
+  ],
+)
