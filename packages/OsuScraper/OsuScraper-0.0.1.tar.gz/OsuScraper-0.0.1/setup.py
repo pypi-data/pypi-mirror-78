b155c4fb8784a563c0bc9356830653746674407b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:53 +0000

    Add package OsuScraper/0.0.1/OsuScraper-0.0.1.tar.gz

---
+++ packages/OsuScraper/OsuScraper-0.0.1.tar.gz/OsuScraper-0.0.1/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="OsuScraper",
+    version="0.0.1",
+    author="imnsomia",
+    author_email="killuasec@memeware.net",
+    description="Simple API to scrape in Osu website",
+    long_description_content_type="text/markdown",
+    url="https://github.com/imnsomia/Osu-Scraper",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+)
