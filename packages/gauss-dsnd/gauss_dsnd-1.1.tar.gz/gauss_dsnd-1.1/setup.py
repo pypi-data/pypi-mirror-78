2dc6f215c0efe26264a62604fb7038c47d03832b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:18 +0000

    Add package gauss-dsnd/1.1/gauss_dsnd-1.1.tar.gz

---
+++ packages/gauss-dsnd/gauss_dsnd-1.1.tar.gz/gauss_dsnd-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gauss_dsnd',
+      version='1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gauss_dsnd'],
+      author="Vikram Shinde",
+      author_email="shindevikram1210@gmail.com",
+      zip_safe=False)
