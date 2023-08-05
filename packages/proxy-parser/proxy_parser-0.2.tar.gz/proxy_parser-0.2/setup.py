07cf6433e773fb1825271490a054a2631ad29de2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:58 +0000

    Add package proxy-parser/0.2/proxy_parser-0.2.tar.gz

---
+++ packages/proxy-parser/proxy_parser-0.2.tar.gz/proxy_parser-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='proxy_parser',
+      version='0.2',
+      description='by @bu77h4ad',
+      packages=['proxy_parser'],
+      author_email='razor-men@mail.ru',
+      zip_safe=False)
