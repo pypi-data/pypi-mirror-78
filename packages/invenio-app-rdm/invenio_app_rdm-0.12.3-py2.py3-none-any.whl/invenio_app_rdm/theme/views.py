543e683e410bcd62a244b43ae27141796f5ef682
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:38 +0000

    Add package invenio-app-rdm/0.12.3/invenio_app_rdm-0.12.3-py2.py3-none-any.whl

---
+++ packages/invenio-app-rdm/invenio_app_rdm-0.12.3-py2.py3-none-any.whl/invenio_app_rdm/theme/views.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019-2020 CERN.
+# Copyright (C) 2019-2020 Northwestern University.
+#
+# Invenio App RDM is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Blueprint used for loading templates.
+
+The sole purpose of this blueprint is to ensure that Invenio can find the
+templates and static files located in the folders of the same names next to
+this file.
+"""
+
+from flask import Blueprint, current_app, render_template
+from flask_menu import current_menu
+from invenio_pidstore.models import PIDStatus
+from invenio_pidstore.providers.recordid_v2 import RecordIdProviderV2
+from invenio_rdm_records.marshmallow.json import MetadataSchemaV1, dump_empty
+from invenio_rdm_records.resources import BibliographicDraftActionResource, \
+    BibliographicDraftResource, BibliographicRecordResource
+from invenio_rdm_records.services import BibliographicRecordService
+from invenio_rdm_records.vocabularies import Vocabularies
+
+blueprint = Blueprint(
+    'invenio_app_rdm',
+    __name__,
+    template_folder='templates',
+    static_folder='static',
+)
+
+
+@blueprint.before_app_first_request
+def init_menu():
+    """Initialize menu before first request."""
+    item = current_menu.submenu('main.deposit')
+    item.register(
+        'invenio_app_rdm.deposits_user',
+        'Uploads',
+        order=1
+    )
+
+
+@blueprint.route('/search')
+def search():
+    """Search page."""
+    return render_template(current_app.config['SEARCH_BASE_TEMPLATE'])
+
+
+@blueprint.route('/deposits/new')
+def deposits_create():
+    """Record creation page."""
+    forms_config = dict(
+        apiUrl='/api/records/',
+        vocabularies=Vocabularies.dump()
+    )
+    searchbar_config = dict(searchUrl='/search')
+    empty_record = dump_empty(MetadataSchemaV1)
+    return render_template(
+        current_app.config['DEPOSITS_FORMS_BASE_TEMPLATE'],
+        forms_config=forms_config,
+        record=empty_record,
+        searchbar_config=searchbar_config
+    )
+
+
+@blueprint.route('/deposits/<string:id>/edit')
+def deposits_edit(id):
+    """Fake deposits edit page."""
+    forms_config = dict(
+        apiUrl='/api/records/',
+        vocabularies=Vocabularies.dump()
+    )
+    # minimal record
+    record = {
+        "_access": {
+            "metadata_restricted": False,
+            "files_restricted": False
+        },
+        "_owners": [1],
+        "_created_by": 1,
+        "access_right": "open",
+        "id": "{}".format(id),
+        "resource_type": {
+            "type": "image",
+            "subtype": "image-photo"
+        },
+        # Technically not required
+        "creators": [],
+        "titles": [{
+            "title": "A Romans story",
+            "type": "Other",
+            "lang": "eng"
+        }],
+        "links": {
+            "edit": "/deposits/{}/edit".format(id)
+        }
+    }
+    searchbar_config = dict(searchUrl='/search')
+
+    initial_record = dump_empty(MetadataSchemaV1)
+    initial_record.update(record)
+    return render_template(
+        current_app.config['DEPOSITS_FORMS_BASE_TEMPLATE'],
+        forms_config=forms_config,
+        record=initial_record,
+        searchbar_config=searchbar_config
+    )
+
+
+@blueprint.route('/deposits')
+def deposits_user():
+    """List of user deposits page."""
+    return render_template(
+        current_app.config['DEPOSITS_UPLOADS_TEMPLATE'],
+        searchbar_config=dict(searchUrl='/search')
+    )
+
+
+record_draft_service = BibliographicRecordService()
+record_bp = BibliographicRecordResource(
+    service=record_draft_service
+).as_blueprint("bibliographic_record_resource")
+draft_bp = BibliographicDraftResource(
+    service=record_draft_service
+).as_blueprint("bibliographic_draft_resource")
+draft_action_bp = BibliographicDraftActionResource(
+    service=record_draft_service
+).as_blueprint("bibliographic_draft_action_resource")
