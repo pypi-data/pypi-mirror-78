68c45dc143fb7c3bda818b50c39cfad0faac1952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:29 +0000

    Add package crf-tagger/0.2.1/crf_tagger-0.2.1.tar.gz

---
+++ packages/crf-tagger/crf_tagger-0.2.1.tar.gz/crf_tagger-0.2.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['crf_tagger']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['python-crfsuite', 'tabulate', 'tqdm']
+
+setup_kwargs = {
+    'name': 'crf-tagger',
+    'version': '0.2.1',
+    'description': 'CRF tagger',
+    'long_description': None,
+    'author': 'severinsimmler',
+    'author_email': 's.simmler@snapaddy.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
