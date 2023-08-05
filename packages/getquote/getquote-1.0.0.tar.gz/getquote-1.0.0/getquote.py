87ddd393009780ef0799d5d1afe7801c8b3dbedb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:55 +0000

    Add package getquote/1.0.0/getquote-1.0.0.tar.gz

---
+++ packages/getquote/getquote-1.0.0.tar.gz/getquote-1.0.0/getquote.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""getquote script for ledger-cli."""
+
+import configparser
+import sys
+from urllib import error
+from urllib import request
+from datetime import datetime
+
+cfg = configparser.ConfigParser()
+cfg.read_file(open('getquote.ini'))
+IEX_TOKEN = cfg.get('IEX', 'TOKEN')
+
+def quote(symbol: str) -> int:
+    """Returns the current price of a security.
+
+    Args:
+        symbol: security to quote.
+    Returns:
+        Price of the security.
+    """
+    url = f'https://cloud.iexapis.com/stable/stock/{symbol}/quote/latestPrice?token={IEX_TOKEN}'
+    try:
+        with request.urlopen(url) as resp:
+            return resp.read().decode('utf-8')
+    except error.HTTPError as exp:
+        raise LookupError(f'Unable to quote "{symbol}"') from exp
+
+def ledger_format(symbol: str) -> str:
+    """Returns a quote formated for ledger-cli.
+
+    Args:
+      symbol: security to quote.
+    Returns:
+      Ledger-Cli Price db entry for the quote.
+    """
+    date = datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+    price = quote(symbol)
+    return f'{date} {symbol} ${price}'
+
+def main():
+    """Entrypoint for getquote."""
+    symbol = sys.argv[1]
+    print(ledger_format(symbol))
+
+if __name__ == '__main__':
+    main()
---
+++ packages/getquote/getquote-1.0.0.tar.gz/getquote-1.0.0/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name='getquote',
+    version='1.0.0',
+    scripts=['getquote.py'],
+    entry_points={
+        'console_scripts': [
+            'getquote = getquote:main',
+        ],
+    },
+    author='Jean-Ralph Aviles',
+    author_email='jeanralph.aviles@gmail.com',
+    description='A ledger-cli getquote implementation using free IEX Cloud API',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    keywords=' '.join([
+        'ledger-cli', 'personal', 'finance', 'stocks',
+    ]),
+    url='https://github.com/jeanralphaviles/getquote',
+    license='MIT',
+)
