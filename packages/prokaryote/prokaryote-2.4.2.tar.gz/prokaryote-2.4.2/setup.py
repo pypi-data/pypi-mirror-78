6eb7dca17a93d755901ce279b35ed329d0b362e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:22 +0000

    Add package prokaryote/2.4.2/prokaryote-2.4.2.tar.gz

---
+++ packages/prokaryote/prokaryote-2.4.2.tar.gz/prokaryote-2.4.2/setup.py
@@ -0,0 +1,54 @@
+import codecs
+import os
+import re
+
+import setuptools.dist
+
+
+def read(*directories):
+    pathname = os.path.abspath(os.path.dirname(__file__))
+
+    return codecs.open(os.path.join(pathname, *directories), "r").read()
+
+
+def find_version(*pathnames):
+    data = read(*pathnames)
+
+    matched = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", data, re.M)
+
+    if matched:
+        return matched.group(1)
+
+    raise RuntimeError("Unable to find version string.")
+
+
+__version__ = find_version("prokaryote", "__init__.py")
+
+setuptools.setup(
+    author="Allen Goodman",
+    author_email="allen.goodman@icloud.com",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Java",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Topic :: Scientific/Engineering :: Image Recognition",
+        "Topic :: Scientific/Engineering"
+    ],
+    license="BSD",
+    name="prokaryote",
+    package_data={
+        "prokaryote": [
+            "prokaryote-{}.jar".format(__version__)
+        ]
+    },
+    packages=setuptools.find_packages(),
+    python_requires=">=2.7, <4",
+    url="https://github.com/CellProfiler/prokaryote",
+    version=__version__,
+    zip_safe=False
+)
