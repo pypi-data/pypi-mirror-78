118e53a49dc78ba5cb234412525cc431da4efed2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:51 +0000

    Add package osahon-distributions/0.1/osahon_distributions-0.1.tar.gz

---
+++ packages/osahon-distributions/osahon_distributions-0.1.tar.gz/osahon_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='osahon_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['osahon_distributions'],
+      zip_safe=False)
