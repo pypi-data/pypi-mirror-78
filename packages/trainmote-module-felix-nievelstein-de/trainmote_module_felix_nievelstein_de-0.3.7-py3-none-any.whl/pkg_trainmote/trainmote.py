163def486f76eddbf2cbe25c1126800f90781cfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:23 +0000

    Add package trainmote-module-felix-nievelstein-de/0.3.7/trainmote_module_felix_nievelstein_de-0.3.7-py3-none-any.whl

---
+++ packages/trainmote-module-felix-nievelstein-de/trainmote_module_felix_nievelstein_de-0.3.7-py3-none-any.whl/pkg_trainmote/trainmote.py
@@ -0,0 +1,65 @@
+from . import gpioservice
+from flask import Flask
+from flask import jsonify
+from .powerControllerModule import PowerThread
+from .configControllerModule import ConfigController
+from . import stateControllerModule
+from .libInstaller import LibInstaller
+from subprocess import call
+import logging
+import logging.handlers
+import argparse
+import sys
+import os
+import time
+
+gpioservice.setup()
+gpioservice.loadInitialData()
+stateController = stateControllerModule.StateController()
+powerThread = PowerThread()
+client_sock = None
+config = ConfigController()  
+app = Flask(__name__)
+
+def loadPersistentData():
+    if config.loadPreferences():
+        if not config.isSQLiteInstalled():
+            libInstaller = LibInstaller()
+            libInstaller.installSQLite()
+            if config.setSQLiteInstalled():
+                restart()
+            else: 
+                shutDown()
+
+def main(): 
+    print("Start webserver")
+    app.run(debug=True, host="0.0.0.0")
+    
+@app.route('/')
+def hello_world():
+    return jsonify(result='Hello World')
+
+@app.route('trainmote/api/v1/switch/<switch_id>', methods=["GET", "PATCH"])
+def switch(switch_id):
+    if request.method == "PATCH":
+        return gpioservice.setSwitch(switch_id)
+    else:
+        return gpioservice.getSwitch(switch_id)
+  
+def restart():
+    shutDown()
+    os.execv(sys.executable, ['python'] + sys.argv)
+
+def shutDown():
+    powerThread.kill.set()
+    powerThread.isTurningOff = True
+    powerThread.join()
+    stateController.setState(stateControllerModule.STATE_SHUTDOWN)
+    print ("Server going down")
+    stateController.stop()
+
+def closeClientConnection():
+    print ("Closing client socket")
+
+if __name__ == '__main__':
+    main()
