ecf7b9aa1a75ea26baf70755c44d70ecd4e933fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:55 +0000

    Add package krishnacalci/0.0.1/krishnacalci-0.0.1.tar.gz

---
+++ packages/krishnacalci/krishnacalci-0.0.1.tar.gz/krishnacalci-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+ 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+ 
+setuptools.setup(
+    name="krishnacalci",
+    version="0.0.1",
+    author="krishna",
+    author_email="cloud@cloudstones.org",
+    description="my description",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://cloudstones.org",
+    py_modules=["calc"],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
