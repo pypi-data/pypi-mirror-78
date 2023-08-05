ff301603f7e4e3e1f9a5064a11e177d8dd75cd40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:19 +0000

    Add package edgar/5.4.0/edgar-5.4.0-py3-none-any.whl

---
+++ packages/edgar/edgar-5.4.0-py3-none-any.whl/edgar/document.py
@@ -0,0 +1,29 @@
+import requests
+from lxml import html
+
+class Document:
+
+  def __init__(self, url, timeout=10):
+    self.url = url
+    self.text = requests.get(self.url, timeout=timeout).content
+
+class Documents(str):
+
+  def __get_text_from_list__(self, arr):
+    return [val.text_content() for val in arr]
+
+  def __init__(self, url, timeout=10):
+    self.url = url
+    page = requests.get(self.url, timeout=timeout)
+    tree = html.fromstring(page.content)
+    content = tree.find_class("formContent")[0]
+    info_head = self.__get_text_from_list__(content.find_class("infoHead"))
+    info = self.__get_text_from_list__(content.find_class("info"))
+    self.content = dict(zip(info_head, info))
+    self.element = html.fromstring(requests.get(self.url, timeout=timeout).content)
+
+  def __repr__(self):
+    return str(self.__dict__)
+
+  def __str__(self):
+    return str(self.__dict__)
