9486816f91fd933f82249bcdaa867e14f49b0b4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:39 +0000

    Add package nbdev/1.0.10/nbdev-1.0.10.tar.gz

---
+++ packages/nbdev/nbdev-1.0.10.tar.gz/nbdev-1.0.10/nbdev/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "1.0.10"
+
+from fastcore.utils import IN_IPYTHON
+
+if IN_IPYTHON:
+    from .flags import *
+    from .showdoc import show_doc
+    #from .export import notebook2script
