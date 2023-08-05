b37761752687893c0883315bc823cd0a24bd0650
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:35 +0000

    Add package ProgettiHWSW/0.0.16/ProgettiHWSW-0.0.16.tar.gz

---
+++ packages/ProgettiHWSW/ProgettiHWSW-0.0.16.tar.gz/ProgettiHWSW-0.0.16/ProgettiHWSW/api.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2020 Arda Seremet <ardaseremet@outlook.com>
+
+import aiohttp
+import asyncio
+import async_timeout
+
+class API:
+	"""Class to interact with the API of ProgettiHWSW boards."""
+
+	def __init__(self, ip: str):
+		"""Initialize the API."""
+		self.ip = ip
+	
+	async def request(self, path: str):
+		with async_timeout.timeout(5):
+			async with aiohttp.request("GET", f"{self.ip}/{path}") as resp:
+				return await resp.text()
+
+	async def execute(self, code: int):
+		"""Make requests with API codes for boards."""
+		try:
+			return await self.request(f"index.htm?execute={code}")
+		except:
+			return False
