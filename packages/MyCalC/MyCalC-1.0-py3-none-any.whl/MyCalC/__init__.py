9a02973c9b35d3d5400b1133bde9b5972f50ed3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:14:21 +0000

    Add package MyCalC/1.0/MyCalC-1.0-py3-none-any.whl

---
+++ packages/MyCalC/MyCalC-1.0-py3-none-any.whl/MyCalC/__init__.py
@@ -0,0 +1,12 @@
+def addnumbers(num1,num2):
+	return num1+num2
+
+def substractnumbers(num1,num2):
+	return num1-num2
+
+def multiplynumbers(num1,num2):
+	return num1*num2
+
+def devision(num1,num2):
+	return num1/num2
+
