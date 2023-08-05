942460f8acb89f4078057444b65a1cbc2c161762
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:41 +0000

    Add package email-sender/1.2/email_sender-1.2.tar.gz

---
+++ packages/email-sender/email_sender-1.2.tar.gz/email_sender-1.2/email_sender/Email.py
@@ -0,0 +1,19 @@
+import smtplib
+from email_sender import Mail
+
+class Email:
+    def __init__(self, server, username, password):
+        self.username = username
+        self.server = smtplib.SMTP(server, 587)
+        self.server.ehlo()
+        self.server.starttls()
+        self.server.login(self.username, password)
+
+    def send(self, email: Mail):
+        self.server.sendmail(self.username, email.to, email.mail)
+
+if __name__ == '__main__':
+    email = Mail('example@gmail.com', 'example@example.com', 'Hey how are you!', 'Hello there :)')
+
+    gmail = Email('smtp.gmail.com', 'example@gmail.com', 'password123')
+    gmail.send(email)
---
+++ packages/email-sender/email_sender-1.2.tar.gz/email_sender-1.2/email_sender/__init__.py
@@ -0,0 +1,2 @@
+from email_sender.Email import Email
+from email_sender.Mail import Mail
