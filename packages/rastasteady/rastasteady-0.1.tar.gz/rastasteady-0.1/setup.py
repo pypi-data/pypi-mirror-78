8aa082b4e7684920fbb0cd735d37702ef905e7ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:43 +0000

    Add package rastasteady/0.1/rastasteady-0.1.tar.gz

---
+++ packages/rastasteady/rastasteady-0.1.tar.gz/rastasteady-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rastasteady',
+    version='0.1',
+    description='',
+    long_description='',
+    install_requires=[
+        'pyfiglet',
+    ],
+    packages=find_packages(),
+    zip_safe=False,
+)
