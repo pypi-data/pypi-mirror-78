d0c6bd76b1f7d95927bd2128fda6ca12a7089015
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:42 +0000

    Add package pytaku/0.3.7/pytaku-0.3.7-py3-none-any.whl

---
+++ packages/pytaku/pytaku-0.3.7-py3-none-any.whl/pytaku/conf.py
@@ -0,0 +1,12 @@
+from secrets import token_urlsafe
+
+from goodconf import GoodConf, Value
+
+
+class Config(GoodConf):
+    MANGADEX_USERNAME = Value()
+    MANGADEX_PASSWORD = Value()
+    FLASK_SECRET_KEY = Value(initial=lambda: token_urlsafe(50))
+
+
+config = Config(default_files=["pytaku.conf.json"])
