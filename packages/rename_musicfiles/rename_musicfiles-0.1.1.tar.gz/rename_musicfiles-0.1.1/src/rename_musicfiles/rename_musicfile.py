4baedbca06684f03c2573a62cf6c6e28c367716e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:08 +0000

    Add package rename_musicfiles/0.1.1/rename_musicfiles-0.1.1.tar.gz

---
+++ packages/rename_musicfiles/rename_musicfiles-0.1.1.tar.gz/rename_musicfiles-0.1.1/src/rename_musicfiles/rename_musicfile.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from phrydy import MediaFile
+from pathlib import Path
+import sys
+
+
+def main():
+    for x in sys.argv[1:]:
+        file = Path(x)
+        try:
+            f = MediaFile(file)
+        except Exception as e:
+            print(f"WARN: Skipping '{x}' because of: {str(e)}")
+            continue
+        new = Path(
+            f"{f.artist}/(-{f.year}-) {f.album}/{f.track:02d} - {f.artist} - {f.title}.{file.name.rsplit('.', 1)[-1]}"
+        )
+        new.parent.mkdir(exist_ok=True, parents=True)
+        file.rename(new)
+
+
+if __name__ == "__main__":
+    main()
