89d2b021ae332b240707251942ddb52404ae47cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:59 +0000

    Add package verse16/0.1.2/verse16-0.1.2.tar.gz

---
+++ packages/verse16/verse16-0.1.2.tar.gz/verse16-0.1.2/setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    "tensorflow-gpu<=1.15.3",
+    "progressbar2",
+    "pronouncing",
+    "syllables",
+    "gpt-2-simple",
+    "progressist"
+]
+
+setup_requirements = []
+
+test_requirements = []
+
+setup(
+    author="pelgo14",
+    author_email='pelgo14@protonmail.com',
+    python_requires='>=3.5',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    description="nlp rap text-generation",
+    entry_points={
+        'console_scripts': [
+            'verse16=verse16.cli:main',
+        ],
+    },
+    install_requires=requirements,
+    license="BSD license",
+    long_description=readme + '\n\n' + history,
+    long_description_content_type='text/x-rst',
+    include_package_data=True,
+    keywords='verse16 nlp rap text-generation',
+    name='verse16',
+    packages=find_packages(include=['verse16', 'verse16.*']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/pelgo14/verse16',
+    version='0.1.2',
+    zip_safe=False,
+)
---
+++ packages/verse16/verse16-0.1.2.tar.gz/verse16-0.1.2/tests/test_verse16.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `verse16` package."""
+
+
+import unittest
+
+from verse16 import verse16
+
+
+class TestVerse16(unittest.TestCase):
+    """Tests for `verse16` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
