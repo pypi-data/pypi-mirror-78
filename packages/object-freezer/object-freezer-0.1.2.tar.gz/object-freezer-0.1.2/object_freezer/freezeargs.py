1d01fb56179e4027a212a85ee6fa00b648a2e64f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:15:19 +0000

    Add package object-freezer/0.1.2/object-freezer-0.1.2.tar.gz

---
+++ packages/object-freezer/object-freezer-0.1.2.tar.gz/object-freezer-0.1.2/object_freezer/freezeargs.py
@@ -0,0 +1,21 @@
+# Based on  https://stackoverflow.com/questions/6358481/using-functools-lru-cache-with-dictionary-arguments/53394430#53394430
+
+import functools
+from .core import freeze
+
+__all__ = [
+    'freezeargs'
+]
+
+def freezeargs(func):
+    '''
+    Transform mutable arguments into immutable.
+    Useful to be compatible with lru_cache
+    '''
+
+    @functools.wraps(func)
+    def wrapped(*args, **kwargs):
+        args = tuple(freeze(arg) for arg in args)
+        kwargs = {k: freeze(v) for k, v in kwargs.items()}
+        return func(*args, **kwargs)
+    return wrapped
---
+++ packages/object-freezer/object-freezer-0.1.2.tar.gz/object-freezer-0.1.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(
+    name='object-freezer',
+    version='0.1.2',
+    description='Python module for freezing objects',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    author='Shunichiro Nomura',
+    author_email='nomura@space.t.u-tokyo.ac.jp',
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+    url='https://github.com/shunichironomura/freezer',
+    download_url='https://github.com/shunichironomura/object-freezer/archive/v0.1.1.tar.gz',
+    license='MIT',
+    packages=find_packages(exclude=('tests', 'docs')),
+    test_suite='tests',
+    zip_safe=False)
