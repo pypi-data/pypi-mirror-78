94ecd2ed186596efcb93c7e5c18774770f6b3614
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:52 +0000

    Add package distributions-testing-peter-lach/1.0/distributions_testing_peter_lach-1.0.tar.gz

---
+++ packages/distributions-testing-peter-lach/distributions_testing_peter_lach-1.0.tar.gz/distributions_testing_peter_lach-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_testing_peter_lach',
+      version='1.0',
+      description='My Testing Of Package Upload - Gaussian distributions',
+      packages=['distributions'],
+      author='Udacity student Peter Lach',
+      author_email = 'peter.lach@cbbsoft.com',
+      zip_safe=False)
