6e60ad1d2f812657248f8d8d774f969069c93fd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:57 +0000

    Add package RackioAdmin/0.5/RackioAdmin-0.5-py3-none-any.whl

---
+++ packages/RackioAdmin/RackioAdmin-0.5-py3-none-any.whl/rackio_admin/core.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""rackio_admin/core.py
+
+This module implements the core app class and methods for Rackio Admin UI.
+"""
+
+from ._singleton import Singleton
+from .resources import AdminStaticResource, AdminViewResouce
+from .tools import resource_paths
+
+
+
+class AdminCore(Singleton):
+
+    def __init__(self):
+
+        super(AdminCore, self).__init__()
+        
+        self.app = None
+
+    def register_admin(self):
+
+        paths = resource_paths()
+
+        _static = AdminStaticResource()
+
+        for path in paths:
+
+            route = "/admin" + path + "/{filename}"
+
+            self.app.add_route(route, _static)
+
+        _view = AdminViewResouce()
+
+        self.app.add_route("/admin", _view)
+
+    def __call__(self, app):
+
+        self.app = app
+
+        self.register_admin()
