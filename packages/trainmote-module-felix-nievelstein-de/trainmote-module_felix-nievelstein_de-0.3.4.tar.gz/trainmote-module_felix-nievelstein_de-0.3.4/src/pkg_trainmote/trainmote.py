c3c4bd77f3581987f3e8cbb7aeb581366ad420b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:06 +0000

    Add package trainmote-module-felix-nievelstein-de/0.3.4/trainmote-module_felix-nievelstein_de-0.3.4.tar.gz

---
+++ packages/trainmote-module-felix-nievelstein-de/trainmote-module_felix-nievelstein_de-0.3.4.tar.gz/trainmote-module_felix-nievelstein_de-0.3.4/src/pkg_trainmote/trainmote.py
@@ -0,0 +1,58 @@
+from . import gpioservice
+from flask import Flask
+from flask import jsonify
+from .powerControllerModule import PowerThread
+from .configControllerModule import ConfigController
+from . import stateControllerModule
+from .libInstaller import LibInstaller
+from subprocess import call
+import logging
+import logging.handlers
+import argparse
+import sys
+import os
+import time
+
+gpioservice.setup()
+gpioservice.loadInitialData()
+stateController = stateControllerModule.StateController()
+powerThread = PowerThread()
+client_sock = None
+config = ConfigController()  
+app = Flask(__name__)
+
+def loadPersistentData():
+    if config.loadPreferences():
+        if not config.isSQLiteInstalled():
+            libInstaller = LibInstaller()
+            libInstaller.installSQLite()
+            if config.setSQLiteInstalled():
+                restart()
+            else: 
+                shutDown()
+    
+@app.route('/')
+def hello_world():
+    return jsonify(result='Hello World')
+  
+def restart():
+    shutDown()
+    os.execv(sys.executable, ['python'] + sys.argv)
+
+def shutDown():
+    powerThread.kill.set()
+    powerThread.isTurningOff = True
+    powerThread.join()
+    stateController.setState(stateControllerModule.STATE_SHUTDOWN)
+    print ("Server going down")
+    stateController.stop()
+
+def closeClientConnection():
+    print ("Closing client socket")
+
+if __name__ == '__main__':
+    print("Start webserver")
+    app.run(debug=True,host="0.0.0.0")  
+
+if __name__ == '__prod__':
+    app.run(debug=False)  
