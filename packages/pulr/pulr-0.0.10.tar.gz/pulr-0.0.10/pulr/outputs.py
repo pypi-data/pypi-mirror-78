cc46489a13bc224d89382284a4e26b682d609798
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:58 +0000

    Add package pulr/0.0.10/pulr-0.0.10.tar.gz

---
+++ packages/pulr/pulr-0.0.10.tar.gz/pulr-0.0.10/pulr/outputs.py
@@ -0,0 +1,53 @@
+import sys
+from functools import partial
+from types import SimpleNamespace
+from .beacons import beacon_empty_line
+
+oprint = partial(print, flush=True)
+eprint = partial(print, flush=True, file=sys.stderr)
+
+_d = SimpleNamespace()
+
+try:
+    import rapidjson as json
+except:
+    import json
+
+
+def print_trace():
+    import traceback
+    eprint(traceback.format_exc())
+
+
+def output_devnull(*args, **kwargs):
+    pass
+
+
+def output_stdout(o, value):
+    oprint(f'{o} {value}')
+
+
+def output_stdout_ndjson(o, value):
+    oprint(json.dumps({'id': o, 'v': value}))
+
+
+def output_eva_datapuller(o, value):
+    val_mode = o.endswith('.value')
+    s = f'{o[:-6 if val_mode else -7]} u '
+    oprint(s + (f'None {value}' if val_mode else str(value)))
+
+
+OUTPUT_METHODS = {
+    None: {
+        'output': output_stdout,
+        'beacon': beacon_empty_line
+    },
+    'ndjson': {
+        'output': output_stdout_ndjson,
+        'beacon': beacon_empty_line
+    },
+    'eva/datapuller': {
+        'output': output_eva_datapuller,
+        'beacon': beacon_empty_line
+    }
+}
