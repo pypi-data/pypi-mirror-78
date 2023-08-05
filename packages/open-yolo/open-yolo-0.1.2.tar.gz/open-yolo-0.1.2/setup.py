128cfdcbbd0b27796dadca371b828ae1085f3ed4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:21 +0000

    Add package open-yolo/0.1.2/open-yolo-0.1.2.tar.gz

---
+++ packages/open-yolo/open-yolo-0.1.2.tar.gz/open-yolo-0.1.2/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+requirements = [
+    'oss2',
+    'numpy',
+    'opencv-python',
+    'flask',
+    'colour',
+    'scikit-learn',
+    'scenedetect',
+    'moviepy',
+    'Pillow',
+    'torch',
+    'torchvision',
+    'joblib',
+    'tensorflow',
+    'tensorflow-hub',
+]
+
+__version__ = '0.1.2'
+
+setup(
+    # Metadata
+    name='open-yolo',
+    version=__version__,
+    author='CachCheng',
+    author_email='tkggpdc2007@163.com',
+    url='https://github.com/CachCheng/OpenYolos',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    description='Open Yolo Toolkit',
+    license='Apache-2.0',
+    packages=find_packages(exclude=('docs', 'tests', 'scripts')),
+    zip_safe=True,
+    include_package_data=True,
+    install_requires=requirements,
+)
