b52c23e03b3989ffc7032dc98f48cff3167a410c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:46 +0000

    Add package psifr/0.4.2/psifr-0.4.2.tar.gz

---
+++ packages/psifr/psifr-0.4.2.tar.gz/psifr-0.4.2/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setuptools.setup(
+    name='psifr',
+    version='0.4.2',
+    description='Package for analysis of free recall data.',
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    author='Neal Morton',
+    author_email='mortonne@gmail.com',
+    license='GPLv3',
+    url='http://github.com/mortonne/psifr',
+    packages=setuptools.find_packages('src'),
+    package_dir={'': 'src'},
+    package_data={
+        'psifr': ['data/*.csv']
+    },
+    install_requires=[
+        'numpy',
+        'pandas',
+        'seaborn',
+    ],
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.8',
+    ]
+)
