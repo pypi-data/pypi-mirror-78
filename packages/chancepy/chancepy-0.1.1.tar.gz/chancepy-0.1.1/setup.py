a1ce9a1962ec17eb1e04fbf95506f47535cb39a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:56 +0000

    Add package chancepy/0.1.1/chancepy-0.1.1.tar.gz

---
+++ packages/chancepy/chancepy-0.1.1.tar.gz/chancepy-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['chancepy']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pytz>=2020.1,<2021.0']
+
+setup_kwargs = {
+    'name': 'chancepy',
+    'version': '0.1.1',
+    'description': 'Random generator helper for Python',
+    'long_description': None,
+    'author': 'Nuni',
+    'author_email': 'nuni@kovrr.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
