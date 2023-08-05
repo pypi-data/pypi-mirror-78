7b9c66e8763590fc1c9e480c1dbf18ecfbce3aff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:55 +0000

    Add package discord-ext-ipc/1.1.3a0/discord-ext-ipc-1.1.3a0.tar.gz

---
+++ packages/discord-ext-ipc/discord-ext-ipc-1.1.3a0.tar.gz/discord-ext-ipc-1.1.3a0/discord/ext/ipc/client.py
@@ -0,0 +1,98 @@
+"""
+    Copyright 2020 Ext-Creators
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+        http://www.apache.org/licenses/LICENSE-2.0
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+
+
+import asyncio
+import json
+import socket
+
+from .errors import *
+
+
+class Node:
+    def __init__(self, client, data):
+        self.client = client
+        self._json = data
+        
+        self.ip = data.get("multicast_grp")
+        self.port = data.get("port")
+        self.endpoints = data.get("endpoints")
+    
+    def get_json(self):
+        """Convert object to json"""
+        return self._json
+
+    async def request(self, endpoint, **kwargs):
+        if endpoint not in self.endpoints.keys():
+            raise NoEndpointFoundError("Endpoint \"{}\" not found".format(endpoint))
+        
+        return await self.client.request(endpoint, self.port, **kwargs)
+    
+    def __str__(self):
+        return str(self.get_json())
+
+class Client:
+    """Main client class, used for delivering data from the web server to the bot"""
+
+    def __init__(self, *, host="localhost", secret_key=None):
+        self.host = host
+        self.secret_key = secret_key
+        
+        self.nodes = {}
+    
+    async def discover(self):
+        """Get the first node found on your network"""
+
+        response = await self.request(None, 20000, multicast=True)
+        
+        if not response:
+            print("No node")
+            return None
+
+        node = Node(self, response)
+        self.nodes[node.port] = node
+
+        return node
+        
+    async def request(self, endpoint, port, **kwargs):
+        """Make a request to the IPC server"""
+        try:
+            reader, writer = await asyncio.open_connection(self.host, port)
+
+            if kwargs.get("multicast") and kwargs.get("multicast") is True:
+                data = {"multicast": True, "data": {"ping": 1}, "headers": {"Authentication": self.secret_key}}
+            else:
+                data = {"endpoint": endpoint, "data": kwargs, "headers": {"Authentication": self.secret_key}}
+
+            writer.write(json.dumps(data).encode("utf-8"))
+            
+            await writer.drain()
+            
+            while True:
+                data = await reader.read()
+                
+                if not data:
+                    return await writer.close()
+                
+                break
+            
+            to_ret = json.loads(data.decode("utf-8"))
+            
+            if to_ret == "null":
+                return None
+            
+            await writer.close()
+
+            return to_ret
+        except ConnectionRefusedError:
+            raise ServerConnectionRefusedError("No server found for ({}, {}), or server isn't accepting connections.".format(self.host, port))
---
+++ packages/discord-ext-ipc/discord-ext-ipc-1.1.3a0.tar.gz/discord-ext-ipc-1.1.3a0/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup
+import re
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+version = ""
+with open("discord/ext/ipc/__init__.py") as f:
+    version = re.search(r"^__version__\s*=\s*[\'\"]([^\'\"]*)[\'\"]", f.read(), re.MULTILINE).group(1)
+
+with open("README.rst", "r") as f:
+    readme = f.read()
+
+setup(
+    name="discord-ext-ipc",
+    author="Ext-Creators",
+    python_requires=">=3.5.3",
+    url="https://github.com/Ext-Creators/discord-ext-ipc",
+    version=version,
+    packages=["discord.ext.ipc"],
+    license="Apache Software License",
+    description="An IPC extension allowing for the communication between a discord.py bot and an asynchronous web-framework (i.e. Quart or aiohttp.web)",
+    long_description_content_type="text/x-rst",
+    long_description=readme,
+    include_package_data=True,
+    install_requires=requirements,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Internet",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+    ]
+)
