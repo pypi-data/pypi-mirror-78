efed9c4c6e670e5b6201cc7f93a87db215c7adc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:20 +0000

    Add package MLFairnessPipeline/0.18/MLFairnessPipeline-0.18-py3-none-any.whl

---
+++ packages/MLFairnessPipeline/MLFairnessPipeline-0.18-py3-none-any.whl/Pipeline/Postprocessing.py
@@ -0,0 +1,10 @@
+from aif360.algorithms.postprocessing.reject_option_classification import RejectOptionClassification
+from Utilities.generate_binary_dataset import generate_binary_label_dataset
+
+def Postprocessing(reweighted_data, pred, label, unprivileged_groups, privileged_groups, protected_attribute, favorable_label, unfavorable_label, threshold=0.01):
+    reweighted_binary_dataset = generate_binary_label_dataset(reweighted_data, label, protected_attribute, favorable_label, unfavorable_label)
+    prediction_binary_dataset = generate_binary_label_dataset(pred, label, protected_attribute, favorable_label, unfavorable_label)
+    ROC = RejectOptionClassification(unprivileged_groups=unprivileged_groups, privileged_groups=privileged_groups, low_class_thresh=threshold)
+    ROC.fit(reweighted_binary_dataset, prediction_binary_dataset)
+
+    return ROC.predict(prediction_binary_dataset).convert_to_dataframe()[0]
