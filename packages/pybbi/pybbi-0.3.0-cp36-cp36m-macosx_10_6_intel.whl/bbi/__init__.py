38faa3a3a745572b9129e4e3ab68cbafae322f0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:28 +0000

    Add package pybbi/0.3.0/pybbi-0.3.0-cp36-cp36m-macosx_10_6_intel.whl

---
+++ packages/pybbi/pybbi-0.3.0-cp36-cp36m-macosx_10_6_intel.whl/bbi/__init__.py
@@ -0,0 +1,19 @@
+from .cbbi import (
+    is_bbi,
+    is_bigwig,
+    is_bigbed,
+    open,
+)
+
+from ._bbi import (
+    info,
+    chromsizes,
+    zooms,
+    fetch_intervals,
+    fetch,
+    stackup,
+)
+
+del cbbi, _bbi
+
+__version__ = '0.3.0'
