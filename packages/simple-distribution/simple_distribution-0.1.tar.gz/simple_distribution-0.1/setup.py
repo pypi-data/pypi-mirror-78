c3154a80665ae2d6bfb07d13c78bd7f38ddd079f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:28 +0000

    Add package simple-distribution/0.1/simple_distribution-0.1.tar.gz

---
+++ packages/simple-distribution/simple_distribution-0.1.tar.gz/simple_distribution-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='simple_distribution',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['simple_distribution'],
+      author = 'forhad',
+      author_email = 'fhossain1@isrt.ac.bd',
+      zip_safe=False)
