3a8eddf1dcedeeafefacf295a350f2e364a44287
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:43 +0000

    Add package pyfzf/0.2.2/pyfzf-0.2.2.tar.gz

---
+++ packages/pyfzf/pyfzf-0.2.2.tar.gz/pyfzf-0.2.2/setup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+# Copyright (c) 2015 Nagarjuna Kumarappan
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+# Author: Nagarjuna Kumarappan <nagarjuna.412@gmail.com>
+
+import os
+from setuptools import setup
+from distutils.command.install import INSTALL_SCHEMES
+
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
+
+prjdir = os.path.dirname(__file__)
+
+def read(filename):
+    return open(os.path.join(prjdir, filename)).read()
+
+LONG_DESC = read('README.md') + '\nCHANGES\n=======\n\n' + read('CHANGES')
+
+setup(name='pyfzf',
+      include_package_data=True,
+      version=read('VERSION').strip('\n'),
+      description="Python wrapper for junegunn's fuzzyfinder (fzf)",
+      long_description=LONG_DESC,
+      author='Nagarjuna Kumarappan',
+      license='MIT',
+      author_email='nagarjuna.412@gmail.com',
+      url='https://github.com/nk412/pyfzf',
+      install_requires=['plumbum'],
+      py_modules=['pyfzf'],
+      packages=['pyfzf'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Terminals'
+          ]
+      )
+
