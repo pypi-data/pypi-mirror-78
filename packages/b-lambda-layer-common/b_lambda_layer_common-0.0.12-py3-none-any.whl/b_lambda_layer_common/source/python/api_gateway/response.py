da72d367d6be9836a4b0dd262abd77f440777e88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:17 +0000

    Add package b-lambda-layer-common/0.0.12/b_lambda_layer_common-0.0.12-py3-none-any.whl

---
+++ packages/b-lambda-layer-common/b_lambda_layer_common-0.0.12-py3-none-any.whl/b_lambda_layer_common/source/python/api_gateway/response.py
@@ -0,0 +1,43 @@
+import json
+import logging
+from typing import Optional, Dict, Any
+
+logger = logging.getLogger(__file__)
+
+try:
+    # Lambda specific imports.
+    from api_gateway.response_headers import ResponseHeaders
+except ImportError as ex:
+    logger.warning(f'Unable to import: {repr(ex)}.')
+
+    # Project specific imports.
+    from b_lambda_layer_common.source.python.api_gateway.response_headers import ResponseHeaders
+
+
+class Response:
+    @staticmethod
+    def message(
+            status: int,
+            message: str,
+            headers: Optional[ResponseHeaders],
+    ) -> Dict[str, Any]:
+        return Response.json(http_status=status, headers=headers, body={'message': message})
+
+    @staticmethod
+    def json(
+            http_status: int,
+            headers: Optional[ResponseHeaders],
+            body: Optional[Dict[Any, Any]] = None
+    ) -> Dict[Any, Any]:
+        r = {
+            'isBase64Encoded': False,
+            'statusCode': http_status
+        }
+
+        if headers:
+            r['headers'] = headers.headers_dict
+
+        if body:
+            r['body'] = json.dumps(body)
+
+        return r
