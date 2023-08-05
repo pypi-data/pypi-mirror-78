0ade9f5d28af99a3ba963673cff68de287e49f7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:26 +0000

    Add package traktor-nml-utils/2.0.5/traktor-nml-utils-2.0.5.tar.gz

---
+++ packages/traktor-nml-utils/traktor-nml-utils-2.0.5.tar.gz/traktor-nml-utils-2.0.5/setup.py
@@ -0,0 +1,25 @@
+from os import path
+
+from setuptools import find_packages, setup
+
+
+def read(fname):
+    this_directory = path.abspath(path.dirname(__file__))
+    return open(path.join(this_directory, fname), encoding="utf-8").read()
+
+
+setup(
+    name="traktor-nml-utils",
+    version="2.0.5",
+    description="Utilities to read and write Traktor NML files",
+    long_description=read("README.pypi.md"),
+    long_description_content_type="text/markdown",
+    url="http://github.com/ifischer/traktor-nml-utils",
+    author="Ingo Fischer",
+    author_email="mail@ingofischer.de",
+    license="GPL",
+    packages=find_packages(exclude=("tests",)),
+    zip_safe=True,
+    python_requires=">=3.7.0",
+    install_requires=["xsdata"],
+)
