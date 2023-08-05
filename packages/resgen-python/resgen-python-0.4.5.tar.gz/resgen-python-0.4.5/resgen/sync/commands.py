e4c31bb14c5db2446ea029cd5b9de772b19acb45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:59 +0000

    Add package resgen-python/0.4.5/resgen-python-0.4.5.tar.gz

---
+++ packages/resgen-python/resgen-python-0.4.5.tar.gz/resgen-python-0.4.5/resgen/sync/commands.py
@@ -0,0 +1,75 @@
+import getpass
+import logging
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+import click
+import resgen as rg
+
+logger = logging.getLogger(__name__)
+
+
+@click.group()
+def sync():
+    pass
+
+
+@click.command()
+@click.argument("gruser")
+@click.argument("project")
+@click.argument("datasets", nargs=-1)
+@click.option("-t", "--tag", multiple=True)
+def datasets(gruser, project, datasets, tag):
+    """Upload if a file with the same name doesn't already exist.
+
+    If files are of the form "filename1,filename2" it will be assumed
+    that filename2 is the index file for filename1.
+    """
+    try:
+        env_path = Path.home() / ".resgen" / "credentials"
+
+        if env_path.exists():
+            load_dotenv(env_path)
+
+            username = os.getenv("RESGEN_USERNAME")
+            password = os.getenv("RESGEN_PASSWORD")
+        else:
+            username = input("Username:")
+            password = getpass.getpass("Password:")
+
+        try:
+            rgc = rg.connect(username=username, password=password)
+        except rg.UnknownConnectionException:
+            logger.error("Unable to login, please check your username and password")
+            return
+
+        project = rgc.find_or_create_project(project, group=gruser)
+
+        metadata = dict([t.split(":")[:2] for t in tag])
+
+        for dataset in datasets:
+            parts = dataset.split(",")
+
+            if len(parts) > 1:
+                logger.info(
+                    "Syncing dataset: %s with indexfile: %s and metadata: %s",
+                    parts[0],
+                    parts[1],
+                    str(metadata),
+                )
+                project.sync_dataset(parts[0], index_filepath=parts[1], **metadata)
+            else:
+                logger.info(
+                    "Syncing dataset: %s with metadata: %s", parts[0], str(metadata),
+                )
+                project.sync_dataset(dataset, **metadata)
+    except rg.InvalidCredentialsException:
+        logger.error(
+            "Invalid credentials. Make sure that they are set in either "
+            "~/.resgen/credentials or in the environment variables RESGEN_USERNAME "
+            "and RESGEN_PASSWORD."
+        )
+
+
+sync.add_command(datasets)
---
+++ packages/resgen-python/resgen-python-0.4.5.tar.gz/resgen-python-0.4.5/setup.py
@@ -0,0 +1,54 @@
+import io
+import os
+import os.path
+
+from setuptools import setup, find_packages, Command
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+
+
+def read(*parts, **kwargs):
+    filepath = os.path.join(HERE, *parts)
+    encoding = kwargs.pop("encoding", "utf-8")
+    with io.open(filepath, encoding=encoding) as fh:
+        text = fh.read()
+    return text
+
+
+def get_requirements(path):
+    content = read(path)
+    return [req for req in content.split("\n") if req != "" and not req.startswith("#")]
+
+
+setup_args = {
+    "name": "resgen-python",
+    "version": "0.4.5",
+    "packages": find_packages(),
+    "description": "Python bindings for the resgen genomic data service",
+    # "long_description": read("README.md"),
+    "long_description_content_type": "text/markdown",
+    "url": "https://github.com/reservoirgenomics/resgen-python",
+    "include_package_data": True,
+    "zip_safe": False,
+    "author": "Peter Kerpedjiev",
+    "author_email": "pkerpedjiev@gmail.com",
+    "keywords": ["resgen"],
+    "classifiers": [
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Multimedia :: Graphics",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    "install_requires": get_requirements("requirements.txt"),
+    "setup_requires": [],
+    "tests_require": ["pytest"],
+    "entry_points": {"console_scripts": ["resgen = resgen.cli:cli"]}
+}
+
+setup(**setup_args)
