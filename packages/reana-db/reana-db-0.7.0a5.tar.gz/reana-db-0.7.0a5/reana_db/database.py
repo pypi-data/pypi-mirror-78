02b43aa824df9c55756a85de68a5f76621c15c40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:53 +0000

    Add package reana-db/0.7.0a5/reana-db-0.7.0a5.tar.gz

---
+++ packages/reana-db/reana-db-0.7.0a5.tar.gz/reana-db-0.7.0a5/reana_db/database.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of REANA.
+# Copyright (C) 2018 CERN.
+#
+# REANA is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Database management for REANA."""
+
+from __future__ import absolute_import
+
+from sqlalchemy import create_engine, event
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.schema import CreateSchema
+from sqlalchemy_utils import create_database, database_exists
+
+from .config import SQLALCHEMY_DATABASE_URI
+
+from reana_db.models import Base  # isort:skip  # noqa
+
+engine = create_engine(SQLALCHEMY_DATABASE_URI)
+Session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+Base.query = Session.query_property()
+
+
+def init_db():
+    """Initialize the DB."""
+    import reana_db.models
+
+    event.listen(Base.metadata, "before_create", CreateSchema("__reana"))
+    if not database_exists(engine.url):
+        create_database(engine.url)
+    Base.metadata.create_all(bind=engine)
