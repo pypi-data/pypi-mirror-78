7bb6fc5250ef3e9ccb3c489907d18f99e4cb2e6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:46 +0000

    Add package PyKhana/0.0.3/PyKhana-0.0.3.tar.gz

---
+++ packages/PyKhana/PyKhana-0.0.3.tar.gz/PyKhana-0.0.3/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8",errors='ignore') as fh:
+    long_description = fh.read()
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+
+setuptools.setup(
+    name = "PyKhana",
+    version = "0.0.3",
+    author = "Dipam Paul",
+    author_email = "dipampaul17@gmail.com",
+    description = ("An demonstration of how to implement Gaussian Integer operations in Python."),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license = "MIT",
+    keywords = "PyKhana, GaussianInt",
+    url = "https://github.com/dipampaul17/PyKhana",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+   
+)
