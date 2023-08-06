7132789d92d47cca7c3860deaa05dccbe73e7e30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:54 +0000

    Add package eric-ide/20.9/eric-ide-20.9.tar.gz

---
+++ packages/eric-ide/eric-ide-20.9.tar.gz/eric-ide-20.9/eric6/UI/Info.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2002 - 2020 Detlev Offenbach <detlev@die-offenbachs.de>
+#
+
+"""
+Module defining some informational strings.
+"""
+
+
+Program = 'eric6'
+Version = '20.9 (rev. e2b36c2167b7)'
+VersionOnly = '20.9'
+Copyright = (
+    'Copyright (c) 2002 - 2020 Detlev Offenbach'
+    ' <detlev@die-offenbachs.de>'
+)
+CopyrightShort = '(c) 2002 - 2020 Detlev Offenbach'
+BugAddress = 'eric-bugs@eric-ide.python-projects.org'
+FeatureAddress = 'eric-featurerequest@eric-ide.python-projects.org'
+Homepage = "https://eric-ide.python-projects.org/index.html"
