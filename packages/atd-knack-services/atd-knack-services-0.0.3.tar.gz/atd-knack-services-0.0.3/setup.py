f76ac8d57d1da4b60cb6a254b9b1641f614efdea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:43 +0000

    Add package atd-knack-services/0.0.3/atd-knack-services-0.0.3.tar.gz

---
+++ packages/atd-knack-services/atd-knack-services-0.0.3.tar.gz/atd-knack-services-0.0.3/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+config = {
+    "author": "John Clary",
+    "author_email": "john.clary@austintexas.gov",
+    "classifiers": [
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: Public Domain",
+        "Programming Language :: Python :: 3",
+    ],
+    "description": "Integration services for ATD's knack applications.",
+    "long_description_content_type": "text/markdown",
+    "long_description": long_description,
+    "install_requires": ["arrow", "boto3", "knackpy", "sodapy"],
+    "keywords": "knack api integration python",
+    "license": "Public Domain",
+    "name": "atd-knack-services",
+    "packages": ["services"],
+    "url": "http://github.com/cityofaustin/atd-knack-services",
+    "version": "0.0.3",
+}
+
+if __name__ == "__main__":
+    setuptools.setup(**config)
