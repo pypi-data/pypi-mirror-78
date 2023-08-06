f01a0a7728c2d96a52d3f105d0c9d590f486cdb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:05 +0000

    Add package django-static-html5shiv/3.7.3.3/django-static-html5shiv-3.7.3.3.tar.gz

---
+++ packages/django-static-html5shiv/django-static-html5shiv-3.7.3.3.tar.gz/django-static-html5shiv-3.7.3.3/setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup
+from setuptools import find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.md'), "r", encoding="utf-8") as fobj:
+    long_description = fobj.read()
+
+with open(os.path.join(here, "requirements.txt"), "r", encoding="utf-8") as fobj:
+    requires = [x.strip() for x in fobj.readlines() if x.strip()]
+
+setup(
+    name="django-static-html5shiv",
+    version="3.7.3.3",
+    description="Django application contain html5shiv static files.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zencore-cn/zencore-issues",
+    author="zencore",
+    author_email="dobetter@zencore.cn",
+    license="MIT",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    keywords=['django-static-html5shiv'],
+    packages=find_packages("."),
+    install_requires=requires,
+    zip_safe=False,
+    include_package_data=True,
+)
