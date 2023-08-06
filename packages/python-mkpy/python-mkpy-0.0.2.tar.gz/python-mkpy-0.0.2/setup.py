911f4831811b9ebc7741f4131933512c62016e39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:13 +0000

    Add package python-mkpy/0.0.2/python-mkpy-0.0.2.tar.gz

---
+++ packages/python-mkpy/python-mkpy-0.0.2.tar.gz/python-mkpy-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name="python-mkpy",
+    author="phipus",
+    author_email="phipuspam@gmx.ch",
+    description="A (minimalistic) build tool simmilar to make. Written in Python 3",
+    url="https://github.com/phipus/mkpy",
+    packages=["mkpy"],
+    version="0.0.2",
+    entry_points={"console_scripts": "mkpy=mkpy.make:main"},
+)
