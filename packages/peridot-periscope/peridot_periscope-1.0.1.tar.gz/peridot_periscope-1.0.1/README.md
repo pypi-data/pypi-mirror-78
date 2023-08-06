8c93629d99ed0713640092f05e05ba77366dfa38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:41 +0000

    Add package peridot-periscope/1.0.1/peridot_periscope-1.0.1.tar.gz

---
+++ packages/peridot-periscope/peridot_periscope-1.0.1.tar.gz/peridot_periscope-1.0.1/README.md
@@ -0,0 +1,36 @@
+# Periscope (1.0)
+
+![Periscope Logo](https://raw.githubusercontent.com/toto-bird/Periscope/master/logo.png)
+
+### Usage:
+Peri.dot
+```
+var periscope = include('periscope')
+
+var pscope_add() -> Null {
+    return(periscope.skip(
+        'Out of date'
+    ))
+}
+
+var pscope_sub() -> Null {
+    assert(10 - 5 == 5)
+
+    return(Null)
+}
+
+var pscope_mul() -> Null {
+    assert(10 - 1 == 10, 'Hmm. Somethings not right')
+
+    return(Null)
+}
+```
+
+Bash
+```bash
+# Test files
+periscope peritests/*
+
+# More help
+periscope -h
+```
---
+++ packages/peridot-periscope/peridot_periscope-1.0.1.tar.gz/peridot_periscope-1.0.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['peridot_periscope', 'peridot_periscope.version']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['peridot>=1.1.1,<2.0.0']
+
+entry_points = \
+{'console_scripts': ['periscope = peridot_periscope:main']}
+
+setup_kwargs = {
+    'name': 'peridot-periscope',
+    'version': '1.0.1',
+    'description': 'Unit testing framework for the Peri.dot Programming Language',
+    'long_description': "# Periscope (1.0)\n\n![Periscope Logo](https://raw.githubusercontent.com/toto-bird/Periscope/master/logo.png)\n\n### Usage:\nPeri.dot\n```\nvar periscope = include('periscope')\n\nvar pscope_add() -> Null {\n    return(periscope.skip(\n        'Out of date'\n    ))\n}\n\nvar pscope_sub() -> Null {\n    assert(10 - 5 == 5)\n\n    return(Null)\n}\n\nvar pscope_mul() -> Null {\n    assert(10 - 1 == 10, 'Hmm. Somethings not right')\n\n    return(Null)\n}\n```\n\nBash\n```bash\n# Test files\nperiscope peritests/*\n\n# More help\nperiscope -h\n```\n",
+    'author': 'Totobird',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
