77cafd553a5abef9202eb3d0409c84b327002e4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:15 +0000

    Add package onlyforfun/0.3/onlyforfun-0.3.tar.gz

---
+++ packages/onlyforfun/onlyforfun-0.3.tar.gz/onlyforfun-0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='onlyforfun',
+      version='0.3',
+      description='Addition methods for test purpose',
+      long_description='I dont know where the long description is gonna fit. But also since it is asking I am giving it',
+      url='https://github.com/sigdelsanjog/onlyforfun',
+      author='Fun Maker',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['onlyforfun'],
+      zip_safe=False)
