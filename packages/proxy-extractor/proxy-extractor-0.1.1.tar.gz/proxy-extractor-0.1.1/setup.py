4e4010534575b40fb1f9858cbf5a05f308250877
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:08 +0000

    Add package proxy-extractor/0.1.1/proxy-extractor-0.1.1.tar.gz

---
+++ packages/proxy-extractor/proxy-extractor-0.1.1.tar.gz/proxy-extractor-0.1.1/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+setuptools.setup(
+    name="proxy-extractor",
+    version="0.1.1",
+    author="Ganofins",
+    author_email="bagariaganesh@gmail.com",
+    description="Package to extract free proxies from various free proxy listings",
+    url="https://github.com/Ganofins/proxy-extractor",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
+)
