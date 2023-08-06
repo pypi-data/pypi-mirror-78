9d8f8842bb6fac1e5ca1be4275eaf4ace2482952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:52 +0000

    Add package syned/1.0.18/syned-1.0.18.tar.gz

---
+++ packages/syned/syned-1.0.18.tar.gz/syned-1.0.18/syned/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM syned SETUP.PY
+short_version = '1.0.18'
+version = '1.0.18'
+full_version = '1.0.18.dev0+49a0184'
+git_revision = '49a01842b40b9ed58bdb0b30b9a3c2fed3c807af'
+release = False
+
+if not release:
+    version = full_version
+    short_version += ".dev"
