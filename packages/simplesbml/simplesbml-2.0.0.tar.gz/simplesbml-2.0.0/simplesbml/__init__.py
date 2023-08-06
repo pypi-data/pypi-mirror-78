35ef5770b1ec4855c126f7d87d882e8a47be60c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:59 +0000

    Add package simplesbml/2.0.0/simplesbml-2.0.0.tar.gz

---
+++ packages/simplesbml/simplesbml-2.0.0.tar.gz/simplesbml-2.0.0/simplesbml/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 27 23:13:42 2015, 2020
+
+@author: carolc24, Kyle Medley, hsauro
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from .simplesbml import SbmlModel
+
+__version__ = '1.3.3'
+
+
+
+
+
+
