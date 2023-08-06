53a4e92acf3baeaf196cc78ff7812a644b1514a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:58 +0000

    Add package marcel/0.10.15/marcel-0.10.15-py3-none-any.whl

---
+++ packages/marcel/marcel-0.10.15-py3-none-any.whl/marcel/builtin.py
@@ -0,0 +1,38 @@
+# This file is part of Marcel.
+# 
+# Marcel is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or at your
+# option) any later version.
+# 
+# Marcel is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Marcel.  If not, see <https://www.gnu.org/licenses/>.
+
+from time import time
+now = time
+
+from marcel.object.process import processes, Process
+from marcel.object.file import File
+from marcel.util import username, groupname, quote_files
+
+
+_COMMANDS = '''
+#    read = [map (f: f.readlines()) | expand]
+'''
+
+
+def minutes(n):
+    return 60 * n
+
+
+def hours(n):
+    return 3600 * n
+
+
+def days(n):
+    return 24 * 3600 * n
---
+++ packages/marcel/marcel-0.10.15-py3-none-any.whl/marcel/op/env.py
@@ -0,0 +1,64 @@
+# This file is part of Marcel.
+# 
+# Marcel is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or at your
+# option) any later version.
+# 
+# Marcel is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Marcel.  If not, see <https://www.gnu.org/licenses/>.
+
+import marcel.argsparser
+import marcel.core
+
+
+HELP = '''
+{L,wrap=F}env
+
+Write the contents of the environment, (i.e., the marcel namespace), to the output stream.
+Each key/value pair is written to the output stream as a tuple,
+(key, value), sorted by key. Python's {n:__builtins__} is part of the marcel namespace, but is omitted
+from output. 
+'''
+
+
+def env(env):
+    return Env(env), []
+
+
+class EnvArgsParser(marcel.argsparser.ArgsParser):
+
+    def __init__(self, env):
+        super().__init__('env', env)
+        self.validate()
+
+
+class Env(marcel.core.Op):
+
+    OMITTED = ['__builtins__']
+
+    def __init__(self, env):
+        super().__init__(env)
+
+    def __repr__(self):
+        return 'env()'
+
+    # AbstractOp
+
+    def receive(self, _):
+        for key, value in sorted(self.env().namespace.items()):
+            if key not in Env.OMITTED:
+                self.send((key, value))
+
+    # Op
+
+    def must_be_first_in_pipeline(self):
+        return True
+
+    def run_in_main_process(self):
+        return True
