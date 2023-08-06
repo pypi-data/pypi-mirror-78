ff53d70e60ebaad56114b60390ebd1b00535ac0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:34 +0000

    Add package climetlab/0.0.82/climetlab-0.0.82.tar.gz

---
+++ packages/climetlab/climetlab-0.0.82.tar.gz/climetlab-0.0.82/climetlab/__init__.py
@@ -0,0 +1,31 @@
+# (C) Copyright 2020 ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+#
+
+from .sources import load as load_source
+from .datasets import load as load_dataset
+from .plotting import plot_map, new_plot
+from .core.settings import SETTINGS as settings
+from .core.caching import CACHE as cache
+
+from climetlab.sources import DataSource
+from climetlab.datasets import Dataset
+
+__version__ = "0.0.82"
+
+
+__all__ = [
+    "load_source",
+    "load_dataset",
+    "plot_map",
+    "new_plot",
+    "settings",
+    "cache",
+    "Dataset",
+    "DataSource",
+]
