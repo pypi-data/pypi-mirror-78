da07467894faffedd502bbc06e8bda88707f5498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:01 +0000

    Add package cproperty/0.7.4/cproperty-0.7.4.tar.gz

---
+++ packages/cproperty/cproperty-0.7.4.tar.gz/cproperty-0.7.4/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+setup(
+    name='cproperty',
+    packages=['cproperty'],
+    version='0.7.4',
+    license='MIT',
+    description='class/method decorator for caching properties',
+    author='n0x1s',
+    author_email='n0x1s0x01@gmail.com',
+    url='https://github.com/n0x1s/cproperty',
+    download_url='https://github.com/N0x1s/cproperty/archive/0.7.4.tar.gz',
+    keywords=['property cache', 'cache decorator', 'cache',
+              'decorator', 'cached properties', 'property timer'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+
+
+    ],
+)
