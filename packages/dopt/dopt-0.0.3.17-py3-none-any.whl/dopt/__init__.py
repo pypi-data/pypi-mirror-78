bd1236294961ec3b3ed6c16d4707791f9421669b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:46 +0000

    Add package dopt/0.0.3.17/dopt-0.0.3.17-py3-none-any.whl

---
+++ packages/dopt/dopt-0.0.3.17-py3-none-any.whl/dopt/__init__.py
@@ -0,0 +1,12 @@
+from dopt.optimizers import *
+from dopt.trainer import Trainer
+from dopt.server import Server
+
+__version__ = "0.0.3.17"
+
+__all__ = [
+    "Trainer",
+    "Optimizer",
+    "NEIOptimizer",
+    "Server"
+]
---
+++ packages/dopt/dopt-0.0.3.17-py3-none-any.whl/dopt/utils/__init__.py
@@ -0,0 +1,2 @@
+from .ssh_utils import process_commands_in_parallel
+from .general_utils import get_output_shape, generate_seed, timer, add_prefix_to_print, get_all_gpu_processes_info
