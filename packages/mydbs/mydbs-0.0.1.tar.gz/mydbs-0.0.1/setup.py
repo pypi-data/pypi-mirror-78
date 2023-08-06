cad74d13fc4c634f8a5b57930408ba0cc0de96ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:12 +0000

    Add package mydbs/0.0.1/mydbs-0.0.1.tar.gz

---
+++ packages/mydbs/mydbs-0.0.1.tar.gz/mydbs-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='mydbs',
+      version='0.0.1',
+      description='databases operations without sqls',
+      author='shockwave',
+      author_email='yao_yingzhe@163.com',
+      license='MIT',
+      packages=['mydbs'])
