ee1325a78c57c7c29bd430b936b20b228eb22f18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:54 +0000

    Add package otrs-somconnexio/0.2.12/otrs_somconnexio-0.2.12.tar.gz

---
+++ packages/otrs-somconnexio/otrs_somconnexio-0.2.12.tar.gz/otrs_somconnexio-0.2.12/tests/user_management_client/test_user_management_response.py
@@ -0,0 +1,51 @@
+import unittest
+import json
+from mock import Mock, patch
+
+from otrs_somconnexio.user_management_client.user_management_response import UserManagementResponse
+from otrs_somconnexio.exceptions import UserManagementResponseEmpty
+
+
+class FakeResponse():
+    def __init__(self, value):
+        self.content = {
+            'Result': value
+        }
+
+    def json(self):
+        return self.content
+
+
+class UserManagementResponseTestCase(unittest.TestCase):
+
+    def test_response_setting_call_ok(self):
+        raw_response = FakeResponse([1])
+
+        response = UserManagementResponse(raw_response)
+
+        self.assertEqual(response.raw_data, [1])
+
+    def test_response_call_ko(self):
+        raw_response = FakeResponse([])
+
+        with self.assertRaises(UserManagementResponseEmpty):
+            UserManagementResponse(raw_response)
+
+
+    def test_response_getting_call_even_list(self):
+        even_list = ['key', 'value']
+        expected_dict = {'key': 'value'}
+        raw_response = FakeResponse(even_list)
+
+        response = UserManagementResponse(raw_response)
+
+        self.assertEqual(response.get_data(), expected_dict)
+
+    def test_response_getting_call_odd_list(self):
+        odd_list = ['key', 'value', 'key2']
+        expected_dict = {'key': 'value', 'key2': None}
+        raw_response = FakeResponse(odd_list)
+
+        response = UserManagementResponse(raw_response)
+
+        self.assertEqual(response.get_data(), expected_dict)
