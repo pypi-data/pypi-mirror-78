face7a62881332ef2c1b46fef970662ab9f83f53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:08 +0000

    Add package test-distributions-upload123/0.2/test_distributions_upload123-0.2.tar.gz

---
+++ packages/test-distributions-upload123/test_distributions_upload123-0.2.tar.gz/test_distributions_upload123-0.2/README.md
@@ -0,0 +1,3 @@
+test upload package to pypi. 
+
+dont really have any good usage of this package at this moment.
---
+++ packages/test-distributions-upload123/test_distributions_upload123-0.2.tar.gz/test_distributions_upload123-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='test_distributions_upload123',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['test_distributions_upload123'],
+      author='Wing Mai',
+      author_email='wxm19071@gmail.com',
+      zip_safe=False)
