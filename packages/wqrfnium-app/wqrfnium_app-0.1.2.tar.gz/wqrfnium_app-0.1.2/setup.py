44587406c3cf890ec1169929c77d57b8d49c3f2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:36 +0000

    Add package wqrfnium-app/0.1.2/wqrfnium_app-0.1.2.tar.gz

---
+++ packages/wqrfnium-app/wqrfnium_app-0.1.2.tar.gz/wqrfnium_app-0.1.2/setup.py
@@ -0,0 +1,21 @@
+#############################################
+# File Name: setup.py
+# Author: wqrf
+# Mail: 1074321997@qq.com
+# Created Time:  2019-10-08 14:39:00
+#############################################
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "wqrfnium_app",
+    version = "0.1.2",
+    keywords = ("wqrf"),
+    description = "to fix the appium error which caused by ui change,wqrfnium_app can find element!",
+    long_description = "If you can't find element,please use this.You can message to 1074321997@qq.com for help!",
+    license = "MIT",
+    url = "https://github.com/Woqurefan/wqrf_selenium",
+    author = "wqrf",
+    author_email = "1074321997@qq.com",
+    packages = ['wqrfnium_app'],
+)
