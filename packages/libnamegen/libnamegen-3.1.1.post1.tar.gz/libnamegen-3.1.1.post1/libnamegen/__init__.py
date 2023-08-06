c116adc291f448cf976f417ed59ecfca879f877e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:31 +0000

    Add package libnamegen/3.1.1.post1/libnamegen-3.1.1.post1.tar.gz

---
+++ packages/libnamegen/libnamegen-3.1.1.post1.tar.gz/libnamegen-3.1.1.post1/libnamegen/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Name Generator Method Package.
+
+by BBaoVanC
+
+!!! THIS PACKAGE WILL NOT WORK IF YOUR PROGRAM IS IN libnamegen/ !!!
+This is a package which contains various name generation methods.
+
+Documentation:
+https://git.bbaovanc.com/bbaovanc/libnamegen
+
+Copyright (C) 2020 BBaoVanC
+"""
+
+# Imports
+import libnamegen.classic  # NOQA
+import libnamegen.random  # NOQA
