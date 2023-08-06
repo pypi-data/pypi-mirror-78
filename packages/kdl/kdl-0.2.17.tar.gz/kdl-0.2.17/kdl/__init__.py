072eb7cce4481b823e96fa0fe60fd8517b20a8d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:24 +0000

    Add package kdl/0.2.17/kdl-0.2.17.tar.gz

---
+++ packages/kdl/kdl-0.2.17.tar.gz/kdl-0.2.17/kdl/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+__version__ = "0.2.17"
+
+from .client import Client
+from .auth import Auth
---
+++ packages/kdl/kdl-0.2.17.tar.gz/kdl-0.2.17/setup.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+import sys
+from setuptools import setup
+
+if sys.version_info < (3,0):
+    long_description_file = open('README.rst').read()
+else:
+    long_description_file=open('README.rst', encoding='UTF-8').read()
+
+setup(
+    name='kdl',
+    version='0.2.17',
+    description=(
+        'kuaidaili api sdk python, site: https://www.kuaidaili.com'
+    ),
+
+    long_description=long_description_file,
+    author='kuaidaili-dev',
+    author_email='service@kuaidaili.com',
+    license='BSD License',
+    packages= [
+        'kdl',
+    ],
+    platforms='any',
+    install_requires=[
+        'requests'
+    ],
+    url='https://github.com/kuaidaili/python-sdk/api-sdk',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries'
+    ],
+)
