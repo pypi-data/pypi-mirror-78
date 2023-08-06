20deede90c4933dcc072493b33dc14829f271e00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:44 +0000

    Add package ghn-distributions/0.1/ghn-distributions-0.1.tar.gz

---
+++ packages/ghn-distributions/ghn-distributions-0.1.tar.gz/ghn-distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ghn-distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['ghn-distributions'],
+      author='Gary Nguyen',
+      author_email='gary.hnguyen.1107@gmail.com',
+      zip_safe=False)
