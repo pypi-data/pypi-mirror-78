762b1790dadd2b899e81c56fdc3af735dcc84205
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:02 +0000

    Add package aiolinebot/0.3/aiolinebot-0.3.tar.gz

---
+++ packages/aiolinebot/aiolinebot-0.3.tar.gz/aiolinebot-0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aiolinebot",
+    version="0.3",
+    url="https://github.com/uezo/aio-linebot-api",
+    author="uezo",
+    author_email="uezo@uezo.net",
+    maintainer="uezo",
+    maintainer_email="uezo@uezo.net",
+    description="AioLineBotApi provides asynchronous interface for LINE messaging API",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(exclude=["examples*"]),
+    install_requires=["line-bot-sdk", "aiohttp"],
+    license="Apache v2",
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ]
+)
