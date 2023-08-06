ac4d54669f9597bda924c6fbc401687dc0f94d0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:09 +0000

    Add package matgen/0.1.1/matgen-0.1.1.tar.gz

---
+++ packages/matgen/matgen-0.1.1.tar.gz/matgen-0.1.1/matgen/__init__.py
@@ -0,0 +1,23 @@
+import numpy as np
+from .matast import Interpreter
+
+class VerticalStacker(object):
+    def __getitem__(self, mats):
+        return np.vstack(mats)
+
+class HorizontalStacker(object):
+    def __getitem__(self, mats):
+        return np.hstack(mats)
+
+v_ = VerticalStacker()
+
+h_ = HorizontalStacker()
+
+def M_(text) -> np.ndarray:
+    interpreter:Interpreter = Interpreter(text)
+    return interpreter.interpret()
+
+
+if __name__ == "__main__":
+    print(v_[np.array([1,2,3]), h_[[[4,5],[4,5]], [[6],[6]]], np.zeros((2,3)), np.eye(3)])
+    print(M_('[1 2 3; np.zeros((2, 2)), np.array([[1, 2]]).T]'))
