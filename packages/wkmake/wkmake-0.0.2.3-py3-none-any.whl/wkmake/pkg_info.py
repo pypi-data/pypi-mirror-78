68dcd9214abd5f12c64d0d67371568c0659bf65c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:08 +0000

    Add package wkmake/0.0.2.3/wkmake-0.0.2.3-py3-none-any.whl

---
+++ packages/wkmake/wkmake-0.0.2.3-py3-none-any.whl/wkmake/pkg_info.py
@@ -0,0 +1,7 @@
+import os
+pkg_root=os.path.dirname(__file__)
+pkg_data_dir=pkg_root+'/data'
+pkg_templates_dir=pkg_data_dir+'/templates'
+
+class TemplatePaths:
+    python_package=pkg_templates_dir+'/python_package'
