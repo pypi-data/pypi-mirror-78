fe6aca8c1f88f492a9e8e42ddebf38f2307c17bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:11 +0000

    Add package gdash/1.0.1/gdash-1.0.1.tar.gz

---
+++ packages/gdash/gdash-1.0.1.tar.gz/gdash-1.0.1/gdash/ui/precache-manifest.152d697c498f73f98467d91a207fca9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b7662546f975cc5a6c977094e8d2eaf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e987613cf25d61cc55e",
+    "url": "/static/css/main.944a9eac.chunk.css"
+  },
+  {
+    "revision": "0224edbf2d8333fe7774",
+    "url": "/static/js/2.c190e7ee.chunk.js"
+  },
+  {
+    "revision": "320addaff3e37295a01ab28fa4b66477",
+    "url": "/static/js/2.c190e7ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e987613cf25d61cc55e",
+    "url": "/static/js/main.061048e0.chunk.js"
+  },
+  {
+    "revision": "717be7891cc3d466bc4e",
+    "url": "/static/js/runtime-main.b43b082e.js"
+  },
+  {
+    "revision": "5e09ece6910218f68b747dbdb39ec4ac",
+    "url": "/static/media/bg1.5e09ece6.svg"
+  }
+]);
