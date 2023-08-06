98606996bfac500a5e581f4033160e824c949de1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:54 +0000

    Add package lyncs-setuptools/0.2.0/lyncs_setuptools-0.2.0.tar.gz

---
+++ packages/lyncs-setuptools/lyncs_setuptools-0.2.0.tar.gz/lyncs_setuptools-0.2.0/lyncs_setuptools/badges.py
@@ -0,0 +1,46 @@
+"""
+Functions for generating code badges
+"""
+
+__all__ = [
+    "print_pylint_badge",
+]
+
+from collections import OrderedDict
+from contextlib import redirect_stdout
+import sys
+from .setup import get_kwargs
+
+
+def print_pylint_badge():
+    "Runs the pylint executable and prints the badge with the score"
+    from pylint.lint import Run
+
+    if "." in sys.argv:
+        sys.argv.remove(".")
+        sys.argv += get_kwargs()["packages"]
+
+    with redirect_stdout(sys.stderr):
+        results = Run(sys.argv[1:], do_exit=False)
+
+    score = results.linter.stats["global_note"]
+    colors = OrderedDict(
+        {
+            9.95: "brightgreen",
+            8.95: "green",
+            7.95: "yellowgreen",
+            6.95: "yellow",
+            5.95: "orange",
+            0.00: "red",
+        }
+    )
+
+    color = "brightgreen"
+    for val, color in colors.items():
+        if score >= val:
+            break
+
+    print(
+        "[![pylint](https://img.shields.io/badge/pylint%%20score-%.1f%%2F10-%s?logo=python&logoColor=white)](http://pylint.pycqa.org/)"
+        % (score, color)
+    )
