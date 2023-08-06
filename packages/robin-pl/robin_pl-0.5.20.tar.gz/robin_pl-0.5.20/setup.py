359c14a1e77c2a4be020121531937e19962be39c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:30 +0000

    Add package robin-pl/0.5.20/robin_pl-0.5.20.tar.gz

---
+++ packages/robin-pl/robin_pl-0.5.20.tar.gz/robin_pl-0.5.20/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='robin_pl',
+      version='0.5.2',
+      description='It is programming language in russian!',
+      packages=['robin_pl'],
+      author_email='foton-pc@inbox.ru',
+      zip_safe=False,
+      home_page='https://ppbe.ru',
+      author='FotonPC',
+      platform=['windows','linux'])
