22853ba4c027d17105403ec1f00e20d91ecf2107
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:13 +0000

    Add package daniel-udacity/0.1/daniel_udacity-0.1.tar.gz

---
+++ packages/daniel-udacity/daniel_udacity-0.1.tar.gz/daniel_udacity-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='daniel_udacity',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['daniel_udacity'],
+      author='Daniel Grzebyk',
+      zip_safe=False)
