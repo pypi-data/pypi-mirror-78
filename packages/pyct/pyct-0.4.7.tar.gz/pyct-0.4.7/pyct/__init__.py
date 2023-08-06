53aa478c72855969b7ecc7181f93593b66b3771b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:32 +0000

    Add package pyct/0.4.7/pyct-0.4.7.tar.gz

---
+++ packages/pyct/pyct-0.4.7.tar.gz/pyct-0.4.7/pyct/__init__.py
@@ -0,0 +1,7 @@
+import param
+
+NAME = "pyct"
+
+# version comes from git if available, otherwise from .version file
+__version__ = str(param.version.Version(fpath=__file__, archive_commit="$Format:%h$",
+                                        reponame=NAME))
