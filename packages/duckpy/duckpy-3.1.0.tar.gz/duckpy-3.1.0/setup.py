e76e8ab3f6eb7f6b97a9519d475bdca50ba5f0e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:41 +0000

    Add package duckpy/3.1.0/duckpy-3.1.0.tar.gz

---
+++ packages/duckpy/duckpy-3.1.0.tar.gz/duckpy-3.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='duckpy',
+    version='3.1.0',
+
+    packages=['duckpy', 'duckpy.aio'],
+    install_requires=['beautifulsoup4>=4.9.1', 'httpx[http2]==0.14.*'],
+
+    url='https://github.com/AmanoTeam/duckpy',
+    python_requires='>=3.6',
+
+    author='Amano Team',
+    author_email='contact@amanoteam.com',
+
+    license='MIT',
+
+    description='A simple module for searching on DuckDuckGo',
+    long_description=readme,
+    long_description_content_type='text/markdown'
+)
