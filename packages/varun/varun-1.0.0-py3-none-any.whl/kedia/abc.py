717bf14229a93dc405b0624159a6bf518bf971d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:26 +0000

    Add package varun/1.0.0/varun-1.0.0-py3-none-any.whl

---
+++ packages/varun/varun-1.0.0-py3-none-any.whl/kedia/abc.py
@@ -0,0 +1,15 @@
+def punit():
+           print("Punit modi")
+           return
+
+def mihir():
+           print("Mihir Porwal")
+           return
+
+def romil():
+           print("Romil Jain")
+           return
+
+def rohit():
+           print("Rohit Jatt")
+           return
