04dd0b09f6c273cba6cdea097383f4f8d74c5073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:47 +0000

    Add package geometricus/0.1.2/geometricus-0.1.2-py3-none-any.whl

---
+++ packages/geometricus/geometricus-0.1.2-py3-none-any.whl/geometricus/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.0.1-dev"
+
+from .geometricus import GeometricusEmbedding, MomentInvariants, SplitType
+from .protein_utility import Structure
+
+__all__ = ["GeometricusEmbedding", "MomentInvariants", "SplitType", "Structure"]
