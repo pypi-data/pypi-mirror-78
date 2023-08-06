3c6960649392cf5d59b2aac52418bce23ad10f10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:59 +0000

    Add package fdk-rdf-parser/0.1.19/fdk_rdf_parser-0.1.19-py3-none-any.whl

---
+++ packages/fdk-rdf-parser/fdk_rdf_parser-0.1.19-py3-none-any.whl/fdk_rdf_parser/rdf_utils/__init__.py
@@ -0,0 +1,24 @@
+from .ns import (
+    admsURI,
+    brURI,
+    dcatApiURI,
+    dcatApNoURI,
+    dcatURI,
+    dctURI,
+    dqvIsoURI,
+    dqvURI,
+    owlTimeURI,
+    provURI,
+    rovURI,
+    schemaURI,
+    vcardURI,
+)
+from .utils import (
+    catalogRef,
+    dateList,
+    dateValue,
+    objectValue,
+    resourceList,
+    valueList,
+    valueTranslations,
+)
