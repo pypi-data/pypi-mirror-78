9ebc0e4b59ca7fd252cd2dfbb55b13262b72a5a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:56 +0000

    Add package pmidcite/0.0.2/pmidcite-0.0.2.tar.gz

---
+++ packages/pmidcite/pmidcite-0.0.2.tar.gz/pmidcite-0.0.2/setup.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""For installing pmidcite package and accompanying scripts."""
+
+from os.path import abspath
+from os.path import dirname
+from os.path import join
+# from distutils.core import setup
+from setuptools import setup
+# import versioneer
+from glob import glob
+
+def get_long_description():
+    """Return the contents of the README.md as a string"""
+    dir_cur = abspath(dirname(__file__))
+    with open(join(dir_cur, 'README.md'), encoding='utf-8') as ifstrm:
+        return ifstrm.read()
+
+
+setup(
+    name='pmidcite',
+    ## version=versioneer.get_version(),
+    version='v0.0.2',
+    author='DV Klopfenstein',
+    author_email='dvklopfenstein@protonmail.com',
+    ## cmdclass=versioneer.get_cmdclass(),
+    packages=[
+        'pmidcite',
+        'pmidcite.cli',
+        'pmidcite.eutils',
+        'pmidcite.eutils.cmds',
+        'pmidcite.eutils.pubmed',
+        'pmidcite.eutils.pubmed.counts',
+        'pmidcite.eutils.pubmed.mesh',
+        'pmidcite.icite',
+        'pmidcite.plot',
+    ],
+    package_dir={
+        'pmidcite': 'src/pmidcite',
+        'pmidcite.cli': 'src/pmidcite/cli',
+        'pmidcite.eutils': 'src/pmidcite/eutils',
+        'pmidcite.eutils.cmds': 'src/pmidcite/eutils/cmds',
+        'pmidcite.eutils.pubmed': 'src/pmidcite/eutils/pubmed',
+        'pmidcite.eutils.pubmed.counts': 'src/pmidcite/eutils/pubmed/counts',
+        'pmidcite.eutils.pubmed.mesh': 'src/pmidcite/eutils/pubmed/mesh',
+        'pmidcite.icite': 'src/pmidcite/icite',
+        'pmidcite.plot': 'src/pmidcite/plot',
+    },
+    scripts=glob('src/bin/*.py'),
+    classifiers=[
+        'Programming Language :: Python',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Version Control :: Git',
+    ],
+    url='http://github.com/dvklopfenstein/pmidcite',
+    description="Augment's a PubMed literature search with citation data from NIH's iCite.",
+    # https://packaging.python.org/guides/making-a-pypi-friendly-readme/
+    long_description=get_long_description(),
+    long_description_content_type='text/markdown',
+    # install_requires=['docopt'],
+)
