25fafeb1de0fa03bfd12b42c957a769c0de470e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:09 +0000

    Add package CipherModule/1.4/CipherModule-1.4.tar.gz

---
+++ packages/CipherModule/CipherModule-1.4.tar.gz/CipherModule-1.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+setuptools.setup(
+     name='CipherModule',
+     version='1.4',
+     scripts=['Cipher.py'],
+     author="Navid_d",
+     author_email="Navid.dargahi@gmail.com",
+     description="A python module to use ciphers easily",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/Navid-d/Cipher",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
