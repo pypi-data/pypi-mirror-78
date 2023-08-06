1585cb0e9d8d16f560033742fdbcdc98d5d1dd24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:01 +0000

    Add package overcooked-ai/1.0.0/overcooked_ai-1.0.0.tar.gz

---
+++ packages/overcooked-ai/overcooked_ai-1.0.0.tar.gz/overcooked_ai-1.0.0/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='overcooked_ai',
+      version='1.0.0',
+      description='Cooperative multi-agent environment based on Overcooked',
+      author='Nathan Miller',
+      author_email='nathan_miller23@berkeley.edu',
+      url='https://github.com/HumanCompatibleAI/overcooked_ai',
+      download_url='https://github.com/HumanCompatibleAI/human_aware_rl/archive/v1.0.0.tar.gz',
+      packages=find_packages('src'),
+      keywords=['Overcooked', 'AI', 'Reinforcement Learning'],
+      package_dir={"": "src"},
+      package_data={
+        'overcooked_ai_py' : ['data/layouts/*.layout', 'data/planners/*.py', 'data/human_data/*.pickle']
+      },
+      install_requires=[
+        'numpy',
+        'tqdm',
+        'gym',
+        'ipython'
+      ]
+    )
