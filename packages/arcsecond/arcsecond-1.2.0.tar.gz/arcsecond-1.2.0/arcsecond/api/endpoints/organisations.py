8528e64a6c182576170662e67f3c850a66afd32a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:47 +0000

    Add package arcsecond/1.2.0/arcsecond-1.2.0.tar.gz

---
+++ packages/arcsecond/arcsecond-1.2.0.tar.gz/arcsecond-1.2.0/arcsecond/api/endpoints/organisations.py
@@ -0,0 +1,18 @@
+from ._base import APIEndPoint
+
+
+class OrganisationsAPIEndPoint(APIEndPoint):
+    name = 'organisations'
+
+    def _list_url(self, **filters):
+        return self._build_url('organisations', **filters)
+
+    def _detail_url(self, subdomain):
+        return self._build_url('organisations', subdomain)
+
+
+class MembersAPIEndPoint(APIEndPoint):
+    name = 'members'
+
+    def _list_url(self, **filters):
+        return self._build_url('members', **filters)
