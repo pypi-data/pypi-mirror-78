6e799dd43501460abb7656e0385dcd4c61c6c450
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:40 +0000

    Add package distributions-trial-acs/0.1/distributions-trial-acs-0.1.tar.gz

---
+++ packages/distributions-trial-acs/distributions-trial-acs-0.1.tar.gz/distributions-trial-acs-0.1/README.md
@@ -0,0 +1,3 @@
+# Distributions Package
+
+This is package to generate Gaussian and Binomial Distributions.
---
+++ packages/distributions-trial-acs/distributions-trial-acs-0.1.tar.gz/distributions-trial-acs-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions-trial-acs',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions-trial-acs'],
+      zip_safe=False)
