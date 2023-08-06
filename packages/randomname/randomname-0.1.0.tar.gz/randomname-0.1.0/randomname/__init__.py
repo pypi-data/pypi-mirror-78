65cfff2df3c0fe5d6ce2178e4dffef1e1d4e8ac2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:56 +0000

    Add package randomname/0.1.0/randomname-0.1.0.tar.gz

---
+++ packages/randomname/randomname-0.1.0.tar.gz/randomname-0.1.0/randomname/__init__.py
@@ -0,0 +1,18 @@
+import functools
+from .core import *
+from . import util
+
+def main():
+    # need to wrap these so they go to stdout - idk whats going on
+    @functools.wraps(get_name)
+    def print_name(*a, **kw):
+        print(get_name(*a, **kw))
+
+    @functools.wraps(generate)
+    def print_generate(*a, **kw):
+        print(generate(*a, **kw))
+
+    import fire
+    fire.Fire({
+        'get': print_name, 'generate': print_generate,
+        'available': available, 'sample': sample, 'util': util})
