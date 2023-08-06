35f80876404a8a4d3124539e13be65cfdb71c632
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:56 +0000

    Add package gong-test-spider/0.0.2/gong_test_spider-0.0.2.tar.gz

---
+++ packages/gong-test-spider/gong_test_spider-0.0.2.tar.gz/gong_test_spider-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gong_test_spider',
+    version='0.0.2',
+    author='gongzhengyang',
+    author_email='2976560783@qq.com',
+    url='https://gitee.com/gongzhengyang/super-spider'
+)
+
+
