068805e4ea89419e4ac08ea628a5e5b525159384
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:52 +0000

    Add package Truvari/2.0.2/Truvari-2.0.2.tar.gz

---
+++ packages/Truvari/Truvari-2.0.2.tar.gz/Truvari-2.0.2/truvari/annos/__init__.py
@@ -0,0 +1,18 @@
+""" annotation modules """
+from truvari.annos.gccontent import gcpct_main
+from truvari.annos.gtcnt import gtcnt_main
+from truvari.annos.trf import trf_main
+from truvari.annos.grm import grm_main
+from truvari.annos.repmask import rmk_main
+from truvari.annos.remap import remap_main
+from truvari.annos.hompct import hompct_main
+
+__all__ = [
+    'gcpct_main',
+    'gtcnt_main',
+    'trf_main',
+    'grm_main',
+    'rmk_main',
+    'remap_main',
+    'hompct_main',
+]
