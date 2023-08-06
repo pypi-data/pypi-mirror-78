5a0de4a403a3e4c2325a7e57e182826e7d5679f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:55 +0000

    Add package uncvalue/0.1.1/uncvalue-0.1.1-py3-none-any.whl

---
+++ packages/uncvalue/uncvalue-0.1.1-py3-none-any.whl/uncvalue/__init__.py
@@ -0,0 +1,9 @@
+from .uncvalue import Value, val, unc, set_unc
+
+__all__ = ['Value', 'val', 'unc', 'set_unc']
+
+__title__ = 'uncvalue'
+__version__ = '0.1.1'
+__author__ = 'Physics Simulations <apuntsdefisica@gmail.com>'
+__license__ = 'Apache, see LICENSE'
+__copyright__ = 'Copyright 2020 Physics-Simulations'
