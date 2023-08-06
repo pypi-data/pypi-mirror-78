f1766cf0e249e63429dafd61ec9da56f79b3fe21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:56 +0000

    Add package speedcord/0.0.6/speedcord-0.0.6.tar.gz

---
+++ packages/speedcord/speedcord-0.0.6.tar.gz/speedcord-0.0.6/speedcord/gateway.py
@@ -0,0 +1,16 @@
+"""
+Created by Epic at 9/2/20
+"""
+from logging import getLogger
+
+
+class DefaultGatewayHandler:
+    def __init__(self, client):
+        self.client = client
+        self.logger = getLogger("speedcord.gateway")
+
+    async def on_receive(self, data: dict):
+        self.logger.debug("Data received: " + str(data))
+        self.client.opcode_dispatcher.dispatch(data["op"], data)
+        if "s" in data.keys() and data["s"] is not None:
+            self.client.last_event_received = data["s"]
