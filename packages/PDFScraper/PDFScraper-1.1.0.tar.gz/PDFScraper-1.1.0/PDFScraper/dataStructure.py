73f36d5da9dd90d071982775f9c15d9e22db36c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:18:07 +0000

    Add package PDFScraper/1.1.0/PDFScraper-1.1.0.tar.gz

---
+++ packages/PDFScraper/PDFScraper-1.1.0.tar.gz/PDFScraper-1.1.0/PDFScraper/dataStructure.py
@@ -0,0 +1,40 @@
+
+class Documents:
+    def __init__(self, path: str):
+        self.num_docs = 0
+        self.docs = []
+        self.path = path
+
+
+class Document:
+    def __init__(self, path: str, parent: Documents, is_pdf: bool):
+        self.parent = parent
+        self.isPDF = is_pdf
+        self.author = "unknown"
+        self.producer = "unknown"
+        self.subject = "unknown"
+        self.title = "unknown"
+        self.path = path
+        self.ocr_path = path
+        self.num_pages = None
+        self.images = []
+        self.tables = []
+        self.paragraphs = []
+        self.table_of_contents = []
+        self.page_layouts = []
+        self.doc = None
+        self.extractable = False
+        self.filename = None
+
+    def document_info_to_string(self):
+        return "Author: " + self.author + "\n" \
+               + "Producer: " + self.producer + "\n" \
+               + "Subject: " + self.subject + "\n" \
+               + "Title: " + self.title + "\n" \
+               + "Number of Pages: " + str(self.num_pages)
+
+    def table_of_contents_to_string(self):
+        output_string = ""
+        for tup in self.table_of_contents:
+            output_string += str(tup[0]) + ': ' + tup[1] + '\n'
+        return output_string
