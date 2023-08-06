198b17b3e23423688f0c4e38c9d4eaaae87ad5d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:01 +0000

    Add package pogoocr/0.0.1/PogoOCR-0.0.1.tar.gz

---
+++ packages/pogoocr/PogoOCR-0.0.1.tar.gz/PogoOCR-0.0.1/PogoOCR/__init__.py
@@ -0,0 +1,19 @@
+"""
+PogoOCR
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+A Python tool for running OCR on Pokemon Screenshots using Google Cloud Vision
+
+:copyright: (c) 2018 TrainerDex
+:licence: Undecided, ask permission before using.
+
+"""
+
+__title__ = "PogoOCR"
+__author__ = "JayTurnr"
+__licence__ = "GNU GENERAL PUBLIC LICENSE 3.0"
+__copyright__ = "2018 TrainerDex"
+__version__ = "0.0.1"
+
+from .cloudvision import *
+from .types import *
---
+++ packages/pogoocr/PogoOCR-0.0.1.tar.gz/PogoOCR-0.0.1/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+import re
+
+with open("README.rst", "r") as f:
+    readme = f.read()
+
+with open("PogoOCR/__init__.py", "r") as f:
+    version = re.search(r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read()).group(1)
+
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name="PogoOCR",
+    version=version,
+    author="Jay Turner",
+    author_email="jay@trainerdex.co.uk",
+    description="A Python tool for running OCR on Pokemon Screenshots",
+    long_description=readme,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/TrainerDex/PogoOCR",
+    packages=setuptools.find_packages(),
+    data_files=[("PogoOCR", ["PogoOCR/testing_images.yaml"])],
+    install_requires=requirements,
+    python_requires=">=3.6",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
