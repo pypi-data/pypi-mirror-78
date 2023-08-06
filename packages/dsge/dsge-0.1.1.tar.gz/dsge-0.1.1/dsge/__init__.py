2b9b0b4a16ccb8cb627ad4dada1527c93a1f716c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:17 +0000

    Add package dsge/0.1.1/dsge-0.1.1.tar.gz

---
+++ packages/dsge/dsge-0.1.1.tar.gz/dsge-0.1.1/dsge/__init__.py
@@ -0,0 +1,7 @@
+import dsge.tests
+
+import dsge.DSGE as DSGE
+
+from numpy.testing import Tester
+
+test = Tester().test
---
+++ packages/dsge/dsge-0.1.1.tar.gz/dsge-0.1.1/setup.py
@@ -0,0 +1,37 @@
+import numpy.distutils.core
+
+from setuptools import find_packages
+
+
+def build_ext(config):
+
+    config_dict = config.todict()
+    config_dict.pop("name")
+    return config_dict
+
+
+if __name__ == "__main__":
+
+    from numpy.distutils.misc_util import Configuration
+
+    config_dict = build_ext(Configuration("dsge", parent_package=None, top_path=None))
+    numpy.distutils.core.setup(
+        name="dsge",
+        version="0.1.1",
+        platforms="linux",
+        packages=find_packages(),
+        test_suite="nose.collector",
+        tests_require=["nose"],
+        package_data={
+            "dsge": [
+                "examples/ar1/*",
+                "examples/DGS/*",
+                "examples/edo/*",
+                "examples/nkmp/*",
+                "examples/schorf_phillips_curve/*",
+                "examples/simple-model/*",
+                "examples/sw/*",
+            ]
+        },
+        install_requires=["pandas", "scipy" "sympy", "pyyaml", "numba"],
+    )
