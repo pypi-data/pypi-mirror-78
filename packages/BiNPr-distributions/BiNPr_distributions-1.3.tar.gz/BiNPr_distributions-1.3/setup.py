7426a8e14dde6a230723efe996b6d46ccf1eb872
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:12 +0000

    Add package BiNPr-distributions/1.3/BiNPr_distributions-1.3.tar.gz

---
+++ packages/BiNPr-distributions/BiNPr_distributions-1.3.tar.gz/BiNPr_distributions-1.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='BiNPr_distributions',
+      version='1.3',
+      description='Gaussian and Binomial probability distributions',
+      packages=['BiNPr_distributions'],
+      author = 'Ahmed Md',
+      author_email = 'freda31k@gmail.com',
+      zip_safe=False)
