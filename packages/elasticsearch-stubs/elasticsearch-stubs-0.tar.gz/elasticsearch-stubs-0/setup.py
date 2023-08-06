0062c7b55e3f4ac5508c6fc634e9e18e412dfe1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:27 +0000

    Add package elasticsearch-stubs/0/elasticsearch-stubs-0.tar.gz

---
+++ packages/elasticsearch-stubs/elasticsearch-stubs-0.tar.gz/elasticsearch-stubs-0/setup.py
@@ -0,0 +1,12 @@
+import sys
+from setuptools import setup
+
+setup(
+    name="elasticsearch-stubs",
+    version="0",
+    description="Reserving this package for future use",
+    url="https://github.com/elastic/elasticsearch",
+    license="Apache-2.0",
+    author="Seth Michael Larson",
+    author_email="seth.larson@elastic.co"
+)
