282aa1b05efe3c368490c5dc3a966371ec258fa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:19 +0000

    Add package python-sonarqube-api/1.0.7/python_sonarqube_api-1.0.7-py3-none-any.whl

---
+++ packages/python-sonarqube-api/python_sonarqube_api-1.0.7-py3-none-any.whl/sonarqube/auth.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Author: Jialiang Shi
+from sonarqube.config import (
+    API_AUTH_LOGIN_ENDPOINT,
+    API_AUTH_LOGOUT_ENDPOINT,
+    API_AUTH_VALIDATE_ENDPOINT
+)
+
+
+class SonarQubeAuth:
+    def __init__(self, sonarqube):
+        self.sonarqube = sonarqube
+
+    def authenticate_user(self, login, password):
+        """
+        Authenticate a user.
+
+        :param login: Login of the user
+        :param password: Password of the user
+        :return:
+        """
+        params = {
+            'login': login,
+            'password': password
+        }
+        self.sonarqube.make_call('post', API_AUTH_LOGIN_ENDPOINT, **params)
+
+    def logout_user(self):
+        """
+        Logout a user.
+
+        :return:
+        """
+        self.sonarqube.make_call('post', API_AUTH_LOGOUT_ENDPOINT)
+
+    def check_credentials(self):
+        """
+        Check credentials.
+
+        :return:
+        """
+        resp = self.sonarqube.make_call('get', API_AUTH_VALIDATE_ENDPOINT)
+        return resp.json()
