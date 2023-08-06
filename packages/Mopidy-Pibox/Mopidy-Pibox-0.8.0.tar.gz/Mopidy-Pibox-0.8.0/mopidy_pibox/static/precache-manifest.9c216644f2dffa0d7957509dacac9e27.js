b91e5141d92937779433eee8221c41c7ec8cd7ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:18 +0000

    Add package Mopidy-Pibox/0.8.0/Mopidy-Pibox-0.8.0.tar.gz

---
+++ packages/Mopidy-Pibox/Mopidy-Pibox-0.8.0.tar.gz/Mopidy-Pibox-0.8.0/mopidy_pibox/static/precache-manifest.9c216644f2dffa0d7957509dacac9e27.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9a5fe5d10b13a99c0e9991d6963a700",
+    "url": "/pibox/index.html"
+  },
+  {
+    "revision": "2727da2b50c5f2a70637",
+    "url": "/pibox/static/css/2.d97600f8.chunk.css"
+  },
+  {
+    "revision": "cfba095140b9724f3dd8",
+    "url": "/pibox/static/css/main.bfa7af45.chunk.css"
+  },
+  {
+    "revision": "2727da2b50c5f2a70637",
+    "url": "/pibox/static/js/2.98e58030.chunk.js"
+  },
+  {
+    "revision": "0980bcfca5e63f1a4bd6d55d4b972d54",
+    "url": "/pibox/static/js/2.98e58030.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfba095140b9724f3dd8",
+    "url": "/pibox/static/js/main.53e5fb90.chunk.js"
+  },
+  {
+    "revision": "167131e9f52b1c74ba69",
+    "url": "/pibox/static/js/runtime-main.7ff7a994.js"
+  },
+  {
+    "revision": "1434bbda5ac53cf3811b429a2d9e85e5",
+    "url": "/pibox/static/media/placeholder.1434bbda.png"
+  }
+]);
