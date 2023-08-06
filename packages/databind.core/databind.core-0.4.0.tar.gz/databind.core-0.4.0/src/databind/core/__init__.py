44916a72ef806dd9c6795bb91ed066053fa082ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:00 +0000

    Add package databind.core/0.4.0/databind.core-0.4.0.tar.gz

---
+++ packages/databind.core/databind.core-0.4.0.tar.gz/databind.core-0.4.0/src/databind/core/__init__.py
@@ -0,0 +1,14 @@
+
+#![feature(dataclasses)]
+#![feature(dict_ordered)]
+#![feature(fstrings)]
+#![feature(member_annotations)]
+
+__author__ = 'Niklas Rosenstein <rosensteinniklas@gmail.com>'
+__version__ = '0.4.0'
+
+from ._converter import *
+from ._datamodel import *
+from ._locator import Locator
+from ._typing import *
+from ._union import *
