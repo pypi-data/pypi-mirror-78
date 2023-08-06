6e670f708034730f49c0089bd92578a77990bd34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:58 +0000

    Add package neural-map/0.0.2/neural_map-0.0.2.tar.gz

---
+++ packages/neural-map/neural_map-0.0.2.tar.gz/neural_map-0.0.2/neural_map/__init__.py
@@ -0,0 +1,4 @@
+from neural_map._neural_map import *
+from neural_map._plot import *
+from neural_map._decay_functions import *
+from neural_map._neighbourhood_functions import *
---
+++ packages/neural-map/neural_map-0.0.2.tar.gz/neural_map-0.0.2/neural_map/_decay_functions.py
@@ -0,0 +1,22 @@
+from numba import jit, float64
+
+
+@jit(float64(float64, float64, float64, float64), nopython=True, fastmath=True)
+def linear(init, final, total_steps, step):
+    return init + step * (final - init) / (total_steps - 1)
+
+
+@jit(float64(float64, float64, float64, float64), nopython=True, fastmath=True)
+def exponential(init, final, total_steps, step):
+    return init * (final / init) ** (step / (total_steps - 1))
+
+
+@jit(float64(float64, float64, float64, float64), nopython=True, fastmath=True)
+def rational(init, final, total_steps, step):
+    value = (total_steps - 1) / ((init / final) - 1)
+    return init * value / (value + step)
+
+
+@jit(float64(float64, float64, float64, float64), nopython=True, fastmath=True)
+def no_decay(init, final, total_steps, step):
+    return init
