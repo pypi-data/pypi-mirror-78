dfa44fffec26d1ad14dc07f89e30d3c809fa4ee9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:30 +0000

    Add package rabbitmq-base-library/0.8.5/rabbitmq-base-library-0.8.5.tar.gz

---
+++ packages/rabbitmq-base-library/rabbitmq-base-library-0.8.5.tar.gz/rabbitmq-base-library-0.8.5/rabbitmqbaselibrary/common/config.py
@@ -0,0 +1,19 @@
+import configparser
+import os
+
+
+class Config(object):
+
+    def __init__(self, path_file: str):
+        self.path_file = path_file
+        self.config = configparser.ConfigParser()
+        self.config.read(path_file)
+
+    def get_value(self, section: str, key: str) -> str:
+        return self.config[section][key]
+
+    def get_path(self, key: str) -> str:
+        return os.path.abspath(os.path.join(os.path.dirname(self.path_file), self.config['Paths'][key]))
+
+    def get_file_path(self, key: str, file_name: str) -> str:
+        return os.path.join(self.get_path(key=key), file_name)
