f49cb8e3df546093f4c70903c246193de325a695
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:02 +0000

    Add package printree/0.1.1/printree-0.1.1.tar.gz

---
+++ packages/printree/printree-0.1.1.tar.gz/printree-0.1.1/README.md
@@ -0,0 +1,65 @@
+# printree
+
+[![Build Status](https://travis-ci.org/chrizzFTD/printree.svg?branch=master)](https://travis-ci.org/chrizzFTD/printree)
+[![Coverage Status](https://coveralls.io/repos/github/chrizzFTD/printree/badge.svg?branch=master)](https://coveralls.io/github/chrizzFTD/printree?branch=master)
+[![Documentation Status](https://readthedocs.org/projects/printree/badge/?version=latest)](https://printree.readthedocs.io/en/latest/?badge=latest)
+[![PyPI version](https://badge.fury.io/py/printree.svg)](https://badge.fury.io/py/printree)
+[![PyPI](https://img.shields.io/pypi/pyversions/printree.svg)](https://pypi.python.org/pypi/printree)
+
+Tree-like formatting for arbitrary python data structures.
+
+Similar to pretty print ([pprint](https://docs.python.org/3/library/pprint.html)) but in the form of a tree:
+
+```python
+>>> from printree import ptree, ftree
+>>> ptree({"x", len, 42})  # will print to the output console
+`- . [items=3]
+   |- 0: <built-in function len>
+   |- 1: 42
+   `- 2: x
+>>> ftree({"x", len, 42})  # will return a string representation
+'`- . [items=3]\n   |- 0: <built-in function len>\n   |- 1: 42\n   `- 2: x'
+```
+
+Instances of [abc.Iterable](https://docs.python.org/3/library/collections.abc.html#collections.abc.Iterable) (with the exception of [str](https://docs.python.org/3/library/stdtypes.html#text-sequence-type-str) & [bytes](https://docs.python.org/3/library/stdtypes.html#bytes-objects)) should be translated into a tree-like form.
+All other objects will be considered "leaf nodes":
+```python
+>>> dct = {
+...     "foo": [],
+...     True: {
+...         "uno": {"ABC", "XYZ"},
+...         "dos": r"B:\newline\tab\like.ext",
+...         "tres": {
+...             "leaf": b"bytes",
+...             "numbers": (42, -17, 0.01)
+...         },
+...     },
+...     ("unsortable", ("tuple", "as", "key")):
+...         {"multi\nlined\n\ttabbed key": "multi\nline\n\ttabbed value"}
+... }
+>>> dct["recursion"] = [1, dct, 2]
+>>> ptree(dct)
+`- . [items=4]
+   |- foo [empty]
+   |- True [items=3]
+   |  |- dos: B:\newline\tab\like.ext
+   |  |- tres [items=2]
+   |  |  |- leaf: b'bytes'
+   |  |  `- numbers [items=3]
+   |  |     |- 0: -17
+   |  |     |- 1: 0.01
+   |  |     `- 2: 42
+   |  `- uno [items=2]
+   |     |- 0: ABC
+   |     `- 1: XYZ
+   |- ('unsortable', ('tuple', 'as', 'key')) [items=1]
+   |  `- multi
+   |     lined
+   |            tabbed key: multi
+   |                        line
+   |                            tabbed value
+   `- recursion [items=3]
+      |- 0: 1
+      |- 1: <Recursion on dict with id=2317960566912>
+      `- 2: 2
+```
---
+++ packages/printree/printree-0.1.1.tar.gz/printree-0.1.1/printree/_ptree.py
@@ -0,0 +1,106 @@
+import textwrap
+import contextvars
+from pprint import isrecursive
+from itertools import count
+from collections import abc
+
+_recursive_ids = contextvars.ContextVar('recursive')
+
+
+def ptree(obj, /) -> None:
+    """Print a tree-like representation of the given object data structure.
+
+    :py:class:`collections.abc.Iterable` instances will be branches, with the exception of :py:class:`str` and :py:class:`bytes`.
+    All other objects will be leaves.
+
+    Examples:
+        >>> ptree({"x", len, 42})
+        `- . [items=3]
+           |- 0: <built-in function len>
+           |- 1: 42
+           `- 2: x
+
+        >>> dct = {"A": {"x\\ny", (42, -17, 0.01), True}, "B": 42}
+        >>> dct["C"] = dct
+        >>> ptree(dct)
+        `- . [items=3]
+           |- A [items=3]
+           |  |- 0: True
+           |  |- 1: x
+           |  |     y
+           |  `- 2 [items=3]
+           |     |- 0: -17
+           |     |- 1: 0.01
+           |     `- 2: 42
+           |- B: 42
+           `- C: <Recursion on dict with id=140712966998864>
+    """
+    def f():
+        _recursive_ids.set(set())
+        for i in _itree(obj):
+            print(i)
+    ctx = contextvars.copy_context()
+    ctx.run(f)
+
+
+def ftree(obj, /) -> str:
+    """Return the formatted tree representation of the given object data structure as a string."""
+    def f():
+        _recursive_ids.set(set())
+        return "\n".join(_itree(obj))
+    ctx = contextvars.copy_context()
+    return ctx.run(f)
+
+
+def _newline_repr(obj_repr, /, prefix) -> str:
+    counter = count()
+    newline = lambda x: next(counter) != 0
+    return textwrap.indent(obj_repr, prefix, newline)
+
+
+def _itree(obj, /, subscription=".", prefix="", last=True):
+    children = []
+    level_suffix = '   ' if last else '|  '
+    newline_prefix = f"{prefix}{level_suffix}"
+    subscription_repr = _newline_repr(f"{subscription}", newline_prefix)
+    recursive_ids = _recursive_ids.get()
+    recursive = isrecursive(obj)
+    objid = id(obj)
+    if recursive and objid in recursive_ids:
+        item_repr = f": <Recursion on {type(obj).__name__} with id={objid}>"
+    elif isinstance(obj, (str, bytes)):
+        # for text, indent new lines with an appropiate prefix so that
+        # a string like "new\nline" is adjusted to something like:
+        #      ...
+        #      |- 42: new
+        #      |      line
+        #      ...
+        # for this, calculate how many characters each new line should have for padding
+        # based on the last line from the subscription repr
+        prefix_len = len(newline_prefix)
+        no_prefix = subscription_repr.splitlines()[-1].expandtabs()[prefix_len:]
+        newline_padding = len(no_prefix) + prefix_len + 2  # last 2 are ": " below
+        item_repr = _newline_repr(f': {obj}', f"{newline_prefix:<{newline_padding}}")
+    elif isinstance(obj, abc.Iterable):
+        # for other iterable objects, sort and ennumerate so that we can anticipate what
+        # prefix we should use (e.g. are we the last item in the iteration?)
+        ismap = isinstance(obj, abc.Mapping)
+        enumerateable = obj.items() if ismap else obj
+        accessor = (lambda i, v: (i, *v)) if ismap else lambda i, v: (i, i, v)
+        try:
+            enumerated = enumerate(sorted(enumerateable))
+        except TypeError:  # un-sortable, enumerate as-is
+            enumerated = enumerate(enumerateable)
+        children.extend(accessor(*enum) for enum in enumerated)
+        item_repr = f' [{items=}]' if (items := len(children)) else " [empty]"
+    else:
+        item_repr = f': {obj}'
+
+    if recursive:
+        recursive_ids.add(objid)
+
+    yield f"{prefix}{'`- ' if last else '|- '}{subscription_repr}{item_repr}"
+    prefix += level_suffix
+    child_count = len(children)
+    for index, key, value in children:
+        yield from _itree(value, subscription=key, prefix=prefix, last=index == (child_count - 1))
