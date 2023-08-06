e6e2929409f033ddfab05f3f84434b34e2e549a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:17:08 +0000

    Add package 31/0.2/31-0.2.tar.gz

---
+++ packages/31/31-0.2.tar.gz/31-0.2/s31/screen.py
@@ -0,0 +1,16 @@
+import subprocess
+import re
+
+
+def get_screen_program(name):
+    if name == "screen":
+        return _screen
+    else:
+        raise RuntimeError("Only screen is currently supported as a screen_program")
+
+
+def _screen(command, name):
+    name = re.sub(r"[^A-Za-z0-9-., ]", "?", name)
+    screen_command = ["screen", "-S", name, "-d", "-m", "--", *command]
+    print(screen_command)
+    subprocess.check_call(screen_command)
