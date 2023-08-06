aa16eaa1faf0320224d1ca4faeeb768196eff2ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:17 +0000

    Add package mab-hakuinadvisors/0.1.1/mab-hakuinadvisors-0.1.1.tar.gz

---
+++ packages/mab-hakuinadvisors/mab-hakuinadvisors-0.1.1.tar.gz/mab-hakuinadvisors-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['mab']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'mab-hakuinadvisors',
+    'version': '0.1.1',
+    'description': 'Multiarm bandit solutions',
+    'long_description': None,
+    'author': 'Alexey',
+    'author_email': 'butirev@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
