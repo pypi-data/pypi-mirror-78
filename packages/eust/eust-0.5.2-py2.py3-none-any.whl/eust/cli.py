5cf8f4ce96a42b2701ee757ff270e77ea8d4f80b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:30 +0000

    Add package eust/0.5.2/eust-0.5.2-py2.py3-none-any.whl

---
+++ packages/eust/eust-0.5.2-py2.py3-none-any.whl/eust/cli.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+
+"""Console script for eust."""
+import sys
+import click
+import eust
+
+
+@click.group()
+def main():
+    pass
+
+
+@main.group()
+def download():
+    pass
+
+
+@download.command()
+@click.argument("tables", type=str, nargs=-1)
+def table(tables):
+    for table in tables:
+        eust.download_table(table)
+
+
+@download.command()
+def nuts_codes():
+    eust.download_nuts_codes()
+
+
+def _iter_table_versions_pairs(latest):
+    for table in eust.list_tables():
+        versions = eust.list_table_versions(table)
+        if latest:
+            versions = versions[-1:]
+
+        for version in versions:
+            yield (table, version)
+
+
+@main.command()
+@click.option(
+    "--versions/--no-versions",
+    is_flag=True,
+    default=True,
+    help="Print version information. (--no-versions implies --latest)",
+)
+@click.option(
+    "--latest",
+    is_flag=True,
+    default=False,
+    help="Print the latest available version only.",
+)
+def list_tables(versions, latest):
+    if not versions:
+        latest = True
+
+    for table, version in _iter_table_versions_pairs(latest):
+        if not versions:
+            click.echo(table)
+        else:
+            click.echo(f"{table:<20} {version}")
+
+
+@main.group()
+def config():
+    pass
+
+
+@config.command()
+def list_paths():
+    for p in eust.list_config_paths():
+        click.echo(p)
+
+
+@config.command()
+@click.argument("key", type=str)
+def read(key):
+    click.echo(eust.conf[key])
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
---
+++ packages/eust/eust-0.5.2-py2.py3-none-any.whl/eust/nuts.py
@@ -0,0 +1,151 @@
+# -*- coding: utf-8 -*-
+
+from typing import Union
+from pathlib import Path
+from tempfile import TemporaryDirectory
+import zipfile
+import os
+import shutil
+
+import pandas as pd
+
+from eust.core import conf, _get_abs_path, _NUTS_DIR, _download_file
+
+
+_EXCEL_COLS = {
+    "COUNTRY": "country_code",  # In NUTS 2006 file
+    "CODE": "geo",
+    "LABEL": "label",
+    "NUTS_LEVEL": "nuts_level",
+    "COUNTRY CODE": "country_code",  # In NUTS 2010, 2013, 2016 files
+    "NUTS CODE": "geo",
+    "NUTS LEVEL": "nuts_level",
+    "NUTS LABEL": "label",
+}
+
+COLS_TO_KEEP = [
+    "geo",
+    "country_code",
+    "nuts_level",
+    "label",
+    "parent_geo",
+]
+
+PARENT_COLNAME = "parent_geo"
+
+
+def _read_and_transform_excel_file(excel_path):
+    indata = pd.read_excel(excel_path)
+    d = indata.reindex(list(_EXCEL_COLS), axis=1).dropna(axis=1, how="all")
+    d = d.rename(columns=_EXCEL_COLS)
+    parents = d[d.nuts_level > 0].geo.str[0:-1]
+    d[PARENT_COLNAME] = parents
+    d = d[COLS_TO_KEEP]
+    d.set_index("geo", inplace=True)
+    d = d.sort_index()
+    assert len(d.index) == len(d.index.unique())
+    return d
+
+
+def _read_csv_file(csv_path):
+    return pd.read_csv(csv_path, index_col="geo")
+
+
+_EXCEL_TEMPLATES = ["NUTS_{year}.xlsx", "NUTS_{year}.xls"]
+_CSV_TEMPLATE = "NUTS_{year}.csv"
+
+
+def _get_nuts_dir():
+    return _get_abs_path(_NUTS_DIR)
+
+
+def _try_read_options(year):
+    the_dir = _get_nuts_dir()
+
+    csv_path = the_dir / _CSV_TEMPLATE.format(year=year)
+    excel_paths = [
+        the_dir / template.format(year=year) for template in _EXCEL_TEMPLATES
+    ]
+
+    d = None
+
+    try:
+        return _read_csv_file(csv_path)
+    except FileNotFoundError:
+        pass
+
+    for excel_path in excel_paths:
+        try:
+            d = _read_and_transform_excel_file(excel_path)
+            d.to_csv(csv_path)
+        except FileNotFoundError:
+            pass
+
+    if d is None:
+        raise FileNotFoundError(f"no NUTS {year} file found")
+
+    return d
+
+
+_COUNTRY_NAMES_LANGUAGE = "English"
+_COUNTRY_NAMES_FILENAME = "country_names.csv"
+
+
+def read_country_names():
+    path = _get_nuts_dir() / _COUNTRY_NAMES_FILENAME
+    d = (
+        pd.read_csv(path)
+        .rename(
+            columns={"Code": "country_code", _COUNTRY_NAMES_LANGUAGE: "country"}
+        )
+        .set_index("country_code")["country"]
+    )
+
+    return d
+
+
+def read_nuts_codes(
+    year: Union[str, int], drop_extra_regio=False
+) -> pd.DataFrame:
+    d = _try_read_options(year)
+
+    d = d.join(read_country_names(), on="country_code")
+
+    if drop_extra_regio:
+        num_countries = len(d.country.unique())
+        is_extra_regio = (d.nuts_level > 0) & d.index.str.endswith("Z")
+        num_extra_regio = is_extra_regio.sum()
+        assert num_extra_regio == 3 * num_countries, num_extra_regio
+        d = d[~is_extra_regio]
+
+    return d
+
+
+def download_nuts_codes():
+    rel_subdir = conf["nuts_zip_subdir"]
+    if not rel_subdir.endswith("/"):
+        rel_subdir += "/"
+
+    target_dir = _get_nuts_dir()
+    os.makedirs(target_dir, exist_ok=True)
+    with TemporaryDirectory() as tempdir:
+        tempdir = Path(tempdir)
+        zip_path = tempdir / "archive.zip"
+        _download_file(conf["nuts_zip_url"], zip_path)
+
+        archive = zipfile.ZipFile(zip_path)
+        if rel_subdir not in archive.namelist():
+            raise ValueError(
+                f"zip archive does not contain expected subdir {rel_subdir}"
+            )
+
+        archive.extractall(path=tempdir)
+        temp_subdir = tempdir / rel_subdir
+        assert temp_subdir.exists(), list(tempdir.iterdir())
+
+        for src_path in temp_subdir.iterdir():
+            relpath = src_path.relative_to(temp_subdir)
+            dst_path = target_dir / relpath
+
+            if not dst_path.exists():
+                shutil.move(src_path, dst_path)
