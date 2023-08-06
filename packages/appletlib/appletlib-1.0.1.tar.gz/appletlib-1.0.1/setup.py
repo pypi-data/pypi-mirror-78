bc0301d12011f22249921eba0b412226460f081a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:08 +0000

    Add package appletlib/1.0.1/appletlib-1.0.1.tar.gz

---
+++ packages/appletlib/appletlib-1.0.1.tar.gz/appletlib-1.0.1/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='appletlib',
+    version='1.0.1',
+    author='Joerg Mechnich',
+    author_email='joerg.mechnich@gmail.com',
+    description='Python library providing a QSystemTrayIcon wrapper',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/jmechnich/appletlib',
+    packages=setuptools.find_packages(),
+    install_requires=["python-daemon", "PyQt5"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
+    ],
+    python_requires='>=3.6',
+)
