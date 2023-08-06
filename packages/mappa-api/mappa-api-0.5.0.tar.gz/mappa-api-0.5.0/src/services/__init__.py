cdfe8b7d0c9ae8764e5c29c9d8ecdbaa2de57bac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:09 +0000

    Add package mappa-api/0.5.0/mappa-api-0.5.0.tar.gz

---
+++ packages/mappa-api/mappa-api-0.5.0.tar.gz/mappa-api-0.5.0/src/services/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ['MAPPAService', 'EscotistaService', 'KBService']
+
+from .escotista_service import EscotistaService
+from .kb_service import KBService
+from .mappa_service import MAPPAService
