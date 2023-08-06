5924d34ac59b353fe81fad5ca45d599309af291a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:20 +0000

    Add package osnma-core/0.0.10/osnma_core-0.0.10.tar.gz

---
+++ packages/osnma-core/osnma_core-0.0.10.tar.gz/osnma_core-0.0.10/auxiliar_data/exceptions.py
@@ -0,0 +1,13 @@
+"""Custom exceptions for the osnma_core package
+"""
+class MissingFieldData(Exception):
+    """Custom exception to be called when a field needed to perfom
+    an operation has no data.
+    """
+    pass
+
+class MissingFieldSize(Exception):
+    """Custom exception to be called when reading the fields from a
+    OSNMA message and the field has no size in it.
+    """
+    pass
