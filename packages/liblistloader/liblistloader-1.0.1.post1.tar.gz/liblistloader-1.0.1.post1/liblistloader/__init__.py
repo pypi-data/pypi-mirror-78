36830b11de64c3adb2083d82ad530619b80f17e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:33 +0000

    Add package liblistloader/1.0.1.post1/liblistloader-1.0.1.post1.tar.gz

---
+++ packages/liblistloader/liblistloader-1.0.1.post1.tar.gz/liblistloader-1.0.1.post1/liblistloader/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Word List Loader.
+
+by BBaoVanC
+
+This package allows for easy loading of word lists.
+Built for https://git.bbaovanc.com/bbaovanc/libnamegen
+
+Copyright (C) 2020 BBaoVanC
+"""
+
+# Imports
+import liblistloader.desiquintans_nounlist  # NOQA
