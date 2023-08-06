da0d6e48af87fbbb98b67358d05b8a98a7fd8a99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:56 +0000

    Add package OreoML/0.0.2/OreoML-0.0.2.win-amd64.zip

---
+++ packages/OreoML/OreoML-0.0.2.win-amd64.zip/Users/Harish/AppData/Local/Programs/Python/Python37/Scripts/oreoml_train-script.py
@@ -0,0 +1,12 @@
+#!C:\Users\Harish\AppData\Local\Programs\Python\Python37\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'OreoML==0.0.2','console_scripts','oreoml_train'
+__requires__ = 'OreoML==0.0.2'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('OreoML==0.0.2', 'console_scripts', 'oreoml_train')()
+    )
