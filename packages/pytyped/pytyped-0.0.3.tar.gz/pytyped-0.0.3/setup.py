89b06e0f42541b2df2aeae1528e08c822e75a0b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:07 +0000

    Add package pytyped/0.0.3/pytyped-0.0.3.tar.gz

---
+++ packages/pytyped/pytyped-0.0.3.tar.gz/pytyped-0.0.3/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pytyped",
+    version="0.0.3",
+    author="Shahab Tasharrofi",
+    author_email="shahab.tasharrofi@gmail.com",
+    description="Type-Driven Development for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/stasharrofi/pytyped",
+    packages=['pytyped', 'pytyped.macros', 'pytyped.json', 'pytyped.metrics'],
+    package_data= {
+        'pytyped': ['py.typed'],
+        'pytyped.macros': ['py.typed'],
+        'pytyped.json': ['py.typed'],
+        'pytyped.metrics': ['py.typed']
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    zip_safe=False,
+)
