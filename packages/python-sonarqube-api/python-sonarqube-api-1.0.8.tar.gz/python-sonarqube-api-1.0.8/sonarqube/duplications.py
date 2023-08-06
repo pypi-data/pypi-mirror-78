f4c6ec34a73104c75bb402d48556269fefd63c46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:55 +0000

    Add package python-sonarqube-api/1.0.8/python-sonarqube-api-1.0.8.tar.gz

---
+++ packages/python-sonarqube-api/python-sonarqube-api-1.0.8.tar.gz/python-sonarqube-api-1.0.8/sonarqube/duplications.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Author: Jialiang Shi
+from sonarqube.config import API_DUPLICATIONS_SHOW_ENDPOINT
+
+
+class SonarQubeDuplications:
+    def __init__(self, sonarqube):
+        self.sonarqube = sonarqube
+
+    def get_duplications(self, key):
+        """
+        Get duplications. Require Browse permission on file's project
+
+        :param key: File key
+        :return:
+        """
+        params = {'key': key}
+        resp = self.sonarqube.make_call('get', API_DUPLICATIONS_SHOW_ENDPOINT, **params)
+        return resp.json()
---
+++ packages/python-sonarqube-api/python-sonarqube-api-1.0.8.tar.gz/python-sonarqube-api-1.0.8/sonarqube/languages.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Author: Jialiang Shi
+from sonarqube.config import API_LANGUAGES_LIST_ENDPOINT
+
+
+class SonarQubeLanguages:
+    def __init__(self, sonarqube):
+        self.sonarqube = sonarqube
+
+    def get_supported_programming_languages(self, q=None):
+        """
+        List supported programming languages
+
+        :param q: A pattern to match language keys/names against
+        :return:
+        """
+        params = {}
+        if q:
+            params.update({'q': q})
+        resp = self.sonarqube.make_call('get', API_LANGUAGES_LIST_ENDPOINT, **params)
+        return resp.json()
