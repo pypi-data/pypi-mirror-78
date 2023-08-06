9082d7ff87f118555cd5fbcba15b47055ae13056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:00 +0000

    Add package distributions-load-analyze/0.1/distributions_load_analyze-0.1.tar.gz

---
+++ packages/distributions-load-analyze/distributions_load_analyze-0.1.tar.gz/distributions_load_analyze-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_load_analyze',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_load_analyze'],
+      zip_safe=False)
