1ea814f06a7e3d8e3ba8e7b8bb64ea455939cee4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:38 +0000

    Add package streamlit-hgb/0.0.1/streamlit_hgb-0.0.1.tar.gz

---
+++ packages/streamlit-hgb/streamlit_hgb-0.0.1.tar.gz/streamlit_hgb-0.0.1/streamlit_hgb/frontend/build/precache-manifest.7a3701d7ea0d46d9859141ebc9cea3f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a52061fc0031df932d39d53c87a4d9af",
+    "url": "./index.html"
+  },
+  {
+    "revision": "54241242c84fe9c0474d",
+    "url": "./static/js/2.f83ab78a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.f83ab78a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "105ef3ec16ff7af23547",
+    "url": "./static/js/main.e6772d3d.chunk.js"
+  },
+  {
+    "revision": "6515c66d2a8747a146d578e1c038a822",
+    "url": "./static/js/main.e6772d3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36637762a444a1909dea",
+    "url": "./static/js/runtime-main.b0c38161.js"
+  }
+]);
