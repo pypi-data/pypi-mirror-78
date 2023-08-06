3df62c9b65ee3fb252e064d819695c5ef43456f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:55 +0000

    Add package Arc-api/1.1/Arc_api-1.1.tar.gz

---
+++ packages/Arc-api/Arc_api-1.1.tar.gz/Arc_api-1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+packages = find_packages(include=('Arcapi', 'Arcapi.*'))
+
+setup(
+    name='Arc_api',
+    version='1.1',
+    author='littlebutt',
+    author_email='luogan199686@gmail.com',
+    license='MIT License',
+    url="https://github.com/littlebutt/Arcapi",
+    description='An API for Arc Prober.',
+    packages=packages,
+    install_requires=['websockets>=8.1', 'websocket-client>=0.57.0','brotli>=1.0.9'],
+    python_requires='>=3.7',
+    platforms='any'
+)
