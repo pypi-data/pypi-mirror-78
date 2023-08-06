48343a8e5dbeeb3a5b5c9211092a06265f8c3a4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:14 +0000

    Add package prepnlp/0.0.6/prepnlp-0.0.6.tar.gz

---
+++ packages/prepnlp/prepnlp-0.0.6.tar.gz/prepnlp-0.0.6/setup.py
@@ -0,0 +1,20 @@
+import pathlib
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+HERE = pathlib.Path(__file__).parent
+TESTS_REQUIRE = (HERE / "test-requirements.txt").read_text().splitlines()[1:]
+setup(
+    name='prepnlp',
+    version='0.0.6',
+    description='Lightweight utilities to train nlp tasks for deep learning',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Arie Pratama Sutiono',
+    packages=find_packages(),
+    url='https://github.com/ariepratama/prepnlp',
+    tests_require=TESTS_REQUIRE,
+    python_requires='>=3.5'
+)
