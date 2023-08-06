b6e2adaad05d83035b2264977a3676dba1dcf9a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:12 +0000

    Add package Flask-Image-Search/0.0.7/flask_image_search-0.0.7.tar.gz

---
+++ packages/Flask-Image-Search/flask_image_search-0.0.7.tar.gz/flask_image_search-0.0.7/examples/simple.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_image_search import ImageSearch
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.sqlite"
+db = SQLAlchemy(app)
+image_search = ImageSearch(app, db)
+
+
+@image_search.register()
+class Image(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.Text)
+
+
+image_search.index_model(Image)
+
+images = Image.query.with_transformation(image_search.query_search(image_path='query.jpg', limit=5)).all()
