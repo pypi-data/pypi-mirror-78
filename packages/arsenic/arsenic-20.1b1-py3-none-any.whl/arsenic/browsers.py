8ec15fc1a9c0d93464f266aff974755931e181b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:41 +0000

    Add package arsenic/20.1b1/arsenic-20.1b1-py3-none-any.whl

---
+++ packages/arsenic/arsenic-20.1b1-py3-none-any.whl/arsenic/browsers.py
@@ -0,0 +1,43 @@
+from arsenic.session import Session, CompatSession
+
+
+class Browser:
+    defaults = {}
+    session_class = Session
+
+    def __init__(self, **overrides):
+        self.capabilities = {**self.defaults, **overrides}
+
+
+class Firefox(Browser):
+    defaults = {
+        "browserName": "firefox",
+        "acceptInsecureCerts": True,
+    }
+
+
+class Chrome(Browser):
+    session_class = CompatSession
+    defaults = {"browserName": "chrome"}
+
+
+class PhantomJS(Browser):
+    session_class = CompatSession
+    defaults = {
+        "browserName": "phantomjs",
+        "version": "",
+        "platform": "ANY",
+        "javascriptEnabled": True,
+    }
+
+
+class InternetExplorer(Browser):
+    session_class = Session
+    defaults = {
+        "browserName": "internet explorer",
+        "version": "",
+        "platform": "WINDOWS",
+    }
+
+
+IE = InternetExplorer
