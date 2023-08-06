6928f209df099fbd7a115fda007ceb5ebb1987e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:50 +0000

    Add package operation-mul-div/0.1.2/operation_mul_div-0.1.2.tar.gz

---
+++ packages/operation-mul-div/operation_mul_div-0.1.2.tar.gz/operation_mul_div-0.1.2/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name="operation_mul_div",
+    version="0.1.2",
+    author="Example lc",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description="hehe",
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
