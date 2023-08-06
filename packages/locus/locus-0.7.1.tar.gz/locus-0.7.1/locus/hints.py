1d13721c12ccb1dc7178df9634a184df2896687d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:51 +0000

    Add package locus/0.7.1/locus-0.7.1.tar.gz

---
+++ packages/locus/locus-0.7.1.tar.gz/locus-0.7.1/locus/hints.py
@@ -0,0 +1,7 @@
+from numbers import Real
+from typing import Tuple
+
+Coordinate = Real
+Point = Tuple[Coordinate, ...]
+Segment = Tuple[Point, Point]
+Interval = Tuple[Tuple[Coordinate, Coordinate], ...]
