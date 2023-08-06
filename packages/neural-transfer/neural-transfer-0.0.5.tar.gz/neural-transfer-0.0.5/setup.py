686c3809be89f62d2de13e9d51be6f7addbff762
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:34 +0000

    Add package neural-transfer/0.0.5/neural-transfer-0.0.5.tar.gz

---
+++ packages/neural-transfer/neural-transfer-0.0.5.tar.gz/neural-transfer-0.0.5/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="neural-transfer", # Replace with your own username
+    version="0.0.5",
+    author="CarlosH",
+    author_email="iwishtherewasone@gmail.com",
+    description="A small example package",
+    install_requires=["torch", "torchvision",
+                      "argparse", "matplotlib"],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 1 - Planning"
+        ],
+    package_data={
+        ## All the images need to be in jpg format
+        "": ["*.jpg"] },
+    python_requires='>=3.6',
+)
