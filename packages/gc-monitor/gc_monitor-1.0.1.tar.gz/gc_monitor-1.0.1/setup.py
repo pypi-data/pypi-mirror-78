bd5368cd7e4cae1b110540e94af747487927dc7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:28 +0000

    Add package gc-monitor/1.0.1/gc_monitor-1.0.1.tar.gz

---
+++ packages/gc-monitor/gc_monitor-1.0.1.tar.gz/gc_monitor-1.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    # dependency_links=['https://pypi.tuna.tsinghua.edu.cn/simple/flask'],
+    name='gc_monitor',
+    description='Test Flask Sample',
+    version='1.0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    author='ChenJie',
+    author_email='chenjie@galachip.com',
+    install_requires=[
+        'Flask', 'blinker', 'Flask-restplus',
+        'raven',
+        'requests',
+    ],
+    entry_points = {'console_scripts': [
+        'gc_monitor = gc_monitor.monitor:main',
+    ]},
+)
