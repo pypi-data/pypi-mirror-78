6be39cf8c0f0b2f6f5567d7ddb6212401895480a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:46 +0000

    Add package micloud/0.2/micloud-0.2.tar.gz

---
+++ packages/micloud/micloud-0.2.tar.gz/micloud-0.2/setup.py
@@ -0,0 +1,26 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name = 'micloud',
+    packages = ['micloud'],
+    include_package_data=True,
+    version = '0.2',
+    license='MIT',
+    description = 'Xiaomi cloud connect library',
+    author = 'Sammy Svensson',
+    author_email = 'sammy@ssvensson.se',
+    url = 'https://github.com/squachen/micloud',
+    download_url = 'https://github.com/Squachen/micloud/archive/v_0.2.tar.gz',
+    install_requires=[
+        'requests',
+        'tzlocal',
+        'click'
+    ],
+    entry_points='''
+        [console_scripts]
+        micloud=micloud.cli:get_devices
+    ''',
+)
