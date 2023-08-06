59c863c1c42b1f807e519cbe772940f877e0a3ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:06 +0000

    Add package gongtestpub/1.0.8/gongtestpub-1.0.8.tar.gz

---
+++ packages/gongtestpub/gongtestpub-1.0.8.tar.gz/gongtestpub-1.0.8/Publish/gong.py
@@ -0,0 +1,12 @@
+import datetime
+
+
+def first_pub():
+    print(f'this time is {datetime.datetime.today()}')
+
+
+if __name__ == '__main__':
+    first_pub()
+
+
+
---
+++ packages/gongtestpub/gongtestpub-1.0.8.tar.gz/gongtestpub-1.0.8/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+here = path.abspath(path.dirname(__file__))
+
+
+setup(
+    name='gongtestpub',
+    version='1.0.8',
+    description=(
+        '<项目的简单描述>'
+    ),
+    long_description=open('README.md').read(),
+    author='gong',
+    author_email='2976560783@qq.com',
+    maintainer='gong',
+    maintainer_email='2976560783@qq.com',
+    license='Apache License',
+    packages=find_packages(),
+    url='https://gitee.com/gongzhengyang',
+    platforms=["all"],
+    classifiers=[
+    ],
+)
