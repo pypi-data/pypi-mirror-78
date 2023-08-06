8c2b36276d2e02c7c36075bfeb6c24ddc946086e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:21 +0000

    Add package paradise/0.0.2/paradise-0.0.2.tar.gz

---
+++ packages/paradise/paradise-0.0.2.tar.gz/paradise-0.0.2/src/paradise/run.py
@@ -0,0 +1,10 @@
+#TODO: Create cmd args for host and port
+
+import uvicorn
+from server import app
+
+def main():
+    uvicorn.run("run:app", host="0.0.0.0", port=8000, log_level="info")
+
+if __name__ == "__main__":
+    main()
---
+++ packages/paradise/paradise-0.0.2.tar.gz/paradise-0.0.2/tests/test_command.py
@@ -0,0 +1,7 @@
+#TODO: Check command
+import os
+import pytest
+
+def test_command():
+    # assert os.system("paradise") == 0
+    assert True
