87850c3bdcff585298a4bba2868b92b647906d27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:35 +0000

    Add package pssst/0.2.0/pssst-0.2.0-py3-none-any.whl

---
+++ packages/pssst/pssst-0.2.0-py3-none-any.whl/pssst/errors.py
@@ -0,0 +1,36 @@
+# This file is part of the PSSST Python module
+# Copyright 2020 Nicko van Someren
+# SPDX-License-Identifier: MIT
+# See the LICENSE.md file for full license terms
+
+"""
+Exception definitions for PSSST
+"""
+
+
+class PSSSTException(Exception):
+    """General PSSST exception"""
+
+
+class PSSSTUnsupportedCipher(PSSSTException):
+    """Cipher suite not supported"""
+
+
+class PSSSTClientAuthFailed(PSSSTException):
+    """Client authentation failed"""
+
+
+class PSSSTReplyMismatch(PSSSTException):
+    """Reply packed does not match request"""
+
+
+class PSSSTNotReply(PSSSTException):
+    """Packet is not a reply"""
+
+
+class PSSSTNotRequest(PSSSTException):
+    """Packet is not a request"""
+
+
+class PSSSTDecryptFailed(PSSSTException):
+    """Authenticated decryption failed"""
