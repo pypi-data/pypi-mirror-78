3d23831928f4fed533aaf26767c5b3eafcdd783b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:15 +0000

    Add package whatsappguru/0.2.5/whatsappguru-0.2.5.tar.gz

---
+++ packages/whatsappguru/whatsappguru-0.2.5.tar.gz/whatsappguru-0.2.5/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="whatsappguru",
+    version="0.2.5",
+    author="Lorenzo Coacci",
+    author_email="lorenzo@coacci.com",
+    description="The package contains the tools to analyze your WhatsApp chats",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lollococce/whatsappguru",
+    packages=setuptools.find_packages(),
+    keywords='',
+    license='MIT',
+    include_package_data=True,
+    install_requires=[
+        'pandas',
+        'emoji',
+        'progressbar',
+        'uuid',
+        'iago',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+    ]
+)
