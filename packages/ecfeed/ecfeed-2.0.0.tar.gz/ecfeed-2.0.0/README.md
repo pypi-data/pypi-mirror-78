e6e669f657ce1545b915b5001e6f1c77bf0d06f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:25 +0000

    Add package ecfeed/2.0.0/ecfeed-2.0.0.tar.gz

---
+++ packages/ecfeed/ecfeed-2.0.0.tar.gz/ecfeed-2.0.0/README.md
@@ -0,0 +1,26 @@
+# ecfeed.python
+
+This library provides API to access ecfeed service from python code. Additionally it contains a command line 
+utility to access ecfeed from a console. 
+
+## Introduction
+
+EcFeed online service provides REST API so that models defined in the online tool can be accessed 
+and used for test generation. Also tests that are generated in the online tool and stored in the 
+model can be downloaded and used for tests locally. This library provides user friendly way to access 
+the generated data from python code.
+
+## Requirements
+
+EcFeed library is built and tested with Python 3.6, although it should work with earlier versions of Python 3. 
+It depends on pyopenssl and requests libraries.
+
+## Installation
+
+```
+pip3 install ecfeed
+```
+
+## Manual
+
+See tutorials at ecfeed.com
---
+++ packages/ecfeed/ecfeed-2.0.0.tar.gz/ecfeed-2.0.0/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ecfeed", 
+    version="2.0.0",
+    author="EcFeed AS",
+    author_email="mail@ecfeed.com",
+    description="Python interface for accessing EcFeed online generator service",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    url="https://github.com/ecfeed/ecfeed.python",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Environment :: Console",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Testing",
+        "Programming Language :: Python :: 3"
+    ],
+    python_requires='>=3.6',
+    keywords = 'testing pairwise test_generation',
+    py_modules=['ecfeed', 'ecfeed_cli'],
+    install_requires=['pyopenssl', 'requests'],
+    entry_points={
+        'console_scripts':[
+            'ecfeed=ecfeed_cli:main'
+        ]
+    },
+)
