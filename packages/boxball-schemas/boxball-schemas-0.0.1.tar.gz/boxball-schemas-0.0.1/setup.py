84d6f5cc16691b136c16694e7011da282fa90486
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:44 +0000

    Add package boxball-schemas/0.0.1/boxball-schemas-0.0.1.tar.gz

---
+++ packages/boxball-schemas/boxball-schemas-0.0.1.tar.gz/boxball-schemas-0.0.1/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="boxball-schemas",
+    version="0.0.1",
+    author="David Roher",
+    description="Schemas for the Boxball project that can be used for extension or code completion",
+    url="https://github.com/droher/boxball",
+    packages=["schemas"],
+    python_requires='>=3.6',
+)
