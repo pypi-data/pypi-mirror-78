39f387ef2cc0d7d58a8da7f4afc9fc6c6f9c5203
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:26 +0000

    Add package visjobs/0.1/visjobs-0.1.tar.gz

---
+++ packages/visjobs/visjobs-0.1.tar.gz/visjobs-0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="visjobs", # Replace with your own username
+    version="0.1",
+    author="Kutay Donmez & Berkay Donmez",
+    author_email="donmezk@outlook.com",
+    description="Get Latest Atmospheric Model Data | Analyse | Visualize Easily",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/donmezkutay/visjobs",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
