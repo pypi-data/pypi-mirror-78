ba2c32a6b6283ed337bce72cf330ab9833932070
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:54 +0000

    Add package xontrib-argcomplete/0.0.5/xontrib-argcomplete-0.0.5.tar.gz

---
+++ packages/xontrib-argcomplete/xontrib-argcomplete-0.0.5.tar.gz/xontrib-argcomplete-0.0.5/README.md
@@ -0,0 +1,44 @@
+<p align="center">
+Adding support of <a href="https://github.com/kislyuk/argcomplete">kislyuk/argcomplete</a> to <a href="https://xon.sh">xonsh</a>.
+</p>
+
+<p align="center">  
+If you like the idea of xontrib-argcomplete click ⭐ on the repo and stay tuned by watching releases.
+</p>
+
+## Install
+```shell script
+xpip install -U xontrib-argcomplete
+echo 'xontrib load argcomplete' >> ~/.xonshrc
+# Reload xonsh
+```
+
+## Usage
+Current version of completion is working only for commands that starts with:
+ * `python <path/to/python_script.py> ...`
+ * `<path/to/python_script.py> ...`
+
+For example create `proto.py` with the content:
+```python
+#!/usr/bin/env python
+# PYTHON_ARGCOMPLETE_OK
+import argparse, argcomplete
+from argcomplete.completers import ChoicesCompleter
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--proto").completer=ChoicesCompleter(('http', 'https', 'ssh', 'rsync', 'wss'))
+argcomplete.autocomplete(parser)
+args = parser.parse_args()
+print('ok')
+```
+Then try completion:
+```bash
+python proto.py <Tab>
+# Suggestions: --help --proto -h
+
+chmod +x proto.py
+./proto.py --proto tt<Tab>
+# Suggestions: http https
+```
+
+The xontrib was tested on Ubuntu+Conda+Xonsh and on Mac+Brew+PyPi+Xonsh.
---
+++ packages/xontrib-argcomplete/xontrib-argcomplete-0.0.5.tar.gz/xontrib-argcomplete-0.0.5/xontrib/argcomplete.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env xonsh
+
+import os, re
+from pathlib import Path
+
+def _xontrib_argcomplete_completer(prefix, line, begidx, endidx, ctx):
+    """
+    Adding support of kislyuk/argcomplete to xonsh.
+    """
+    m = re.match('^python[0-9.]* ([\']*.+?\\.py[\']*)', line)
+    m = re.match('^([\']*.+?\\.py[\']*)', line) if not m else m
+    file = m.group(1) if m else None
+
+    if not file:
+        return None
+
+    file = file[1:-1] if file[0] == "'" and file[-1] == "'" else file
+    filep = Path(file)
+    if not filep.exists():
+        return ((prefix, 'argcomplete: file does not exists'), len(prefix))
+
+    found_argcomplete = False
+    with open(filep) as f:
+        for x in range(100):
+            if 'PYTHON_ARGCOMPLETE_OK' in next(f):
+                found_argcomplete = True
+                break
+
+    if found_argcomplete:
+        with __xonsh__.env.swap(_ARGCOMPLETE=str(1), _ARGCOMPLETE_IFS='\n', COMP_LINE=str(line), COMP_POINT=str(begidx)):
+            result = __xonsh__.subproc_captured_object(['bash', '-c', f"python '{file}' 8>&1"])
+            print(result, file=open(os.devnull, 'w')) # workaround for xonsh issue when object property is empty until use
+        tokens = set([t.replace(r'\ ', ' ') for t in result.output.split('\n') if prefix in t])
+
+        if len(tokens) == 0:
+            return ((prefix, 'argcomplete: completions not found'), len(prefix))
+
+        return (tokens, len(prefix))
+
+    return None
+
+__xonsh__.completers['xontrib_argcomplete'] = _xontrib_argcomplete_completer
+__xonsh__.completers.move_to_end('xontrib_argcomplete', last=False)
