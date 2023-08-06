2538ca9c33a2fffadc121992b2d4fecf70391649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:16 +0000

    Add package python-sonarqube-api/1.1.0/python_sonarqube_api-1.1.0-py3-none-any.whl

---
+++ packages/python-sonarqube-api/python_sonarqube_api-1.1.0-py3-none-any.whl/sonarqube/metrics.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Author: Jialiang Shi
+from sonarqube.config import (
+    API_METRICS_SEARCH_ENDPOINT,
+    API_METRICS_TYPES_ENDPOINT
+)
+
+
+class SonarQubeMetrics:
+    def __init__(self, sonarqube):
+        self.sonarqube = sonarqube
+
+    def search_metrics(self):
+        """
+        Search for metrics
+
+        :return:
+        """
+
+        params = {}
+
+        page_num = 1
+        page_size = 1
+        total = 2
+
+        while page_num * page_size < total:
+            resp = self.sonarqube.make_call('get', API_METRICS_SEARCH_ENDPOINT, **params)
+            response = resp.json()
+
+            page_num = response['p']
+            page_size = response['ps']
+            total = response['total']
+
+            params['p'] = page_num + 1
+
+            for metric in response['metrics']:
+                yield metric
+
+    def get_metrics_types(self):
+        """
+        List all available metric types.
+
+        :return:
+        """
+        resp = self.sonarqube.make_call('get', API_METRICS_TYPES_ENDPOINT)
+        response = resp.json()
+        return response['types']
---
+++ packages/python-sonarqube-api/python_sonarqube_api-1.1.0-py3-none-any.whl/sonarqube/server.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Author: Jialiang Shi
+from sonarqube.config import API_SERVER_VERSION_ENDPOINT
+
+
+class SonarQubeServer:
+    def __init__(self, sonarqube):
+        self.sonarqube = sonarqube
+
+    def get_server_version(self):
+        """
+        Version of SonarQube in plain text
+
+        :return:
+        """
+        resp = self.sonarqube.make_call('get', API_SERVER_VERSION_ENDPOINT)
+        return resp.text
