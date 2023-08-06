bb40f51289dc7861ff1396bd302731c94669aad7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:53 +0000

    Add package np-chaonay/1.3.0.dev1/np_chaonay-1.3.0.dev1.tar.gz

---
+++ packages/np-chaonay/np_chaonay-1.3.0.dev1.tar.gz/np_chaonay-1.3.0.dev1/np_chaonay/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# Name: Python package "np_chaonay"
+# Description: Objects collection Python package by NP-chaonay
+# Author: NP-chaonay (Nuttapong Punpipat)
+# Version: (See on Python metadata below)
+# Version Note:
+#       - Major version: indicates of very significant changes or changes that break compatibility on some system/platforms.
+#       - Minor version: indicates of significant changes or features adding.
+#       - Micro version: indicates of small changes or bug patches, or even typo revising.
+# Revised Date: 2020-09-08 11:59 (UTC)
+# License: MIT License
+# Programming Language: Python
+# CUI/GUI Language: English
+
+__version__='1.3.0.dev1'
+__doc__ = """
+Objects collection package by NP-chaonay
+
+WARNING: This package should be executed on Python 3.y.z .
+
+Contents:
+- main: Module contains object for general usage
+- math: Module contains object for mathematics usage
+- humour (package): Humour-related objects collection package
+- pymusicnote: Module contains object for musical notation
+"""
