7de1d9aa2f28221b06be9b358fdbaa3dfb057bc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:05 +0000

    Add package wlnupdates/1.0.4/wlnupdates-1.0.4.tar.gz

---
+++ packages/wlnupdates/wlnupdates-1.0.4.tar.gz/wlnupdates-1.0.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    readme = f.read()
+
+
+setup(
+    name='wlnupdates',
+    version='1.0.4',
+    url = 'https://github.com/AlphaNeo513/wlnupdates-api.py',
+    python_requires=">= 3.7",
+    description='A simple API wrapper for wlnupdates',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author='AlphaNeo',
+    author_email='sewon360@gmail.com',
+    license='MIT',
+    package=find_packages("wlnupdates"),
+     
+)
