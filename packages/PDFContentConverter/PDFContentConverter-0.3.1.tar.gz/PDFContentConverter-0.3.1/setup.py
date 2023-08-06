b0163d2c36c4bc57df1f195c16be046be0272ff0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:11 +0000

    Add package PDFContentConverter/0.3.1/PDFContentConverter-0.3.1.tar.gz

---
+++ packages/PDFContentConverter/PDFContentConverter-0.3.1.tar.gz/PDFContentConverter-0.3.1/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+
+setup(
+  name='PDFContentConverter',
+  packages=['PDFContentConverter', 'PDFContentConverter.util'],
+  version='0.3.1',
+  license='MIT',
+  description='A tool for converting PDF text as well as structural features into a pandas dataframe. ',
+  long_description_content_type='text/markdown',
+  author='Michael Aigner, Florian Preis',
+  # author_email='your.email@domain.com',
+  url='https://github.com/MBAigner/PDFContentConverter',
+  download_url='https://github.com/MBAigner/PDFContentConverter/archive/v0.3.tar.gz',
+  keywords=['pdf-converter', 'pdf pdf-document-processor', 'pdf-data-extraction', 'pandas', 'pandas-dataframe', 'python'],
+  install_requires=[
+          'pdfminer==20191125',
+          'pandas',
+          'numpy',
+          ''
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7'
+  ],
+)
