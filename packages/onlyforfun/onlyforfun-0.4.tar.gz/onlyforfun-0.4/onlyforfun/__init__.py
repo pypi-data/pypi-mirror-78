acb0e43851b570459a547c016819510e3a83cae1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:20 +0000

    Add package onlyforfun/0.4/onlyforfun-0.4.tar.gz

---
+++ packages/onlyforfun/onlyforfun-0.4.tar.gz/onlyforfun-0.4/onlyforfun/__init__.py
@@ -0,0 +1,7 @@
+def joke():
+    return (u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
+            u'Beiherhund das Oder die Flipperwaldt gersput.')
+
+
+def just_learning():
+    return('This change to check if the change is reflected without committing the code on github')
---
+++ packages/onlyforfun/onlyforfun-0.4.tar.gz/onlyforfun-0.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='onlyforfun',
+      version='0.4',
+      description='Addition methods for test purpose',
+      long_description='Committed on git and testing to see the udpates ',
+      url='https://github.com/sigdelsanjog/onlyforfun',
+      author='Fun Maker',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['onlyforfun'],
+      zip_safe=False)
