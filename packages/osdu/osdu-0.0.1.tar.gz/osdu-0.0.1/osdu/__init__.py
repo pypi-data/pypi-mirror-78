f503eb4295eb97334dca4eb9cf82ca2bed953bc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:11 +0000

    Add package osdu/0.0.1/osdu-0.0.1.tar.gz

---
+++ packages/osdu/osdu-0.0.1.tar.gz/osdu-0.0.1/osdu/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""Package __init__"""
+
+from osdu._version import VERSION_INFO
+from osdu._version import VERSION_MINOR
+from osdu._version import VERSION_MAJOR
+from osdu._version import VERSION_PATCH
+from osdu._version import __version__
+
+
+__all__ = [
+    "VERSION_MAJOR",
+    "VERSION_MINOR",
+    "VERSION_PATCH",
+    "VERSION_INFO",
+    "__version__",
+]
