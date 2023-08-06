c72fbcf0319f687eb2b7650be22e16e44176ad2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:48 +0000

    Add package geospots/0.1.4/geospots-0.1.4.tar.gz

---
+++ packages/geospots/geospots-0.1.4.tar.gz/geospots-0.1.4/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = ['Click>=7.0', ]
+
+setup_requirements = [ ]
+
+test_requirements = [ ]
+
+setup(
+    author="Pedro Portela",
+    author_email='pedro.portela@indicium.tech',
+    python_requires='>=3.5',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="Geospots contains functions for dealing with spatial data",
+    entry_points={
+        'console_scripts': [
+            'geospots=geospots.cli:main',
+        ],
+    },
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='geospots',
+    name='geospots',
+    packages=find_packages(include=['geospots', 'geospots.*']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://bitbucket.org/indiciumtech/indicium_geospots',
+    version='0.1.4',
+    zip_safe=False,
+)
