92766fcb321bc609899cb3081951fa2354afb717
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:19 +0000

    Add package great-expectations/0.12.1/great_expectations-0.12.1.tar.gz

---
+++ packages/great-expectations/great_expectations-0.12.1.tar.gz/great_expectations-0.12.1/tests/test_plugins/fake_actions.py
@@ -0,0 +1,8 @@
+class TemporaryNoOpSummarizer(object):
+    def render(self, input):
+        return input
+
+
+class DropAllVowelsSummarizer(object):
+    def render(self, input):
+        return input
