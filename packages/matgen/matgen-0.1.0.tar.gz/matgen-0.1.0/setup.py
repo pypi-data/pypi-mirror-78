b0f2b65604e7a17625954b8ad1497a10dcb570d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:03 +0000

    Add package matgen/0.1.0/matgen-0.1.0.tar.gz

---
+++ packages/matgen/matgen-0.1.0.tar.gz/matgen-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding='utf8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='matgen',
+    version='0.1.0',
+    author='junruoyu-zheng',
+    author_email='zhengjry@outlook.com',
+    url='https://gitee.com/junruoyu-zheng/mat-gen',
+    description=u'Fast matrix generation in python, including python style and matlab style support',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['matgen'],
+    install_requires=['numpy'],
+    entry_points={},
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
