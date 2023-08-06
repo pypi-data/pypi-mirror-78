0232ee06a5c30e08894c9664ff24d532c0a9de56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:53 +0000

    Add package pycodetimer/0.0.2/pycodetimer-0.0.2.tar.gz

---
+++ packages/pycodetimer/pycodetimer-0.0.2.tar.gz/pycodetimer-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import, print_function
+
+from setuptools import setup
+
+setup(
+    name='pycodetimer',
+    version='0.0.2',
+    author='Lucien Shui',
+    author_email='lucien@lucien.ink',
+    url='https://github.com/LucienShui/pycodetimer',
+    description=u'Python Code Timer',
+    packages=['pycodetimer'],
+)
