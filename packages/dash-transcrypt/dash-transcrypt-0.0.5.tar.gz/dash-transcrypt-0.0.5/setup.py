d61077b91d274133e92c9645258e64522870a776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:21 +0000

    Add package dash-transcrypt/0.0.5/dash-transcrypt-0.0.5.tar.gz

---
+++ packages/dash-transcrypt/dash-transcrypt-0.0.5.tar.gz/dash-transcrypt-0.0.5/setup.py
@@ -0,0 +1,27 @@
+import pathlib
+
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="dash-transcrypt",
+    version="0.0.5",
+    author="Emil Haldrup Eriksen",
+    packages=["dash_transcrypt"],
+    url="https://github.com/thedirtyfew/dash-transcrypt/",
+    include_package_data=True,
+    license="MIT",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    description="Transcrypt bindings for Plotly Dash.",
+    install_requires=["transcrypt"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        'Framework :: Dash',
+    ],
+)
