800eb7251e1aa7b7fb4dccdc8bb8c15cfd2057a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:00 +0000

    Add package nvuti/0.1/nvuti-0.1-py3-none-any.whl

---
+++ packages/nvuti/nvuti-0.1-py3-none-any.whl/Nvuti_Bot/browser.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import random
+
+def browser_nvutiadd():
+	return "New Browser successfully added!"
+
+def browser_getData(hash):
+	return "Game data has been saved"
+
+def data():
+	return str(random.randint(100000, 999999)) + "\n" + str(random.randint(100000, 999999)) + "\n" + str(random.randint(100000, 999999)) + "nvuti \n" + str(random.randint(100000, 999999))
+
+def browser_set(data):
+	return "Success"
---
+++ packages/nvuti/nvuti-0.1-py3-none-any.whl/Nvuti_UDP/udp.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+def login_udp(login, password, hash):
+	return "Логин " + login + " Пароль: " + password + " Хэш: " + hash + "были введены верно!"
+
+def udp_setm():
+	return "menj"
+
+def udp_setb():
+	return "bolj"
