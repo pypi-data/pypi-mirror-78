ed6dc049ed07fdd820292c206acb4041583c9e70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:57 +0000

    Add package django-app-permissions/2.0.0/django-app-permissions-2.0.0.tar.gz

---
+++ packages/django-app-permissions/django-app-permissions-2.0.0.tar.gz/django-app-permissions-2.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import (
+    setup,
+    find_packages,
+)
+
+
+
+setup(
+    name='django-app-permissions',
+    version='2.0.0',
+    url='https://github.com/amp89/django-app-permissions',
+    download_url="https://github.com/amp89/django-app-permissions/blob/master/dist/django-app-permissions-2.0.0.tar.gz",
+    license='MIT',
+    description='Automatic app level authentication for django apps using django',
+    long_description=open('README.rst', 'r', encoding='utf-8').read(),
+    author='Alex Peterson',
+    author_email='contact@alexpeterson.tech',
+    install_requires=[
+        'django',
+        'djangorestframework',
+        'django-drf-advanced-token',
+    ],
+    python_requires='>=3.6',
+    
+)
