aece4073cd34ba601bbe5a696c684311de667d77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:31 +0000

    Add package contextualized-topic-models/1.4.3/contextualized_topic_models-1.4.3.tar.gz

---
+++ packages/contextualized-topic-models/contextualized_topic_models-1.4.3.tar.gz/contextualized_topic_models-1.4.3/HISTORY.rst
@@ -0,0 +1,34 @@
+=======
+History
+=======
+
+1.4.3 (2020-09-03)
+------------------
+
+* Updating sentence-transformers package to avoid errors
+
+1.4.2 (2020-08-04)
+------------------
+
+* Changed the encoding on file load for the SBERT embedding function
+
+1.4.1 (2020-08-04)
+------------------
+
+* Fixed bug over sparse matrices
+
+1.4.0 (2020-08-01)
+------------------
+
+* New feature handling sparse bow for optimized processing
+* New method to return topic distributions for words
+
+1.0.0 (2020-04-05)
+------------------
+
+* Released models with the main features implemented
+
+0.1.0 (2020-04-04)
+------------------
+
+* First release on PyPI.
---
+++ packages/contextualized-topic-models/contextualized_topic_models-1.4.3.tar.gz/contextualized_topic_models-1.4.3/contextualized_topic_models/datasets/dataset.py
@@ -0,0 +1,35 @@
+import torch
+from torch.utils.data import Dataset
+import numpy as np
+
+
+class CTMDataset(Dataset):
+
+    """Class to load BOW dataset."""
+
+    def __init__(self, X, X_bert, idx2token):
+        """
+        Args
+            X : array-like, shape=(n_samples, n_features)
+                Document word matrix.
+        """
+        if X.shape[0] != len(X_bert):
+            raise Exception("Wait! BoW and Contextual Embeddings have different sizes! "
+                            "You might want to check if the BoW preparation method has removed some documents. ")
+
+        self.X = X
+        self.X_bert = X_bert
+        self.idx2token = idx2token
+
+    def __len__(self):
+        """Return length of dataset."""
+        return self.X.shape[0]
+
+    def __getitem__(self, i):
+        """Return sample from dataset at index i."""
+        X = torch.FloatTensor(self.X[i].todense())
+        X_bert = torch.FloatTensor(self.X_bert[i])
+
+        return {'X': X, 'X_bert': X_bert}
+
+
