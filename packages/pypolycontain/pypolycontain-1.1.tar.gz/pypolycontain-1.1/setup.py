23259ed2e175048288269041e1c21b655ef58eaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:19 +0000

    Add package pypolycontain/1.1/pypolycontain-1.1.tar.gz

---
+++ packages/pypolycontain/pypolycontain-1.1.tar.gz/pypolycontain-1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pypolycontain',
+    author='Sadra Sadraddini',
+    description='A python package for polytopic objects, operations, and containment encodings',
+    author_email='sadra@mit.edu',
+    version='1.1',
+    packages=['pypolycontain'], #fix
+    long_description='A python package for polytopic objects, operations, and containment encodings',
+#    package_dir={'pypolycontain': 'lib'},
+    license='MIT',
+    url="https://github.com/sadraddini/pypolycontain",
+    zip_safe=False,
+)
