5d887501af53ae1a4c723f679cc66a7a1e69d00e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:10 +0000

    Add package dbt-synapse/0.18.0rc3/dbt-synapse-0.18.0rc3.tar.gz

---
+++ packages/dbt-synapse/dbt-synapse-0.18.0rc3.tar.gz/dbt-synapse-0.18.0rc3/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from setuptools import find_packages
+from distutils.core import setup
+
+package_name = "dbt-synapse"
+package_version = "0.18.0rc3"
+description = """A Azure Synapse adpter plugin for dbt (data build tool)"""
+
+setup(
+    name=package_name,
+    version=package_version,
+    description=description,
+    long_description=description,
+    long_description_content_type='text/markdown',
+    author="Anders Swanson",
+    author_email="swanson.anders@gmail.com",
+    url="https://github.com/swanderz/dbt-synapse",
+    packages=find_packages(),
+    package_data={
+        'dbt': [
+            'include/sqlserver/dbt_project.yml',
+            'include/sqlserver/macros/*.sql',
+            'include/sqlserver/macros/**/*.sql',
+            'include/sqlserver/macros/**/**/*.sql',
+        ]
+    },
+    install_requires=[
+        'dbt-core==0.18.0',
+        'pyodbc>=4.0.27',
+    ]
+)
