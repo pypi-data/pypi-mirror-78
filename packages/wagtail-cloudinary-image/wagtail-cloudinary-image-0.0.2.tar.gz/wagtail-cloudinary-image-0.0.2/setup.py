0b1fe5e4e7794921e2e661fa52f467c495f39768
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:31 +0000

    Add package wagtail-cloudinary-image/0.0.2/wagtail-cloudinary-image-0.0.2.tar.gz

---
+++ packages/wagtail-cloudinary-image/wagtail-cloudinary-image-0.0.2.tar.gz/wagtail-cloudinary-image-0.0.2/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['wagtail_cloudinary_image', 'wagtail_cloudinary_image.migrations']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Django>=2.2',
+ 'cloudinary>=1.22.0',
+ 'django-cloudinary-storage>=0.3.0',
+ 'wagtail>=2.9']
+
+setup_kwargs = {
+    'name': 'wagtail-cloudinary-image',
+    'version': '0.0.2',
+    'description': 'Wagtail Image model that utilized django-cloudinary-storage',
+    'long_description': None,
+    'author': 'David Burke',
+    'author_email': 'david@burkesoftware.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
