6d83d545f404b3f2fc5fadd55473bc23ebec459b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:36 +0000

    Add package icd2phenotype/0.14/icd2phenotype-0.14.tar.gz

---
+++ packages/icd2phenotype/icd2phenotype-0.14.tar.gz/icd2phenotype-0.14/icd2phenotype/__init__.py
@@ -0,0 +1,1 @@
+from icd2phenotype.icd2pheno import icd2phenotype,printrepos,getrepo,printphenotypes
---
+++ packages/icd2phenotype/icd2phenotype-0.14.tar.gz/icd2phenotype-0.14/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+setup(
+  name = 'icd2phenotype',        
+  packages = ['icd2phenotype'],  
+  version = '0.14',      
+  license='MIT',        
+  description = 'Identify phenotypes from ICD code list',   
+  include_package_data=True,
+  author = 'Xiaoqian Jiang, Luyao Chen',                  
+  author_email = 'your.email@domain.com',      
+  url = 'https://github.com/Luyaochen1/icd2phenotype',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/Luyaochen1/icd2phenotype/archive/V_014.tar.gz',     
+  keywords = ['icd 9', 'icd 10', 'phenotypes'],   
+  install_requires=[            # I get to this in a second
+          'pandas',
+        ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',       
+    'Intended Audience :: Developers',       
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7', 
+    'Programming Language :: Python :: 3.8',
+  ],
+)
