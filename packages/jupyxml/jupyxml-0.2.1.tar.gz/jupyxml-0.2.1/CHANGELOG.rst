c9dff3bda1fd692d4df29e5709d9936e3c373141
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:16 +0000

    Add package jupyxml/0.2.1/jupyxml-0.2.1.tar.gz

---
+++ packages/jupyxml/jupyxml-0.2.1.tar.gz/jupyxml-0.2.1/CHANGELOG.rst
@@ -0,0 +1,17 @@
+=========
+Changelog
+=========
+
+Version 0.2
+===========
+- Added docs with jupyter-book
+- Fixed bug stopping multiple instanced of a JupyXML from working in the same notebook.
+  Each instance now has a unique identifier.
+
+Version 0.1
+===========
+
+- Added README.md
+- Added basic HTML version
+- Added Basic Usage example
+- Added one test - importing
---
+++ packages/jupyxml/jupyxml-0.2.1.tar.gz/jupyxml-0.2.1/src/jupyxml/visualiser.py
@@ -0,0 +1,42 @@
+import os
+from datetime import datetime
+from io import StringIO
+
+from lxml import etree
+
+try:
+    import importlib.resources as pkg_resources
+except ImportError:
+    # Try backported to PY<37 `importlib_resources`.
+    import importlib_resources as pkg_resources
+
+from typing import Union, TextIO, AnyStr
+
+from jupyxml.accordian_creator import create_recursive_accordian_html
+from . import styles
+
+
+class JupyXML:
+
+    def __init__(self, xml_file_or_path: Union[AnyStr, TextIO]):
+        self.xml_file_or_path = xml_file_or_path
+        if os.path.exists(xml_file_or_path):
+            self._file = self.xml_file_or_path
+        else:
+            self._file = StringIO(xml_file_or_path)
+        self.root = etree.parse(self._file)
+
+    def _repr_html_(self) -> AnyStr:
+        root_identifier = str(datetime.now().timestamp()).replace(".", "")
+        cards = create_recursive_accordian_html(self.root.getroot(), root_identifier=root_identifier,
+                                                max_depth=10, current_depth=0)
+        css = pkg_resources.read_text(styles, 'jupyxml.css')
+        # Concatenate css and cards. TODO: It would be nice if the css was "once-only"
+        # TODO: Ability to turn off CSS adding
+        html_representation = f"""
+            <style>
+                {css}
+            </style>
+            {cards}
+        """
+        return html_representation
