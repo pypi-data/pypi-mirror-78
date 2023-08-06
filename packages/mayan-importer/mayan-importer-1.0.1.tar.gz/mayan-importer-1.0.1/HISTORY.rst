3a7988781765de3e9e3bacb7e35ddc44d6d6e7ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:58 +0000

    Add package mayan-importer/1.0.1/mayan-importer-1.0.1.tar.gz

---
+++ packages/mayan-importer/mayan-importer-1.0.1.tar.gz/mayan-importer-1.0.1/HISTORY.rst
@@ -0,0 +1,8 @@
+1.0.1 (2020-09-07)
+==================
+- Update absolute imports to the Credentials app.
+
+1.0.0 (2020-09-01)
+==================
+
+- Initial release
---
+++ packages/mayan-importer/mayan-importer-1.0.1.tar.gz/mayan-importer-1.0.1/importer/permissions.py
@@ -0,0 +1,21 @@
+from django.utils.translation import ugettext_lazy as _
+
+from mayan.apps.permissions import PermissionNamespace
+
+namespace = PermissionNamespace(label=_('Importer'), name='importer')
+
+permission_import_setup_create = namespace.add_permission(
+    label=_('Create import setups'), name='import_setup_create'
+)
+permission_import_setup_delete = namespace.add_permission(
+    label=_('Delete import setups'), name='import_setup_delete'
+)
+permission_import_setup_edit = namespace.add_permission(
+    label=_('Edit import setups'), name='import_setup_edit'
+)
+permission_import_setup_use = namespace.add_permission(
+    label=_('Use import setups'), name='import_setup_use'
+)
+permission_import_setup_view = namespace.add_permission(
+    label=_('View import setups'), name='import_setup_view'
+)
