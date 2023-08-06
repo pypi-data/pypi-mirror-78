ccb4722dcf575416349c35f6bfd5c20d11765e4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:49 +0000

    Add package yfms/0.0.1/yfms-0.0.1.tar.gz

---
+++ packages/yfms/yfms-0.0.1.tar.gz/yfms-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='yfms',
+      version='0.0.1',
+      description='Financial Market Data',
+      url='http://github.com/byteatwork/yfms',
+      author='byteatwork',
+      author_email='byteatwork@gmail.com',
+      license='MIT',
+      packages=['yfms'],
+      zip_safe=False)
