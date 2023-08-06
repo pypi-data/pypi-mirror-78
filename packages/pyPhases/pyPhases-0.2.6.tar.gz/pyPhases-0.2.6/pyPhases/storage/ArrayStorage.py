7f6c872f1de46c87d09bc52330cf23cef7929b48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:37 +0000

    Add package pyPhases/0.2.6/pyPhases-0.2.6.tar.gz

---
+++ packages/pyPhases/pyPhases-0.2.6.tar.gz/pyPhases-0.2.6/pyPhases/storage/ArrayStorage.py
@@ -0,0 +1,18 @@
+from .Storage import Storage
+from ..Data import DataNotFound
+
+class ArrayStorage(Storage):
+    array = {}
+
+    def __init__(self, basePath="."):
+        self.basePath = basePath
+
+    def read(self, path):
+        path = self.getPath(path)
+        if(not path in self.array):
+            raise DataNotFound("Data was not found:" + path)
+        return self.array[path]
+
+    def write(self, path, data):
+        path = self.getPath(path)
+        self.array[path] = data
