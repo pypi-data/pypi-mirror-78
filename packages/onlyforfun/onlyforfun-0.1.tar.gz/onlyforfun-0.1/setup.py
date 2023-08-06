0ab7e8ae846333d28677831cbe4981db315704aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:07 +0000

    Add package onlyforfun/0.1/onlyforfun-0.1.tar.gz

---
+++ packages/onlyforfun/onlyforfun-0.1.tar.gz/onlyforfun-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='onlyforfun',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/sigdelsanjog/onlyforfun',
+      author='Fun Maker',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['onlyforfun'],
+      zip_safe=False)
