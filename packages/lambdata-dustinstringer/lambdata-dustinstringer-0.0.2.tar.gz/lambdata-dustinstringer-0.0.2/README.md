3fb3ef55038a1853a3815c56e894de0ebdf460a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:29 +0000

    Add package lambdata-dustinstringer/0.0.2/lambdata-dustinstringer-0.0.2.tar.gz

---
+++ packages/lambdata-dustinstringer/lambdata-dustinstringer-0.0.2.tar.gz/lambdata-dustinstringer-0.0.2/README.md
@@ -0,0 +1,9 @@
+# lambdata-dustinstringer
+A collection of datascience utility functions.
+
+## Installation:
+    pip install lambdata-dustinstringer
+  
+## Usage:
+    from dustydata import ds_utils
+> ds_utils is where the utility functions are held.
---
+++ packages/lambdata-dustinstringer/lambdata-dustinstringer-0.0.2.tar.gz/lambdata-dustinstringer-0.0.2/dustydata/ds_utils.py
@@ -0,0 +1,21 @@
+def cardinality_cutter(df, threshold=50):
+    """
+    Cut the cardinality of categorical data.
+    Return a list of features that made the cut.
+    """
+    card = df.select_dtypes(exclude='number').nunique()
+    card_cut = card[card <= threshold].index.tolist()
+    return card_cut    
+
+
+def tri_split(data):
+    x, y = train_test_split(data)
+    x, z = train_test_split(x)
+    return x, y, z
+
+
+# if __name__ == '__main__':  # run the name space when impoted in test_functions
+#     pass
+    
+    
+    
