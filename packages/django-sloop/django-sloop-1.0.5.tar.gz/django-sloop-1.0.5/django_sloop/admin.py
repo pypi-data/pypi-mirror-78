7c8df435a99e7c50ce195312e2e0a326def8843f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:22 +0000

    Add package django-sloop/1.0.5/django-sloop-1.0.5.tar.gz

---
+++ packages/django-sloop/django-sloop-1.0.5.tar.gz/django-sloop-1.0.5/django_sloop/admin.py
@@ -0,0 +1,135 @@
+from django.conf.urls import url
+from django.contrib import admin
+from django.contrib import messages
+from django.urls import reverse
+from django.utils.http import urlencode
+from django.views.generic import FormView
+from django.template.response import TemplateResponse
+from django import forms
+
+import json
+
+from django_sloop.models import PushMessage
+
+
+class PushNotificationForm(forms.Form):
+
+    message = forms.CharField(max_length=255, label='Message:')
+    extra = forms.CharField(max_length=255, widget=forms.Textarea, required=False, initial=json.dumps(dict()), label='Extra data as JSON:')
+    url = forms.CharField(max_length=255, required=False, label='URL:')
+    receivers = forms.CharField(widget=forms.HiddenInput)
+
+    def clean(self):
+        try:
+            self.cleaned_data['extra'] = json.loads(self.cleaned_data['extra'])
+            if self.cleaned_data['url']:
+                self.cleaned_data['extra']["url"] = self.cleaned_data['url']
+            self.cleaned_data['receivers'] = json.loads(self.cleaned_data['receivers'])
+        except (TypeError, ValueError) as ex:
+            self.add_error('extra', ex.message)
+        except KeyError:
+            self.add_error(None, 'Could not retrieve push notification receivers, please try again.')
+
+        return self.cleaned_data
+
+
+class PushNotificationView(FormView):
+
+    template_name = 'django_sloop/push_notification.html'
+    form_class = PushNotificationForm
+
+    def form_valid(self, form):
+        self.receivers = form.cleaned_data['receivers']
+        model_admin = self.kwargs.get('model_admin')
+        receiver_ids = model_admin.get_receivers_queryset(self.receivers)
+        for receiver_id in receiver_ids:
+            user = model_admin.model.objects.get(pk=receiver_id)
+            user.send_push_notification_async(form.cleaned_data['message'], extra=form.cleaned_data['extra'])
+
+        return super(PushNotificationView, self).form_valid(form)
+
+    def get_initial(self):
+        initial = super(PushNotificationView, self).get_initial()
+        if not initial.get("receivers") and self.request.GET.get("receivers"):
+            initial["receivers"] = json.loads(self.request.GET.get("receivers"))
+        return initial
+
+    def get_success_url(self):
+        messages.info(self.request, "Push notification has been sent.")
+        model_admin = self.kwargs.get('model_admin')
+        url = reverse('admin:%s_%s_send_push_notification' % (model_admin.model._meta.app_label, model_admin.model._meta.model_name))
+        url += "?" + urlencode({
+            'receivers': json.dumps(self.receivers)
+        })
+        return url
+
+    def get_context_data(self, **kwargs):
+        context = super(PushNotificationView, self).get_context_data(**kwargs)
+        model_admin = self.kwargs.get('model_admin')
+        admin_site = model_admin.admin_site
+        opts = model_admin.model._meta
+
+        context.update({
+            'admin_site': admin_site.name,
+            'title': 'Send Push Notification',
+            'opts': opts,
+            'app_label': opts.app_label,
+            'view_url': reverse('admin:%s_%s_send_push_notification' % (opts.app_label, opts.model_name))
+        })
+
+        return context
+
+
+class SloopAdminMixin(object):
+    """
+    Sloop admin mixin for sending push notifications
+    """
+    def get_urls(self):
+        urls = super(SloopAdminMixin, self).get_urls()
+        return [
+            url(r'^send-push-notification/$', self.admin_site.admin_view(self.push_notification_view),
+                name='%s_%s_send_push_notification' % (self.model._meta.app_label, self.model._meta.model_name))
+        ] + urls
+
+    def get_receivers_queryset(self, receiver_ids):
+        """
+        Filter your queryset as you want, then return it.
+        """
+        return receiver_ids
+
+    def push_notification_view(self, request):
+        push_notification_view = PushNotificationView.as_view()
+        return push_notification_view(request, model_admin=self)
+
+    def send_push_notification(self, request, queryset):
+        # Action method.
+
+        admin_site = self.admin_site
+        opts = self.model._meta
+        receivers = list(queryset.values_list('id', flat=True))
+        form = PushNotificationForm(initial={'receivers': json.dumps(receivers)})
+
+        context = {
+            'form': form,
+            'admin_site': admin_site.name,
+            'title': 'Send Push Notification',
+            'opts': opts,
+            'app_label': opts.app_label,
+            'view_url': reverse('admin:%s_%s_send_push_notification' % (opts.app_label, opts.model_name))
+        }
+
+        return TemplateResponse(request, 'django_sloop/push_notification.html', context=context)
+
+
+class PushMessageAdmin(admin.ModelAdmin):
+
+    search_fields = ["body", "sns_message_id"]
+    list_display = ["id", "body", "error_message", "device", "sns_message_id", "date_created", "date_updated"]
+    readonly_fields = ["id", "device",  "body",  "data", "sns_message_id", "sns_response", "date_created", "date_updated"]
+
+    def error_message(self, obj):
+        error = json.loads(obj.sns_response).get("Error")
+        if error:
+            return error.get("Message")
+
+admin.site.register(PushMessage, PushMessageAdmin)
---
+++ packages/django-sloop/django-sloop-1.0.5.tar.gz/django-sloop-1.0.5/django_sloop/models.py
@@ -0,0 +1,184 @@
+import json
+
+from django.conf import settings
+from django.contrib.gis.db import models
+from django.core.exceptions import ObjectDoesNotExist
+from django.utils import timezone
+from django.utils.encoding import smart_text
+from django.utils.translation import ugettext_lazy as _
+from django.template.defaultfilters import truncatechars
+
+from django_sloop.exceptions import DeviceIsNotActive
+from .settings import DJANGO_SLOOP_SETTINGS
+from . import tasks
+
+
+class PushNotificationMixin(object):
+    """
+    A Mixin that handles push notification sending through the User model.
+    """
+
+    def get_badge_count(self):
+        return 0
+
+    def get_active_pushable_device(self):
+        """
+        Finds and returns the last active device with push token for this user, if available
+        """
+        try:
+            device = self.devices.filter(deleted_at__isnull=True).order_by("-date_created").first()
+        except ObjectDoesNotExist:
+            return None
+        return device
+
+    def send_push_notification_async(self, message, url=None, sound=None, extra=None, category=None, **kwargs):
+        device = self.get_active_pushable_device()
+        if not device:
+            return False
+
+        # Print message to console if this is a development environment.
+        if settings.DEBUG:
+            print("Push notification: %s / Receiver: %s" % (message, self))
+
+        sound = sound or DJANGO_SLOOP_SETTINGS.get("DEFAULT_SOUND") or None
+
+        tasks.send_push_notification.delay(
+            device.id,
+            message,
+            url,
+            self.get_badge_count(),
+            sound,
+            extra,
+            category,
+            **kwargs
+        )
+
+    def send_silent_push_notification_async(self, extra=None, content_available=True, **kwargs):
+        """
+        Sends a push notification to the user's last active device
+        """
+        # Print message to console if this is a development environment.
+        if settings.DEBUG:
+            print("Silent push notification to: %s" % self)
+
+        device = self.get_active_pushable_device()
+        if not device:
+            return False
+
+        tasks.send_silent_push_notification.delay(
+            device.id,
+            extra,
+            self.get_badge_count(),
+            content_available,
+            **kwargs
+        )
+
+
+class AbstractSNSDevice(models.Model):
+
+    PLATFORM_IOS = "ios"
+    PLATFORM_ANDROID = "android"
+
+    PLATFORM_CHOICES = (
+        (PLATFORM_IOS, "iOS"),
+        (PLATFORM_ANDROID, "Android"),
+    )
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="devices", on_delete=models.CASCADE)
+    locale = models.CharField(max_length=255, default="en_US")
+    push_token = models.CharField(max_length=255, null=True)
+    platform = models.CharField(max_length=255, choices=PLATFORM_CHOICES)
+    model = models.CharField(max_length=255, blank=True)
+    sns_platform_endpoint_arn = models.CharField(_("SNS Platform Endpoint"), max_length=255, null=True, blank=True, unique=True)
+
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    date_created = models.DateTimeField(default=timezone.now)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = _("Device")
+        verbose_name_plural = _("Devices")
+        unique_together = ("push_token", "platform")
+        ordering = ("-date_updated",)
+        get_latest_by = "date_updated"
+        abstract = True
+
+    def __str__(self):
+        return smart_text(_("Push Token %(push_token)s for %(user)s") % {
+            "user": str(self.user),
+            "push_token": self.push_token,
+        })
+
+    def invalidate(self):
+        self.deleted_at = timezone.now()
+        self.save()
+
+    def prepare_message(self, message):
+        """
+        Prepares message before sending.
+        """
+        return truncatechars(message, 255)
+
+    def send_push_notification(self, message, url=None, badge_count=None, sound=None, extra=None, category=None, **kwargs):
+        """
+        Sends push message using device push token
+        """
+        from .handlers import SNSHandler
+
+        if self.deleted_at:
+            raise DeviceIsNotActive
+
+        message = self.prepare_message(message)
+
+        handler = SNSHandler(device=self)
+        message_payload, response = handler.send_push_notification(message, url, badge_count, sound, extra, category, **kwargs)
+
+        if DJANGO_SLOOP_SETTINGS["LOG_SENT_MESSAGES"]:
+            PushMessage.objects.create(
+                device=self,
+                body=message,
+                data=message_payload,
+                sns_message_id=response.get("MessageId") or None,  # Can be null for failed message.
+                sns_response=json.dumps(response)
+            )
+
+        return response
+
+    def send_silent_push_notification(self, extra=None, badge_count=None, content_available=None, **kwargs):
+        """
+        Sends silent push notification
+        """
+        from .handlers import SNSHandler
+
+        if self.deleted_at:
+            raise DeviceIsNotActive
+
+        handler = SNSHandler(device=self)
+        message_payload, response = handler.send_silent_push_notification(extra, badge_count, content_available, **kwargs)
+
+        if DJANGO_SLOOP_SETTINGS["LOG_SENT_MESSAGES"]:
+            PushMessage.objects.create(
+                device=self,
+                data=message_payload,
+                sns_message_id=response.get("MessageId") or None,  # Can be null for failed message.
+                sns_response=json.dumps(response)
+            )
+
+        return response
+
+
+class PushMessage(models.Model):
+
+    device = models.ForeignKey(DJANGO_SLOOP_SETTINGS["DEVICE_MODEL"], related_name="push_messages", on_delete=models.CASCADE)
+    body = models.TextField()
+    data = models.TextField()
+    sns_message_id = models.CharField(max_length=255, unique=True, blank=True, null=True)
+    sns_response = models.TextField()
+
+    date_created = models.DateTimeField(default=timezone.now)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Push Message"
+        verbose_name_plural = "Push Messages"
