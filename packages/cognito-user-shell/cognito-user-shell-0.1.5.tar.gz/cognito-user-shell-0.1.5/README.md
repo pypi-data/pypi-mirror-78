0efe2a6fd7d9dfa2a5fa4130b923147a1aba5f3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:29 +0000

    Add package cognito-user-shell/0.1.5/cognito-user-shell-0.1.5.tar.gz

---
+++ packages/cognito-user-shell/cognito-user-shell-0.1.5.tar.gz/cognito-user-shell-0.1.5/README.md
@@ -0,0 +1,16 @@
+# cognito-user-shell
+## Extendable Python shell/CLI tool/API client with AWS Cognito authentication
+Example package shows how you can expand and implement your own tools.
+
+### Installation
+`pip install cognito-user-shell`
+
+### Run the example
+```bash
+git clone https://github.com/jussiniinikoski/cognito-user-shell.git
+cd cognito-user-shell
+# Set up some environment variables
+export COGNITO_CLIENT_ID=add-your-aws-cognito-client-id-here
+export API_URL=http://127.0.0.1:8000
+python example
+```
---
+++ packages/cognito-user-shell/cognito-user-shell-0.1.5.tar.gz/cognito-user-shell-0.1.5/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cognito-user-shell",  # Replace with your own username
+    version="0.1.5",
+    author="Jussi Niinikoski",
+    author_email="jussi.niinikoski@perjantai.fi",
+    description="Extendable shell/CLI tool with AWS Cognito authentication.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jussiniinikoski/cognito-user-shell",
+    packages=['cognito_user_shell', ],
+    install_requires=['boto3', 'requests', ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
