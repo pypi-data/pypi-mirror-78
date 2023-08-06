2b854eb30189f931f8a9ba78f1cd2e290de93522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:51 +0000

    Add package origo-devportal-poctools/0.0.5/origo-devportal-poctools-0.0.5.tar.gz

---
+++ packages/origo-devportal-poctools/origo-devportal-poctools-0.0.5.tar.gz/origo-devportal-poctools-0.0.5/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='origo-devportal-poctools',
+    version='0.0.5',
+    author='Oslo Origo',
+    author_email='developerportal@oslo.kommune.no',
+    description='A set of tools and models used by all parts of the harvest poc',
+    project_urls={
+        'Bug Tracker': 'https://github.com/oslokommune/devportal-harvest-poc/issues',
+        'Source Code': 'https://github.com/oslokommune/devportal-harvest-poc'
+    },
+    license='https://mit-license.org/',
+    packages=find_namespace_packages(include='origo.*'),
+    install_requires=[
+        'pyyaml==5.3.1'
+    ]
+)
