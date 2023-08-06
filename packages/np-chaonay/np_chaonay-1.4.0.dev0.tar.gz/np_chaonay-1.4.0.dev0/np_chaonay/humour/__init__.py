a428fc67e9423a4fafa6a5729ee6bf32a2bb41db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:04 +0000

    Add package np-chaonay/1.4.0.dev0/np_chaonay-1.4.0.dev0.tar.gz

---
+++ packages/np-chaonay/np_chaonay-1.4.0.dev0.tar.gz/np_chaonay-1.4.0.dev0/np_chaonay/humour/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# Name: Python package "np_chaonay.humour"
+# Description: Humour-related objects collection package
+# Author: NP-chaonay (Nuttapong Punpipat)
+# Version: (See on Python metadata below)
+# Version Note: 
+#       - Major version: indicates of very significant changes or changes that break compatibility on some system/platforms.
+#       - Minor version: indicates of significant changes or features adding.
+#       - Micro version: indicates of small changes or bug patches, or even typo revising.
+# Revised Date: 2020-07-01 16:41 (UTC)
+# License: MIT License
+# Programming Language: Python
+# CUI/GUI Language: English
+
+__version__='1.0.3.dev'
+__doc__ = """
+Humour-related objects collection package
+
+Contents:
+- main: Main module for humour-related objects
+
+HAVING FUN!!!
+"""
