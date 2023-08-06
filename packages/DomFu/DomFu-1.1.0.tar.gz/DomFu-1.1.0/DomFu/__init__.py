a59a7c944c2fd69da7ed9e88e2f3d6c1b3d8b69a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:51 +0000

    Add package DomFu/1.1.0/DomFu-1.1.0.tar.gz

---
+++ packages/DomFu/DomFu-1.1.0.tar.gz/DomFu-1.1.0/DomFu/__init__.py
@@ -0,0 +1,6 @@
+from .modules.crtsh import fetchCrtSh
+from .modules.BufferOverRun import fetchBufferOverRun
+from .modules.HackerTarget import fetchHackerTarget
+from .modules.ThreatCrowd import fetchThreatCrowd
+from .modules.VirusTotal import fetchVirusTotal
+from .modules.subdomain import fetchAll as subdomain
