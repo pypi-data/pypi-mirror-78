60cefb191e360861cc8ac7a1dfc434134e7967bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:29 +0000

    Add package actuarydesk/0.1.4/actuarydesk-0.1.4.tar.gz

---
+++ packages/actuarydesk/actuarydesk-0.1.4.tar.gz/actuarydesk-0.1.4/actuarydesk/__init__.py
@@ -0,0 +1,2 @@
+#version 0.1.4
+#Author: Arief Anbiya (anbarief@live.com)
---
+++ packages/actuarydesk/actuarydesk-0.1.4.tar.gz/actuarydesk-0.1.4/actuarydesk/math_economics_viz/__init__.py
@@ -0,0 +1,6 @@
+#version 0.1.4
+
+samples = ['federal_interest_rates_kaggle', \
+           'financial_indicators_US_stocks_kaggle', \
+           'average_interest_rates_US_treasury', \
+           'stochastic_random_walk']
