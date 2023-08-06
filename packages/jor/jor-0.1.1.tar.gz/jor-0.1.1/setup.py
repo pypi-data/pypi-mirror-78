fc75715dab21c26f7cab1ffb5308d8cd0d212151
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:27 +0000

    Add package jor/0.1.1/jor-0.1.1.tar.gz

---
+++ packages/jor/jor-0.1.1.tar.gz/jor-0.1.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='jor',
+      version='0.1.1',
+      author='Markus Helmer',
+      url='https://github.com/mdhelmer/jor',
+      description='Lighweight JOb Runner for HPC environments',
+      long_description=readme(),
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering',
+        'Intended Audience :: Science/Research',
+      ],
+      python_requires='>=3.6',
+      license='GPLv3',
+      packages=find_packages(),
+      install_requires=[],
+      zip_safe=True,
+      include_package_data=True,
+      scripts=['bin/jor'],
+)
