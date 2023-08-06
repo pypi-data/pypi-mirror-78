803662f28c2afa8223ac3a5100c8804d19b7cb82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:33 +0000

    Add package chunli/0.11.1/chunli-0.11.1.tar.gz

---
+++ packages/chunli/chunli-0.11.1.tar.gz/chunli-0.11.1/docs/changelog.md
@@ -0,0 +1,95 @@
+## CHANGELOG
+
+### 0.11.1 - 2020-09-03
+
+ - Improve caller
+
+### 0.11.0 - 2020-09-03
+
+ - Create ramp-up feature
+
+### 0.10.5 - 2020-05-08
+
+ - Fix tests for new apidaora version
+
+### 0.10.4 - 2020-04-14
+
+ - Update tests for new apidaora version
+
+### 0.10.3 - 2020-04-14
+
+ - Update tests for new apidaora version
+
+### 0.10.2 - 2020-04-13
+
+ - Fix new apidaora interface
+
+### 0.10.1 - 2020-04-12
+
+ - Fix tests for new apidaora version
+
+### 0.10.0 - 2020-03-06
+
+ - Move the Call class to a isolated module
+
+### 0.9.0 - 2020-03-06
+
+ - Improve Caller to do synchronous calls when the calls are in blocks
+
+### 0.8.0 - 2020-03-06
+
+ - Support body on calls
+
+ - Fix run calls with python script
+
+### 0.7.0 - 2020-03-06
+
+ - Add support to run the calls with a python script
+
+### 0.6.0 - 2020-03-06
+
+ - Add support for make calls in block
+
+### 0.5.2 - 2020-02-27
+
+ - Update requirements
+
+ - Fix config class
+
+### 0.5.1 - 2020-02-27
+
+ - Fix rps_per_node calculation
+
+### 0.5.0 - 2019-11-08
+
+ - Add status endpoint
+
+ - Fix caller inputs management
+
+### 0.4.2 - 2019-11-08
+
+ - Fix background caller, but needs gunicorn
+
+### 0.4.1 - 2019-11-07
+
+ - Skip errors on background caller
+
+### 0.4.0 - 2019-11-07
+
+ - Skip errors and count them
+
+### 0.3.0 - 2019-11-07
+
+ - Add support for multiple workers
+
+### 0.2.1 - 2019-11-07
+
+ - Fix requirements
+
+### 0.2.0 - 2019-11-07
+
+ - Add support for distribution calls
+
+### 0.1.0 - 2019-11-04
+
+ - Create first version of the application
---
+++ packages/chunli/chunli-0.11.1.tar.gz/chunli-0.11.1/docs/src/test.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+
+echo -e '\n---------------------------------------'
+echo -e 'Running examples outputs assertion...\n'
+
+# export DEBUG=1
+
+source ${VIRTUAL_ENV}/bin/activate
+
+test_path=$(dirname ${BASH_SOURCE[0]})
+test_regex="s%${test_path}/[^/]+/(.*)\.test.bash%\1%g"
+test_files="$(find ${test_path}/**/*.test.bash)"
+md5_cmd=$(which md5sum >/dev/null 2>&1 && echo md5sum || echo 'md5 -r')
+uvicorn_output_file=/tmp/uvicorn.output
+uvicorn_hello_output_file=/tmp/uvicorn-index_hello_app.output
+
+coverage run -p $(which gunicorn) chunli.app:app -k uvicorn.workers.UvicornWorker \
+    -c gunicorn_conf.py >${uvicorn_output_file} 2>&1 &
+coverage run -p $(which uvicorn) docs.src.index.index_hello_app:app --port 8001  \
+    >${uvicorn_hello_output_file} 2>&1 &
+sleep 4
+
+for filepath in ${test_files}; do
+    filename=$(echo ${filepath} | sed -r -e ${test_regex})
+    test_dir=$(dirname ${filepath})
+    curl_file=${test_dir}/${filename}_curl.bash 
+    output_tmpfile=/tmp/${filename}.output
+    output_file=${curl_file}.output
+    checksum_file=/tmp/${filename}.checksum
+    curl_file2=${test_dir}/${filename}_curl2.bash 
+    tmp_curl_file2=/tmp/${filename}_curl2.bash 
+    output_file2=${curl_file2}.output
+    output_tmpfile2=/tmp/${filename}2.output
+    checksum_file2=/tmp/${filename}2.checksum
+    date_regex="^date: .*"
+    date_sub="date: Thu, 1st January 1970 00:00:00 GMT"
+    task_time_regex='"(start|end)_time":"[0-9T:+-]+"'
+    task_time_sub='"\1_time":"1970-01-01T00:00:00+00:00"'
+    task_id_regex='.*"task_id":"([0-9a-z-]+)".*'
+    task_id_sub='\1'
+    test_module=$(echo ${filepath} | tr '/' '.' | sed -r -e 's/\.py//g')
+    PYTHONPATH=${test_dir}:${PYTHONPATH}
+
+    echo Testing ${filename}..
+
+    bash ${test_dir}/${filename}.test.bash >/dev/null 2>&1
+
+    bash ${curl_file} 2>/dev/null | \
+        sed -r -e "s/${date_regex}/${date_sub}/g" \
+            -e "s/${task_time_regex}/${task_time_sub}/g" \
+            -e '$ s/(.*)/\1\n/g' > ${output_tmpfile}
+    task_id=$(\
+        cat ${output_tmpfile} | \
+        grep -E "${task_id_regex}" | \
+        sed -r -e "s/.*${task_id_regex}.*/${task_id_sub}/g" \
+    )
+    sed ${output_tmpfile} -i -r -e \
+        "s/${task_id}/4ee301eb-6487-48a0-b6ed-e5f576accfc2/g" 2>/dev/null
+    $md5_cmd ${output_file} ${output_tmpfile} > ${checksum_file}
+
+    sleep 5
+
+    cp ${curl_file2} ${tmp_curl_file2}
+    sed ${tmp_curl_file2} -i -r -e \
+        "s/4ee301eb-6487-48a0-b6ed-e5f576accfc2/${task_id}/g" 2>/dev/null
+    bash ${tmp_curl_file2} 2>/dev/null | \
+        sed -r -e "s/${date_regex}/${date_sub}/g" \
+            -e "s/${task_time_regex}/${task_time_sub}/g" \
+            -e '$ s/(.*)/\1\n/g' > ${output_tmpfile2}
+    sed ${output_tmpfile2} -i -r \
+        -e "s/${task_id}/4ee301eb-6487-48a0-b6ed-e5f576accfc2/g" \
+        -e "s/content-length: [0-9]+/content-length: 409/g" \
+        -e 's/[0-9]+\.[0-9]+/1.0/g' 2>/dev/null
+
+    if test "${filename}" = "index_00_rampup_input"; then
+        sed ${output_tmpfile2} -i -r \
+        -e 's/"duration":1.0/"duration":10.0/g' \
+        -e 's/"rampup_time":1.0/"rampup_time":5/g' \
+        -e 's/"requested_rps_per_node":1.0/"requested_rps_per_node":10.0/g' \
+        -e 's/"realized_requests":1.0/"realized_requests":73.0/g' \
+        -e 's/"realized_rps":1.0/"realized_rps":7.3/g' 2>/dev/null
+    fi
+
+    $md5_cmd ${output_file2} ${output_tmpfile2} > ${checksum_file2}
+
+    output=$(sed -r -e 's/(.*) .*/\1/g' ${checksum_file} | uniq | wc -l)
+
+    if ! [ ${output} -eq 1 ]; then
+        echo -e '\n\n\e[91mOutput assertion error!\e[0m\n\n'
+        diff -u ${output_file} ${output_tmpfile}
+        echo -e "\nuvicorn output: ${uvicorn_output_file}\n"
+        cat ${uvicorn_output_file}
+
+        ps ax | (ps ax | awk "/uvicorn docs.src.index.index_hello_app:app/ {print \$1}" | xargs kill -SIGKILL 2>/dev/null)
+        ps ax | (ps ax | awk "/gunicorn chunli.app:app/ {print \$1}" | xargs kill -SIGKILL 2>/dev/null)
+
+        exit 1
+    fi
+
+    output2=$(sed -r -e 's/(.*) .*/\1/g' ${checksum_file2} | uniq | wc -l)
+
+    if ! [ ${output2} -eq 1 ]; then
+        echo -e '\n\n\e[91mOutput assertion error!\e[0m\n\n'
+        diff -u ${output_file2} ${output_tmpfile2}
+        echo -e "\nuvicorn output: ${uvicorn_output_file}\n"
+        cat ${uvicorn_output_file}
+
+        ps ax | (ps ax | awk "/uvicorn docs.src.index.index_hello_app:app/ {print \$1}" | xargs kill -SIGKILL 2>/dev/null)
+        ps ax | (ps ax | awk "/gunicorn chunli.app:app/ {print \$1}" | xargs kill -SIGKILL 2>/dev/null)
+
+        exit 1
+    fi
+
+    echo OK
+done
+
+ps ax | (ps ax | awk "/uvicorn docs.src.index.index_hello_app:app/ {print \$1}" | xargs kill -SIGTERM 2>/dev/null)
+ps ax | (ps ax | awk "/gunicorn chunli.app:app/ {print \$1}" | xargs kill -SIGTERM 2>/dev/null)
+
+echo 'Docs examples outputs assertion passed!'
+echo -e '---------------------------------------\n'
