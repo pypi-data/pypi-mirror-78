3fdf440414b143a892f53f4a5a35a6fd3263b302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:00 +0000

    Add package distributions-mbm/0.1/distributions_mbm-0.1.tar.gz

---
+++ packages/distributions-mbm/distributions_mbm-0.1.tar.gz/distributions_mbm-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_mbm',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_mbm'],
+      zip_safe=False)
