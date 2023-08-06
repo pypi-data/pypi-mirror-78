d4221b667e86106b07e57f38f5157a8a4ea0ec31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:58 +0000

    Add package ytstreamer/1.1.0/ytstreamer-1.1.0.tar.gz

---
+++ packages/ytstreamer/ytstreamer-1.1.0.tar.gz/ytstreamer-1.1.0/README.md
@@ -0,0 +1,11 @@
+# YT_AudioStream
+```diff
++ This is a simple script that can stream audio from youtube based on search results you give.
+! You should have VLC media player installed on your system to use this.
+@@ EDIT: You can now install this via pip @@
+```
+Works on linux.
+Not tested on Windows and Mac.
+
+
+```pip install ytstreamer```
---
+++ packages/ytstreamer/ytstreamer-1.1.0.tar.gz/ytstreamer-1.1.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+	requirements = f.readlines()
+
+long_description = 'Latest version is available on all operating systems and does not depend on vlc anymore.(I have deleted all the previous versions)'
+
+setup(
+		name ='ytstreamer',
+		version ='1.1.0',
+		author ='Icelain',
+		author_email ='xerneas965@gmail.com',
+		url ='https://github.com/Icelain/YTStreamer',
+		description ='A lightweight, minimal and cross-platform youtube audio streamer',
+		long_description = long_description,
+		long_description_content_type ="text/markdown",
+		license ='MIT',
+		packages = find_packages(),
+		entry_points ={
+			'console_scripts': [
+				'ytstream = ytstream.streamer:main'
+			]
+		},
+		classifiers =(
+			"Programming Language :: Python :: 3",
+			"License :: OSI Approved :: MIT License",
+			"Operating System :: OS Independent",
+		),
+		keywords ='youtube audio streamer stream music player',
+		install_requires = requirements,
+		zip_safe = False
+)
