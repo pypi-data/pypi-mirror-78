df11179b8790d86751b9c2c61305301f063ed523
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:09 +0000

    Add package SoccerNet/0.0.27/SoccerNet-0.0.27-py2.py3-none-any.whl

---
+++ packages/SoccerNet/SoccerNet-0.0.27-py2.py3-none-any.whl/SoccerNet/__init__.py
@@ -0,0 +1,26 @@
+
+__version_info__ = ('0', '0', '27')
+__version__ = '.'.join(__version_info__)
+__authors__ = "Silvio Giancola"
+__authors_username__ = "giancos"
+__author_email__ = "silvio.giancola@kaust.edu.sa"
+__github__ = 'https://github.com/SilvioGiancola/SoccerNetv2'
+
+
+import logging
+from SoccerNet.downloader import SoccerNetDownloader
+
+try:
+    from SoccerNet.DataLoaderTorch import SoccerNetDataLoaderTorch
+except:
+    logging.info("Install Torch for SoccerNetDataLoaderTorch")
+    pass
+
+
+try:
+    from SoccerNet.DataLoaderTensorFlow import SoccerNetDataLoaderTensorFlow
+except:
+    logging.info("Install TensorFlow for SoccerNetDataLoaderTensorFlow")
+    pass
+
+
---
+++ packages/SoccerNet/SoccerNet-0.0.27-py2.py3-none-any.whl/SoccerNet/utils.py
@@ -0,0 +1,44 @@
+
+from pathlib import Path
+import json
+import os
+
+
+def getListGames(jsonGamesFile=None):
+
+    if jsonGamesFile is None:
+        jsonGamesFile = Path(__file__).parent / "data/SoccerNetGames.json"
+
+    print("jsonGamesFile:", jsonGamesFile)
+    # print(Path(__file__).parent+"data/cap_data.txt")
+    with open(jsonGamesFile, "r") as json_file:
+        dictionary = json.load(json_file)
+
+    for championship in dictionary:
+        for season in dictionary[championship]:
+            for game in dictionary[championship][season]:
+
+                game = os.path.join(championship, season, game)
+                yield(game)
+
+
+def getListTestGames(jsonGamesFile=None):
+
+    if jsonGamesFile is None:
+        jsonGamesFile = Path(__file__).parent / "data/SoccerNetTestGames.json"
+
+    print("jsonGamesFile:", jsonGamesFile)
+    # print(Path(__file__).parent+"data/cap_data.txt")
+    with open(jsonGamesFile, "r") as json_file:
+        dictionary = json.load(json_file)
+
+    for championship in dictionary:
+        for season in dictionary[championship]:
+            for game in dictionary[championship][season]:
+
+                game = os.path.join(championship, season, game)
+                yield(game)
+
+
+if __name__ == "__main__":
+    print([r for r in getListGames()])
