82b68d4e2ac603c157d2c5ceb74f77742b10874e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:07 +0000

    Add package asks/2.4.10/asks-2.4.10.tar.gz

---
+++ packages/asks/asks-2.4.10.tar.gz/asks-2.4.10/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+from sys import version_info
+
+if version_info < (3, 5, 2):
+    # 3.5.2 is when __aiter__ became a synchronous function
+    raise SystemExit('Sorry! asks requires python 3.5.2 or later.')
+
+setup(
+    name='asks',
+    description='asks - async http',
+    long_description='asks is an async http lib for curio, trio and asyncio',
+    license='MIT',
+    version='2.4.10',
+    author='Mark Jameson - aka theelous3',
+    url='https://github.com/theelous3/asks',
+    packages=['asks'],
+    install_requires=['h11', 'async_generator', 'anyio<2'],
+    tests_require=['pytest', 'curio', 'overly'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Framework :: Trio',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
