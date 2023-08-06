a0de112de9a6f64b2e9dbd5d1687f86b6baa60fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:59 +0000

    Add package norwegian-forest-cat/0.0.4/norwegian_forest_cat-0.0.4-py3-none-any.whl

---
+++ packages/norwegian-forest-cat/norwegian_forest_cat-0.0.4-py3-none-any.whl/norwegian_forest_cat/cat_method.py
@@ -0,0 +1,19 @@
+import requests
+
+
+class Cat:
+    def __init__(self, name, age=0):
+        self.name = name
+        self.age = age
+
+    def get_name(self):
+        return self.name
+
+    def get_age(self):
+        return self.age
+
+
+if __name__ == "__main__":
+    cat = Cat("inside poyo")
+    print(cat.get_name())
+    print(cat.get_age())
