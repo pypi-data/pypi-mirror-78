6469f27a004397ae792abfd391b434a06bc7c937
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:52 +0000

    Add package oscn/0.0.0.77/oscn-0.0.0.77.tar.gz

---
+++ packages/oscn/oscn-0.0.0.77.tar.gz/oscn-0.0.0.77/oscn/request/parties.py
@@ -0,0 +1,20 @@
+from .. import settings
+from ..parse import append_parsers
+
+from oscn._meta import party_get
+
+
+# ?db=tulsa&cn=CF-2020-1&id=12576087
+
+@append_parsers
+class Party(object):
+
+    def __init__(self, party_id, db='oklahoma'):
+        self.id = party_id
+        self.db = db
+        self._request()
+
+    def _request(self):
+        response = party_get(self.id, self.db)
+        self.source = response.request.url
+        self.text = response.text
