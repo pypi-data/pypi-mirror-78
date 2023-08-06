a8b2a36dfa31bcbbb04cf2e6be96974b178d93c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:49 +0000

    Add package py-distributions-dsnd/0.2/py_distributions_dsnd-0.2.tar.gz

---
+++ packages/py-distributions-dsnd/py_distributions_dsnd-0.2.tar.gz/py_distributions_dsnd-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='py_distributions_dsnd',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['py_distributions_dsnd'],
+      author = 'Ayushi Mittal',
+      author_email = '',
+      zip_safe=False)
