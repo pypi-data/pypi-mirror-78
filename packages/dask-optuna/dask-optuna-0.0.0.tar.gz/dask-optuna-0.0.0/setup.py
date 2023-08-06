ab785d9f437c62226d41b2dcdf7f31b8de9c77e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:15 +0000

    Add package dask-optuna/0.0.0/dask-optuna-0.0.0.tar.gz

---
+++ packages/dask-optuna/dask-optuna-0.0.0.tar.gz/dask-optuna-0.0.0/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="dask-optuna",
+    packages=["dask_optuna"],
+    include_package_data=True,
+    zip_safe=False,
+)
