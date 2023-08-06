7d4cfdb3ce4f9f064d3eed69cbea2075648b86da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:17 +0000

    Add package tensor-sensor/0.1a0/tensor-sensor-0.1a0.tar.gz

---
+++ packages/tensor-sensor/tensor-sensor-0.1a0.tar.gz/tensor-sensor-0.1a0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='tensor-sensor',
+    version='0.1a',
+    url='https://github.com/parrt/tensor-sensor',
+    license='MIT',
+    py_modules=['tsensor'],
+    author='Terence Parr',
+    author_email='parrt@cs.usfca.edu',
+    python_requires='>=3.6',
+    install_requires=['graphviz','numpy','pytorch'],
+    description='The goal of this library is to generate more helpful exception messages for numpy/pytorch tensor algebra expressions.',
+#    keywords='visualization data structures',
+    classifiers=['License :: OSI Approved :: MIT License',
+                 'Intended Audience :: Developers']
+)
