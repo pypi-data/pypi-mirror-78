4fd79af3f2d2f4d7c985712e394e9d49778d9f1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:05 +0000

    Add package pygalmesh/0.7.0/pygalmesh-0.7.0.tar.gz

---
+++ packages/pygalmesh/pygalmesh-0.7.0.tar.gz/pygalmesh-0.7.0/test/test_inr.py
@@ -0,0 +1,28 @@
+import os
+
+import helpers
+
+import pygalmesh
+
+
+def test_inr():
+    this_dir = os.path.dirname(os.path.abspath(__file__))
+    mesh = pygalmesh.generate_from_inr(
+        os.path.join(this_dir, "meshes", "skull_2.9.inr"), cell_size=5.0, verbose=False
+    )
+
+    tol = 2.0e-3
+    ref = [2.031053e02, 3.739508e01, 2.425594e02, 2.558910e01, 2.300883e02, 1.775010e00]
+    assert abs(max(mesh.points[:, 0]) - ref[0]) < tol * ref[0]
+    assert abs(min(mesh.points[:, 0]) - ref[1]) < tol * ref[1]
+    assert abs(max(mesh.points[:, 1]) - ref[2]) < tol * ref[2]
+    assert abs(min(mesh.points[:, 1]) - ref[3]) < tol * ref[3]
+    assert abs(max(mesh.points[:, 2]) - ref[4]) < tol * ref[4]
+    tol = 3.0e-2
+    assert abs(min(mesh.points[:, 2]) - ref[5]) < tol * ref[5]
+
+    vol = sum(helpers.compute_volumes(mesh.points, mesh.get_cells_type("tetra")))
+    ref = 2.725335e06
+    # Debian needs 2.0e-2 here.
+    # <https://github.com/nschloe/pygalmesh/issues/60>
+    assert abs(vol - ref) < ref * 2.0e-2
---
+++ packages/pygalmesh/pygalmesh-0.7.0.tar.gz/pygalmesh-0.7.0/test/test_volume_from_surface.py
@@ -0,0 +1,28 @@
+import os
+
+import helpers
+
+import pygalmesh
+
+
+def test_volume_from_surface():
+    this_dir = os.path.dirname(os.path.abspath(__file__))
+    mesh = pygalmesh.generate_volume_mesh_from_surface_mesh(
+        os.path.join(this_dir, "meshes", "elephant.vtu"),
+        facet_angle=25.0,
+        facet_size=0.15,
+        facet_distance=0.008,
+        cell_radius_edge_ratio=3.0,
+        verbose=False,
+    )
+
+    tol = 2.0e-2
+    assert abs(max(mesh.points[:, 0]) - 0.357612477657) < tol
+    assert abs(min(mesh.points[:, 0]) + 0.358747130015) < tol
+    assert abs(max(mesh.points[:, 1]) - 0.496137874959) < tol
+    assert abs(min(mesh.points[:, 1]) + 0.495301051456) < tol
+    assert abs(max(mesh.points[:, 2]) - 0.298780230629) < tol
+    assert abs(min(mesh.points[:, 2]) + 0.300472866512) < tol
+
+    vol = sum(helpers.compute_volumes(mesh.points, mesh.get_cells_type("tetra")))
+    assert abs(vol - 0.044164693065) < tol
