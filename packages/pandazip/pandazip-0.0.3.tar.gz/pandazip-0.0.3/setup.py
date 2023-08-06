e03917725af8d50e69446e96929c6c56c688c323
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:57 +0000

    Add package pandazip/0.0.3/pandazip-0.0.3.tar.gz

---
+++ packages/pandazip/pandazip-0.0.3.tar.gz/pandazip-0.0.3/setup.py
@@ -0,0 +1,25 @@
+
+
+import setuptools
+
+setuptools.setup(
+    name='pandazip',
+    version='0.0.3',
+    description='Cut memory footprint by half in just 2 lines of code. Compress Pandas DataFrame without losing information.',
+    url='https://github.com/meetyildiz/pandazip',
+    author='MEHMET YILDIZ',
+    author_email='myildiz.ie@gmail.com',
+    packages=setuptools.find_packages(where='./pandazip'),
+    package_dir={
+        '': 'pandazip'},
+    install_requires=['pandas',
+                      'numpy',
+                      'joblib',
+                      ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
