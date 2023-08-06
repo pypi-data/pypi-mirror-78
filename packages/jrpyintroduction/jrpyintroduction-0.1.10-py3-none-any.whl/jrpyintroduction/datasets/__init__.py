482e8fc8a8395f694b1d6b890a5f97821835839d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:55 +0000

    Add package jrpyintroduction/0.1.10/jrpyintroduction-0.1.10-py3-none-any.whl

---
+++ packages/jrpyintroduction/jrpyintroduction-0.1.10-py3-none-any.whl/jrpyintroduction/datasets/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+from . import movies  # noqa F401 
---
+++ packages/jrpyintroduction/jrpyintroduction-0.1.10-py3-none-any.whl/jrpyintroduction/get_numeric_list.py
@@ -0,0 +1,9 @@
+from numpy.random import uniform
+from numpy.random import seed
+from numpy import round
+
+
+def get_numeric_list():
+    seed(0)  # set the seed so always the same values
+    x = list(round(uniform(-40, 90, 55748), decimals=2))
+    return x
