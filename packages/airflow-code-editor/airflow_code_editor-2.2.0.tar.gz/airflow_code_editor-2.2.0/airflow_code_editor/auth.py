a0c1f1760328b1e381855ed38f46e34b24e1315f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:45 +0000

    Add package airflow-code-editor/2.2.0/airflow_code_editor-2.2.0.tar.gz

---
+++ packages/airflow-code-editor/airflow_code_editor-2.2.0.tar.gz/airflow_code_editor-2.2.0/airflow_code_editor/auth.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import airflow
+from functools import wraps
+
+__all__ = [
+    'login_required'
+]
+
+
+def login_required(func):
+    # when airflow loads plugins, login is still None.
+    @wraps(func)
+    def func_wrapper(*args, **kwargs):
+        if airflow.login:
+            return airflow.login.login_required(func)(*args, **kwargs)
+        return func(*args, **kwargs)
+    return func_wrapper
