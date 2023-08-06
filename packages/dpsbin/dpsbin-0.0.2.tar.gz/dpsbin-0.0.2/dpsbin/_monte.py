a3115bf8cd60861c766d0374905d5faa81609452
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:33 +0000

    Add package dpsbin/0.0.2/dpsbin-0.0.2.tar.gz

---
+++ packages/dpsbin/dpsbin-0.0.2.tar.gz/dpsbin-0.0.2/dpsbin/_monte.py
@@ -0,0 +1,75 @@
+import numpy as np
+from random import randint, random
+from math import inf
+
+def pmf(M: int, T: int, trial_count: int=10_000) -> np.array:
+    """Computes chance of stopping after k rolls of 1dM trying to reach total T
+
+    Args:
+        M: positive integer, die roll from 1 -> M (inclusive)
+        T: non-negative integer, total we want to meet or exceed
+
+    Kwargs:
+        trial_count: positive integer (default 10_000), number of
+        simulations used to estimate result probabilities.
+        Bigger values give better results but take more time.
+
+    Returns:
+        An array of probabilities with shape (T+1,)
+        rtn[k] == <chance of stopping after roll k>
+
+    Notes:
+        Argument validation is the responsibility of the caller.
+
+    Design:
+        This is intended to be a reference implementation to which
+        other methods whose correctness is non-obvious can be compared.
+        The use of sequence accelerators and other such techniques should
+        be relegated to other implementations.
+    """
+    def simulate():
+        t, n = 0, 0
+        while t < T:
+            t, n = t+randint(1, M), n+1
+        return n
+
+    rtn = np.zeros(T+1)
+    for _ in range(trial_count):
+        rtn[simulate()] += 1
+
+    return rtn/np.sum(rtn)
+
+def zero_pmf(M: int, T: int, p_zero: float, trial_count: int=10_000) -> np.array:
+    """Simulates rolling 1dM with a p_zero chance of failure till a total of T is reached
+
+    Args:
+        M: positive integer, max die roll
+        T: non-negative integer, total we're attempting to reach
+        p_zero: float in [0, 1] (inclusive)
+
+    Kwargs:
+        trial_count: positive integer (default 10_000), bigger is better
+            and slower
+    """
+    def simulate():
+        if p_zero==1:
+            return inf
+        t, n = 0, 0
+        while t < T:
+            t, n = t+randint(1,M)*(random()>p_zero), n+1
+        return n
+
+    rtn = [0]*(T+1)
+    for _ in range(trial_count):
+        v = simulate()
+        if v==inf:
+            continue
+        if v >= len(rtn):
+            rtn += [0]*len(rtn)
+        rtn[v] += 1
+    rtn = np.array(rtn)
+    s = np.sum(rtn)
+
+    if s:
+        return rtn/s
+    return rtn
---
+++ packages/dpsbin/dpsbin-0.0.2.tar.gz/dpsbin-0.0.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dpsbin",
+    version="0.0.2",
+    author="Hans Musgrave",
+    author_email="Hans.Musgrave@gmail.com",
+    description="Compute exact DPS for turn-based games",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hmusgrave/dpsbin",
+    download_url="https://github.com/hmusgrave/dpsbin/archive/0.0.2.tar.gz",
+    packages=setuptools.find_packages(),
+    install_requires=['numpy', 'scipy>=1.0.0'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+)
