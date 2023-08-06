339a09fd348cde7fd43bd16c61a60943d6651cd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:01 +0000

    Add package pogoocr/0.1.0/PogoOCR-0.1.0.tar.gz

---
+++ packages/pogoocr/PogoOCR-0.1.0.tar.gz/PogoOCR-0.1.0/PogoOCR/cloudvision.py
@@ -0,0 +1,21 @@
+from google.cloud import vision
+from google.cloud.vision import types
+
+
+class Image:
+    def __init__(self, image_uri):
+        self.google = vision.ImageAnnotatorClient()
+        self.image_uri = image_uri
+
+    @property
+    def image(self):
+        image = types.Image()
+        image.source.image_uri = self.image_uri
+        return image
+
+    def get_text(self):
+        print(
+            "Requesting TEXT_DETECTION from Google Cloud API. This will cost us 0.0015 USD."
+        )
+        response = self.google.text_detection(image=self.image)
+        self.text_found = response.text_annotations
