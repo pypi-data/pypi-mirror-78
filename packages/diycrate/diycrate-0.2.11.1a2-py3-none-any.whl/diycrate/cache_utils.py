082ff1d0f0eeecc43aa5c0e07f26fa1239769af7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:17 +0000

    Add package diycrate/0.2.11.1a2/diycrate-0.2.11.1a2-py3-none-any.whl

---
+++ packages/diycrate/diycrate-0.2.11.1a2-py3-none-any.whl/diycrate/cache_utils.py
@@ -0,0 +1,118 @@
+import json
+import logging
+import os
+import time
+from os import PathLike
+from pathlib import Path
+from typing import Union, Optional, List
+
+import redis
+from boxsdk.object.file import File
+from boxsdk.object.folder import Folder
+
+from . import setup_logger
+
+setup_logger()
+
+crate_logger = logging.getLogger(__name__)
+
+
+def redis_key(key):
+    """
+
+    :param key:
+    :return:
+    """
+    return "diy_crate.version.{}".format(key)
+
+
+def redis_set(
+    cache_client: redis.Redis,
+    cloud_item: Union[File, Folder],
+    last_modified_time: float,
+    box_dir_path: PathLike,
+    fresh_download: bool = False,
+    folder: str = None,
+    sub_ids: Optional[List[str]] = None,
+    parent_id: Optional[str] = None,
+) -> None:
+    """
+
+    :param cache_client:
+    :param cloud_item:
+    :param last_modified_time:
+    :param box_dir_path:
+    :param fresh_download:
+    :param folder:
+    :param sub_ids:
+    :param parent_id:
+    :return:
+    """
+    key = redis_key(cloud_item.object_id)
+    if "etag" not in cloud_item:
+        cloud_item = cloud_item.get(fields=["etag", "path_collection", "name"])
+    if folder:
+        path = Path(folder)
+    elif int(cloud_item["path_collection"]["total_count"]) > 1:
+        path = Path(
+            os.path.sep.join(
+                [
+                    folder["name"]
+                    for folder in cloud_item["path_collection"]["entries"][1:]
+                ]
+            )
+        )
+    else:
+        path = Path()
+    path = Path(box_dir_path) / path
+    item_info = {
+        "fresh_download": fresh_download,
+        "time_stamp": last_modified_time,
+        "etag": cloud_item["etag"],
+        "file_path": (path / cloud_item["name"]).as_posix(),
+    }
+    if sub_ids is not None:
+        item_info["sub_ids"] = sub_ids
+    if parent_id:
+        item_info["parent_id"] = parent_id
+    cache_client.set(key, json.dumps(item_info))
+    cache_client.set("diy_crate.last_save_time_stamp", int(time.time()))
+    crate_logger.debug(f"Storing/updating info to redis: {item_info}")
+    # assert redis_get(obj)
+
+
+def redis_get(cache_client, obj):
+    """
+
+    :param cache_client:
+    :param obj:
+    :return:
+    """
+    key = redis_key(obj.object_id)
+    return json.loads(str(cache_client.get(key), encoding="utf-8", errors="strict"))
+
+
+def id_for_file_path(cache_client, file_path):
+    """
+
+    :param cache_client:
+    :param file_path:
+    :return:
+    """
+    for key in cache_client.keys("diy_crate.version.*"):
+        value = json.loads(
+            str(cache_client.get(key), encoding="utf-8", errors="strict")
+        )
+        if value.get("file_path") == file_path:
+            return str(key, encoding="utf-8", errors="strict").split(".")[-1]
+
+
+r_c = redis.StrictRedis()
+
+
+def local_or_box_file_m_time_key_func(x: Union[PathLike, str], from_box: bool) -> str:
+    return f"diy_crate.{'box' if from_box else 'local'}_mtime.{x}"
+
+
+def local_or_box_file_m_time(x: PathLike, from_box: bool) -> float:
+    return float(r_c.get(local_or_box_file_m_time_key_func(x, from_box)))
---
+++ packages/diycrate/diycrate-0.2.11.1a2-py3-none-any.whl/diycrate/oauth_utils.py
@@ -0,0 +1,128 @@
+import webbrowser
+
+import httpx
+
+from boxsdk import OAuth2
+from boxsdk.auth import RemoteOAuth2
+
+from .cache_utils import r_c
+
+import logging
+
+from .log_utils import setup_logger
+
+setup_logger()
+
+crate_logger = logging.getLogger(__name__)
+
+
+def setup_oauth(cache_client, conf_object, callback):
+    """
+    sets up the oauth instance with credentials and runtime callback.
+    :param cache_client:
+    :param conf_object:
+    :param callback:
+    :return:
+    """
+    access_token = cache_client.get("diy_crate.auth.access_token")
+    refresh_token = cache_client.get("diy_crate.auth.refresh_token")
+    if isinstance(access_token, bytes):
+        access_token = access_token.decode(encoding="utf-8")
+    if isinstance(refresh_token, bytes):
+        refresh_token = refresh_token.decode(encoding="utf-8")
+    oauth = OAuth2(
+        client_id=conf_object["oauth2"]["client_id"],
+        client_secret=conf_object["oauth2"]["client_secret"],
+        store_tokens=callback,
+        access_token=access_token,
+        refresh_token=refresh_token,
+    )
+    return oauth
+
+
+def get_access_token(access_token):
+    """
+
+    :param access_token: token we will try to refresh/get anew
+    :return:
+    """
+    from .file_operations import conf_obj
+
+    if isinstance(access_token, bytes):
+        access_token = access_token.decode(encoding="utf-8")
+
+    conf_object = conf_obj
+    remote_url = conf_object["box"]["token_url"]
+    refresh_token = r_c.get("diy_crate.auth.refresh_token")
+
+    if isinstance(refresh_token, bytes):
+        refresh_token = refresh_token.decode(encoding="utf-8")
+
+    access_token, refresh_token = httpx.post(
+        remote_url,
+        data={"refresh_token": refresh_token, "access_token": access_token},
+        verify=True,
+    ).json()
+    r_c.set("diy_crate.auth.access_token", access_token)
+    r_c.set("diy_crate.auth.refresh_token", refresh_token)
+    crate_logger.debug(
+        f"new access_token: {access_token}, refresh_token: {refresh_token}"
+    )
+    return access_token
+
+
+def setup_remote_oauth(cache_client, retrieve_access_token=get_access_token):
+    """
+    sets up the oauth instance with credentials and runtime callback.
+    :param cache_client:
+    :param retrieve_access_token:
+    :return:
+    """
+    access_token = cache_client.get("diy_crate.auth.access_token")
+
+    if isinstance(access_token, bytes):
+        access_token = access_token.decode(encoding="utf-8")
+    refresh_token = cache_client.get("diy_crate.auth.refresh_token")
+
+    if isinstance(refresh_token, bytes):
+        refresh_token = refresh_token.decode(encoding="utf-8")
+    oauth = RemoteOAuth2(
+        client_id="",
+        client_secret="",
+        access_token=access_token,
+        refresh_token=refresh_token,
+        retrieve_access_token=retrieve_access_token,
+    )
+    return oauth
+
+
+def store_tokens_callback(access_token, refresh_token):
+    """
+    Intention is to store the oauth tokens
+    :param access_token:
+    :param refresh_token:
+    :return:
+    """
+    assert access_token
+    assert refresh_token
+    if isinstance(access_token, bytes):
+        access_token = access_token.decode(encoding="utf-8")
+
+    if isinstance(refresh_token, bytes):
+        refresh_token = refresh_token.decode(encoding="utf-8")
+    r_c.set("diy_crate.auth.access_token", access_token)
+    r_c.set("diy_crate.auth.refresh_token", refresh_token)
+
+
+def oauth_dance(redis_client, conf, bottle_app, file_event_handler, bottle_thread=None):
+    bottle_app.oauth = file_event_handler.oauth = setup_remote_oauth(redis_client)
+
+    resp = httpx.get(
+        conf["box"]["authorization_url"],
+        params={"redirect_url": f"https://localhost:{conf['box']['web_server_port']}/"},
+        verify=True,
+    )
+    auth_url, bottle_app.csrf_token = resp.json()
+    if bottle_thread and not bottle_thread.is_alive():
+        bottle_thread.start()
+    webbrowser.open_new_tab(auth_url)  # make it easy for the end-user to start auth
