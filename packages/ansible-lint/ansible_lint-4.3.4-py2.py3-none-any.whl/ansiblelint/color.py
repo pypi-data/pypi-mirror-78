e02b82af0b04635ba39682bbb1859b164fcebe27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:17 +0000

    Add package ansible-lint/4.3.4/ansible_lint-4.3.4-py2.py3-none-any.whl

---
+++ packages/ansible-lint/ansible_lint-4.3.4-py2.py3-none-any.whl/ansiblelint/color.py
@@ -0,0 +1,29 @@
+"""Console coloring and terminal support."""
+from enum import Enum
+
+from rich.console import Console
+from rich.theme import Theme
+
+_theme = Theme({
+    "info": "cyan",
+    "warning": "dim yellow",
+    "danger": "bold red",
+    "title": "yellow"
+})
+console = Console(theme=_theme)
+
+
+class Color(Enum):
+    """Color styles."""
+
+    reset = "0"
+    error_code = "1;31"  # bright red
+    error_title = "0;31"  # red
+    filename = "0;34"  # blue
+    linenumber = "0;36"  # cyan
+    line = "0;35"  # purple
+
+
+def colorize(text: str, color: Color) -> str:
+    """Return ANSI formated string."""
+    return f"\u001b[{color.value}m{text}\u001b[{Color.reset.value}m"
---
+++ packages/ansible-lint/ansible_lint-4.3.4-py2.py3-none-any.whl/ansiblelint/rules/MissingFilePermissionsRule.py
@@ -0,0 +1,75 @@
+# Copyright (c) 2020 Sorin Sbarnea <sorin.sbarnea@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+from ansiblelint.rules import AnsibleLintRule
+
+
+class MissingFilePermissionsRule(AnsibleLintRule):
+    id = "208"
+    shortdesc = 'File permissions not mentioned'
+    description = (
+        "Missing mode parameter can cause unexpected file permissions based "
+        "on version of Ansible being used. Be explicit, or if you still "
+        "want the default behavior you can use ``mode: preserve`` to avoid "
+        "hitting this rule. See "
+        "https://github.com/ansible/ansible/issues/71200"
+    )
+    severity = 'VERY_HIGH'
+    tags = ['unpredictability']
+    version_added = 'v4.3.0'
+
+    _modules = (
+        'assemble',
+        'archive',
+        'copy',
+        'file',
+        'template',
+        'unarchive',
+    )
+
+    _modules_with_create = (
+        'blockinfile',
+        'ini_file',
+        'lineinfile'
+    )
+
+    def matchtask(self, file, task):
+        if task["action"]["__ansible_module__"] not in self._modules and \
+                task["action"]["__ansible_module__"] not in self._modules_with_create:
+            return False
+
+        if task["action"]["__ansible_module__"] in self._modules_with_create and \
+                not task["action"].get("create", False):
+            return False
+
+        # A file that doesn't exist cannot have a mode
+        if task['action'].get('state', None) == "absent":
+            return False
+
+        # A symlink always has mode 0o777
+        if task['action'].get('state', None) == "link":
+            return False
+
+        # The file module does not create anything when state==file (default)
+        if task["action"]["__ansible_module__"] == "file" and \
+                task['action'].get('state', 'file') == 'file':
+            return False
+
+        mode = task['action'].get('mode', None)
+        return mode is None
