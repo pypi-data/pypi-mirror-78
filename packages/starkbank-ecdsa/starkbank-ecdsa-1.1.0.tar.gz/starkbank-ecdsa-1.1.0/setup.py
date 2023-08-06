d1abd44d8e689bdb9a43678ec4155b036b0a8ff0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:46 +0000

    Add package starkbank-ecdsa/1.1.0/starkbank-ecdsa-1.1.0.tar.gz

---
+++ packages/starkbank-ecdsa/starkbank-ecdsa-1.1.0.tar.gz/starkbank-ecdsa-1.1.0/setup.py
@@ -0,0 +1,36 @@
+from os import path
+from setuptools import setup, find_packages
+
+
+with open(path.join(path.dirname(__file__), "README.md")) as readme:
+    README = readme.read()
+
+
+setup(
+    name="starkbank-ecdsa",
+    packages=find_packages(),
+    include_package_data=True,
+    description="A lightweight and fast pure python ECDSA library",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    license="MIT License",
+    url="https://github.com/starkbank/ecdsa-python.git",
+    author="Stark Bank",
+    author_email="developers@starkbank.com",
+    keywords=["ecdsa", "elliptic curve", "elliptic", "curve", "stark bank", "starkbank", "cryptograph"],
+    version="1.1.0"
+)
+
+
+### Create a source distribution:
+
+#Run ```python setup.py sdist``` inside the project directory.
+
+### Install twine:
+
+#```pip install twine```
+
+### Upload package to pypi:
+
+#```twine upload dist/*```
+
