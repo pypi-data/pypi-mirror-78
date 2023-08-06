9067e95142f0682a8274bbc208227f8583ca7564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:42 +0000

    Add package cnn-colorflow/0.1/cnn_colorflow-0.1.tar.gz

---
+++ packages/cnn-colorflow/cnn_colorflow-0.1.tar.gz/cnn_colorflow-0.1/README.md
@@ -0,0 +1,3 @@
+# cnn_colorflow
+A series of useful functions developed during the writing of
+"Identifying the Quantum Properties of Hadronic Resonances using Machine Learning"
---
+++ packages/cnn-colorflow/cnn_colorflow-0.1.tar.gz/cnn_colorflow-0.1/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='cnn_colorflow',
+      version='0.1',
+      description='A series of useful functions developed during the writing of "Identifying the Quantum Properties of Hadronic Resonances using Machine Learning"',
+      author='John Kruper',
+      author_email='jk6.28@outlook.com')
