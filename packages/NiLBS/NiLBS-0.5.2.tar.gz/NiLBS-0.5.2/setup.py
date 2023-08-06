e3bc6b68a186a7aa91cdd2edf9d20bfeb6da9087
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:37 +0000

    Add package NiLBS/0.5.2/NiLBS-0.5.2.tar.gz

---
+++ packages/NiLBS/NiLBS-0.5.2.tar.gz/NiLBS-0.5.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='NiLBS',
+   version='0.5.2',
+   description='NiLBS 3D implementation.',
+   author='Joe March',
+   author_email='jgm45@cam.ac.uk',
+   packages=['NiLBS'],  #same as name
+   install_requires=['trimesh', 'pyrender', 'tensorflow'], #external packages as dependencies
+)
