17254b9a84c823b951c4e47d75fb1e425997afa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:18 +0000

    Add package eth-enr/0.2.1/eth_enr-0.2.1-py3-none-any.whl

---
+++ packages/eth-enr/eth_enr-0.2.1-py3-none-any.whl/eth_enr/constants.py
@@ -0,0 +1,9 @@
+IDENTITY_SCHEME_ENR_KEY = b"id"
+ENR_REPR_PREFIX = "enr:"  # prefix used when printing an ENR
+MAX_ENR_SIZE = 300  # maximum allowed size of an ENR
+IP_V4_ADDRESS_ENR_KEY = b"ip"
+UDP_PORT_ENR_KEY = b"udp"
+TCP_PORT_ENR_KEY = b"tcp"
+
+IP_V4_SIZE = 4  # size of an IPv4 address
+IP_V6_SIZE = 16  # size of an IPv6 address
