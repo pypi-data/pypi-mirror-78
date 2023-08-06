b5636890ce6a69d65560456179a080dba889daa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:55 +0000

    Add package RiteshYadavCalculator/0.0.1/RiteshYadavCalculator-0.0.1.tar.gz

---
+++ packages/RiteshYadavCalculator/RiteshYadavCalculator-0.0.1.tar.gz/RiteshYadavCalculator-0.0.1/README.txt
@@ -0,0 +1,1 @@
+This is a very simple calculator that take two no and apply operation on it.
---
+++ packages/RiteshYadavCalculator/RiteshYadavCalculator-0.0.1.tar.gz/RiteshYadavCalculator-0.0.1/my_calculator/__init__.py
@@ -0,0 +1,14 @@
+def add_number(num1, num2):
+    return num1 + num2
+
+
+def subtract_number(num1, num2):
+    return num1 - num2
+
+
+def multiply_number(num1, num2):
+    return num1 * num2
+
+
+def divide_number(num1, num2):
+    return num1/num2
