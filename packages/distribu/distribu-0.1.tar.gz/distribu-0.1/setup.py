7ecf2cb52687f4e0144d8f6e5ecc4a6980b86776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:27 +0000

    Add package distribu/0.1/distribu-0.1.tar.gz

---
+++ packages/distribu/distribu-0.1.tar.gz/distribu-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distribu',
+      version='0.1',
+      author = 'Priyanshu Arya',
+      author_email = 'priyanshucoder@gmail.com',
+      description='Gaussian distributions',
+      packages=['distribu'],
+      zip_safe=False)
