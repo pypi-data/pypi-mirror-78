3b083ada00d09fb1d8aba63f4ae79608a856f532
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:17:24 +0000

    Add package styler-validation/1.0.0/styler_validation-1.0.0-py2.py3-none-any.whl

---
+++ packages/styler-validation/styler_validation-1.0.0-py2.py3-none-any.whl/styler_validation/messages.py
@@ -0,0 +1,14 @@
+""" Common validation messages
+"""
+
+INVALID_VALUE = 'invalid_value'
+REQUIRED_VALUE = 'required_value'
+GREATER_THAN = 'greater_than'
+GREATER_OR_EQUAL_THAN = 'greater_or_equal_than'
+LESS_THAN = 'less_than'
+LESS_OR_EQUAL_THAN = 'less_or_equal_than'
+POSITIVE_VALUE = 'positive_value'
+NOT_ZERO = 'not_zero'
+INVALID_TIME = 'invalid_time'
+NOT_EMPTY = 'not_empty'
+STRING_TOO_LONG = 'string_too_long'
