38a8f79378081083ba25b22d28e01eaa0c4f8eef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:03 +0000

    Add package soloman/2.4b0/soloman-2.4b0.tar.gz

---
+++ packages/soloman/soloman-2.4b0.tar.gz/soloman-2.4b0/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as rm:
+    long_desc = rm.read()
+
+setup(
+    name='soloman',
+    version='2.4-beta',
+    description='For the love of python and qml',
+    long_description=long_desc,
+    long_description_content_type='text/markdown',
+    install_requires=['numpy', 'pyaudio', 'pyffmpeg'],
+    keywords="audio, pyaudio, ffmpeg, qml, pyffmpeg",
+    url='https://github.com/deuteronomy-works/soloman',
+    author='Amoh - Gyebi Godwin Ampofo Michael',
+    author_email='amohgyebigodwin@gmail.com',
+    project_urls={
+        "Bug Tracker": "https://github.com/deuteronomy-works/soloman/issues/",
+        "Documentation": "https://github.com/deuteronomy-works/soloman/wiki/",
+        "Source Code": "https://github.com/deuteronomy-works/soloman/",
+    },
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+    packages=['soloman', 'soloman.audio', 'soloman.video', 'PyQt5'],
+    package_data={'PyQt5': ['Qt/qml/soloman/qmldir', 'Qt/qml/soloman/*.qml']},
+)
---
+++ packages/soloman/soloman-2.4b0.tar.gz/soloman-2.4b0/soloman/__init__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 31 2020 12:50:44
+
+"""
+import os
+from time import sleep
+from PyQt5.QtQml import qmlRegisterType
+
+# import Audio and Video here so people can import it without
+# going into the audio and video packages to access them
+from .audio import Audio
+from .video import Video
+from soloman.pyqt_inter_audio import QAudio
+from soloman.pyqt_inter_video import QVideo
+from soloman.fps_clocks import *
+
+
+def register():
+
+    # Delete the contents of our Temp folder
+    path = os.path.join(os.environ['TEMP'], 'soloman', 'converts')
+    if os.path.exists(path):
+        os.system('RD "' + path + '" /S /Q')
+
+    qmlRegisterType(QAudio, 'soloman', 2, 4, 'SAudio')
+    qmlRegisterType(QVideo, 'soloman', 2, 4, 'QVideo')
+
+
+register()
