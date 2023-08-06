64f1c491805d0dec5fe1bbb4158274dee464dbf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:07 +0000

    Add package lowclouds/0.1.0/lowclouds-0.1.0.tar.gz

---
+++ packages/lowclouds/lowclouds-0.1.0.tar.gz/lowclouds-0.1.0/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['lowclouds']
+
+package_data = \
+{'': ['*']}
+
+extras_require = \
+{'bigquery': ['google-cloud-bigquery>=1.27.2,<2.0.0',
+              'google-cloud-bigquery-storage>=1.0.0,<2.0.0']}
+
+setup_kwargs = {
+    'name': 'lowclouds',
+    'version': '0.1.0',
+    'description': 'The lowclouds is a shortcut library for the following cloud service libraries.',
+    'long_description': '# lowclouds\n\nThe lowclouds is a shortcut library for the following cloud service libraries.\n\nIt is designed for processes with short lifetimes, not for processes with long lifetimes.\n\n## APIs\n\n',
+    'author': 'rhoboro',
+    'author_email': 'rhoboro@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'extras_require': extras_require,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
