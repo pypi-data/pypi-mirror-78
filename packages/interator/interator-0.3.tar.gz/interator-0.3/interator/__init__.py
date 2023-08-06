f8dca7f2e0a72e575189d2a689e72605687ac23c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:50 +0000

    Add package interator/0.3/interator-0.3.tar.gz

---
+++ packages/interator/interator-0.3.tar.gz/interator-0.3/interator/__init__.py
@@ -0,0 +1,9 @@
+ #!/usr/bin/env python
+"""__init__ file for interator module."""
+
+from .interator import (prime_stream, composite_stream, polygonal_stream,
+                       fibonacci_stream, negafibonacci_stream, lucas_stream,
+                       is_prime, miller_rabin, is_composite, is_polygonal,
+                       is_fibonacci, is_lucas, nth_fibonacci)
+
+
