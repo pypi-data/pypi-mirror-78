865ae3a257bebbf29d588a40fa0d655531dbb01f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:36 +0000

    Add package colmet/0.6.2.dev0/colmet-0.6.2.dev0.tar.gz

---
+++ packages/colmet/colmet-0.6.2.dev0.tar.gz/colmet-0.6.2.dev0/colmet/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+'''
+    Colmet
+    ------
+    Collecting metrics about jobs (task/process/cgroup/etc.) running in a
+    distributed environnement
+
+'''
+__version__ = '0.6.2.dev0'
+VERSION = __version__
