e33a6666d3037cb5f8d92a6c378f527f3ec76754
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:54 +0000

    Add package doif/0.5.0/doif-0.5.0.tar.gz

---
+++ packages/doif/doif-0.5.0.tar.gz/doif-0.5.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['doif']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['rich>=5.2.0,<6.0.0']
+
+setup_kwargs = {
+    'name': 'doif',
+    'version': '0.5.0',
+    'description': 'TryHackMe',
+    'long_description': None,
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
