e0e5cfae5023484937abdde8193f23d7292e1799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:05 +0000

    Add package emhub/0.0.1a1.post1/emhub-0.0.1a1.post1.tar.gz

---
+++ packages/emhub/emhub-0.0.1a1.post1.tar.gz/emhub-0.0.1a1.post1/emhub/data/__main__.py
@@ -0,0 +1,41 @@
+
+import os
+import sys
+
+from .data_manager import DataManager
+from .imports.testdata import TestData
+from .imports.scilifelab_portal import PortalData
+
+
+if __name__ == '__main__':
+    instance_path = os.path.abspath(os.environ.get("EMHUB_INSTANCE",
+                                                   'instance'))
+
+    if not os.path.exists(instance_path):
+        raise Exception("Please execute the test command from the root of the "
+                        "working directory.")
+
+    dbPath = os.path.join(instance_path, 'emhub.sqlite')
+
+    if os.path.exists(dbPath):
+        print("Deleting existing file: ", dbPath)
+        os.remove(dbPath)
+
+    if len(sys.argv) > 1:
+        portalDataJson = sys.argv[1]
+        bookingsJson = sys.argv[2]
+
+        if not os.path.exists(portalDataJson):
+            print("JSON data file '%s' does not exists. " % portalDataJson)
+
+        if not os.path.exists(bookingsJson):
+            print("JSON bookings file '%s' does not exists. " % bookingsJson)
+    else:
+        portalDataJson = None
+
+    dm = DataManager(sqlitePath=dbPath)
+
+    if portalDataJson:
+        PortalData(dm, portalDataJson, bookingsJson)
+    else:
+        TestData(dm)
