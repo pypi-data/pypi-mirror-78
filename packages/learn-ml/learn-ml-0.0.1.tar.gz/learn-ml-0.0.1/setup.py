79d1d5da9a6af604a98bf8b3ace43205f4573d19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:09 +0000

    Add package learn-ml/0.0.1/learn-ml-0.0.1.tar.gz

---
+++ packages/learn-ml/learn-ml-0.0.1.tar.gz/learn-ml-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='learn-ml',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='',
+    author='Bruno Charron',
+    author_email='bruno@charron.email',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
