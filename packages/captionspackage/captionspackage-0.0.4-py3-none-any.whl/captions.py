14d701fdc730364399ce30145d943f4989ad0a58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:28 +0000

    Add package captionspackage/0.0.4/captionspackage-0.0.4-py3-none-any.whl

---
+++ packages/captionspackage/captionspackage-0.0.4-py3-none-any.whl/captions.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+from youtube_transcript_api import YouTubeTranscriptApi
+import json
+
+app = Flask(__name__)
+
+@app.route('/video/<id>')
+def index(id):
+    data = YouTubeTranscriptApi.get_transcript(id, languages=['es'])
+    transcription = json.dumps(data)
+    return jsonify(transcription)
+
+def hi():
+    return "hola"
