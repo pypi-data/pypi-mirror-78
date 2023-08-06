4ce9775dd5f4c0c0b8680afd483f664ad28048c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:12 +0000

    Add package Mezzanine/5.0.0a1/Mezzanine-5.0.0a1-py2.py3-none-any.whl

---
+++ packages/Mezzanine/Mezzanine-5.0.0a1-py2.py3-none-any.whl/mezzanine/__init__.py
@@ -0,0 +1,1 @@
+__version__ = "5.0.0-alpha.1"  # Do not edit, managed by semantic-release
---
+++ packages/Mezzanine/Mezzanine-5.0.0a1-py2.py3-none-any.whl/mezzanine/pages/urls.py
@@ -0,0 +1,17 @@
+from django.urls import re_path
+from django.conf import settings
+
+from mezzanine.pages import page_processors, views
+
+
+page_processors.autodiscover()
+
+
+# Page patterns.
+urlpatterns = [
+    re_path(
+        "(?P<slug>.*)%s$" % ("/" if settings.APPEND_SLASH else ""),
+        views.page,
+        name="page",
+    ),
+]
