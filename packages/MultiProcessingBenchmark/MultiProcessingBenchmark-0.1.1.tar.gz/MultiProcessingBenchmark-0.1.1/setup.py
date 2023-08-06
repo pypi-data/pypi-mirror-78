1b3adbbe110681b945cce7c0c53b977eacb93e37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:51 +0000

    Add package MultiProcessingBenchmark/0.1.1/MultiProcessingBenchmark-0.1.1.tar.gz

---
+++ packages/MultiProcessingBenchmark/MultiProcessingBenchmark-0.1.1.tar.gz/MultiProcessingBenchmark-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='MultiProcessingBenchmark',
+      version='0.1.1',
+      url='https://github.com/srivassid/MultiProcessingBenchmark',
+      license='MIT',
+      author='Siddharth Srivastava',
+      author_email='s.srivas@hotmail.com',
+      description='Benchmarking library single core vs multi core for common pandas functions',
+      packages=find_packages(),
+      long_description=open('README.md').read(),
+      zip_safe=False)
