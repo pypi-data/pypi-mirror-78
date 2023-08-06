5c902a88b71bd7af693a29a9dafc72523e1044ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:43 +0000

    Add package whatsappguru/0.2.6/whatsappguru-0.2.6.tar.gz

---
+++ packages/whatsappguru/whatsappguru-0.2.6.tar.gz/whatsappguru-0.2.6/whatsappguru/__init__.py
@@ -0,0 +1,16 @@
+# import submodules you want to install
+from .whatsapp import *
+
+# module level doc-string
+__doc__ = """
+whatsapp - a library for Python to analyze WhatsApp chats
+=====================================================================
+
+**whatsapp** is a Python package providing many different useful functions
+to analyze your exported whatsapp chats
+
+Main Features
+-------------
+Here are just a few of the things that whatsapp does well:
+TODO
+"""
