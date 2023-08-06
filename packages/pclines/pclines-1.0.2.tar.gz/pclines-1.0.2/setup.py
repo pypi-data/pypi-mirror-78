816b983737001c8cf360a0c7803a858106450a5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:20 +0000

    Add package pclines/1.0.2/pclines-1.0.2.tar.gz

---
+++ packages/pclines/pclines-1.0.2.tar.gz/pclines-1.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+import os
+
+current_folder = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(current_folder, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='pclines',
+    version='1.0.2',
+    description='PCLines transform for python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/RomanJuranek/pclines-python',
+    author='Roman Juranek',
+    author_email='ijuranek@fit.vutbr.cz',
+    license='BSD3',
+    keywords='pclines, hough transform, line detection',
+    packages=["pclines"],
+    install_requires=['numpy','numba','scikit-image'],
+    python_requires='>=3.6',
+    project_urls={
+        "Bug reports": 'https://github.com/RomanJuranek/pclines-python/issues',
+    },
+)
