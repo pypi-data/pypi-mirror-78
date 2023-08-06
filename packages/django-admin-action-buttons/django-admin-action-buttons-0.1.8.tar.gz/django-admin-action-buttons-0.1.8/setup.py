f36094a78d3d9d0f408f903576de36dcb0bcbb85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:49 +0000

    Add package django-admin-action-buttons/0.1.8/django-admin-action-buttons-0.1.8.tar.gz

---
+++ packages/django-admin-action-buttons/django-admin-action-buttons-0.1.8.tar.gz/django-admin-action-buttons-0.1.8/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup
+
+setup(
+    name='django-admin-action-buttons',
+    description='Django admin tweak that replaces list action dropdown with '
+                'buttons',
+    version='0.1.8',
+    url='https://github.com/frnhr/django-admin-action-buttons',
+    author='Fran Hrzenjak',
+    author_email='fran@changeset.hr',
+    scripts=[],
+    packages=['admin_action_buttons'],
+    package_data={
+        'admin_action_buttons': [
+            'static/admin_action_buttons/*',
+            'templates/admin_action_buttons/*',
+        ],
+    },
+    license='MIT',
+    keywords='',
+    install_requires=[],
+    extras_require={},
+)
