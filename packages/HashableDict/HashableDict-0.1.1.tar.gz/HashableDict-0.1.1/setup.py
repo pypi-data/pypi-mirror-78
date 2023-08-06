2d467d8bac2e8ae1f99ecc65de038186e1cb9aca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:50 +0000

    Add package HashableDict/0.1.1/HashableDict-0.1.1.tar.gz

---
+++ packages/HashableDict/HashableDict-0.1.1.tar.gz/HashableDict-0.1.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="HashableDict",
+    version="0.1.1",
+    author="IFcoltransG",
+    license="unlicense",
+    author_email="IFcoltransG+PyPI@protonmail.ch",
+    description="A hashable immutable dictionary",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=["dictionary", "dict", "frozen", "hashable", "immutable"],
+    url="https://github.com/IFcoltransG/HashableDict",
+    download_url="https://github.com/IFcoltransG/HashableDict/archive/v0.1.1.tar.gz",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Public Domain",
+        "Operating System :: OS Independent",
+        "Topic :: Utilities",
+    ],
+    python_requires='>=3.7',
+)
