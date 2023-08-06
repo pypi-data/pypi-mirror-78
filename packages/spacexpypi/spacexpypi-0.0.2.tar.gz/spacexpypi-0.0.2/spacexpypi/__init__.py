a02f2b0dcda4b7933cb010d944e73b42b67a7d62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:30 +0000

    Add package spacexpypi/0.0.2/spacexpypi-0.0.2.tar.gz

---
+++ packages/spacexpypi/spacexpypi-0.0.2.tar.gz/spacexpypi-0.0.2/spacexpypi/__init__.py
@@ -0,0 +1,63 @@
+"""Simple PyPi Wrapper for the SpaceX APIs."""
+
+import logging
+import aiohttp
+import json
+
+_LOGGER = logging.getLogger("spacex-pypi")
+
+BASE_URL = "https://api.spacexdata.com/v3/"
+
+class SpaceX:
+    def __init__(self):
+        """Initialize the session."""
+        self.retry = 5
+        self._session = aiohttp.ClientSession()
+
+    async def close(self):
+        """Close the session."""
+        await self._session.close()
+
+    async def get_roadster_status(self):
+        """Get the roadster status."""
+        response = {}
+
+        roadsterURL = BASE_URL + "roadster"
+
+        async with await self._session.get(roadsterURL) as resp:
+            response = await resp.text()
+        
+        return json.loads(response)
+
+    async def get_next_launch(self):
+        """Get the next SpaceX Launch details."""
+        response = {}
+
+        launchURL = BASE_URL + "launches/next"
+
+        async with await self._session.get(launchURL) as resp:
+            response = await resp.text()
+
+        return json.loads(response)
+
+    async def get_latest_launch(self):
+        """Get the latest SpaceX Launch details."""
+        response = {}
+
+        launchURL = BASE_URL + "launches/latest"
+
+        async with await self._session.get(launchURL) as resp:
+            response = await resp.text()
+
+        return json.loads(response)
+
+    async def get_upcoming_launches(self):
+        """Get all upcoming launches."""
+        response = {}
+
+        launchURL = BASE_URL + "launches/upcoming"
+
+        async with await self._session.get(launchURL) as resp:
+            response = await resp.text()
+
+        return json.loads(response)
