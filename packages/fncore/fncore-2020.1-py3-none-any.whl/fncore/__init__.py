868e1285a96c02fdf4faaf76e8b25c5fb8e03686
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:35 +0000

    Add package fncore/2020.1/fncore-2020.1-py3-none-any.whl

---
+++ packages/fncore/fncore-2020.1-py3-none-any.whl/fncore/__init__.py
@@ -0,0 +1,9 @@
+from fncore import fn
+from functools import reduce, partial
+from toolz.functoolz import curry
+from toolz.dicttoolz import merge, merge_with
+
+__all__=[
+    "fn", "reduce", "partial",
+    "curry", "merge", "merge_with"
+]
