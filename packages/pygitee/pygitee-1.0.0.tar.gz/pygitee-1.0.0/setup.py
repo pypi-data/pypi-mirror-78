24a84e14f6ab3670e076730d316e43ce60d42ea4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:30 +0000

    Add package pygitee/1.0.0/pygitee-1.0.0.tar.gz

---
+++ packages/pygitee/pygitee-1.0.0.tar.gz/pygitee-1.0.0/setup.py
@@ -0,0 +1,31 @@
+# coding: utf-8
+
+
+from setuptools import setup  # noqa: H301
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+NAME = "pygitee"
+VERSION = "1.0.0"
+# To install the library, run the following
+#
+# python setup.py install
+#
+# prerequisite: setuptools
+# http://pypi.python.org/pypi/setuptools
+
+REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil"]
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="码云 Open API",
+    author_email="",
+    url="https://gitee.com/kingreatwill/pygitee",
+    keywords=["Swagger", "码云 Open API"],
+    install_requires=REQUIRES,
+    packages=["gitee"],
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
