119df436fe5b02312bd09d39bef73c2948839363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:13 +0000

    Add package django-ok-redirects/0.1.3/django_ok_redirects-0.1.3-py3-none-any.whl

---
+++ packages/django-ok-redirects/django_ok_redirects-0.1.3-py3-none-any.whl/redirects/conf.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+
+__all__ = (
+    'REDIRECTS_IGNORE_PATH_PREFIXES',
+)
+
+REDIRECTS_IGNORE_PATH_PREFIXES = getattr(
+    settings,
+    'REDIRECTS_IGNORE_PATH_PREFIXES',
+    ()
+)
