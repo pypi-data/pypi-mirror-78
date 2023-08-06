38527514944935358d6ba951cc3aa6be068f5113
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:38 +0000

    Add package easydata/0.0.2/easydata-0.0.2.tar.gz

---
+++ packages/easydata/easydata-0.0.2.tar.gz/easydata-0.0.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='easydata',
+    version='0.0.2',
+    description='Data transformation and manipulation library',
+    long_description=open('README.rst').read(),
+    long_description_content_type="text/x-rst",
+    author='Rok Grabnar',
+    author_email='grabnarrok@gmail.com',
+    url='https://github.com/sitegroove/easydata',
+    packages=find_packages(exclude=['tests']),
+    install_requires=[
+        'dateparser',
+        'easytxt',
+        'furl',
+        'ftfy',
+        'jmespath',
+        'price-parser',
+        'pyquery',
+        'xmltodict'
+    ],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
