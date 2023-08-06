dc9f72fe8459e084dce1180e4174a37148f6b25d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:19 +0000

    Add package docker-devbox-ddb/1.0.4/docker-devbox-ddb-1.0.4.tar.gz

---
+++ packages/docker-devbox-ddb/docker-devbox-ddb-1.0.4.tar.gz/docker-devbox-ddb-1.0.4/CONTRIBUTING.md
@@ -0,0 +1,38 @@
+## Contribute to docs
+
+You may run the docs locally with docker
+
+```bash
+docker run --rm -it -p 8000:8000 --user $(id -u):$(id -g) -v ${PWD}:/docs squidfunk/mkdocs-material serve
+```
+
+Docs are then available on http://localhost:8000.
+
+## Publish docs
+
+```bash
+docker run --rm -it -p 8000:8000 --user $(id -u):$(id -g) -v ${PWD}/..:/docs -v ${HOME}/.netrc:/.netrc:ro --workdir=/docs/ddb squidfunk/mkdocs-material gh-deploy
+```
+
+## Build and release process
+
+The release process requires docker with custom images of `cdrx/pyinstaller-linux` and `cdrx/pyinstaller-windows` that 
+should be build from [Toilal/docker-pyinstaller#more-variations](https://github.com/Toilal/docker-pyinstaller/tree/more-variations) with `build.sh` script.
+
+A [pull request is opened](https://github.com/cdrx/docker-pyinstaller/pull/90) and I hope it will be merged and available on docker hub soon.
+
+```
+prerelease
+
+rm -Rf dist/ &&\
+python setup.py clean build bdist bdist_wheel bdist_pex --pex-args="--disable-cache" --bdist-all &&\
+docker run --rm --init -v "$(pwd):/src/" -e "DISABLE_REQUIREMENTS=1" cdrx/pyinstaller-linux:xenial "pip install --upgrade setuptools && pip install -r requirements.txt && pyinstaller --clean -y --dist ./dist --workpath /tmp *.spec" &&\
+docker run --rm --init -v "$(pwd):/src/" -e "DISABLE_REQUIREMENTS=1" cdrx/pyinstaller-windows "pip install --upgrade setuptools && pip install whl/jsonnet-0.15.0-cp37-cp37m-win_amd64.whl && pip install -r requirements.txt && pyinstaller --clean -y --dist ./dist --workpath /tmp *.spec"
+
+release
+
+githubrelease release gfi-centre-ouest/docker-devbox-ddb create $(python -m ddb --version -s) --publish "dist/*"
+
+postrelease
+```
+
---
+++ packages/docker-devbox-ddb/docker-devbox-ddb-1.0.4.tar.gz/docker-devbox-ddb-1.0.4/ddb/feature/certs/actions.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+import os
+
+import cfssl as cfssl_client
+
+from .cfssl import checksums, writer
+from ...action import Action
+from ...config import config
+from ...context import context
+from ...event import events
+from ...utils.file import force_remove
+
+
+class GenerateCertAction(Action):
+    """
+    Generate a certificate for a domain.
+    """
+
+    @property
+    def event_bindings(self):
+        return events.certs.generate
+
+    @property
+    def name(self) -> str:
+        return "certs:generate"
+
+    @staticmethod
+    def execute(domain, wildcard=True):
+        """
+        Generate a certificate
+        """
+        if config.data.get('certs.type') == 'cfssl':
+            GenerateCertAction.generate_cfssl(domain, wildcard)
+
+    @staticmethod
+    def generate_cfssl(domain, wildcard=True):
+        """
+        Generate CA certificate with CFSSL.
+        """
+        certificate_path, private_key_path = writer.get_certs_path(domain,
+                                                                   config.data['certs.cfssl.writer'],
+                                                                   config.data['certs.destination'])
+        if not os.path.exists(certificate_path) or not os.path.exists(private_key_path):
+            client_config = config.data.get('certs.cfssl.server')
+
+            client = cfssl_client.CFSSL(**client_config)
+
+            certificate_request = cfssl_client.CertificateRequest()
+            certificate_request.common_name = domain
+            certificate_request.hosts = (domain, "*.%s" % domain) if wildcard else domain
+
+            response = client.new_cert(certificate_request)
+            checksums.validate_checksums(response)
+
+            destination = config.data['certs.destination']
+            if not os.path.isdir(destination):
+                os.makedirs(destination)
+
+            generated = writer.write_files(response,
+                                           domain,
+                                           None,
+                                           None,
+                                           config.data['certs.cfssl.writer'],
+                                           config.data['certs.destination'],
+                                           config.data['certs.cfssl.append_ca_certificate'],
+                                           client,
+                                           config.data['certs.cfssl.verify_checksum'])
+
+            context.log.success("TLS certificates generated for domain %s", domain)
+
+            for generated_file in generated.values():
+                events.file.generated(source=None, target=generated_file)
+
+            private_key_path, certificate_path = generated['private_key'], generated['certificate']
+            events.certs.generated(domain=domain, wildcard=wildcard, private_key=certificate_path,
+                                   certificate=private_key_path)
+        else:
+            context.log.notice("TLS certificates exists for domain %s", domain)
+
+        events.certs.available(domain=domain, wildcard=wildcard, private_key=private_key_path,
+                               certificate=certificate_path)
+
+
+class RemoveCertAction(Action):
+    """
+    Remove a certificate for a domain.
+    """
+
+    @property
+    def event_bindings(self):
+        return events.certs.remove
+
+    @property
+    def name(self) -> str:
+        return "certs:remove"
+
+    @staticmethod
+    def execute(domain):
+        """
+        Remove certificate
+        """
+        if RemoveCertAction.remove_cfssl(domain):
+            events.certs.removed(domain=domain)
+
+    @staticmethod
+    def remove_cfssl(domain):
+        """
+        Remove CA certificate generated with CFSSL.
+        """
+        certificate_path, private_key_path = writer.get_certs_path(domain,
+                                                                   config.data['certs.cfssl.writer'],
+                                                                   config.data['certs.destination'])
+
+        removed = False
+        if os.path.exists(certificate_path):
+            force_remove(certificate_path)
+            events.file.deleted(certificate_path)
+            removed = True
+
+        if os.path.exists(private_key_path):
+            force_remove(private_key_path)
+            events.file.deleted(certificate_path)
+            removed = True
+
+        return removed
