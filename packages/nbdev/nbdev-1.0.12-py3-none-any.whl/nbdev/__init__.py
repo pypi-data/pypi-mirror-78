b65bd896e13d8fb97ef90cd272588c2096bb166c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:25 +0000

    Add package nbdev/1.0.12/nbdev-1.0.12-py3-none-any.whl

---
+++ packages/nbdev/nbdev-1.0.12-py3-none-any.whl/nbdev/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "1.0.12"
+
+from fastcore.utils import IN_IPYTHON
+from .imports import *
+
+if IN_IPYTHON:
+    from .flags import *
+    from .showdoc import show_doc
+    from .export import notebook2script
