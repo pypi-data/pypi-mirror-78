de07aded98b4831e24ebf1d8694762060c15fc26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:27 +0000

    Add package tox-constraints/0.8/tox-constraints-0.8.tar.gz

---
+++ packages/tox-constraints/tox-constraints-0.8.tar.gz/tox-constraints-0.8/README.md
@@ -0,0 +1,12 @@
+# tox-constraints
+
+*Reproducible tests, with minimal configuration, by default*
+
+
+## Limitations
+
+This section lists known problems with the current implementation.
+
+### PyPy compatibility
+
+The version of pip that ships with pypy does not seem to like the `-r` option on which this plugin depends.
---
+++ packages/tox-constraints/tox-constraints-0.8.tar.gz/tox-constraints-0.8/setup.py
@@ -0,0 +1,34 @@
+import os
+
+import setuptools
+
+with open(
+    os.path.join(os.path.dirname(__file__), "requirements", "install_requires.txt")
+) as fp:
+    install_requires = list(fp)
+
+with open(os.path.join(os.path.dirname(__file__), "README.md"), "r") as fp:
+    long_description = fp.read()
+
+setuptools.setup(
+    name="tox-constraints",
+    version="0.8",
+    author="AP Ljungquist",
+    author_email="ap@ljungquist.eu",
+    url="https://github.com/apljungquist/tox-constraints",
+    description="Reproducible tests, by default.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=install_requires,
+    packages=setuptools.find_packages("src"),
+    package_dir={"": "src"},
+    entry_points={
+        "tox": ["constraints = tox_constraints.hooks"],
+        "console_scripts": [
+            "toxc-install=tox_constraints.git_filter:install",
+            "toxc-clean=tox_constraints.git_filter:clean",
+            "toxc-smudge=tox_constraints.git_filter:smudge",
+        ],
+    },
+    classifiers=["License :: OSI Approved :: MIT License"],
+)
