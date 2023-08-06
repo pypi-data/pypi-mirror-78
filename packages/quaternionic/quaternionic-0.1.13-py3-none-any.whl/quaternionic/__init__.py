389382727ceca6d65facf3ca9d69d049309c6dc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:44 +0000

    Add package quaternionic/0.1.13/quaternionic-0.1.13-py3-none-any.whl

---
+++ packages/quaternionic/quaternionic-0.1.13-py3-none-any.whl/quaternionic/__init__.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2020, Michael Boyle
+# See LICENSE file for details:
+# <https://github.com/moble/quaternionic/blob/master/LICENSE>
+
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:  # pragma: no cover
+    import importlib_metadata
+
+__version__ = importlib_metadata.version(__name__)
+
+from .utilities import jit, guvectorize, float64, boolean
+from . import algebra, properties, converters, distance, utilities
+
+algebra_ufuncs = type('AlgebraUfuncs', (object,), dict())()
+utilities.guvectorize_module_functions(algebra, algebra_ufuncs)
+
+from .arrays import QuaternionicArray, array
