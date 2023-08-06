b8f0b6bbb8156a7040f2c6556f02e183fdf818bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:54 +0000

    Add package xxlib/1.0.0/xxlib-1.0.0.tar.gz

---
+++ packages/xxlib/xxlib-1.0.0.tar.gz/xxlib-1.0.0/setup.py
@@ -0,0 +1,17 @@
+import  setuptools
+
+setuptools.setup(
+    name="xxlib",
+    version="1.0.0",
+    author="轩轩",
+    author_email="3603695237@qq.com",
+    description="轩轩库，轩轩工作室-轩轩制作，翻版、抄袭必究",
+    long_desription_content_type="text/markdown",
+    url="https://gitee.com/cyx200902/xxw.com",
+    classmethod=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    py_modules=['xxlib\\xxsearch'],
+)
