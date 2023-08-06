d7d48c5526144930f665fdd6f76b57589695c672
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:44 +0000

    Add package DRecPy/0.2.0/DRecPy-0.2.0-py3-none-any.whl

---
+++ packages/DRecPy/DRecPy-0.2.0-py3-none-any.whl/DRecPy/Recommender/__init__.py
@@ -0,0 +1,13 @@
+from .recommender_abc import RecommenderABC
+from .dmf import DMF
+from .cdae import CDAE
+from .neumf import NeuMF
+from .caser import Caser
+
+__all__ = [
+    'RecommenderABC',
+    'DMF',
+    'CDAE',
+    'NeuMF',
+    'Caser'
+]
