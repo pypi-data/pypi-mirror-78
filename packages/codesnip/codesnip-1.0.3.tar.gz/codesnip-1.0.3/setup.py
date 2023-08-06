8b3ca383a9dbfb922d0165da242a1a593d776d58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:19 +0000

    Add package codesnip/1.0.3/codesnip-1.0.3.tar.gz

---
+++ packages/codesnip/codesnip-1.0.3.tar.gz/codesnip-1.0.3/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as file:
+    long_description = file.read() 
+  
+  
+# specify requirements of your package here 
+REQUIREMENTS = ['pyperclip'] 
+  
+# some more details 
+CLASSIFIERS = [ 
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers', 
+    'Topic :: Utilities',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python', 
+    'Programming Language :: Python :: 2', 
+    'Programming Language :: Python :: 2.6', 
+    'Programming Language :: Python :: 2.7', 
+    'Programming Language :: Python :: 3', 
+    'Programming Language :: Python :: 3.3', 
+    'Programming Language :: Python :: 3.4', 
+    'Programming Language :: Python :: 3.5', 
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',  
+    ] 
+  
+# calling the setup function  
+setup(name='codesnip',
+      version='1.0.3',
+      description='A small program for storing and managing your code snippets.',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      py_modules = ["codesnipp"],
+      entry_points = {
+      "console_scripts": ['codesnip = src.codesnippet:main']
+      },
+      url='https://github.com/sarveshbhatnagar/codesnip',
+      author='Sarvesh Bhatnagar', 
+      author_email='sarveshbhatnagar08@gmail.com', 
+      license='MIT', 
+      packages=find_packages(),
+      classifiers=CLASSIFIERS, 
+      install_requires=REQUIREMENTS, 
+      keywords='snippet management code save'
+      ) 
---
+++ packages/codesnip/codesnip-1.0.3.tar.gz/codesnip-1.0.3/src/codesnippet.py
@@ -0,0 +1,49 @@
+import argparse
+from codesnipp import datahandler, folder
+import pyperclip
+import os
+
+
+def main():
+    parser = argparse.ArgumentParser(description="This Program lets you save and retrieve code snippets. To save a snippet first copy a code snippet and run -s command. To fetch snippet, use -f command and it will be copied to clipboard!")
+    parser.add_argument('-s','--save',type=str,help="Saves your code snippet from clipboard along with the keys you pass. e.g. codesnippet -s 'Flutter database retrieve'")
+    parser.add_argument('-f','--fetch',type=str,help="Fetches records from the database. e.g. codesnippet -f 'flutter database' or 'flutter retrieve database'")
+    parser.add_argument('-d','--deleterecord',type=str,help="Delete's a records from the database. e.g. codesnippet -d 'flutter database' or 'flutter retrieve database'")
+    parser.add_argument('-p','--printfolders',help='Used to print all the available folders')
+    args = parser.parse_args()
+    myfilepath = os.path.join(os.path.dirname(folder.__file__),'newsavefile')
+    if(args.save):
+        lis = args.save.split(' ')
+        #Create folder object
+        folderobj = folder.Folder(name=lis[0],keys=lis[1:],snippet=pyperclip.paste())
+        #Add that folder object to list in dictionary we need to get dictionary.
+        datahandler.DataHandler(objectp=folderobj,fpath=myfilepath).handleSave()
+        
+    elif args.fetch:
+        lis = args.fetch.split(' ')
+        datahandler.DataHandler(folder.Folder(),myfilepath).handleFetch(lis[0],lis[1:])
+        #Data handler works but I need to consider all the keys.
+    elif args.deleterecord:
+        lis = args.deleterecord.split(' ')
+        datahandler.DataHandler(folder.Folder(),myfilepath).handleDelete(lis[0],lis[1:])
+    elif args.printfolders:
+       
+        print('Will be soon functional.')
+        # print(os.path.dirname(folder.__file__))
+    else:
+        print("""\
+Please use either -s command to save code snippet or -f command to fetch code snippet
+Use -d command to delete a code snippet from saved snippets.
+
+Usage: 
+    codesnippet -s '[FOLDER_NAME [KEY STRING]'
+    codesnippet -f '[FOLDER_NAME] [ANY KEY]'
+    codesnippet -d '[FOLDER_NAME [ANY KEY]'
+        
+e.g. 
+    codesnippet -s 'flutter save to database'
+    codesnippet -f 'flutter save database' or 'flutter database'
+    codesnippet -d 'flutter save database' or 'flutter database'
+""")
+
+
