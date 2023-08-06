ceecc1b5d26464c82070337c91cfa52f02b5326a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:44 +0000

    Add package arceus/2.0.1/arceus-2.0.1.tar.gz

---
+++ packages/arceus/arceus-2.0.1.tar.gz/arceus-2.0.1/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+try:
+    long_description = open("../README.md").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="arceus",
+    version="2.0.1",
+    description="Minecraft name sniper.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="GPL-3.0-or-later",
+    author="Aquild",
+    packages=find_packages(),
+    install_requires=[
+        "arceus-net",
+        "requests",
+        "requests-random-user-agent",
+        "tcp-latency",
+        "bs4",
+        "pause",
+        "click",
+        "PyInquirer",
+        "colorama",
+        "termcolor",
+        "pyfiglet",
+    ],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+    ],
+    entry_points={"console_scripts": ["arceus=arceus.cli:cli"]},
+)
