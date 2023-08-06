35330976491e6adb0406f8203b3dab6af4de8f14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:12 +0000

    Add package wk-classify/0.0.0.2/wk-classify-0.0.0.2.tar.gz

---
+++ packages/wk-classify/wk-classify-0.0.0.2.tar.gz/wk-classify-0.0.0.2/wcf/networks/__init__.py
@@ -0,0 +1,4 @@
+from .ptcv_models import *
+from .torchvision_models import *
+from .resnets import *
+from .utils import *
---
+++ packages/wk-classify/wk-classify-0.0.0.2.tar.gz/wk-classify-0.0.0.2/wcf/networks/ptcv_models.py
@@ -0,0 +1,2 @@
+from pytorchcv.model_provider import *
+from pytorchcv.model_provider import _models as ptcv_models_dict
