6a3275f69fa3b152f3db030c17a117bf73358b74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:09 +0000

    Add package poetry-demo-cs151/0.1.0/poetry-demo-cs151-0.1.0.tar.gz

---
+++ packages/poetry-demo-cs151/poetry-demo-cs151-0.1.0.tar.gz/poetry-demo-cs151-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['poetry_demo_cs151']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'poetry-demo-cs151',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Craig Saxton',
+    'author_email': 'csaxton171@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
