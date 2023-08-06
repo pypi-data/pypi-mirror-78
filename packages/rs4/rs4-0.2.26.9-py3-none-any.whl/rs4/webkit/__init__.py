989ff8eb91dde3160575acf6fa04dbda2a458caa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:01 +0000

    Add package rs4/0.2.26.9/rs4-0.2.26.9-py3-none-any.whl

---
+++ packages/rs4/rs4-0.2.26.9-py3-none-any.whl/rs4/webkit/__init__.py
@@ -0,0 +1,15 @@
+
+from ..annotations import Uninstalled
+from .webtest import Target
+
+Site = Target
+
+try:
+    from .drivers import Chrome, Firefox, IE
+except ModuleNotFoundError:
+    IE = Firefox = Chrome = Uninstalled ('selenium')
+
+try:
+    from .nops import nops
+except ModuleNotFoundError:
+    nops = Uninstalled ('cssselect==0.9.1, lxml==4.4.0 and html5lib==0.999999999')
