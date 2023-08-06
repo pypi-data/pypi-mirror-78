55345b32737f78b263a1aaf8316f37017cad5a07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:41 +0000

    Add package EduTerminal/0.7/EduTerminal-0.7.tar.gz

---
+++ packages/EduTerminal/EduTerminal-0.7.tar.gz/EduTerminal-0.7/EduTerminal/_init_.py
@@ -0,0 +1,1 @@
+from .Edu import Edu
---
+++ packages/EduTerminal/EduTerminal-0.7.tar.gz/EduTerminal-0.7/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='EduTerminal',
+      version='0.7',
+      description='Edu tatar library for open-sources projects ',
+      packages=['EduTerminal'],
+      install_requires=[
+          'requests',
+          'bs4',
+          'wheel'],
+      author_email='alphaste08@gmail.com',
+      zip_safe=False)
