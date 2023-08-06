e03865147a7c596992bddf64bded2dbd351cf7dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:01 +0000

    Add package omni-reports/0.0.1/omni_reports-0.0.1.tar.gz

---
+++ packages/omni-reports/omni_reports-0.0.1.tar.gz/omni_reports-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+  name='omni_reports',
+  packages=['omni_reports'],
+  version='0.0.1',
+  description='Omni Report Definition',
+  author='Pareto Group',
+  author_email='noreply@paretogroup.com.br',
+  url='https://github.com/paretogroup/omni-reports',
+  keywords=['pareto', 'api', 'reports'],
+  classifiers=[],
+)
