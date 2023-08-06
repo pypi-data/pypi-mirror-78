1e9ae0216c0b1c0a97d16ad9130842a0c27991f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:41 +0000

    Add package redisconnection/0.0.2/redisconnection-0.0.2.tar.gz

---
+++ packages/redisconnection/redisconnection-0.0.2.tar.gz/redisconnection-0.0.2/test/test.py
@@ -0,0 +1,17 @@
+import redisconnection
+from redisconnection import logger, redis_connection
+
+
+def test_connection():
+    rd = redis_connection.RedisConnection()
+    conn = rd.connection
+    rd.close_connection()
+
+
+if __name__ == '__main__':
+    logger.warning(f'Executing {__file__}')
+    test_connection()
+
+
+
+
