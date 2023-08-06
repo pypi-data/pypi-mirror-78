c008294f8ef200f727dcd9f8323f21b2d400ee67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:56 +0000

    Add package awesome-panel-extensions/20200906.2/awesome_panel_extensions-20200906.2-py3-none-any.whl

---
+++ packages/awesome-panel-extensions/awesome_panel_extensions-20200906.2-py3-none-any.whl/awesome_panel_extensions/frameworks/fast/__init__.py
@@ -0,0 +1,5 @@
+from . import config
+from .fast_button import FastButton
+from .fast_template import FastTemplate
+from .fast_checkbox import FastCheckbox
+from .fast_textinput import FastTextInput
