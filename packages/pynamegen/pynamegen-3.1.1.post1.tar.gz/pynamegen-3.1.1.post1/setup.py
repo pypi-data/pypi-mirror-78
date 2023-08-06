22582e7d4bbe6dac4617630a4facb6f1969bd9c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:22 +0000

    Add package pynamegen/3.1.1.post1/pynamegen-3.1.1.post1.tar.gz

---
+++ packages/pynamegen/pynamegen-3.1.1.post1.tar.gz/pynamegen-3.1.1.post1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pynamegen",
+    version="3.1.1.post1",
+    author="BBaoVanC",
+    author_email="pypi@bbaovanc.com",
+    description="PyNameGen is a CLI for libnamegen",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://git.bbaovanc.com/bbaovanc/pynamegen",
+    packages=setuptools.find_packages(),
+    scripts=["scripts/pynamegen"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+          'libprogress<3.0.2',
+          'libnamegen<3.2.0',
+    ],
+    python_requires='>=3.7',
+)
