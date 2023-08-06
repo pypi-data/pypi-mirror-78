b7673bbcf26b723fb0130d1398c4f953e2c8a6e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:20 +0000

    Add package shikher-probability/1.0/shikher_probability-1.0.tar.gz

---
+++ packages/shikher-probability/shikher_probability-1.0.tar.gz/shikher_probability-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='shikher_probability',
+      version='1.0',
+      description='Gaussian  and Binomial distributions',      
+      packages=['shikher_probability'], 
+      author = 'Shikher Srivastava',
+      author_email = 'shikher707@gmail.com',
+    zip_safe=False)
