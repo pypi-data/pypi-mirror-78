11244c55a7df95f7d245672a688f241061b41826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:19 +0000

    Add package zohocrmapi/0.0.6/zohocrmapi-0.0.6.tar.gz

---
+++ packages/zohocrmapi/zohocrmapi-0.0.6.tar.gz/zohocrmapi-0.0.6/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="zohocrmapi",
+    version="0.0.6",
+    author="Adonis Gaitatzis",
+    author_email="backupbrain@gmail.com",
+    description="A Zoho CRM API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/backupbrain/zoho-crm-api",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+
+    ],
+    python_requires='>=3.6',
+)
