9ceb4044f31517f937b149241f45a7bc98344e47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:56 +0000

    Add package localcosmos-appkit-utils/0.2/localcosmos_appkit_utils-0.2.tar.gz

---
+++ packages/localcosmos-appkit-utils/localcosmos_appkit_utils-0.2.tar.gz/localcosmos_appkit_utils-0.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+install_requires = [
+    'Pillow',
+]
+
+setup(
+    name='localcosmos_appkit_utils',
+    version='0.2',
+    description='Local Cosmos AppKit utils. Creates images and more for mobile apps created with Local Cosmos.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='The MIT License',
+    platforms=['OS Independent'],
+    keywords='localcosmos, app kit, app kit utils',
+    author='Thomas Uher',
+    author_email='thomas.uher@sisol-systems.com',
+    url='https://github.com/SiSol-Systems/localcosmos-appkit-utils',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    include_package_data=True,
+    install_requires=install_requires,
+)
