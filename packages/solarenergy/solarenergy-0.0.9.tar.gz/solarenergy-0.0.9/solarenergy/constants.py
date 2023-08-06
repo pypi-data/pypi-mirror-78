a258c9125d6c7052f46fdc88aa0d80882fd368a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:59 +0000

    Add package solarenergy/0.0.9/solarenergy-0.0.9.tar.gz

---
+++ packages/solarenergy/solarenergy-0.0.9.tar.gz/solarenergy-0.0.9/solarenergy/constants.py
@@ -0,0 +1,24 @@
+#  Copyright (c) 2020  Marc van der Sluys - marc.vandersluys.nl
+#  
+#  This file is part of the SolarEnergy Python package, containing a Python module to do simple modelling in
+#  the field of solar energy.  See: https://github.com/MarcvdSluys/SolarEnergy
+#  
+#  This is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+#  
+#  This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+#  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License along with this code.  If not, see
+#  <http://www.gnu.org/licenses/>.
+
+
+"""Constants for solar energy.
+
+"""
+
+
+d2r = 0.01745329251994329576   # Degrees to radians conversion factor = pi/180
+r2d = 57.29577951308232087680  # Radians to degrees conversion factor = 180/pi
+
+solConst = 1361.5  # Solar constant: ~1361-1362 W/m² - https://en.wikipedia.org/wiki/Solar_constant
