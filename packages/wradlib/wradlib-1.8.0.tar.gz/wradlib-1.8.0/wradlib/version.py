15c4c49f2acc19b2ee5ca0f53699c2a7da2b8b93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:17:02 +0000

    Add package wradlib/1.8.0/wradlib-1.8.0.tar.gz

---
+++ packages/wradlib/wradlib-1.8.0.tar.gz/wradlib-1.8.0/wradlib/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM WRADLIB SETUP.PY
+short_version = '1.8.0'
+version = '1.8.0'
+full_version = '1.8.0-dev0+g7c9c574'
+git_revision = '7c9c574293d1224da5754eb16195e3e332c7b7af'
+release = True
