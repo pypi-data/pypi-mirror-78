55483cdc93b5b5d8ce83ad4e04db63895141b82e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:26 +0000

    Add package machaao/0.1.7/machaao-0.1.7.tar.gz

---
+++ packages/machaao/machaao-0.1.7.tar.gz/machaao-0.1.7/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="machaao",
+    version="0.1.7",
+    author="Abhishek Raj",
+    author_email="abhishek@machaao.com",
+    description="An easy to use module for python developers looking to build and develop chat apps using MessengerX.io",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/machaao/machaao-py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "requests",
+        "pyjwt",
+        "asyncio",
+        "flask",
+        "httpx",
+        "bs4",
+        "Flask-API",
+        "click",
+        "certifi",
+        "websockets",
+        "aiohttp",
+    ],
+    scripts=['bin/machaao']
+)
