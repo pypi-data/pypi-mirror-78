ff9416c572a2d31f2f0c2070bea53ae8266e290f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:28 +0000

    Add package ovretl/3.7.3/ovretl-3.7.3.tar.gz

---
+++ packages/ovretl/ovretl-3.7.3.tar.gz/ovretl-3.7.3/ovretl/performances_utils/tests/test_calculate_shipments_treatment_steps_time.py
@@ -0,0 +1,19 @@
+import os
+
+import pandas as pd
+import pytest
+
+from ovretl.performances_utils.calculate_shipments_treatment_steps_time import calculate_shipments_treatment_steps_time
+
+cwd = os.path.dirname(__file__)
+
+
+@pytest.mark.skip(reason="Need to disable timezone")
+def test_calculate_shipments_treatment_steps_time(snapshot):
+    events_shipment_df = pd.read_csv(os.path.join(cwd, "./events_shipment.csv"))
+    activities_clean_df = pd.read_csv(os.path.join(cwd, "./activities_clean.csv"))
+    shipments_df = pd.read_csv(os.path.join(cwd, "./shipments_df.csv"))
+    result = calculate_shipments_treatment_steps_time(
+        events_shipment_df=events_shipment_df, activities_clean_df=activities_clean_df, shipments_df=shipments_df,
+    )
+    snapshot.assert_match(result)
