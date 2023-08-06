bf60b5287190fe2a6c4fd2ff2104a9ab8415bbac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:31 +0000

    Add package coopstarter-data/0.1.86/coopstarter_data-0.1.86-py3-none-any.whl

---
+++ packages/coopstarter-data/coopstarter_data-0.1.86-py3-none-any.whl/coopstarter_data/context_processors.py
@@ -0,0 +1,11 @@
+from django.conf import settings as django_settings
+
+
+def settings(request):
+    settings_in_templates = {}
+    for attr in ["SITE_URL", "INSTANCE_DEFAULT_CLIENT"]: # Write here the settings you want to expose to the templates.
+        if (hasattr(django_settings, attr)):
+            settings_in_templates[attr] = getattr(django_settings, attr)
+    return {
+        'settings': settings_in_templates,
+    }
