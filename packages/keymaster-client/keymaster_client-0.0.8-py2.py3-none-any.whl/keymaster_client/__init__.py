cb21bb9e0d30d8090886698e8f4471beba70cb33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:40 +0000

    Add package keymaster-client/0.0.8/keymaster_client-0.0.8-py2.py3-none-any.whl

---
+++ packages/keymaster-client/keymaster_client-0.0.8-py2.py3-none-any.whl/keymaster_client/__init__.py
@@ -0,0 +1,14 @@
+"""
+.. include:: ../README.md
+"""
+
+__author__ = 'Adam Pickering'
+__email__ = 'adamkpickering@gmail.com'
+__version__ = '0.0.8'
+
+from keymaster_client.config_scheme import wgConfigScheme, UCIConfigScheme
+from keymaster_client.wireguard import (
+    WireguardInterface,
+    get_public_key,
+    generate_private_key
+)
