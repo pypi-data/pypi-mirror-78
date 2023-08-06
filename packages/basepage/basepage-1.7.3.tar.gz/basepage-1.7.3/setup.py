34a5a68b808d152035f8c5b838f6dcb71e6736b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:47 +0000

    Add package basepage/1.7.3/basepage-1.7.3.tar.gz

---
+++ packages/basepage/basepage-1.7.3.tar.gz/basepage-1.7.3/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+setup(
+    name='basepage',
+    packages=['basepage'],
+    version='1.7.3',
+    description='A wrapper for WebDriver that provides some extended and additional functionality',
+    author='Bijwen Aziz',
+    author_email='baziz@planview.com',
+    url='https://github.com/Projectplace/basepage',
+    download_url='https://github.com/Projectplace/basepage/tarball/1.7.3',
+    keywords=['testing', 'webdriver', 'selenium'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Pytest',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Topic :: Software Development :: Quality Assurance',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Utilities',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6']
+)
