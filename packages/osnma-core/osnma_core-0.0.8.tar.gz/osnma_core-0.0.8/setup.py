57fec060c019d49a4fb6c540b1aef1892c85e3fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:14 +0000

    Add package osnma-core/0.0.8/osnma_core-0.0.8.tar.gz

---
+++ packages/osnma-core/osnma_core-0.0.8.tar.gz/osnma_core-0.0.8/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="osnma_core",
+    version="0.0.8",
+    author="Aleix Galan",
+    author_email="algafi@protonmail.com",
+    description="Implementation of OSNMA protocol functions and data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Algafix/osnma_core",
+    py_modules=['osnma_core'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+    'bitstring',
+    'ecdsa',
+    'pycryptodome'
+    ],
+)
