3c96f7669c5d7e9df987a443fbeb7de0630a252b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:32 +0000

    Add package failstodeliver/0.0.2/failstodeliver-0.0.2.tar.gz

---
+++ packages/failstodeliver/failstodeliver-0.0.2.tar.gz/failstodeliver-0.0.2/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="failstodeliver",
+    version="0.0.2",
+    license='MIT',
+    author="Yuvraj Chandra",
+    author_email="Singhyuvraj179@gmail.com",
+    description="A simple package to download failstodeliver data from SEC EDGAR Databse",
+    long_description=long_description,
+    url="https://github.com/Yuvrajchandra/failstodeliver",
+    download_url = 'https://github.com/Yuvrajchandra/failstodeliver/archive/v0.0.2.tar.gz',
+    keywords = ['EDGAR', 'SEC', 'failstodeliver', 'Database'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[            # I get to this in a second
+          'requests',
+          'beautifulsoup4',
+      ],
+    python_requires='>=3.6',
+)
