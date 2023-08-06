dc9c42b4ce6bb780649ebfc846cedc340d0dea82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:49 +0000

    Add package ferien-api/0.3.6/ferien-api-0.3.6.tar.gz

---
+++ packages/ferien-api/ferien-api-0.3.6.tar.gz/ferien-api-0.3.6/ferien/model.py
@@ -0,0 +1,77 @@
+"""Contains data models to convert result json to our business object model"""
+
+from datetime import datetime
+
+import attr
+
+from .const import TZ_GERMANY
+from .types import StateCode, APIItem
+
+
+# pylint: disable=too-few-public-methods
+@attr.s
+class Vacation:
+    """
+    Internal representation of a single vacation
+    (from .. to, naming, ...)
+
+    A typical result json looks like that one below:
+    {
+      "start":"2017-01-29T23:00",
+      "end":"2017-01-31T23:00",
+      "year":2017,
+      "stateCode":"HB",
+      "name":"winterferien",
+      "slug":"winterferien-2017-HB"
+    }
+    """
+    start = attr.ib(
+        type=datetime,
+        validator=attr.validators.instance_of(datetime)
+    )  # type: datetime
+    end = attr.ib(
+        type=datetime,
+        validator=attr.validators.instance_of(datetime)
+    )  # type: datetime
+    year = attr.ib(
+        type=int,
+        converter=int
+    )  # type: int
+    state_code = attr.ib(
+        type=str,
+        converter=str
+    )  # type: StateCode
+    name = attr.ib(
+        type=str,
+        converter=str
+    )  # type: str
+    slug = attr.ib(
+        type=str,
+        converter=str
+    )  # type: str
+
+    @staticmethod
+    def _parse_date(candidate: str) -> datetime:
+        # Parse iso format
+        try:
+            # Date format before 2020-09-07
+            # Keep it if the Z remainder was introduced by accident
+            dt = datetime.strptime(candidate, '%Y-%m-%dT%H:%M')
+        except ValueError:
+            # Date format after 2020-09-07
+            dt = datetime.strptime(candidate, '%Y-%m-%dT%H:%MZ')
+        # All dates from the api are Europe/Berlin (CET/CEST)
+        return TZ_GERMANY.localize(dt)
+
+    @classmethod
+    def from_dict(cls, dct: APIItem) -> 'Vacation':
+        """Initializes the Vacation model from a dictionary instance."""
+
+        return cls(
+            start=cls._parse_date(dct['start']),
+            end=cls._parse_date(dct['end']),
+            year=dct['year'],
+            state_code=dct['stateCode'],
+            name=dct.get('name', 'none'),
+            slug=dct.get('slug', 'none')
+        )
---
+++ packages/ferien-api/ferien-api-0.3.6.tar.gz/ferien-api-0.3.6/ferien/util.py
@@ -0,0 +1,97 @@
+"""Contains utility functions used by sync and async code"""
+from datetime import datetime
+from typing import Iterable, Any, Optional, cast
+
+from .const import ALL_STATE_CODES, TZ_GERMANY
+from .model import Vacation
+
+
+def is_tz_aware_timestamp(dt: datetime) -> bool:
+    """Checks if the passed timestamp is tz aware or not."""
+    return dt.tzinfo is not None and dt.tzinfo.utcoffset(dt) is not None
+
+
+def make_tz_aware_timestamp(dt: Optional[datetime]) -> datetime:
+    """Make a timezone aware timestamp based on the given dt.
+    1. dt is None: datetime.now() in german timezone
+    2. dt has timezone: Return as is
+    3. dt has no timezone: Assume that is german tz and set it."""
+    dt = dt or datetime.now(tz=TZ_GERMANY)
+    if not is_tz_aware_timestamp(dt):
+        dt = dt.replace(tzinfo=TZ_GERMANY)
+    return dt
+
+
+def parse_state_code(candidate: Any) -> str:
+    """Parses the given candidate as a state code. Raises a ValueError
+    when the candidate is not a valid state code."""
+    state_code = str(candidate)
+    if state_code not in ALL_STATE_CODES:
+        raise ValueError("Argument state_code (current: '{}') is expected "
+                         "to be one of {}".format(state_code, ALL_STATE_CODES))
+    return state_code
+
+
+def parse_year(candidate: Any) -> int:
+    """Parses the given candidate as a year literal. Raises a ValueError
+    when the candidate is not a valid year."""
+    if candidate is not None and not isinstance(candidate, int):
+        raise TypeError("Argument year is expected to be an int, "
+                        "but is {}".format(type(candidate)))
+    return cast(int, candidate)
+
+
+def is_iterable_but_no_str(candidate: Any) -> bool:
+    """Tests if the given candidate is an iterable (list, tuple, ...)
+    but not a string."""
+    return (
+        hasattr(candidate, '__iter__')
+        and not isinstance(candidate, (str, bytes))
+    )
+
+
+def check_vac_list(vacs: Iterable[Vacation]) -> None:
+    """Checks if the given list is an actual list of vacations."""
+    if not is_iterable_but_no_str(vacs):
+        raise TypeError("Argument 'vacs' is expected to an iterable, "
+                        "but is {}".format(type(vacs)))
+    for i, val in enumerate(vacs):
+        if not isinstance(val, Vacation):
+            raise TypeError("Item {} of argument 'vacs' is expected to be of "
+                            "type 'Vacation', but is {}".format(i, type(val)))
+
+
+def check_datetime(dt: Any) -> None:
+    """Checks if the argument dt is a valid datetime."""
+    if dt and not isinstance(dt, datetime):
+        raise TypeError("Argument 'dt' is expected to be of type 'datetime', "
+                        "but is {}".format(type(dt)))
+
+
+def find_current(vacs: Iterable[Vacation],
+                 dt: Optional[datetime] = None) -> Optional[Vacation]:
+    """Returns the current vacation based on the given dt.
+    Returns None if no vacation surrounds (start, end) the
+    given dt."""
+    check_vac_list(vacs)
+    check_datetime(dt)
+
+    dt = make_tz_aware_timestamp(dt)
+    res = [i for i in vacs if i.start <= dt <= i.end][-1:]
+    if not res:
+        return None
+    return res[0]
+
+
+def find_next(vacs: Iterable[Vacation],
+              dt: Optional[datetime] = None) -> Optional[Vacation]:
+    """Returns the next vacation based on the given dt.
+    Returns None if no vacation is left."""
+    check_vac_list(vacs)
+    check_datetime(dt)
+
+    dt = make_tz_aware_timestamp(dt)
+    res = sorted([i for i in vacs if i.start >= dt], key=lambda i: i.start)
+    if not res:
+        return None
+    return res[0]
