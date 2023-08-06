aaa7daf7ccb438b33ab8a97c4945404d75f892bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:28 +0000

    Add package djangocms-icon/2.0.0/djangocms-icon-2.0.0.tar.gz

---
+++ packages/djangocms-icon/djangocms-icon-2.0.0.tar.gz/djangocms-icon-2.0.0/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+from djangocms_icon import __version__
+
+
+REQUIREMENTS = [
+    'django-cms>=3.7',
+    'djangocms-attributes-field>=1',
+]
+
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Web Environment',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Framework :: Django',
+    'Framework :: Django :: 2.2',
+    'Framework :: Django :: 3.0',
+    'Framework :: Django :: 3.1',
+    'Framework :: Django CMS',
+    'Framework :: Django CMS :: 3.7',
+    'Framework :: Django CMS :: 3.8',
+    'Topic :: Internet :: WWW/HTTP',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Software Development',
+    'Topic :: Software Development :: Libraries',
+]
+
+
+setup(
+    name='djangocms-icon',
+    version=__version__,
+    author='Divio AG',
+    author_email='info@divio.com',
+    url='https://github.com/divio/djangocms-icon',
+    license='BSD-3-Clause',
+    description='Adds icon plugin to django CMS.',
+    long_description=open('README.rst').read(),
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=REQUIREMENTS,
+    classifiers=CLASSIFIERS,
+    test_suite='tests.settings.run',
+)
