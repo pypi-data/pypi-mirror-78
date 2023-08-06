c007ee3ceba2f290902e241ecfed2ad8abfe7b5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:10 +0000

    Add package climetlab/0.0.85/climetlab-0.0.85.tar.gz

---
+++ packages/climetlab/climetlab-0.0.85.tar.gz/climetlab-0.0.85/climetlab/sources/readers/odb.py
@@ -0,0 +1,22 @@
+# (C) Copyright 2020 ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+#
+
+from . import Reader
+
+try:
+    import codc as odc
+except Exception:
+    import pyodc as odc
+
+    # print("Using pure Python odc decoder.", file=sys.stderr)
+
+
+class ODBReader(Reader):
+    def to_pandas(self):
+        return odc.read_odb(self.path, single=True)
