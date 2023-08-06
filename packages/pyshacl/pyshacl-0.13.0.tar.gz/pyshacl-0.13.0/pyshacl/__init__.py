16c5e64b34f477020f18ae7a874fa3c5bb53176e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:54 +0000

    Add package pyshacl/0.13.0/pyshacl-0.13.0.tar.gz

---
+++ packages/pyshacl/pyshacl-0.13.0.tar.gz/pyshacl-0.13.0/pyshacl/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: latin-1 -*-
+#
+from pyshacl.validate import Validator, validate
+
+
+# version compliant with https://www.python.org/dev/peps/pep-0440/
+__version__ = '0.13.0'
+# Don't forget to change the version number in pyproject.toml along with this one
+
+__all__ = ['validate', 'Validator', '__version__']
