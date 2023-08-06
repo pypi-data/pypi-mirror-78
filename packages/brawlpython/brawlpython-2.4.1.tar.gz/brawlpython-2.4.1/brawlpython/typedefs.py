5e25d8c78b68c07a2440acfd007bb23395dcd38d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:54 +0000

    Add package brawlpython/2.4.1/brawlpython-2.4.1.tar.gz

---
+++ packages/brawlpython/brawlpython-2.4.1.tar.gz/brawlpython-2.4.1/brawlpython/typedefs.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from typing import TypeVar
+
+__all__ = (
+    "R",
+    "L",
+    "URLS",
+    "PARAMS",
+    "RETURN",
+)
+
+R = TypeVar("R", bound="Union[Dict[str, Any], List[Dict[str, Any]]]")
+
+L = TypeVar("L", bound="List[Union[Dict[str, Any], List[Dict[str, Any]]]]")
+
+URLS = TypeVar("URLS", bound="Union[List[str], str]")
+
+PARAMS = TypeVar(
+    "PARAMS", bound="Sequence[Sequence[Sequence[Any], Mapping[Any, Any]]]")
+
+RETURN = TypeVar("RETURN", bound="Dict[str, Any]")
