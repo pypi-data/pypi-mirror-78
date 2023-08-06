e7dabee58c83eb0a655c162df969d20586f19dad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:13 +0000

    Add package ansible-doctor/0.1.6/ansible-doctor-0.1.6.tar.gz

---
+++ packages/ansible-doctor/ansible-doctor-0.1.6.tar.gz/ansible-doctor-0.1.6/ansibledoctor/__init__.py
@@ -0,0 +1,9 @@
+"""Default package."""
+
+__author__ = "Robert Kaussow"
+__project__ = "ansible-doctor"
+__version__ = "0.1.6"
+__license__ = "GPL-3.0"
+__maintainer__ = "Robert Kaussow"
+__email__ = "mail@geeklabor.de"
+__url__ = "https://github.com/xoxys/ansible-doctor"
