fd061502adf6c028a667e73e1af6e95263c1f9fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:46 +0000

    Add package ettuparser/1.1/ettuparser-1.1.tar.gz

---
+++ packages/ettuparser/ettuparser-1.1.tar.gz/ettuparser-1.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ettuparser", # Replace with your own username
+    version="1.1",
+    author="Djentelman",
+    author_email="djentelman@fjandinn.ml",
+    description="This package parses ETTU site for mass transit schedule.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/petruccinator3000/ETTUParser",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3', 
+    install_requiers={
+    	'pandas', 
+    	'lxml', 
+    	'requests' 
+    } 
+)
