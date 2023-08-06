e7706d94b7e73287b9df7e266ce7fe9958d9bc64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:27 +0000

    Add package AIOwiki/1.2/AIOwiki-1.2.tar.gz

---
+++ packages/AIOwiki/AIOwiki-1.2.tar.gz/AIOwiki-1.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='AIOwiki',
+    version='1.2',
+    packages=['AIO'],
+    install_requires=[
+        'requests'
+    ],
+    url='https://github.com/themooer1/AIO',
+    license='MIT',
+    author='David Smith',
+    author_email='fluffy1781@gmail.com',
+    description='A Python API for accessing information about episodes on AIOwiki.com'
+)
