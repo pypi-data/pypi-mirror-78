44db120dc37970015639cca33468bf4ac3961469
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:56 +0000

    Add package plone.app.fhirfield/4.1.0/plone.app.fhirfield-4.1.0.tar.gz

---
+++ packages/plone.app.fhirfield/plone.app.fhirfield-4.1.0.tar.gz/plone.app.fhirfield-4.1.0/src/plone/app/fhirfield/deserializer.py
@@ -0,0 +1,28 @@
+# _*_ coding: utf-8 _*_
+import six
+from plone.app.fhirfield.interfaces import IFhirResource
+from plone.dexterity.interfaces import IDexterityContent
+from plone.restapi.deserializer.dxfields import DefaultFieldDeserializer
+from plone.restapi.interfaces import IFieldDeserializer
+from zope.component import adapter
+from zope.interface import implementer
+from zope.publisher.interfaces.browser import IBrowserRequest
+
+
+__author__ = "Md Nazrul Islam <email2nazrul@gmail.com>"
+
+
+@implementer(IFieldDeserializer)
+@adapter(IFhirResource, IDexterityContent, IBrowserRequest)
+class FhirResourceDeserializer(DefaultFieldDeserializer):
+    def __call__(self, value):
+        """ """
+        if isinstance(value, six.string_types):
+            return IFhirResource(self.field).fromUnicode(value)
+        elif isinstance(value, dict):
+            return IFhirResource(self.field).from_dict(value)
+        else:
+            raise ValueError(
+                "Invalid data type({0}) provided! only dict or "
+                "string data type is accepted.".format(type(value))
+            )
