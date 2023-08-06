27717d7db06d100003c0a1e418a4a19c63698c24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:38 +0000

    Add package Products.isurlinportal/1.1.1/Products.isurlinportal-1.1.1.tar.gz

---
+++ packages/Products.isurlinportal/Products.isurlinportal-1.1.1.tar.gz/Products.isurlinportal-1.1.1/CHANGES.rst
@@ -0,0 +1,37 @@
+Changelog
+=========
+
+
+.. You should *NOT* be adding new change log entries to this file.
+   You should create a file in the news directory instead.
+   For helpful instructions, please see:
+   https://github.com/plone/plone.releaser/blob/master/ADD-A-NEWS-ITEM.rst
+
+.. towncrier release notes start
+
+1.1.1 (2020-09-07)
+------------------
+
+Bug fixes:
+
+
+- Add testing for Python 3.6 + 3.8; add Python 3.8 trove classifier.
+  [tschorr] (#3)
+
+
+1.1.0 (2020-08-16)
+------------------
+
+New features:
+
+
+- Harden against tricky whitespace in urls.
+  [maurits] (#1)
+
+
+1.0.0 (2020-07-21)
+------------------
+
+- Initial release.
+  Code is the same as current Plone 4.3.19, 5.1.6, 5.2.1, with the January 2020 hotfix applied, plus compatibility fixes.
+  [maurits]
---
+++ packages/Products.isurlinportal/Products.isurlinportal-1.1.1.tar.gz/Products.isurlinportal-1.1.1/setup.py
@@ -0,0 +1,43 @@
+from setuptools import find_packages
+from setuptools import setup
+
+import os
+
+with open("README.rst") as myfile:
+    readme = myfile.read()
+with open("CHANGES.rst") as myfile:
+    changes = myfile.read()
+long_description = readme + "\n" + changes
+
+
+setup(
+    name="Products.isurlinportal",
+    version="1.1.1",
+    description="Replacement for isURLInPortal method in Plone",
+    long_description=long_description,
+    # Get more strings from https://pypi.org/classifiers/
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.3",
+        "Framework :: Plone :: 5.0",
+        "Framework :: Plone :: 5.1",
+        "Framework :: Plone :: 5.2",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    keywords="plone security hotfix patch",
+    author="Plone Security Team",
+    author_email="security@plone.org",
+    url="https://github.org/plone/Products.isurlinportal",
+    license="GPL",
+    packages=find_packages(),
+    namespace_packages=["Products"],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=["setuptools"],
+)
