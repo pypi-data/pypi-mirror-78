3e1a0bb269d517231bee37d1bee8e35e72704f10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:31 +0000

    Add package jsonclasses/0.5.1/jsonclasses-0.5.1-py3-none-any.whl

---
+++ packages/jsonclasses/jsonclasses-0.5.1-py3-none-any.whl/jsonclasses/validators/date_validator.py
@@ -0,0 +1,28 @@
+from typing import Any
+from datetime import date
+from ..config import Config
+from ..exceptions import ValidationException
+from .validator import Validator
+from ..utils.reference_map import referenced
+
+@referenced
+class DateValidator(Validator):
+
+  def validate(self, value: Any, key_path: str, root: Any, all_fields: bool, config: Config):
+    if value is not None and type(value) is not date:
+      raise ValidationException(
+        { key_path: f'Value \'{value}\' at \'{key_path}\' should be date.' },
+        root
+      )
+
+  def transform(self, value: Any, key_path: str, root: Any, all_fields: bool, config: Config):
+    if value is None:
+      return None
+    elif type(value) is str:
+      return date.fromisoformat(value[:10])
+    else:
+      return value
+
+  def tojson(self, value: Any, config: Config):
+    if value is not None:
+      return value.isoformat() + 'T00:00:00.000Z'
