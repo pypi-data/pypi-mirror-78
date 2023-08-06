7b19224d9eaf86f3f8128c9bfb6afafb93f9831d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:26 +0000

    Add package gym-handOfJustice/0.0.12/gym_handOfJustice-0.0.12.tar.gz

---
+++ packages/gym-handOfJustice/gym_handOfJustice-0.0.12.tar.gz/gym_handOfJustice-0.0.12/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='gym_handOfJustice',
+    version='0.0.12',
+    description='A openAI Gym Env for a Robotic arm that imitates an arm which would be given through a feed',
+    packages=setuptools.find_packages(include="gym_handOfJustice*"),
+    intall_required = ['gym','pybullet','opencv-python'],
+    package_data={'':['meshes/*.stl','hand.urdf']}
+    
+    )
