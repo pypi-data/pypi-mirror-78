922bd312355da2948fddd97e58d1a07eb57f40b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:41 +0000

    Add package lambdata-dustinstringer/0.0.1/lambdata_dustinstringer-0.0.1-py3-none-any.whl

---
+++ packages/lambdata-dustinstringer/lambdata_dustinstringer-0.0.1-py3-none-any.whl/dustydata/ds_utils.py
@@ -0,0 +1,11 @@
+def enlarge(n):
+    """
+    Param n is a number
+    Function will enlarge the number
+    """
+    return n * 100
+
+print(enlarge(5))
+
+if __name__ == '__main__':
+    print(enlarge(5))
