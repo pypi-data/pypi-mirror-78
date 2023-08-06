fb0c38120594c94dc111cc4548d04ba94278caa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:56 +0000

    Add package relpath/1.3/relpath-1.3.tar.gz

---
+++ packages/relpath/relpath-1.3.tar.gz/relpath-1.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "relpath",
+    version = '1.3',
+    description = 'relative path from the python file itself',
+    author = 'le latelle',
+    author_email = 'g.tiger.ml@gmail.com',
+    url = 'https://github.co.jp/',
+)
