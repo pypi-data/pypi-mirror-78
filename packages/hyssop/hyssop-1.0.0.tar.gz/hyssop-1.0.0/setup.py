af2805b0291fa8836725a738d5b8ebf1839c289c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:00 +0000

    Add package hyssop/1.0.0/hyssop-1.0.0.tar.gz

---
+++ packages/hyssop/hyssop-1.0.0.tar.gz/hyssop-1.0.0/setup.py
@@ -0,0 +1,45 @@
+# Copyright (C) 2020-Present the hyssop authors and contributors.
+#
+# This module is part of hyssop and is released under
+# the MIT License: http://www.opensource.org/licenses/mit-license.php
+
+'''
+File created: August 21st 2020
+
+Modified By: hsky77
+Last Updated: September 4th 2020 14:43:30 pm
+'''
+
+import setuptools
+from hyssop import Version, __name__
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name=__name__,
+    version=Version,
+    author="hsky77",
+    author_email="howardlkung@gmail.com",
+    description="component-based architecture and project hierarchy based on opensource web framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hsky77/hyssop",
+    packages=setuptools.find_packages(),
+    license="MIT License",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['tornado>=6.0.3',
+                      'tornado-swagger>=1.2.4',
+                      'PyYAML>=5.1.1',
+                      'coloredlogs>=10.0',
+                      'aiohttp>=3.6.2',
+                      'requests>=2.20.0'],
+    python_requires='>=3.6',
+    package_data={'': ['*.yaml', '*.csv']}
+)
