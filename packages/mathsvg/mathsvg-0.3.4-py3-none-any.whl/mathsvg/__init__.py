4306968e3560b113d31337ec6d39b6e41ff2b573
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:04 +0000

    Add package mathsvg/0.3.4/mathsvg-0.3.4-py3-none-any.whl

---
+++ packages/mathsvg/mathsvg-0.3.4-py3-none-any.whl/mathsvg/__init__.py
@@ -0,0 +1,24 @@
+# Author:  alexn11 (alexn11.gh@gmail.com)
+# Created: 2018-10-21
+# Copyright (C) 2018, 2020 Alexandre De Zotti
+# License: MIT License
+
+"""mathsvg, a Python library to draw mathematical objects
+
+Quick start guide:
+------------------
+
+Create an object ``SvgImage``. Draw your image by calling relevant members of this object. Call ``save()`` to save a SVG file.
+
+Type::
+
+  help (mathsvg . SvgImage)
+
+for more details.
+"""
+
+__version__ = "0.3.4"
+
+from mathsvg.mathsvg import SvgImage
+
+
