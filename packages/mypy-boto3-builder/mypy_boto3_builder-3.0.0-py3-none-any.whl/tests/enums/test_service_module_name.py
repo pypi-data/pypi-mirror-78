b9991a33a7ed79d1cdc8aab21ea11f30aedc5bdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:11 +0000

    Add package mypy-boto3-builder/3.0.0/mypy_boto3_builder-3.0.0-py3-none-any.whl

---
+++ packages/mypy-boto3-builder/mypy_boto3_builder-3.0.0-py3-none-any.whl/tests/enums/test_service_module_name.py
@@ -0,0 +1,7 @@
+from mypy_boto3_builder.enums.service_module_name import ServiceModuleName
+
+
+class TestServiceModuleName:
+    def test_properties(self) -> None:
+        assert ServiceModuleName.paginator.file_name == "paginator.pyi"
+        assert ServiceModuleName.paginator.template_name == "paginator.pyi.jinja2"
