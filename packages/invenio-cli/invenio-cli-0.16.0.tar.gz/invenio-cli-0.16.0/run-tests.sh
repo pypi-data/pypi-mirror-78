fb5dbedf7a4a9bab62e4be1454a5c38643ef032e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:52 +0000

    Add package invenio-cli/0.16.0/invenio-cli-0.16.0.tar.gz

---
+++ packages/invenio-cli/invenio-cli-0.16.0.tar.gz/invenio-cli-0.16.0/run-tests.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019 CERN.
+# Copyright (C) 2019 Northwestern University.
+#
+# Invenio-Cli is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+pydocstyle invenio_cli tests docs && \
+isort invenio_cli tests --check-only --diff && \
+check-manifest --ignore ".travis-*" && \
+sphinx-build -qnNW docs docs/_build/html && \
+pytest
