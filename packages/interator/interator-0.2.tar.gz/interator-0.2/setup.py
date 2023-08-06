1df64a2e0bbc1a7c9a3213ccbdeb6103ec68abaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:26 +0000

    Add package interator/0.2/interator-0.2.tar.gz

---
+++ packages/interator/interator-0.2.tar.gz/interator-0.2/setup.py
@@ -0,0 +1,28 @@
+ #!/usr/bin/env python
+"""Setup file for interator module."""
+
+import setuptools
+
+setuptools.setup(
+    name='interator',
+    version='v0.2',
+    author="Will Leskowitz",
+    author_email="willleskowitz@gmail.com",
+    packages=setuptools.find_packages(),
+    url='https://github.com/willleskowitz/interator',
+    description='Integer sequence generation and related conditional tests.',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    keywords=['iterator', 'integer', 'sequence', 'generator', 'Fibonacci',
+              'Lucas', 'generalizations', 'prime', 'numbers', 'polygonal',
+              'tests'],
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    setup_requires=['wheel'],
+    install_requires=['numpy']
+)
