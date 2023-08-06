e82854890bb1d728b22396f14f3d4dd87dad50bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:51 +0000

    Add package julabo/2.2.0/julabo-2.2.0.tar.gz

---
+++ packages/julabo/julabo-2.2.0.tar.gz/julabo-2.2.0/julabo/__init__.py
@@ -0,0 +1,6 @@
+from .connection import connection_for_url
+from .protocol import Protocol, protocol_for_url
+from .device import JulaboCF, JulaboHL, JulaboFC, SelfTunning, ExternalInput, TemperatureControl, ControlMode
+
+
+__version__ = "2.2.0"
