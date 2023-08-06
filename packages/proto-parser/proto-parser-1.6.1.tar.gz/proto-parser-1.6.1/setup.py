77015a558411ff5c2029256cf7effad08866f589
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:06 +0000

    Add package proto-parser/1.6.1/proto-parser-1.6.1.tar.gz

---
+++ packages/proto-parser/proto-parser-1.6.1.tar.gz/proto-parser-1.6.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name="proto-parser",
+  version="1.6.1",
+  author="xiaochun.liu",
+  author_email="liuxiaochun@apache.org",
+  description="A package for parsing proto files",
+  long_description_content_type="text/markdown",
+  url="https://github.com/khadgarmage/protoparser",
+  packages=setuptools.find_packages(),
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: Apache Software License",
+    "Operating System :: OS Independent",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+  ],
+  install_requires=[
+    'lark-parser>=0.8.6',
+    'numpy>=1.14.0'
+  ],
+)
