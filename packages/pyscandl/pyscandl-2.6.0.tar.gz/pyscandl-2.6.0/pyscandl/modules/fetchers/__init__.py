0c6e8faff21e20d3962bac228362651f7a5a8e91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:46 +0000

    Add package pyscandl/2.6.0/pyscandl-2.6.0.tar.gz

---
+++ packages/pyscandl/pyscandl-2.6.0.tar.gz/pyscandl-2.6.0/pyscandl/modules/fetchers/__init__.py
@@ -0,0 +1,13 @@
+# referencing the fetchers for the PyPI package
+
+from .fanfox import Fanfox
+from .mangadex import Mangadex
+from .nh import NHentai
+
+# naver webtoons
+from .naver import NaverWebtoon
+from .naver import NaverBestChallenge
+from .naver import NaverChallenge
+
+# getting the enumeration
+from .fetcher_enum import FetcherEnum
