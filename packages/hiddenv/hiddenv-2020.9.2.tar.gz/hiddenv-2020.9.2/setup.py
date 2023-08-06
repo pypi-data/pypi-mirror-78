9e8220391624e565f8880f5ba2f870675cef8cc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:17 +0000

    Add package hiddenv/2020.9.2/hiddenv-2020.9.2.tar.gz

---
+++ packages/hiddenv/hiddenv-2020.9.2.tar.gz/hiddenv-2020.9.2/setup.py
@@ -0,0 +1,13 @@
+import pkg_resources
+import setuptools
+
+pkg_resources.require("setuptools>=40.9.0")
+setuptools.setup(
+    packages=setuptools.find_packages(exclude="tests*"),
+    package_data={
+        "hiddenv": [
+            "py.typed",
+            "*.pyi"
+        ]
+    }
+)
