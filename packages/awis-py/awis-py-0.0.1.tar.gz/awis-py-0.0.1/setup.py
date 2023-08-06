1a015ebae6a8f906d497cee3ca7cc04ac330e8e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:50 +0000

    Add package awis-py/0.0.1/awis-py-0.0.1.tar.gz

---
+++ packages/awis-py/awis-py-0.0.1.tar.gz/awis-py-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='awis-py',
+    version='0.0.1',
+    url='https://github.com/whistlebee/awis-py',
+    packages=find_packages(),
+    python_requires='>=3.6'
+)
