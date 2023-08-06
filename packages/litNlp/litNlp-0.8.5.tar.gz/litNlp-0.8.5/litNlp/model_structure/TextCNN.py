aa98de2814d9599703c47a3966e4a4b9f93eb4fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:12 +0000

    Add package litNlp/0.8.5/litNlp-0.8.5.tar.gz

---
+++ packages/litNlp/litNlp-0.8.5.tar.gz/litNlp-0.8.5/litNlp/model_structure/TextCNN.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# @Time: 2020/6/20 0020 10:38
+from tensorflow.keras.layers import Dense, Embedding, Flatten,Dropout,Convolution1D,BatchNormalization
+from tensorflow.keras.optimizers import Adam,RMSprop
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import concatenate,GlobalAveragePooling1D,Activation,MaxPooling1D,Input
+class sa_model:
+    def create_model(self, max_words, embedding_dim, maxlen):
+        model = Sequential()
+        #  embedding layer
+        model.add(Embedding(max_words, embedding_dim, input_length=maxlen))
+        model.add(Convolution1D(64, 3, input_shape=(-1, embedding_dim)))
+        model.add(Activation('relu'))
+        model.add(MaxPooling1D(2, 2))
+        model.add(Flatten())
+        model.add(Dense(512, activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(1,  activation='sigmoid'))
+        model.compile(loss='binary_crossentropy',optimizer=Adam(lr=1e-3), metrics=['accuracy'])
+        return model
