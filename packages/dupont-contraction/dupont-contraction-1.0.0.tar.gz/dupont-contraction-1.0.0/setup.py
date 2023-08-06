9cfda650d1257fdcb16700f2121ce9b7a746271b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:16 +0000

    Add package dupont-contraction/1.0.0/dupont-contraction-1.0.0.tar.gz

---
+++ packages/dupont-contraction/dupont-contraction-1.0.0.tar.gz/dupont-contraction-1.0.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+long_description = """
+Please find the package documentation in the original Github repository_. We 
+recommend the use of a LaTeX rendered for Github.
+
+.. _repository: https://github.com/DanielRobertNicoud/dupont-contraction
+"""
+    
+setuptools.setup(
+    name='dupont-contraction',
+    version='1.0.0',
+    author='Daniel Robert-Nicoud',
+    author_email='daniel.robertnicoud@gmail.com',
+    description="A package for computations using Sullivan and Dupont forms," \
+        " and the Dupont contraction.",
+    long_description=long_description,
+    url='https://github.com/DanielRobertNicoud/dupont-contraction',
+    install_requires=[
+        'numpy'
+    ],
+    packages=setuptools.find_packages(),
+    namespace_packages=['dupontcontraction']
+)
