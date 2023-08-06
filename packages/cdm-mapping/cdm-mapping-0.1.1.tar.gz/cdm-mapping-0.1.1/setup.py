4c3242018a8e75a395201ead9e4774e8c7a3d41a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:46 +0000

    Add package cdm-mapping/0.1.1/cdm-mapping-0.1.1.tar.gz

---
+++ packages/cdm-mapping/cdm-mapping-0.1.1.tar.gz/cdm-mapping-0.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['cdm_mapping']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'cdm-mapping',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'mountain',
+    'author_email': 'tim@lustberg.nl',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
