45dd291cb3b616afd1e3ad153100dea5dfaadc53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:35 +0000

    Add package lambdata-dustinstringer/0.0.7/lambdata-dustinstringer-0.0.7.tar.gz

---
+++ packages/lambdata-dustinstringer/lambdata-dustinstringer-0.0.7.tar.gz/lambdata-dustinstringer-0.0.7/dustydata/test_functions.py
@@ -0,0 +1,17 @@
+from dustydata.wild_west import johnWayne
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame({
+    0: [1, 2.9, 3, 4, 5, 6, 7],
+    1: ['a', 'a', 'a', 'a', 'a', 'a', 'a'],
+    2: ['b', 'b', np.NaN, 'b', 'b', 'b', 'b'],
+    3: ['t', np.NaN, 'f', 'f', 't', 'h', 'j'],
+    4: ['f', 'h', 'd', 'k', 'j', 'y', 'j']})
+
+jw = johnWayne(df)
+jw.cardinality_cutter(threshold=5)
+X_train, X_val, X_test = jw.tri_split(.25, .25)
+print(X_train)
+print(X_val)
+print(X_test)
