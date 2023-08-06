972ec596ffa68afc56dc94d7213a98a7a1f49bda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:29 +0000

    Add package distributions9/0.1/distributions9-0.1.tar.gz

---
+++ packages/distributions9/distributions9-0.1.tar.gz/distributions9-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions9',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions9'],
+      author = 'Parth Shah',
+      author_email = 'parthrrs19@gmail.com',
+      zip_safe=False)
