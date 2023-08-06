f25c18780bbda8f54857624f1b479692f33cba21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:03 +0000

    Add package asks/2.4.9/asks-2.4.9.tar.gz

---
+++ packages/asks/asks-2.4.9.tar.gz/asks-2.4.9/asks/__init__.py
@@ -0,0 +1,21 @@
+# pylint: disable=wildcard-import
+# pylint: disable=wrong-import-position
+
+__version__ = '2.4.9'
+
+from .auth import *
+from .base_funcs import *
+from .sessions import *
+
+from warnings import warn
+
+
+def init(library):
+    """
+    Unused. asks+anyio auto-detects your library.
+    """
+    warn(
+        "init is deprecated. asks + anyio now auto-detects" " your async library",
+        DeprecationWarning,
+    )
+    pass
---
+++ packages/asks/asks-2.4.9.tar.gz/asks-2.4.9/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+from sys import version_info
+from asks import __version__ as version
+
+if version_info < (3, 5, 2):
+    # 3.5.2 is when __aiter__ became a synchronous function
+    raise SystemExit('Sorry! asks requires python 3.5.2 or later.')
+
+setup(
+    name='asks',
+    description='asks - async http',
+    long_description='asks is an async http lib for curio, trio and asyncio',
+    license='MIT',
+    version=version,
+    author='Mark Jameson - aka theelous3',
+    url='https://github.com/theelous3/asks',
+    packages=['asks'],
+    install_requires=['h11', 'async_generator', 'anyio<2'],
+    tests_require=['pytest', 'curio', 'overly'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Framework :: Trio',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
