96a2d101905eff67140a65d27709782d88f78043
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:08 +0000

    Add package dsatools/0.1.31/dsatools-0.1.31.tar.gz

---
+++ packages/dsatools/dsatools-0.1.31.tar.gz/dsatools-0.1.31/setup.py
@@ -0,0 +1,29 @@
+
+from setuptools import find_packages, setup
+
+with open('README.md') as f:
+    readme = f.read()
+    
+setup(
+    name="dsatools",
+    packages=find_packages(),
+    version="0.1.31",
+    description=""" The library with a most popular Digital Signal Analysis (DSA) tools """,
+    long_description=readme,
+    author="Mikhail Ronkin",
+	author_email="mvRonkin@gmail.com",
+	url = 'https://github.com/MVRonkin/dsatools',
+	download_url = "https://github.com/MVRonkin/dsatools/archive/547abfbb9e452703bb83f90c4d66a350ee4e2ae3.zip",
+	keywords = ['Digital Signal Analysis', 
+			'Signal Processing', 
+			'Digital Signal Processing',
+			'Digital Spectrum Processing',
+			'Digital Spectrum Analysis'],
+			
+    license="MIT",
+    install_requires=[
+          'numpy',
+          'scipy',
+		  'matplotlib'
+      ],
+)
