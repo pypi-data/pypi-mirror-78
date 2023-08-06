97444c58aab8f6d61c33a3d18c1276c3c0de06c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:06 +0000

    Add package newrelic-lambda-cli/0.2.6/newrelic-lambda-cli-0.2.6.tar.gz

---
+++ packages/newrelic-lambda-cli/newrelic-lambda-cli-0.2.6.tar.gz/newrelic-lambda-cli-0.2.6/newrelic_lambda_cli/cli/layers.py
@@ -0,0 +1,146 @@
+# -*- coding: utf-8 -*-
+
+from concurrent.futures import as_completed, ThreadPoolExecutor
+
+import boto3
+import click
+
+from newrelic_lambda_cli import layers, permissions
+from newrelic_lambda_cli.cliutils import done, failure
+from newrelic_lambda_cli.cli.decorators import add_options, AWS_OPTIONS
+from newrelic_lambda_cli.functions import get_aliased_functions
+
+
+@click.group(name="layers")
+def layers_group():
+    """Manage New Relic AWS Lambda Layers"""
+    pass
+
+
+def register(group):
+    group.add_command(layers_group)
+    layers_group.add_command(install)
+    layers_group.add_command(uninstall)
+
+
+@click.command(name="install")
+@click.option(
+    "--nr-account-id",
+    "-a",
+    envvar="NEW_RELIC_ACCOUNT_ID",
+    help="New Relic Account ID",
+    metavar="<account_id>",
+    required=True,
+    type=click.INT,
+)
+@add_options(AWS_OPTIONS)
+@click.option(
+    "functions",
+    "--function",
+    "-f",
+    help="AWS Lambda function name or ARN",
+    metavar="<arn>",
+    multiple=True,
+    required=True,
+)
+@click.option(
+    "excludes",
+    "--exclude",
+    "-e",
+    help="Functions to exclude (if using 'all, 'installed', 'not-installed aliases)",
+    metavar="<name>",
+    multiple=True,
+)
+@click.option(
+    "--layer-arn",
+    "-l",
+    help="ARN for New Relic layer (default: auto-detect)",
+    metavar="<arn>",
+)
+@click.option(
+    "--upgrade",
+    "-u",
+    help="Permit upgrade of function layers to new version.",
+    is_flag=True,
+)
+@click.pass_context
+def install(
+    ctx,
+    nr_account_id,
+    aws_profile,
+    aws_region,
+    aws_permissions_check,
+    functions,
+    excludes,
+    layer_arn,
+    upgrade,
+):
+    """Install New Relic AWS Lambda Layers"""
+    session = boto3.Session(profile_name=aws_profile, region_name=aws_region)
+
+    if aws_permissions_check:
+        permissions.ensure_lambda_install_permissions(session)
+
+    functions = get_aliased_functions(session, functions, excludes)
+
+    with ThreadPoolExecutor() as executor:
+        futures = [
+            executor.submit(
+                layers.install,
+                session,
+                function,
+                layer_arn,
+                nr_account_id,
+                upgrade,
+                ctx.obj["VERBOSE"],
+            )
+            for function in functions
+        ]
+        install_success = all(future.result() for future in as_completed(futures))
+
+    if install_success:
+        done("Install Complete")
+    else:
+        failure("Install Incomplete. See messages above for details.", exit=True)
+
+
+@click.command(name="uninstall")
+@add_options(AWS_OPTIONS)
+@click.option(
+    "functions",
+    "--function",
+    "-f",
+    help="Lambda function name or ARN",
+    metavar="<arn>",
+    multiple=True,
+    required=True,
+)
+@click.option(
+    "excludes",
+    "--exclude",
+    "-e",
+    help="Functions to exclude (if using 'all, 'installed', 'not-installed aliases)",
+    metavar="<name>",
+    multiple=True,
+)
+@click.pass_context
+def uninstall(ctx, aws_profile, aws_region, aws_permissions_check, functions, excludes):
+    """Uninstall New Relic AWS Lambda Layers"""
+    session = boto3.Session(profile_name=aws_profile, region_name=aws_region)
+
+    if aws_permissions_check:
+        permissions.ensure_lambda_uninstall_permissions(session)
+
+    functions = get_aliased_functions(session, functions, excludes)
+
+    with ThreadPoolExecutor() as executor:
+        futures = [
+            executor.submit(layers.uninstall, session, function, ctx.obj["VERBOSE"])
+            for function in functions
+        ]
+        uninstall_success = all(future.result() for future in as_completed(futures))
+
+    if uninstall_success:
+        done("Uninstall Complete")
+    else:
+        failure("Uninstall Incomplete. See messages above for details.", exit=True)
---
+++ packages/newrelic-lambda-cli/newrelic-lambda-cli-0.2.6.tar.gz/newrelic-lambda-cli-0.2.6/newrelic_lambda_cli/layers.py
@@ -0,0 +1,216 @@
+# -*- coding: utf-8 -*-
+
+import botocore
+import click
+import json
+import requests
+
+from newrelic_lambda_cli import utils
+from newrelic_lambda_cli.cliutils import failure, success
+from newrelic_lambda_cli.functions import get_function
+
+
+def index(region, runtime):
+    req = requests.get(
+        "https://%s.layers.newrelic-external.com/get-layers?CompatibleRuntime=%s"
+        % (region, runtime)
+    )
+    layers_response = req.json()
+    return layers_response.get("Layers", [])
+
+
+def _add_new_relic(config, region, layer_arn, account_id, allow_upgrade):
+    runtime = config["Configuration"]["Runtime"]
+    if runtime not in utils.RUNTIME_CONFIG:
+        failure(
+            "Unsupported Lambda runtime for '%s': %s"
+            % (config["Configuration"]["FunctionArn"], runtime)
+        )
+        return True
+
+    handler = config["Configuration"]["Handler"]
+    runtime_handler = utils.RUNTIME_CONFIG.get(runtime, {}).get("Handler")
+    if not allow_upgrade and handler == runtime_handler:
+        success(
+            "Already installed on function '%s'. Pass --upgrade (or -u) to allow "
+            "upgrade or reinstall to latest layer version."
+            % config["Configuration"]["FunctionArn"]
+        )
+        return False
+
+    existing_layers = [
+        layer["Arn"]
+        for layer in config["Configuration"].get("Layers", [])
+        if not layer["Arn"].startswith(utils.get_arn_prefix(region))
+    ]
+
+    new_relic_layers = []
+    if layer_arn:
+        new_relic_layers = [layer_arn]
+    else:
+        # discover compatible layers...
+        available_layers = index(region, runtime)
+
+        if not available_layers:
+            failure(
+                "No Lambda layers published for '%s' runtime: %s"
+                % (config["Configuration"]["FunctionArn"], runtime)
+            )
+            return False
+
+        # TODO: MAke this a layer selection screen
+        if len(available_layers) > 1:
+            message = ["Discovered layers for runtime (%s)" % runtime]
+            for layer in available_layers:
+                message.append(
+                    "%s\t%s"
+                    % (
+                        layer["LatestMatchingVersion"]["LayerVersionArn"],
+                        layer.get("Description", ""),
+                    )
+                )
+            message.append(
+                "\nMultiple layers found. Pass --layer-arn to specify layer ARN"
+            )
+            raise click.UsageError("\n".join(message))
+
+        new_relic_layers = [
+            available_layers[0]["LatestMatchingVersion"]["LayerVersionArn"]
+        ]
+
+    update_kwargs = {
+        "FunctionName": config["Configuration"]["FunctionArn"],
+        "Handler": runtime_handler,
+        "Environment": {
+            "Variables": config["Configuration"]
+            .get("Environment", {})
+            .get("Variables", {})
+        },
+        "Layers": new_relic_layers + existing_layers,
+    }
+
+    # Update the account id
+    update_kwargs["Environment"]["Variables"]["NEW_RELIC_ACCOUNT_ID"] = str(account_id)
+
+    # Update the NEW_RELIC_LAMBDA_HANDLER envvars only when it's a new install.
+    if handler != runtime_handler:
+        update_kwargs["Environment"]["Variables"]["NEW_RELIC_LAMBDA_HANDLER"] = handler
+
+    return update_kwargs
+
+
+def install(session, function_arn, layer_arn, account_id, allow_upgrade, verbose):
+    client = session.client("lambda")
+    config = get_function(session, function_arn)
+    if not config:
+        failure("Could not find function: %s" % function_arn)
+        return False
+
+    region = session.region_name
+
+    update_kwargs = _add_new_relic(config, region, layer_arn, account_id, allow_upgrade)
+    if not update_kwargs:
+        return False
+
+    try:
+        res = client.update_function_configuration(**update_kwargs)
+    except botocore.exceptions.ClientError as e:
+        failure(
+            "Failed to update configuration for '%s': %s"
+            % (config["Configuration"]["FunctionArn"], e)
+        )
+        return False
+    else:
+        if verbose:
+            click.echo(json.dumps(res, indent=2))
+        success("Successfully installed layer on %s" % function_arn)
+        return True
+
+
+def _remove_new_relic(config, region):
+    runtime = config["Configuration"]["Runtime"]
+    if runtime not in utils.RUNTIME_CONFIG:
+        failure(
+            "Unsupported Lambda runtime for '%s': %s"
+            % (config["Configuration"]["FunctionArn"], runtime)
+        )
+        return True
+
+    handler = config["Configuration"]["Handler"]
+
+    # Detect non-New Relic handler and error if necessary.
+    if not utils.is_valid_handler(runtime, handler):
+        failure(
+            "New Relic installation (via layers) not auto-detected for the specified "
+            "function '%s'. Unrecognized handler in deployed function."
+            % config["Configuration"]["FunctionArn"]
+        )
+        return False
+
+    env_handler = (
+        config["Configuration"]
+        .get("Environment", {})
+        .get("Variables", {})
+        .get("NEW_RELIC_LAMBDA_HANDLER")
+    )
+
+    if not env_handler:
+        failure(
+            "New Relic installation (via layers) not auto-detected for the specified "
+            "function '%s'. Environment variable NEW_RELIC_LAMBDA_HANDLER not found."
+            % config["Configuration"]["FunctionArn"]
+        )
+        return False
+
+    # Delete New Relic env vars
+    config["Configuration"]["Environment"]["Variables"] = {
+        key: value
+        for key, value in config["Configuration"]
+        .get("Environment", {})
+        .get("Variables", {})
+        .items()
+        if not key.startswith("NEW_RELIC")
+    }
+
+    # Remove New Relic layers
+    layers = [
+        layer["Arn"]
+        for layer in config["Configuration"].get("Layers")
+        if not layer["Arn"].startswith(utils.get_arn_prefix(region))
+    ]
+
+    return {
+        "FunctionName": config["Configuration"]["FunctionArn"],
+        "Handler": env_handler,
+        "Environment": config["Configuration"]["Environment"],
+        "Layers": layers,
+    }
+
+
+def uninstall(session, function_arn, verbose):
+    client = session.client("lambda")
+
+    config = get_function(session, function_arn)
+    if not config:
+        failure("Could not find function: %s" % function_arn)
+        return False
+
+    region = session.region_name
+
+    update_kwargs = _remove_new_relic(config, region)
+    if not update_kwargs:
+        return False
+
+    try:
+        res = client.update_function_configuration(**update_kwargs)
+    except botocore.exceptions.ClientError as e:
+        failure(
+            "Failed to update configuration for '%s': %s"
+            % (config["Configuration"]["FunctionArn"], e)
+        )
+        return False
+    else:
+        if verbose:
+            click.echo(json.dumps(res, indent=2))
+        success("Successfully uninstalled layer on %s" % function_arn)
+        return True
