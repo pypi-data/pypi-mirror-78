fb41f7f5f497b53386d71c23416816cb159d4b95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:10 +0000

    Add package pyExceptions/1.0.1/pyExceptions-1.0.1.tar.gz

---
+++ packages/pyExceptions/pyExceptions-1.0.1.tar.gz/pyExceptions-1.0.1/README.md
@@ -0,0 +1,38 @@
+[![Sourcecode on GitHub](https://img.shields.io/badge/Paebbels-pyExceptions-323131.svg?logo=github&longCache=true)](https://github.com/Paebbels/pyExceptions)
+[![License](https://img.shields.io/badge/code%20license-Apache%20License%2C%202.0-lightgrey?logo=GitHub)](LICENSE.md)
+[![GitHub tag (latest SemVer incl. pre-release)](https://img.shields.io/github/v/tag/Paebbels/pyExceptions?logo=GitHub&include_prereleases)](https://github.com/Paebbels/pyExceptions/tags)
+[![GitHub release (latest SemVer incl. including pre-releases)](https://img.shields.io/github/v/release/Paebbels/pyExceptions?logo=GitHub&include_prereleases)](https://github.com/Paebbels/pyExceptions/releases/latest)
+[![GitHub release date](https://img.shields.io/github/release-date/Paebbels/pyExceptions?logo=GitHub&)](https://github.com/Paebbels/pyExceptions/releases)  
+[![GitHub Workflow Status](https://img.shields.io/github/workflow/status/Paebbels/pyExceptions/Test,%20Coverage%20and%20Release?label=Workflow&logo=GitHub)](https://github.com/Paebbels/pyExceptions/actions?query=workflow%3A%22Test%2C+Coverage+and+Release%22)
+[![PyPI](https://img.shields.io/pypi/v/pyExceptions?logo=PyPI)](https://pypi.org/project/pyExceptions/)
+![PyPI - Status](https://img.shields.io/pypi/status/pyExceptions?logo=PyPI)
+![PyPI - Python Version](https://img.shields.io/pypi/pyversions/pyExceptions?logo=PyPI)
+[![Dependent repos (via libraries.io)](https://img.shields.io/librariesio/dependent-repos/pypi/pyExceptions)](https://github.com/Paebbels/pyExceptions/network/dependents)  
+[![Libraries.io status for latest release](https://img.shields.io/librariesio/release/pypi/pyExceptions)](https://libraries.io/github/Paebbels/pyExceptions)
+[![Requires.io](https://img.shields.io/requires/github/Paebbels/pyExceptions)](https://requires.io/github/Paebbels/pyExceptions/requirements/?branch=master)  
+[![Codacy - Quality](https://img.shields.io/codacy/grade/4d4f2d6010574e06a6451eb8291b537b?logo=Codacy)](https://www.codacy.com/manual/Paebbels/pyExceptions)
+[![Codacy - Coverage](https://img.shields.io/codacy/coverage/4d4f2d6010574e06a6451eb8291b537b?logo=Codacy)](https://www.codacy.com/manual/Paebbels/pyExceptions)
+[![Codecov - Branch Coverage](https://img.shields.io/codecov/c/github/Paebbels/pyExceptions?logo=Codecov)](https://codecov.io/gh/Paebbels/pyExceptions)
+[![Libraries.io SourceRank](https://img.shields.io/librariesio/sourcerank/pypi/pyExceptions)](https://libraries.io/github/Paebbels/pyExceptions/sourcerank)  
+[![Read the Docs](https://img.shields.io/readthedocs/pyexceptions)](https://pyExceptions.readthedocs.io/en/latest/) 
+
+# pyExceptions
+
+An exception base-class to derive more powerful exceptions.
+
+
+## Contributors
+
+* [Patrick Lehmann](https://github.com/Paebbels) (Maintainer)
+
+
+## License
+
+This Python package (source code) is licensed under [Apache License 2.0](LICENSE.md).
+
+<!-- The accompanying documentation is licensed under Creative Commons - Attribution-4.0 (CC-BY 4.0). -->
+
+
+-------------------------
+
+SPDX-License-Identifier: Apache-2.0
---
+++ packages/pyExceptions/pyExceptions-1.0.1.tar.gz/pyExceptions-1.0.1/setup.py
@@ -0,0 +1,91 @@
+# EMACS settings: -*- tab-width: 2; indent-tabs-mode: t; python-indent-offset: 2 -*-
+# vim: tabstop=2:shiftwidth=2:noexpandtab
+# kate: tab-width 2; replace-tabs off; indent-width 2;
+# =============================================================================
+#              _____                    _   _
+#  _ __  _   _| ____|_  _____ ___ _ __ | |_(_) ___  _ __  ___
+# | '_ \| | | |  _| \ \/ / __/ _ \ '_ \| __| |/ _ \| '_ \/ __|
+# | |_) | |_| | |___ >  < (_|  __/ |_) | |_| | (_) | | | \__ \
+# | .__/ \__, |_____/_/\_\___\___| .__/ \__|_|\___/|_| |_|___/
+# |_|    |___/                   |_|
+# ==============================================================================
+# Authors:            Patrick Lehmann
+#
+# Package installer:  Basic pyExceptions Implementations.
+#
+# License:
+# ==============================================================================
+# Copyright 2017-2020 Patrick Lehmann - Bötzingen, Germany
+# Copyright 2007-2016 Technische Universität Dresden - Germany
+#                     Chair of VLSI-Design, Diagnostics and Architecture
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+# ==============================================================================
+#
+import setuptools
+
+with open("README.md", "r") as file:
+	long_description = file.read()
+
+requirements = []
+with open("requirements.txt") as file:
+	for line in file.readlines():
+		requirements.append(line)
+
+projectName = "pyExceptions"
+
+github_url =  "https://github.com/Paebbels/" + projectName
+rtd_url =     "https://" + projectName + ".readthedocs.io/en/latest/"
+
+setuptools.setup(
+	name=projectName,
+	version="1.0.1",
+
+	author="Patrick Lehmann",
+	author_email="Paebbels@gmail.com",
+	# maintainer="Patrick Lehmann",
+	# maintainer_email="Paebbels@gmail.com",
+
+	description="An exceptions base-class to derive more powerful exceptions.",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+
+	url=github_url,
+	project_urls={
+		'Documentation': rtd_url,
+		'Source Code':   github_url,
+		'Issue Tracker': github_url + "/issues"
+	},
+	# download_url="",
+
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"License :: OSI Approved :: Apache Software License",
+		"Operating System :: OS Independent",
+		"Programming Language :: Python :: 3 :: Only",
+		"Programming Language :: Python :: 3.6",
+		"Programming Language :: Python :: 3.7",
+		"Programming Language :: Python :: 3.8",
+		"Development Status :: 5 - Production/Stable",
+		"Intended Audience :: Developers",
+		"Topic :: Utilities"
+	],
+	keywords="Python3 Exceptions",
+
+	python_requires='>=3.6',
+	install_requires=requirements,
+	# provides=
+	# obsoletes=
+)
