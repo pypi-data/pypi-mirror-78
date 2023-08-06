f5b455d7adbb9ec009904788c4e50ae6579463f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:12 +0000

    Add package cumulusci/3.18.0/cumulusci-3.18.0.tar.gz

---
+++ packages/cumulusci/cumulusci-3.18.0.tar.gz/cumulusci-3.18.0/cumulusci/tasks/bulkdata/utils.py
@@ -0,0 +1,156 @@
+import datetime
+
+from sqlalchemy import types
+from sqlalchemy import event
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import Table
+from sqlalchemy import Unicode
+from sqlalchemy.orm import mapper
+
+from cumulusci.core.exceptions import BulkDataException
+
+
+# Create a custom sqlalchemy field type for sqlite datetime fields which are stored as integer of epoch time
+class EpochType(types.TypeDecorator):
+    impl = types.Integer
+
+    epoch = datetime.datetime(1970, 1, 1, 0, 0, 0)
+
+    def process_bind_param(self, value, dialect):
+        return int((value - self.epoch).total_seconds()) * 1000
+
+    def process_result_value(self, value, dialect):
+        if value is not None:
+            return self.epoch + datetime.timedelta(seconds=value / 1000)
+
+
+# Listen for sqlalchemy column_reflect event and map datetime fields to EpochType
+@event.listens_for(Table, "column_reflect")
+def setup_epoch(inspector, table, column_info):
+    if isinstance(column_info["type"], types.DateTime):
+        column_info["type"] = EpochType()
+
+
+class SqlAlchemyMixin:
+    def _sql_bulk_insert_from_records(
+        self, *, connection, table, columns, record_iterable
+    ):
+        """Persist records from the given generator into the local database."""
+        table = self.metadata.tables[table]
+
+        connection.execute(
+            table.insert(), [dict(zip(columns, row)) for row in record_iterable]
+        )
+
+        self.session.flush()
+
+    def _create_record_type_table(self, table_name):
+        """Create a table to store mapping between Record Type Ids and Developer Names."""
+        rt_map_model_name = f"{table_name}Model"
+        self.models[table_name] = type(rt_map_model_name, (object,), {})
+        rt_map_fields = [
+            Column("record_type_id", Unicode(18), primary_key=True),
+            Column("developer_name", Unicode(255)),
+        ]
+        rt_map_table = Table(table_name, self.metadata, *rt_map_fields)
+        mapper(self.models[table_name], rt_map_table)
+
+    def _extract_record_types(self, sobject, table, conn):
+        """Query for Record Type information and persist it in the database."""
+        self.logger.info(f"Extracting Record Types for {sobject}")
+        query = (
+            f"SELECT Id, DeveloperName FROM RecordType WHERE SObjectType='{sobject}'"
+        )
+
+        result = self.sf.query(query)
+
+        if result["totalSize"]:
+            self._sql_bulk_insert_from_records(
+                connection=conn,
+                table=table,
+                columns=["record_type_id", "developer_name"],
+                record_iterable=(
+                    [rt["Id"], rt["DeveloperName"]] for rt in result["records"]
+                ),
+            )
+
+
+def _handle_primary_key(mapping, fields):
+    """Provide support for legacy mappings which used the OID as the pk but
+    default to using an autoincrementing int pk and a separate sf_id column"""
+
+    mapping["oid_as_pk"] = bool(mapping.get("fields", {}).get("Id"))
+    if mapping["oid_as_pk"]:
+        id_column = mapping["fields"]["Id"]
+        fields.append(Column(id_column, Unicode(255), primary_key=True))
+    else:
+        fields.append(Column("id", Integer(), primary_key=True, autoincrement=True))
+
+
+def create_table(mapping, metadata):
+    """Given a mapping data structure (from mapping.yml) and SQLAlchemy
+    metadata, create a table matching the mapping.
+
+    Mapping should be a dict-like with keys "fields", "table" and
+    optionally "oid_as_pk" and "record_type" """
+
+    fields = []
+    _handle_primary_key(mapping, fields)
+
+    # make a field list to create
+    for field in fields_for_mapping(mapping):
+        if mapping["oid_as_pk"] and field["sf"] == "Id":
+            continue
+        fields.append(Column(field["db"], Unicode(255)))
+
+    if "record_type" in mapping:
+        fields.append(Column("record_type", Unicode(255)))
+    t = Table(mapping["table"], metadata, *fields)
+    if t.exists():
+        raise BulkDataException(f"Table already exists: {mapping['table']}")
+    return t
+
+
+def fields_for_mapping(mapping):
+    """Summarize the list of fields in a table mapping"""
+    fields = []
+    for sf_field, db_field in mapping.get("fields", {}).items():
+        fields.append({"sf": sf_field, "db": db_field})
+    for sf_field, lookup in mapping.get("lookups", {}).items():
+        fields.append({"sf": sf_field, "db": lookup.get_lookup_key_field()})
+    return fields
+
+
+def generate_batches(num_records, batch_size):
+    """Generate batch size list for splitting a number of tasks into batch jobs.
+
+    Given a number of records to split up, and a batch size, generate a
+    stream of batchsize, index pairs"""
+    num_batches = (num_records // batch_size) + 1
+    for i in range(0, num_batches):
+        if i == num_batches - 1:  # last batch
+            batch_size = num_records - (batch_size * i)  # leftovers
+        if batch_size > 0:
+            yield batch_size, i
+
+
+class RowErrorChecker:
+    def __init__(self, logger, ignore_row_errors, row_warning_limit):
+        self.logger = logger
+        self.ignore_row_errors = ignore_row_errors
+        self.row_warning_limit = row_warning_limit
+        self.row_error_count = 0
+
+    def check_for_row_error(self, result, row_id):
+        if not result.success:
+            msg = f"Error on record with id {row_id}: {result.error}"
+            if self.ignore_row_errors:
+                if self.row_error_count < self.row_warning_limit:
+                    self.logger.warning(msg)
+                elif self.row_error_count == self.row_warning_limit:
+                    self.logger.warning("Further warnings suppressed")
+                self.row_error_count += 1
+                return self.row_error_count
+            else:
+                raise BulkDataException(msg)
