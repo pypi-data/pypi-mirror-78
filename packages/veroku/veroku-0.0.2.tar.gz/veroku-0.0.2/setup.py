9090c09c060944866e8f464a9863e1a5c40754fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:08 +0000

    Add package veroku/0.0.2/veroku-0.0.2.tar.gz

---
+++ packages/veroku/veroku-0.0.2.tar.gz/veroku-0.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="veroku",
+    version="0.0.2",
+    author="EJ Louw",
+    author_email="ejlouw00@gmail.com",
+    description="An open source library for building and performing inference with probabilistic graphical models.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="BSD 3-Clause License",
+    url="https://github.com/EJLouw/veroku",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
