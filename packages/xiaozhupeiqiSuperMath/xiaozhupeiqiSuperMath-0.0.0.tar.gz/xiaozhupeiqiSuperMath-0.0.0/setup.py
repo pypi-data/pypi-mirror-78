efed6944f130adc821efb0646a12c9462ea7cb45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:57 +0000

    Add package xiaozhupeiqiSuperMath/0.0.0/xiaozhupeiqiSuperMath-0.0.0.tar.gz

---
+++ packages/xiaozhupeiqiSuperMath/xiaozhupeiqiSuperMath-0.0.0.tar.gz/xiaozhupeiqiSuperMath-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+setup(
+name='xiaozhupeiqiSuperMath', #对外我们模块的名字
+# version='1.0', #版本号
+description='这是第一个对外发布的模块，测试哦', #描述
+author='minmin', #作者
+author_email='gaoqi110@163.com',
+py_modules=['xiaozhupeiqiSuperMath.demo1','xiaozhupeiqiSuperMath.demo2'] #要发布的模块
+)
