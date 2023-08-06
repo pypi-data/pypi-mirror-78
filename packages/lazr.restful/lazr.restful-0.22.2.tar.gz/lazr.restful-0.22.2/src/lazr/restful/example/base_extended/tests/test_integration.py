35ee34ee90837afbf6a5245c28119109f74275b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:32 +0000

    Add package lazr.restful/0.22.2/lazr.restful-0.22.2.tar.gz

---
+++ packages/lazr.restful/lazr.restful-0.22.2.tar.gz/lazr.restful-0.22.2/src/lazr/restful/example/base_extended/tests/test_integration.py
@@ -0,0 +1,46 @@
+# Copyright 20010 Canonical Ltd.  All rights reserved.
+
+"""Test harness for LAZR doctests."""
+
+from __future__ import absolute_import, print_function
+
+__metaclass__ = type
+__all__ = []
+
+import os
+import doctest
+from pkg_resources import resource_filename
+
+from van.testing.layer import zcml_layer, wsgi_intercept_layer
+
+from lazr.restful.example.base.tests.test_integration import (
+    CookbookWebServiceTestPublication, DOCTEST_FLAGS)
+from lazr.restful.testing.webservice import WebServiceApplication
+
+
+class FunctionalLayer:
+    allow_teardown = False
+    zcml = os.path.abspath(resource_filename(
+        'lazr.restful.example.base_extended', 'site.zcml'))
+zcml_layer(FunctionalLayer)
+
+
+class WSGILayer(FunctionalLayer):
+    @classmethod
+    def make_application(self):
+        return WebServiceApplication({}, CookbookWebServiceTestPublication)
+wsgi_intercept_layer(WSGILayer)
+
+
+def load_tests(loader, tests, pattern):
+    """See `zope.testing.testrunner`."""
+    doctest_files = ['../README.txt']
+    globs = {
+        'absolute_import': absolute_import,
+        'print_function': print_function,
+        }
+    suite = doctest.DocFileSuite(
+        *doctest_files, optionflags=DOCTEST_FLAGS, globs=globs)
+    suite.layer = WSGILayer
+    tests.addTest(suite)
+    return tests
