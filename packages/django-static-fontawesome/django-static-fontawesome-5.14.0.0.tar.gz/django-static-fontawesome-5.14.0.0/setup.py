10267e6087ac5ead9a80199cf709f485abb89158
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:40 +0000

    Add package django-static-fontawesome/5.14.0.0/django-static-fontawesome-5.14.0.0.tar.gz

---
+++ packages/django-static-fontawesome/django-static-fontawesome-5.14.0.0.tar.gz/django-static-fontawesome-5.14.0.0/setup.py
@@ -0,0 +1,39 @@
+import os
+from setuptools import setup
+from setuptools import find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.md'), "r", encoding="utf-8") as fobj:
+    long_description = fobj.read()
+
+with open(os.path.join(here, 'requirements.txt'), "r", encoding="utf-8") as fobj:
+    requires = [x.strip() for x in fobj.readlines() if x.strip()]
+
+
+setup(
+    name="django-static-fontawesome",
+    version="5.14.0.0",
+    description="Django application contain font-awesome static files",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author="zencore",
+    author_email="dobetter@zencore.cn",
+    url="https://github.com/zencore-cn/zencore-issues",
+    license="MIT",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    keywords=['django admin extentions', 'django static fontawesome'],
+    packages=find_packages(".", exclude=["django_static_fontawesome_demo"]),
+    py_moduels=["django_static_fontawesome"],
+    requires=requires,
+    install_requires=requires,
+    zip_safe=False,
+    include_package_data=True,
+)
