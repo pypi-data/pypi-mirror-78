0c3aada53117597ca379d20643ddfb8b3c0d880f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:38 +0000

    Add package nnc/0.0.1/nnc-0.0.1.tar.gz

---
+++ packages/nnc/nnc-0.0.1.tar.gz/nnc-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='nnc',
+    version='0.0.1',
+    description='Neural Network Control',
+    long_description='A package for neural network control of deterministic continuous time differentiable systems on graphs.',
+    url='https://www.example.com',
+    author='Thomas Asikis',
+    author_email='asikist@ethz.ch',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
