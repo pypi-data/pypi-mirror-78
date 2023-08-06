fdd0cb913498f1f87e7e716f80f94d2ca1ca066e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:36 +0000

    Add package PyProfane/1.0.3.post1/PyProfane-1.0.3.post1.tar.gz

---
+++ packages/PyProfane/PyProfane-1.0.3.post1.tar.gz/PyProfane-1.0.3.post1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fileObj:
+  longDescription = fileObj.read()
+
+setuptools.setup(name="PyProfane",
+  packages=setuptools.find_packages(),
+  version="1.0.3-1",
+  author="Ieshaan Saxena",
+  author_email="ieshaan1999@gmail.com",
+  description="A library to censor and detect offensive words in strings.",
+  long_description=longDescription,
+  long_description_content_type="text/markdown",
+  license="MIT",
+  url="https://github.com/ieshaan12/PyProfane",
+  download_url="https://github.com/ieshaan12/PyProfane/archive/v1.0.3.tar.gz",
+  package_data={'PyProfane': ['data/swearWords.txt', 'data/comments.txt']},
+  classifiers=[
+    "Development Status :: 3 - Alpha",
+    "Natural Language :: English",
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],)
