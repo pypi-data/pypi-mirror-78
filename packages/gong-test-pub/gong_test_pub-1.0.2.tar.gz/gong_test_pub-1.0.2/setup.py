c2fa047ebdc3870f2bfbdfd22ade86d10c231ddd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:39 +0000

    Add package gong-test-pub/1.0.2/gong_test_pub-1.0.2.tar.gz

---
+++ packages/gong-test-pub/gong_test_pub-1.0.2.tar.gz/gong_test_pub-1.0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gong_test_pub',
+    version='1.0.2',
+    description=(
+        '<项目的简单描述>'
+    ),
+    long_description=open('README.md').read(),
+    author='gong',
+    author_email='2976560783@qq.com',
+    maintainer='gong',
+    maintainer_email='2976560783@qq.com',
+    license='Apache License',
+    packages=find_packages(),
+    url='https://gitee.com/gongzhengyang',
+    platforms=["all"],
+    classifiers=[
+    ],
+)
