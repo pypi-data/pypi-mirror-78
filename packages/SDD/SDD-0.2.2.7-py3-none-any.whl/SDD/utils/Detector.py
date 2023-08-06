a7921c3d4c4042c2508727f0f1d170564162f177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:04 +0000

    Add package SDD/0.2.2.7/SDD-0.2.2.7-py3-none-any.whl

---
+++ packages/SDD/SDD-0.2.2.7-py3-none-any.whl/SDD/utils/Detector.py
@@ -0,0 +1,144 @@
+import mxnet as mx
+import numpy as np
+import cv2
+from skimage import io
+import os
+import gluoncv
+import csv
+import re 
+
+from matplotlib import pyplot as plt
+from mxnet import nd
+from copy import deepcopy
+from tqdm import tqdm
+from gluoncv import model_zoo, data, utils
+def accur_metric(TP, TN, FP, FN):
+    precision = 1 if TP + FP == 0 else TP / (TP + FP)
+    recall = 1 if TP + FN == 0 else TP / (TP + FN)
+    return precision, recall
+class VideoDetector:  
+    def __init__(self, model, threshold = 0.7, save_path = './detections'):
+        '''
+        Parameters
+        ----------
+        model: selected model
+        threshold: bounding box with score larger than threshold will be kept
+        save_path: output path of the labelled video
+        '''
+        self.detector = model
+        self.save_path = save_path
+        self.threshold = threshold
+
+    def __call__(self, filename, groundTruth = None, metric = 'mean', scale = 1):
+        '''
+        Parameters
+        ----------
+        filename: path to input video
+        groundTruth: file contain social distance groundtruths
+        metric: metric used to calculate distance between bounding boxes
+        scale: bounding boxes will be scaled before calculating distance between them
+        '''
+        all_dets = distance_gt = None
+        v_cap = cv2.VideoCapture(filename)
+        v_len = int(v_cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        frame_size = (v_cap.get(cv2.CAP_PROP_FRAME_WIDTH), v_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        
+        fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+        fps = v_cap.get(cv2.CAP_PROP_FPS)
+        if not os.path.exists(self.save_path):
+            os.mkdir(self.save_path)
+
+        out = cv2.VideoWriter(f'{self.save_path}/{filename.split("/")[-1]}', fourcc, fps,\
+                            (int(frame_size[0]), int(frame_size[1])))   
+        
+        frame = p1 = p2 = p3 = bbox_center = edges = None
+        TP = FP = TN = FN = extra = 0
+        precision_per_frame = np.array([])
+        recall_per_frame = np.array([])
+        for i in tqdm(range(v_len)):#200
+            success = v_cap.grab()
+            success, frame = v_cap.retrieve()
+            if not success:
+                continue
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            frame, _TP, _FP, _TN, _FN, _extra = self.detector(frame, groundTruth, metric = metric, scale = scale, frame_number = i, threshold = self.threshold)
+            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+            out.write(frame)
+            TP += _TP
+            FP += _FP
+            TN += _TN
+            FN += _FN
+            extra += _extra
+            precision_frame, recall_frame = accur_metric(TP, TN, FP, FN)
+            precision_per_frame = np.hstack((precision_per_frame, [precision_frame]))
+            recall_per_frame = np.hstack((recall_per_frame, [recall_frame]))
+        v_cap.release()
+        if groundTruth != None:
+            print('\n Mean Average Precision and Recall is', np.mean(precision_per_frame), np.mean(recall_per_frame))
+        print(f'Video saved at {self.save_path}/{filename.split("/")[-1]}')
+        return out, TP, FP, TN, FN, extra
+class ImageDetector:  
+    def __init__(self, model, threshold = 0.7, save_path = './detections'):
+        '''
+        Parameters
+        ----------
+        model: selected model
+        threshold: bounding box with score larger than threshold will be kept
+        save_path: output path of the labelled video
+        '''
+        self.detector = model
+        self.save_path = save_path
+        self.threshold = threshold
+
+    def __call__(self, imgpath, groundTruth=None, metric = 'mean', scale = 1):
+        '''
+        Parameters
+        ----------
+        filename: path to input video
+        groundTruth: file contain social distance groundtruths
+        metric: metric used to calculate distance between bounding boxes
+        scale: bounding boxes will be scaled before calculating distance between them
+        '''
+        all_dets = distance_gt = None
+        if groundTruth:
+            all_dets = np.loadtxt(groundTruth, delimiter = ',')
+            distance_gt = list(map(int, list(set(all_dets[:,1]))))
+        if not os.path.exists(self.save_path):
+            os.mkdir(self.save_path)
+        frames = os.listdir(imgpath)
+        frame = p1 = p2 = p3 = bbox_center = edges = None
+        TP = FP = TN = FN = extra = 0
+        precision_per_frame = np.array([])
+        recall_per_frame = np.array([])
+        for i in tqdm(range(len(frames))):
+            try:
+                fnumbers = frames[i].split('.')[0]
+                fnumbers = int(re.findall(r'[0-9]+', fnumbers)[0])
+            except:
+                fnumbers = i
+            
+            if not imgpath + '/' + frames[i]:
+                continue
+            try:
+              frame = cv2.imread(imgpath + '/' + frames[i])
+              frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            except:
+              continue
+            frame, _TP, _FP, _TN, _FN, _extra  = self.detector(frame, groundTruth, metric = metric, scale = scale, frame_number = fnumbers, threshold = self.threshold)
+            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+            cv2.imwrite(self.save_path + '/saved_'+frames[i], frame)
+
+            TP += _TP
+            FP += _FP
+            TN += _TN
+            FN += _FN
+            extra += _extra
+            precision_frame, recall_frame = accur_metric(TP, TN, FP, FN)
+            precision_per_frame = np.hstack((precision_per_frame, [precision_frame]))
+            recall_per_frame = np.hstack((recall_per_frame, [recall_frame]))
+
+        if groundTruth != None:
+            print('Mean Average Precision and Recall is', np.mean(precision_per_frame), np.mean(recall_per_frame))
+       
+        print(f'Video saved at {self.save_path}/')
+        return None, TP, FP, TN, FN, extra
---
+++ packages/SDD/SDD-0.2.2.7-py3-none-any.whl/SDD/utils/Run.py
@@ -0,0 +1,92 @@
+#main
+# from Transformer import Bird_eye_view_Transformer
+import cv2
+import mxnet as mx
+from .View_Transformer import Bird_eye_view_Transformer
+from .Detector import VideoDetector, ImageDetector
+from .Models import Bbox_detector
+class Detect:
+    def __init__(self, keypoints = [(1175,  189), (1574,  235), (364,  694), (976,  831)], \
+        keypoints_birds_eye_view = [(900,  300), (1200,  300), (900,  900), (1200,  900)], \
+        actual_length = 10,  actual_width = 5, pretrained_models = 'yolo_v3', trained = False):
+        '''
+        main function used for detection and labelling
+
+        Parameters
+        ----------
+        keypoints: selected key points from first frame of the input video
+        keypoints_birds_eye_view: mapping location of keypoints on the bird-eye view image
+        actual_length: actual length in real-world
+        actual_width: actual width in real-world
+        pretrained_models: selected pretrained models -- yolo_v3, ssd, faster_rcnn, center_net
+        '''
+        self.trained = trained
+        self.keypoints = keypoints
+        self.keypoints_birds_eye_view = keypoints_birds_eye_view
+        self.actual_length = actual_length
+        self.actual_width = actual_width
+        self.pretrained_models = pretrained_models
+        self.transformer = Bird_eye_view_Transformer(self.keypoints, self.keypoints_birds_eye_view, self.actual_length, self.actual_width, multi_pts = False)
+    
+    def __call__(self, save_path, video = True, threshold = 0.5, need_view_tranformer = False, device = mx.gpu()):
+        '''
+        Parameters
+        ----------
+        save_path: path where the labelled video will be saved
+        video: if True, video detector will be used, otherwise, image detector will be used
+        threshold: bounding box with score larger than threshold will be kept
+        need_view_tranformer: if True, video frame will be transformed to bird-eye view for process
+        '''
+        transformer = self.transformer if need_view_tranformer == True else None
+        # detect_model = Bbox_detector('ssd_trained',)
+        detect_model = Bbox_detector(self.pretrained_models, trained = self.trained, transformer = transformer, device = device)
+        detector = VideoDetector(detect_model, threshold = threshold, save_path = save_path) if video else ImageDetector(detect_model, threshold = threshold, save_path = save_path)
+        return detector
+
+
+# if __name__ == '__main__':
+#     video_path = '/Users/congcong/Desktop/SocialDistanceDetector/SDD/data/TownCenter_dataset/TownCentreXVID.avi'#,CUHKSquare.mp4,crowds_zara01.avi
+#     img_path = '/Users/congcong/Desktop/SocialDistanceDetector/SDD/data/mall_dataset/selectframes'#'/content/drive/My Drive/SocialDistance/data/CUHK/test'#'/content/drive/My Drive/SocialDistance/data/Mall'
+#     output_path_video = '/Users/congcong/Desktop/SocialDistanceDetector/SDD/outputs/video'#'/Users/congcong/Desktop/SocialDistanceDetector/yolo/outputs/video'
+#     output_path_image = '/Users/congcong/Desktop/SocialDistanceDetector/SDD/outputs/image'#'/Users/congcong/Desktop/SocialDistanceDetector/yolo/outputs/image'
+#     video_groundTruth = '/Users/congcong/Desktop/SocialDistanceDetector/SDD/data/TownCenter_dataset/TownCentreXVID-groundtruth.top'
+#     image_groundTruth = '/Users/congcong/Desktop/SocialDistanceDetector/SDD/data/mall_dataset/Mall-groundtruth.top'
+
+#     #YOLOv3 test
+#     # detect = Detect(pretrained_models = 'yolo_v3')
+#     # detector = detect(save_path = output_path_image, video = False, need_view_tranformer = False, device = mx.cpu())
+#     # out, TP, FP, TN, FN, extra = detector(img_path, image_groundTruth)
+
+#     # detect = Detect(pretrained_models = 'yolo_v3', trained = True)
+#     # detector = detect(save_path = output_path_image, video = False, need_view_tranformer = False, device = mx.cpu())
+#     # out, TP, FP, TN, FN, extra = detector(img_path, image_groundTruth)
+
+#     # detect = Detect(pretrained_models = 'yolo_v3')
+#     # detector = detect(save_path = output_path_image, video = False, need_view_tranformer = False, device = mx.cpu())
+#     # out, TP, FP, TN, FN, extra = detector(img_path, image_groundTruth,  metric='min')
+
+#     # detect = Detect(pretrained_models = 'yolo_v3', trained = True)
+#     # detector = detect(save_path = output_path_image, video = False, need_view_tranformer = False, device = mx.cpu())
+#     # out, TP, FP, TN, FN, extra = detector(img_path, image_groundTruth, metric='scale',scale=0.8)
+
+#     # detect = Detect(pretrained_models = 'yolo_v3')
+#     # detector = detect(save_path = output_path_video, video = True, need_view_tranformer = False, device = mx.cpu())
+#     # out, TP, FP, TN, FN, extra = detector(video_path)
+
+#     #SSD test
+#     detect = Detect(pretrained_models = 'ssd')
+#     detector = detect(save_path = output_path_image, video = False, need_view_tranformer = False, device = mx.cpu())
+#     out, TP, FP, TN, FN, extra = detector(img_path, image_groundTruth)
+
+#     detect = Detect(pretrained_models = 'ssd', trained = True)
+#     detector = detect(save_path = output_path_image, video = False, need_view_tranformer = False, device = mx.cpu())
+#     out, TP, FP, TN, FN, extra = detector(img_path, image_groundTruth, metric='max')
+
+#     detect = Detect(pretrained_models = 'ssd')
+#     detector = detect(save_path = output_path_image, video = False, need_view_tranformer = False, device = mx.cpu())
+#     out, TP, FP, TN, FN, extra = detector(img_path, image_groundTruth,metric='scale',scale=0.8)
+
+#     detect = Detect(pretrained_models = 'ssd', trained = True)
+#     detector = detect(save_path = output_path_video, video = True, need_view_tranformer = False, device = mx.cpu())
+#     out, TP, FP, TN, FN, extra = detector(video_path, video_groundTruth)
+
