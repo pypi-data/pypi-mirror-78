3e24e89d43a883962ab084c029f22a96858c19fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:01 +0000

    Add package sphinxcontrib.relative-link-corrector/0.0.1/sphinxcontrib.relative-link-corrector-0.0.1.tar.gz

---
+++ packages/sphinxcontrib.relative-link-corrector/sphinxcontrib.relative-link-corrector-0.0.1.tar.gz/sphinxcontrib.relative-link-corrector-0.0.1/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# Copyright 2020 Nokia
+# Licensed under the Apache License 2.0.
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+from setuptools import setup
+
+
+def slurp(filename):
+    """
+    Return whole file contents as string. File is searched relative to
+    directory where this `setup.py` is located.
+    """
+    with open(os.path.join(os.path.dirname(__file__), filename)) as f:
+        return f.read()
+
+
+setup(
+    name                 = "sphinxcontrib.relative-link-corrector",
+    version              = "0.0.1",
+    packages             = ["sphinxcontrib", "sphinxcontrib.relative-link-corrector"],
+    namespace_packages   = ["sphinxcontrib"],
+    package_dir          = {'': "src"},
+    author               = "Gergely Csatari",
+    author_email         = "gergely.csatari@nokia.com",
+    license              = "Apache License 2.0",
+    url                  = "https://github.com/cntt-n/sphinxcontrib-relative-link-corrector",
+    keywords             = ["helpers"],
+    install_requires     = ["beautifulsoup4"],
+    long_description_content_type = "text/x-rst",
+    description          = ("Corrects relative links when generating documents  "
+                            "from .md to .html with commonmark"),
+    long_description     = slurp("README.rst"),
+    classifiers          = ["Programming Language :: Python :: 3",
+                            "Development Status :: 4 - Beta",
+                            "Topic :: Documentation",
+                            "Intended Audience :: Developers",
+                            "License :: OSI Approved :: Apache Software License",
+                            "Operating System :: POSIX"])
+
