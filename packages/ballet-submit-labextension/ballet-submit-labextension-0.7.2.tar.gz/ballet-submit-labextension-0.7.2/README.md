d687371503b64fa29fabcfd9d90a2ddea072fe40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:30 +0000

    Add package ballet-submit-labextension/0.7.2/ballet-submit-labextension-0.7.2.tar.gz

---
+++ packages/ballet-submit-labextension/ballet-submit-labextension-0.7.2.tar.gz/ballet-submit-labextension-0.7.2/README.md
@@ -0,0 +1,175 @@
+[![Github Actions Status](https://github.com/HDI-Project/ballet-submit-labextension/workflows/Main/badge.svg)](https://github.com/HDI-Project/ballet-submit-labextension/actions)
+[![PyPI Shield](https://img.shields.io/pypi/v/ballet-submit-labextension.svg)](https://pypi.org/project/ballet-submit-labextension)
+[![npm version](https://img.shields.io/npm/v/ballet-submit-labextension)](https://www.npmjs.com/package/ballet-submit-labextension)
+
+# ballet-submit-labextension
+
+Submit ballet modules from within JupyterLab
+
+This extension is composed of a Python package named `ballet-submit-labextension`
+for the server extension and a NPM package also named `ballet-submit-labextension`
+for the frontend extension.
+
+## Requirements
+
+- JupyterLab >= 2.0
+
+## Install
+
+Installation can be done completely using `pip`, which installs both the 
+server and the frontend extensions. The frontend extension only can be 
+installed using `jupyter labextension install` but will not function properly
+without the corresponding server extension.
+
+```bash
+pip install ballet-submit-labextension
+jupyter lab build
+```
+
+Note: You will need NodeJS to install the extension; the installation process
+will complain if it is not found.
+
+## Authenticate with GitHub
+
+The extension provides an in-lab experience for authenticating
+with GitHub. When you open a notebook, you should see the GitHub icon to the
+right on the Notebook toolbar. The icon should be grey at first, indicating
+you are not authenticated. Click the icon to open a login window, in which
+you can enter your GitHub username and password. These will be exchanged by
+the extension for an OAuth token and will be used to propose changes to the
+upstream Ballet project on your behalf (if you attempt to submit features).
+
+Alternately, you can provide a personal access token directly using the
+configuration approaches below.
+
+## Configure
+
+The extension ties into the same configuration system as Jupyter [Lab] itself.
+You can configure the extension with command line arguments or via the
+config file, just like you configure Jupyter Notebook or Jupyter Lab.
+
+### All configuration options
+    
+The following configuration options are available:
+
+```
+$ python -c 'from ballet_submit_labextension.app import print_help;print_help()'
+
+BalletApp options
+-----------------
+--BalletApp.access_token_timeout=<Int>
+    Default: 60
+    timeout to receive access token from server via polling
+--BalletApp.ballet_yml_path=<Unicode>
+    Default: ''
+    path to ballet.yml file of Ballet project (if Lab is not run from project
+    directory)
+--BalletApp.debug=<Bool>
+    Default: False
+    enable debug mode (no changes made on GitHub), will read from $BALLET_DEBUG
+    if present
+--BalletApp.github_token=<Unicode>
+    Default: ''
+    github access token, will read from $GITHUB_TOKEN if present
+--BalletApp.oauth_gateway_url=<Unicode>
+    Default: 'https://github-oauth-gateway.herokuapp.com/'
+    url to github-oauth-gateway server
+```
+
+### Command line arguments
+
+Invoke Jupyter Lab with command line arguments providing config to the ballet
+extension, for example:
+
+```
+jupyter lab --BalletApp.debug=True
+```
+
+### Config file
+
+1. Determine the path to your jupyter config file (you may have to create it 
+if it does not exist):
+
+    ```bash
+    touch "$(jupyter --config-dir)/jupyter_notebook_config.py"
+    ```
+
+2. Append desired config to the end of the file, for example:
+
+    ```python
+    c.BalletApp.debug = True
+    ```
+   
+## Troubleshoot
+
+If you are see the frontend extension but it is not working, check
+that the server extension is enabled:
+
+```bash
+jupyter serverextension list
+```
+
+If the server extension is installed and enabled but your not seeing
+the frontend, check the frontend is installed:
+
+```bash
+jupyter labextension list
+```
+
+If it is installed, try:
+
+```bash
+jupyter lab clean
+jupyter lab build
+```
+
+## Contributing
+
+### Development Install
+
+The `jlpm` command is JupyterLab's pinned version of
+[yarn](https://yarnpkg.com/) that is installed with JupyterLab. You may use
+`yarn` or `npm` in lieu of `jlpm` below.
+
+```bash
+# Clone the repo to your local environment
+# Move to ballet-submit-labextension directory
+# Install server extension
+pip install -e .
+# Register server extension
+jupyter serverextension enable --py ballet-submit-labextension
+# Install dependencies
+jlpm
+# Build Typescript source
+jlpm build
+# Link your development version of the extension with JupyterLab
+jupyter labextension link .
+
+# Rebuild Typescript source after making changes
+jlpm build
+# Rebuild JupyterLab after making any changes
+jupyter lab build
+```
+
+You can watch the source directory and run JupyterLab in watch mode to watch for changes in the extension's source and automatically rebuild the extension and application.
+
+```bash
+# Watch the source directory in another terminal tab
+jlpm watch
+# Run jupyterlab in watch mode in one terminal tab
+jupyter lab --watch
+```
+
+### Uninstall
+
+```bash
+pip uninstall ballet-submit-labextension
+jupyter labextension uninstall ballet-submit-labextension
+```
+
+### Release process
+
+```
+bumpversion <part>
+make release
+```
---
+++ packages/ballet-submit-labextension/ballet-submit-labextension-0.7.2.tar.gz/ballet-submit-labextension-0.7.2/src/index.ts
@@ -0,0 +1,198 @@
+import {
+  Dialog,
+  showDialog,
+  showErrorMessage,
+  ToolbarButton,
+  ICommandPalette
+} from '@jupyterlab/apputils';
+
+import { IDisposable, DisposableDelegate } from '@lumino/disposable';
+
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { DocumentRegistry } from '@jupyterlab/docregistry';
+
+import { NotebookPanel, INotebookModel } from '@jupyterlab/notebook';
+
+import { ISettingRegistry } from '@jupyterlab/settingregistry';
+
+import { ConfirmWidget, FeatureSubmittedOkayWidget } from './widgets';
+
+import {
+  ISubmissionResponse,
+  checkStatus,
+  getEndpointUrl,
+  submit,
+  request,
+  isAuthenticated
+} from './serverextension';
+
+import $ from 'jquery';
+
+const EXTENSION_NAME = 'ballet-submit-labextension';
+const PLUGIN_ID = `${EXTENSION_NAME}:plugin`;
+
+/**
+ * A notebook widget extension that adds a submit button to the toolbar.
+ */
+export class BalletSubmitButtonExtension
+  implements DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel> {
+  settingRegistry: ISettingRegistry;
+
+  constructor(settingRegistry: ISettingRegistry) {
+    this.settingRegistry = settingRegistry;
+  }
+
+  async loadSetting(settingName: string): Promise<string> {
+    return (await this.settingRegistry.get(PLUGIN_ID, settingName))
+      .composite as string;
+  }
+
+  /**
+   * Create a new extension object.
+   */
+  createNew(
+    panel: NotebookPanel,
+    context: DocumentRegistry.IContext<INotebookModel>
+  ): IDisposable {
+    let button = new ToolbarButton({
+      label: 'Submit',
+      iconClass: 'fa fa-share ballet-submitButtonIcon',
+      onClick: async () => {
+        // check if authenticated
+        if (!(await isAuthenticated())) {
+          void showErrorMessage(
+            'Not authenticated',
+            "You're not authenticated with GitHub - click the GitHub icon in the toolbar to connect!"
+          );
+          return;
+        }
+
+        // load current cell
+        let notebook = panel.content;
+        let activeCell = notebook.activeCell;
+        let contents = activeCell.model.value.text;
+
+        // confirm to proceed
+        const confirmDialog = await showDialog({
+          title: 'Submit feature?',
+          body: new ConfirmWidget(contents)
+        });
+        if (!confirmDialog.button.accept) {
+          return;
+        }
+
+        // post contents to ballet-submit-server
+        console.log(contents);
+        const result: ISubmissionResponse = await submit(contents);
+
+        // try to add a message to cell outputs
+        if (result.result) {
+          void showDialog({
+            title: 'Feature submitted successfully',
+            body: new FeatureSubmittedOkayWidget(result.url),
+            buttons: [Dialog.okButton()]
+          });
+        } else {
+          const message =
+            result.message !== undefined && result.message !== null
+              ? `: ${result.message}.`
+              : '.';
+          void showErrorMessage(
+            'Error submitting feature',
+            `Oops - there was a problem submitting your feature${message}`
+          );
+          console.error(result)
+        }
+      },
+      tooltip: 'Submit current cell to Ballet project'
+    });
+    panel.toolbar.addItem('balletSubmitButton', button);
+
+    let githubAuthButton = new ToolbarButton({
+      iconClass: 'fa fa-github ballet-githubAuthButtonIcon',
+      onClick: async () => {
+        if (!(await isAuthenticated())) {
+          window.open(
+            getEndpointUrl('auth/authorize'),
+            '_blank',
+            'width=350,height=600'
+          );
+          // async
+          void request<void>('auth/token', {
+            method: 'POST'
+          });
+        } else {
+          void showDialog({
+            title: 'Already authenticated',
+            body: 'You have successfully authenticated with GitHub.',
+            buttons: [Dialog.okButton()]
+          });
+        }
+      },
+      tooltip: 'Authenticate with GitHub'
+    });
+    panel.toolbar.addItem('githubAuthButton', githubAuthButton);
+
+    // ugh
+    // TODO - remove callback when authenticated, assuming that we will never become un-authenticated
+    setInterval(async () => {
+      $('.ballet-githubAuthButtonIcon').toggleClass(
+        'ballet-githubAuthButtonIcon-authenticated',
+        await isAuthenticated()
+      );
+    }, 5 * 1000);
+
+    return new DisposableDelegate(() => {
+      button.dispose();
+      githubAuthButton.dispose();
+    });
+  }
+}
+
+async function activate(
+  app: JupyterFrontEnd,
+  palette: ICommandPalette,
+  settingRegistry: ISettingRegistry
+): Promise<void> {
+  console.log(`JupyterLab extension ${EXTENSION_NAME} is activated!`);
+
+  // add button to toolbar
+  app.docRegistry.addWidgetExtension(
+    'Notebook',
+    new BalletSubmitButtonExtension(settingRegistry)
+  );
+
+  // create submit command
+  const submitCommand: string = 'ballet:submit';
+  app.commands.addCommand(submitCommand, {
+    label: 'Submit Feature',
+    execute: () => {
+      console.log('Submit feature executed (TODO)');
+    }
+  });
+  palette.addItem({ command: submitCommand, category: 'Ballet' });
+
+  // check status of /ballet endpoints
+  try {
+    await checkStatus();
+    console.log('Connected to /ballet endpoints');
+  } catch {
+    console.error("Can't connect to /ballet endpoints");
+  }
+}
+
+/**
+ * Initialization data for the ballet-submit-labextension extension.
+ */
+const extension: JupyterFrontEndPlugin<void> = {
+  id: PLUGIN_ID,
+  autoStart: true,
+  requires: [ICommandPalette, ISettingRegistry],
+  activate: activate
+};
+
+export default extension;
