8b97c4dee4ec25d7d820a4de342818672b32378a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:37 +0000

    Add package Mopidy-Pibox/0.7.5/Mopidy-Pibox-0.7.5.tar.gz

---
+++ packages/Mopidy-Pibox/Mopidy-Pibox-0.7.5.tar.gz/Mopidy-Pibox-0.7.5/mopidy_pibox/__init__.py
@@ -0,0 +1,57 @@
+from __future__ import unicode_literals
+
+import logging
+import os
+import tornado.web
+
+from mopidy import config, ext
+import sys
+from .pibox_api import api, session, socket
+
+__version__ = '0.7.5'
+
+def my_app_factory(config, core):
+
+    this_session = session.PiboxSession(2)
+
+    path = os.path.join( os.path.dirname(__file__), 'static')
+
+    logger = logging.getLogger(__name__)
+    logger.info(path)
+    
+    return [
+        (r'/ws/?', socket.PiboxWebSocket),
+        (r'/api/tracklist/?', api.TracklistHandler, {'core': core, 'session': this_session}),
+        (r'/api/vote/?', api.VoteHandler, {'core': core, 'session': this_session}),
+        (r'/api/session/?', api.SessionHandler, {'core': core, 'session': this_session}),
+        (r'/(.*)', tornado.web.StaticFileHandler, {
+            'path': path,
+            'default_filename': 'index.html'
+        }),
+    ]
+
+
+class Extension(ext.Extension):
+
+    dist_name = 'Mopidy-Pibox'
+    ext_name = 'pibox'
+    version = __version__
+
+    def get_default_config(self):
+        conf_file = os.path.join(os.path.dirname(__file__), 'ext.conf')
+        return config.read(conf_file)
+
+    def get_config_schema(self):
+        schema = super(Extension, self).get_config_schema()
+        schema['default_playlist'] = config.String()
+        return schema
+
+    def setup(self, registry):
+
+        from .frontend import PiboxFrontend
+        registry.add('frontend', PiboxFrontend)
+
+        registry.add('http:app', {
+            'name': self.ext_name,
+            'factory': my_app_factory,
+        })
---
+++ packages/Mopidy-Pibox/Mopidy-Pibox-0.7.5.tar.gz/Mopidy-Pibox-0.7.5/mopidy_pibox/static/precache-manifest.ccd4e6943c1e49f1a244ba2613201bfc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ba7bde3a33fd5b713616cabd6bb771e",
+    "url": "/pibox/index.html"
+  },
+  {
+    "revision": "619c5bb6c71940ce46a7",
+    "url": "/pibox/static/css/2.d97600f8.chunk.css"
+  },
+  {
+    "revision": "eaf58a1136cac6c8cc10",
+    "url": "/pibox/static/css/main.6eae9811.chunk.css"
+  },
+  {
+    "revision": "619c5bb6c71940ce46a7",
+    "url": "/pibox/static/js/2.704d0bbc.chunk.js"
+  },
+  {
+    "revision": "0980bcfca5e63f1a4bd6d55d4b972d54",
+    "url": "/pibox/static/js/2.704d0bbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaf58a1136cac6c8cc10",
+    "url": "/pibox/static/js/main.8cb738f6.chunk.js"
+  },
+  {
+    "revision": "167131e9f52b1c74ba69",
+    "url": "/pibox/static/js/runtime-main.7ff7a994.js"
+  }
+]);
