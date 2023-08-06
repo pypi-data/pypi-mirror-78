83859ccecf69857aeb5e4fb65a029ed8391c3af1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:54 +0000

    Add package iam.ext.idc/0.0.1/iam.ext.idc-0.0.1.tar.gz

---
+++ packages/iam.ext.idc/iam.ext.idc-0.0.1.tar.gz/iam.ext.idc-0.0.1/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2019-2020 Cochise Ruhulessin
+#
+# This file is part of iam.ext.{{ pkg_name }}.
+#
+# iam.ext.{{ pkg_name }} is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+#
+# iam.ext.{{ pkg_name }} is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with iam.ext.{{ pkg_name }}.  If not, see <https://www.gnu.org/licenses/>.
+import json
+import os
+import sys
+from setuptools import find_namespace_packages
+from setuptools import setup
+
+
+version = str.strip(open('iam/ext/idc/VERSION').read())
+
+
+setup(
+    name='iam.ext.idc',
+    version=version,
+    packages=find_namespace_packages(),
+    include_package_data=True,
+    **json.loads((open('iam/ext/idc/package.json').read()))
+)
